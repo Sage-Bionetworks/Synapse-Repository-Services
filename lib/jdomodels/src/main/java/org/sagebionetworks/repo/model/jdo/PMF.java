030c442d6a969ff1cffb9e31c2f4898de75a5aff
deflaux <nicole.deflaux@sagebase.org>
2011-03-03 23:05:55 +0000

    Two bug fixes: (1) MySQL has a max value for LIMIT of 50 million rows (which is still an enormously large number) (2) Passing database username and password as separate environment variables since datanucleus logs the connection string

--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/jdo/PMF.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/jdo/PMF.java
@@ -2,6 +2,9 @@ package org.sagebionetworks.repo.model.jdo;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
@@ -14,6 +17,7 @@ import javax.jdo.PersistenceManagerFactory;
  * 
  */
 public final class PMF {
+	private static final Logger log = Logger.getLogger(PMF.class.getName());
 	private static PersistenceManagerFactory pmfInstance = null;
 	private static String memoryPersistenceImpl = "memorydb-transactions-optional";
 	private static String mysqlPersistenceImpl = "mysql-transactions-optional";
@@ -34,17 +38,63 @@ public final class PMF {
 			synchronized (PMF.class) {
 				if ((null == System.getProperty("JDBC_CONNECTION_STRING"))
 						&& (null == System.getenv("JDBC_CONNECTION_STRING"))) {
+					log.info("JDO Persistence unit to be used: "
+							+ memoryPersistenceImpl);
 					pmfInstance = JDOHelper
 							.getPersistenceManagerFactory(memoryPersistenceImpl);
 				} else {
-					Map<String,String> overrides = new HashMap<String,String>();
+					Map<String, String> overrides = new HashMap<String, String>();
+
+					/**
+					 * User Elastic Beanstalk's variable name of
+					 * JDBC_CONNECTION_STRING for the database url and also to
+					 * indicate that we want to use MySQL in the first place
+					 * since the memory db is our default
+					 */
 					String jdbcConnection = (null != System
 							.getProperty("JDBC_CONNECTION_STRING")) ? System
 							.getProperty("JDBC_CONNECTION_STRING") : System
 							.getenv("JDBC_CONNECTION_STRING");
-					overrides.put("javax.jdo.option.ConnectionURL", jdbcConnection);
-					pmfInstance = JDOHelper
-							.getPersistenceManagerFactory(overrides, mysqlPersistenceImpl);
+					overrides.put("javax.jdo.option.ConnectionURL",
+							jdbcConnection);
+					log
+							.info("Overriding MySQL connection url from jdoconfig.xml with "
+									+ jdbcConnection);
+
+					/**
+					 * User Elastic Beanstalk's variable name of PARAM1 for the
+					 * database user
+					 */
+					String username = (null != System.getProperty("PARAM1")) ? System
+							.getProperty("PARAM1")
+							: System.getenv("PARAM1");
+					if (null != username) {
+						overrides.put("javax.jdo.option.ConnectionUserName",
+								username);
+						log
+								.info("Overriding MySQL user from jdoconfig.xml with user "
+										+ username);
+					}
+
+					/**
+					 * User Elastic Beanstalk's variable name of PARAM2 for the
+					 * database password
+					 */
+					String password = (null != System.getProperty("PARAM2")) ? System
+							.getProperty("PARAM2")
+							: System.getenv("PARAM2");
+					if (null != password) {
+						overrides.put("javax.jdo.option.ConnectionPassword",
+								password);
+						log
+								.info("Overriding MySQL password from jdoconfig.xml");
+
+					}
+
+					log.info("JDO Persistence unit to be used: "
+							+ mysqlPersistenceImpl);
+					pmfInstance = JDOHelper.getPersistenceManagerFactory(
+							overrides, mysqlPersistenceImpl);
 
 				}
 			}
