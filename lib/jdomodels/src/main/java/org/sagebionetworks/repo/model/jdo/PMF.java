00e9951075602b26a00bfa0f2985723c03b66aed
deflaux <nicole.deflaux@sagebase.org>
2011-03-03 21:28:01 +0000

    (1) make the use of MySQL configurable through system and environment properties (2) add the MySQL JDBC connector as a runtime dependency of the datanucleus plugin so that it can create schemas

--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/jdo/PMF.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/jdo/PMF.java
@@ -1,5 +1,8 @@
 package org.sagebionetworks.repo.model.jdo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
@@ -12,16 +15,38 @@ import javax.jdo.PersistenceManagerFactory;
  */
 public final class PMF {
 	private static PersistenceManagerFactory pmfInstance = null;
-	private static String persistenceImpl = "memorydb-transactions-optional";
+	private static String memoryPersistenceImpl = "memorydb-transactions-optional";
+	private static String mysqlPersistenceImpl = "mysql-transactions-optional";
 
 	private PMF() {
 	}
 
+	/**
+	 * Get the Persistence Manager. By default it returns a Persistence Manager
+	 * for an in-memory database. If you set the system property or environment
+	 * variable JDBC_CONNECTION_STRING it will attempt to make a connection to a
+	 * MySQL database with that connection URL.
+	 * 
+	 * @return the PersistenceManager
+	 */
 	public static PersistenceManager get() {
 		if (null == pmfInstance) {
 			synchronized (PMF.class) {
-				pmfInstance = JDOHelper
-						.getPersistenceManagerFactory(persistenceImpl);
+				if ((null == System.getProperty("JDBC_CONNECTION_STRING"))
+						&& (null == System.getenv("JDBC_CONNECTION_STRING"))) {
+					pmfInstance = JDOHelper
+							.getPersistenceManagerFactory(memoryPersistenceImpl);
+				} else {
+					Map<String,String> overrides = new HashMap<String,String>();
+					String jdbcConnection = (null != System
+							.getProperty("JDBC_CONNECTION_STRING")) ? System
+							.getProperty("JDBC_CONNECTION_STRING") : System
+							.getenv("JDBC_CONNECTION_STRING");
+					overrides.put("javax.jdo.option.ConnectionURL", jdbcConnection);
+					pmfInstance = JDOHelper
+							.getPersistenceManagerFactory(overrides, mysqlPersistenceImpl);
+
+				}
 			}
 		}
 		return pmfInstance.getPersistenceManager();
