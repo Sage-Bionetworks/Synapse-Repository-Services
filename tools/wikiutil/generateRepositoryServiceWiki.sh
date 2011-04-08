f04008a27f733b0c03b494cd4611b1012e385898
deflaux <nicole.deflaux@sagebase.org>
2011-04-08 20:13:01 +0000

    Added CUD operations too!

--- tools/wikiutil/generateRepositoryServiceWiki.sh
+++ tools/wikiutil/generateRepositoryServiceWiki.sh
@@ -5,7 +5,9 @@ cat src/main/resources/RepositoryWikiPrefix.txt
 
 mvn exec:java -Dexec.mainClass="org.sagebionetworks.repo.ReadOnlyWikiGenerator" -Dexec.args=$1 | grep -v "\[INFO\]" | grep -v "DEBUG \[httpclient.wire.header\] >> \"" | grep -v "WARN \[org.apache.commons.httpclient.HttpMethodBase\] Going to buffer response body of large or unknown size. Using getResponseBodyAsStream instead is recommended." | sed "s/ INFO \[org.sagebionetworks.repo.WikiGenerator\] //" | sed 's/DEBUG \[httpclient.wire.header\] << "//' | sed 's/\[\\r\]\[\\n\]"//' | uniq 
 
-mvn exec:java -Dexec.mainClass="org.sagebionetworks.repo.CRUDWikiGenerator" -Dexec.args=$1 | grep -v "\[INFO\]" | grep -v "DEBUG \[httpclient.wire.header\] >> \"" | grep -v "WARN \[org.apache.commons.httpclient.HttpMethodBase\] Going to buffer response body of large or unknown size. Using getResponseBodyAsStream instead is recommended." | sed "s/ INFO \[org.sagebionetworks.repo.WikiGenerator\] //" | sed 's/DEBUG \[httpclient.wire.header\] << "//' | sed 's/\[\\r\]\[\\n\]"//' | uniq 
+# only call this on localhost so that we do not accidentally put bad
+# data in prod
+mvn exec:java -Dexec.mainClass="org.sagebionetworks.repo.CRUDWikiGenerator" -Dexec.args=http://localhost:8080 | grep -v "\[INFO\]" | grep -v "DEBUG \[httpclient.wire.header\] >> \"" | grep -v "WARN \[org.apache.commons.httpclient.HttpMethodBase\] Going to buffer response body of large or unknown size. Using getResponseBodyAsStream instead is recommended." | sed "s/ INFO \[org.sagebionetworks.repo.WikiGenerator\] //" | sed 's/DEBUG \[httpclient.wire.header\] << "//' | sed 's/\[\\r\]\[\\n\]"//' | uniq 
 
 cat src/main/resources/RepositoryWikiSuffix.txt
 
