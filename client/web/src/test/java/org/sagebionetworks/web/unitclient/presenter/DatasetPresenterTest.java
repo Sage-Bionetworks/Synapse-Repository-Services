8905755a5d9675d3df0694fec68c38b9cea1b568
dburdick <dburdick@gmail.com>
2011-04-27 20:30:09 +0000

    added to test

--- client/web/src/test/java/org/sagebionetworks/web/unitclient/presenter/DatasetPresenterTest.java
+++ client/web/src/test/java/org/sagebionetworks/web/unitclient/presenter/DatasetPresenterTest.java
@@ -1,6 +1,7 @@
 package org.sagebionetworks.web.unitclient.presenter;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
@@ -12,19 +13,25 @@ import org.sagebionetworks.web.client.presenter.DatasetPresenter;
 import org.sagebionetworks.web.client.view.DatasetView;
 import org.sagebionetworks.web.client.widget.licenseddownloader.LicenceServiceAsync;
 
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
+
 public class DatasetPresenterTest {
 	
 	DatasetPresenter datasetPresenter;
 	DatasetView mockView;
 	DatasetServiceAsync mockDatasetService;
 	LicenceServiceAsync mockLicenseService;
+	String datasetId = "1";
+	Dataset place = new Dataset("Dataset:"+ datasetId);
 	
 	@Before
 	public void setup(){
 		mockView = mock(DatasetView.class);
 		mockDatasetService = mock(DatasetServiceAsync.class);
 		mockLicenseService = mock(LicenceServiceAsync.class);
-		datasetPresenter = new DatasetPresenter(mockView, mockDatasetService, mockLicenseService);
+		datasetPresenter = new DatasetPresenter(mockView, mockDatasetService, mockLicenseService);		
+		datasetPresenter.setPlace(place);
 		
 		verify(mockView).setPresenter(datasetPresenter);
 	}	
@@ -35,6 +42,29 @@ public class DatasetPresenterTest {
 		datasetPresenter.setPlace(place);
 	}	
 	
+	@Test
+	public void testStart() {
+		reset(mockView);
+		reset(mockDatasetService);
+		reset(mockLicenseService);
+		datasetPresenter = new DatasetPresenter(mockView, mockDatasetService, mockLicenseService);		
+		datasetPresenter.setPlace(place);		
+		AcceptsOneWidget panel = mock(AcceptsOneWidget.class);
+		EventBus eventBus = mock(EventBus.class);		
+		
+		datasetPresenter.start(panel, eventBus);		
+		verify(panel).setWidget(mockView);		
+	}
 	
+	@Test
+	public void testRefreshFromServer() {
+		reset(mockView);
+		reset(mockDatasetService);
+		reset(mockLicenseService);
+		datasetPresenter = new DatasetPresenter(mockView, mockDatasetService, mockLicenseService);		
+		datasetPresenter.setPlace(place);		
+		
+		datasetPresenter.refreshFromServer();		
+	}
 	
 }
