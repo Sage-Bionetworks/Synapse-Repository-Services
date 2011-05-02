db7c29ef42f19465c16e125dcf290ee5c5f2adfb
bruce.hoff <bruce.hoff@sagebase.org>
2011-05-02 00:56:17 +0000

    updates to NodeManager and AuthorizationDAO

--- lib/models/src/main/java/org/sagebionetworks/repo/model/AuthorizationDAO.java
+++ lib/models/src/main/java/org/sagebionetworks/repo/model/AuthorizationDAO.java
@@ -5,39 +5,42 @@ import org.sagebionetworks.repo.web.NotFoundException;
 
 public interface AuthorizationDAO {
 	
-	/**
-	 * @param groupId
-	 * @param nodeId
-	 * @param accessTypes
-	 * @param userId the user making the request
-	 * 
-	 * sets the access that given group has for the given node
-	 * 
-	 * @exception NotFoundException if the group or node is invalid; 
-	 * UnauthorizedException if the given user doesn't have 'share'
-	 * permission on the given node
-	 * 
-	 */
-	public void setAccess(String groupId, String nodeId, Collection<String> accessTypes, String userId)
-		throws NotFoundException, UnauthorizedException, DatastoreException;
+	public static final String READ_ACCESS = "READ";
+	public static final String CHANGE_ACCESS = "READ";
+	
+//	/**
+//	 * @param groupId
+//	 * @param nodeId
+//	 * @param accessTypes
+//	 * @param userId the user making the request
+//	 * 
+//	 * sets the access that given group has for the given node
+//	 * 
+//	 * @exception NotFoundException if the group or node is invalid; 
+//	 * UnauthorizedException if the given user doesn't have 'share'
+//	 * permission on the given node
+//	 * 
+//	 */
+//	public void setAccess(String groupId, String nodeId, Collection<String> accessTypes, String userId)
+//		throws NotFoundException, UnauthorizedException, DatastoreException;
+//	
+//	/**
+//	 * @param groupId
+//	 * @param nodeId
+//	 * @param userId the user making the request
+//	 * 
+//	 * removes the access to the given node from given group
+//	 * 
+//	 * @exception NotFoundException if the group or node is invalid; 
+//	 * UnauthorizedException if the given user doesn't have 'share'
+//	 * permission on the given node
+//	 * 
+//	 */
+//	public void removeAccess(String groupId, String nodeId, String userId) 
+//		throws NotFoundException, UnauthorizedException, DatastoreException;
 	
 	/**
-	 * @param groupId
-	 * @param nodeId
-	 * @param userId the user making the request
-	 * 
-	 * removes the access to the given node from given group
-	 * 
-	 * @exception NotFoundException if the group or node is invalid; 
-	 * UnauthorizedException if the given user doesn't have 'share'
-	 * permission on the given node
-	 * 
-	 */
-	public void removeAccess(String groupId, String nodeId, String userId) 
-		throws NotFoundException, UnauthorizedException, DatastoreException;
-	
-	/**
-	 * @param groupId
+	 * @param userName
 	 * @param nodeId
 	 * @param accessType
 	 * 
@@ -46,9 +49,9 @@ public interface AuthorizationDAO {
 	 * @exception NotFoundException if the group or node is invalid
 	 * 
 	 */
-	public boolean canAccess(String groupId, String nodeId, String accessType) 
+	public boolean canAccess(String userName, String nodeId, String accessType) 
 		throws NotFoundException, DatastoreException;
-
+	
 	// at this level there is no representation of distinct resource types
 	// either (1) have to add a 'type' attribute to a Node; or (2) have to
 	// do the check at a higher level, prior to reaching this one
