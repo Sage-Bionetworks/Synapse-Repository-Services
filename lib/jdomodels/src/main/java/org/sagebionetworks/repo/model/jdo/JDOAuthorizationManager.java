c87d1d7b267f5aac107031cbd0f907e7ef5233c3
bruce.hoff <bruce.hoff@sagebase.org>
2011-05-11 18:21:14 +0000

    adding missing file...

---
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/jdo/JDOAuthorizationManager.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.sagebionetworks.repo.model.jdo;
+
+import org.sagebionetworks.repo.model.AuthorizationManager;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * @author bhoff
+ *
+ */
+public interface JDOAuthorizationManager extends AuthorizationManager, InitializingBean {
+
+}
