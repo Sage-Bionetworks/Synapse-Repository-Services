54bcd32e0baa87cf287daaa081d667e210bc8f50
bruce.hoff <bruce.hoff@sagebase.org>
2011-05-25 22:24:56 +0000

    user dao proxy, auth tests

---
+++ services/repository/src/main/java/org/sagebionetworks/repo/util/UserDAOProxy.java
@@ -0,0 +1,65 @@
+package org.sagebionetworks.repo.util;
+
+import java.util.Collection;
+
+import org.sagebionetworks.repo.model.DatastoreException;
+import org.sagebionetworks.repo.model.InvalidModelException;
+import org.sagebionetworks.repo.model.User;
+import org.sagebionetworks.repo.model.UserDAO;
+import org.sagebionetworks.repo.web.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserDAOProxy implements UserDAO {
+	
+	@Autowired
+	private UserDAO userDAOImpl;
+
+	
+	public UserDAOProxy() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		String implementingClassName = System.getProperty(getClass().getName());
+		if (implementingClassName==null || implementingClassName.length()==0) {
+			return;
+		}
+		userDAOImpl = (UserDAO)Class.forName(implementingClassName).newInstance();
+	}
+
+	@Override
+	public String create(User dto) throws DatastoreException,
+			InvalidModelException {
+		return userDAOImpl.create(dto);
+	}
+
+	@Override
+	public User get(String id) throws DatastoreException, NotFoundException {
+		return userDAOImpl.get(id);
+	}
+
+	@Override
+	public Collection<User> getAll() throws DatastoreException {
+		return userDAOImpl.getAll();
+	}
+
+	@Override
+	public void update(User dto) throws DatastoreException,
+			InvalidModelException, NotFoundException {
+		userDAOImpl.update(dto);
+	}
+
+	@Override
+	public void delete(String id) throws DatastoreException, NotFoundException {
+		userDAOImpl.delete(id);
+	}
+
+	@Override
+	public User getUser(String userName) throws DatastoreException,
+			NotFoundException {
+		return userDAOImpl.getUser(userName);
+	}
+
+	@Override
+	public Collection<String> getUserGroupNames(String userName)
+			throws NotFoundException, DatastoreException {
+		return userDAOImpl.getUserGroupNames(userName);
+	}
+
+}
--- services/repository/src/test/java/org/sagebionetworks/repo/manager/PermissionsManagerImplTest.java
+++ services/repository/src/test/java/org/sagebionetworks/repo/manager/PermissionsManagerImplTest.java
@@ -2,10 +2,25 @@ package org.sagebionetworks.repo.manager;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.sagebionetworks.repo.model.AccessControlList;
+import org.sagebionetworks.repo.model.AccessControlListDAO;
+import org.sagebionetworks.repo.model.AuthorizationConstants;
+import org.sagebionetworks.repo.model.Node;
+import org.sagebionetworks.repo.model.NodeDAO;
+import org.sagebionetworks.repo.model.ObjectType;
+import org.sagebionetworks.repo.model.UserGroup;
+import org.sagebionetworks.repo.model.UserGroupDAO;
+import org.sagebionetworks.repo.model.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -13,32 +28,101 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = { "classpath:manager-test-context.xml" })
 public class PermissionsManagerImplTest {
 
+	@Autowired
+	AuthorizationManager authorizationManager;
+	@Autowired
+	private NodeDAO nodeDAO;
+	
+	@Autowired
+	private UserManager userManager;
+	
+	@Autowired
+	private UserGroupDAO userGroupDAO;
+	
+	@Autowired
+	AccessControlListDAO accessControlListDAO;
+	
+	@Autowired
+	PermissionsManager permissionsManager;
+		
+	private Collection<Node> nodeList = new ArrayList<Node>();
+	private Node node = null;
+	private Node childNode = null;
+//	private UserInfo userInfo = null;
+	
+	private static final String TEST_USER = "test-user";
+	
+	private Node createDTO(String name, String createdBy, String modifiedBy, String parentId) {
+		Node node = new Node();
+		node.setName(name);
+		node.setCreatedOn(new Date());
+		node.setCreatedBy(createdBy);
+		node.setModifiedOn(new Date());
+		node.setModifiedBy(modifiedBy);
+		node.setNodeType(ObjectType.project.name());
+		if (parentId!=null) node.setParentId(parentId);
+		return node;
+	}
+	
+	private Node createNode(String name, String createdBy, String modifiedBy, String parentId) throws Exception {
+		Node node = createDTO(name, createdBy, modifiedBy, parentId);
+		String nodeId = nodeDAO.createNew(node);
+		assertNotNull(nodeId);
+		node.setId(nodeId);
+		return node;
+	}
+
 	@Before
 	public void setUp() throws Exception {
-		// create a node
-		// create a child
-		// set ACL on parent
+		// create a resource
+		node = createNode("foo", "me", "metoo", null);
+		nodeList.add(node);
+				
+		childNode = createNode("foo2", "me2", "metoo2", node.getId());
+		
+		// userInfo
+		userManager.setUserDAO(new TestUserDAO()); // could use Mockito here
+//		userInfo = userManager.getUserInfo(TEST_USER);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		// delete parent node
+		for (Node n : nodeList) nodeDAO.delete(n.getId());
+		this.node=null;
+		
+		for (UserGroup g: userGroupDAO.getAll(true)) {
+			if (g.getName().equals(AuthorizationConstants.ADMIN_GROUP_NAME)) {
+				// leave it
+			} else if (g.getName().equals(AuthorizationConstants.PUBLIC_GROUP_NAME)) {
+				// leave it
+			} else if (g.getName().equals(AuthorizationConstants.ANONYMOUS_USER_ID)) {
+				// leave it
+			} else {
+				userGroupDAO.delete(g.getId());
+			}
+		}
+		
 	}
-
+	
 	@Test
-	public void testGetACL() {
+	@Ignore
+	public void testGetACL() throws Exception {
 		// retrieve parent acl
+		UserInfo adminInfo = userManager.getUserInfo(TestUserDAO.ADMIN_USER_NAME);
+		AccessControlList acl = permissionsManager.getACL(node.getId(), adminInfo);
+		assertEquals(node.getId(), acl.getResourceId());
 		// retrieve child acl.  should get parent's
+		acl = permissionsManager.getACL(childNode.getId(), adminInfo);
+		assertEquals(node.getId(), acl.getResourceId());
+	}
+
+	@Test
+	public void testValidateContent()throws Exception {
 //		fail("Not yet implemented");
 	}
 
 	@Test
-	public void testValidateContent() {
-//		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testUpdateACL() {
+	public void testUpdateACL() throws Exception {
 		// check that you get an exception if
 		// group id is null
 		// resource id is null
