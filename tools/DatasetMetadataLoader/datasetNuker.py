c3e9598e984629272787ba6b398e868c36cbbe24
deflaux <nicole.deflaux@sagebase.org>
2011-05-30 21:10:11 +0000

    (1) convert nuker to use more of the python client (2) modify loader to optionally use fake local data for situations such as integration testing where the actual datapackets will not be available

--- tools/DatasetMetadataLoader/datasetNuker.py
+++ tools/DatasetMetadataLoader/datasetNuker.py
@@ -9,39 +9,12 @@ parser = synapse.utils.createBasicArgParser('Tool to nuke all datasets currently
 
 synapse.client.addArguments(parser)
 
-#-------------------[ Constants ]----------------------
-HEADERS = {
-    "Accept": "application/json",
-    }
-
-#--------------------[ getAllDatasets ]-----------------------------
-# TODO put an improved version of this in synapse.client
-def getAllDatasets():
-    conn = httplib.HTTPConnection(args.serviceEndpoint, timeout=30)
-    if(args.debug):
-        conn.set_debuglevel(10);
-    try:
-        conn.request("GET", args.servletPrefix + "/dataset?limit=500", None, HEADERS)
-        resp = conn.getresponse()
-        output = resp.read()
-        if args.debug:
-            print output
-        if resp.status == 200:
-            results = json.loads(output)
-            return results["results"]
-        else:
-            print resp.status, resp.reason
-            return None;
-    except Exception, err:
-        print(err)
-    conn.close()
-
 #--------------------[ Main ]-----------------------------
 
 args = parser.parse_args()
 synapse = synapse.client.factory(args)
-allDatasets = getAllDatasets();
-for dataset in allDatasets:
+allDatasets = synapse.getEntity("/dataset?limit=500");
+for dataset in allDatasets["results"]:
     print "About to nuke: " + dataset["uri"]
     synapse.deleteEntity(dataset["uri"])
     
