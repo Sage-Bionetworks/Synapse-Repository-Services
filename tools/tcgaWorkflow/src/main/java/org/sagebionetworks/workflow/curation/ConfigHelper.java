ce98c43cda0ebbb9265859b71c12d0e4ddef9826
deflaux <nicole.deflaux@sagebase.org>
2011-05-31 01:35:43 +0000

    Fix for build breakage due to "unmappable character for encoding ASCII"

--- tools/tcgaWorkflow/src/main/java/org/sagebionetworks/workflow/curation/ConfigHelper.java
+++ tools/tcgaWorkflow/src/main/java/org/sagebionetworks/workflow/curation/ConfigHelper.java
@@ -1,14 +1,10 @@
-// Copyright [2011]-[2011] Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// Licensed under the Amazon Web Services Customer Agreement (the “License”).
-// You may not use this file except in compliance with the License. A copy of the License is located at
-//     http://aws.amazon.com/agreement/
-// or in the “license” file accompanying this file.
-// This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and limitations under the License.
 package org.sagebionetworks.workflow.curation;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
 
@@ -21,57 +17,94 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.simpleworkflow.AmazonSimpleWorkflow;
 import com.amazonaws.services.simpleworkflow.AmazonSimpleWorkflowClient;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
 
 /**
- * Configuration Helper to used to create SWF and S3 clients
+ * Configuration Helper to used to create Synapse and AWS service clients.
  */
 public class ConfigHelper {
 
+	/**
+	 * The system property key with which to pass the AWS access key id
+	 */
+	public static final String ACCESS_ID_KEY = "AWS_ACCESS_KEY_ID";
+	/**
+	 * The system property key with which to pass the AWS secret key
+	 */
+	public static final String SECRET_KEY_KEY = "AWS_SECRET_KEY";
+
 	private static final String PROPERTIES_FILENAME = "workflow.properties";
 
-	private static final String SYNAPSE_SERVICE_URL_KEY = "synapse.url";
-	private static final String SWF_SERVICE_URL_KEY = "service.url";
-	private static final String SWF_ACCESS_ID_KEY = "AWS_ACCESS_KEY_ID";
-	private static final String SWF_SECRET_KEY_KEY = "AWS_SECRET_KEY";
+	private static final String SYNAPSE_AUTH_ENDPOINT_KEY = "synapse.auth.endpoint";
+	private static final String SYNAPSE_REPO_ENDPOINT_KEY = "synapse.repo.endpoint";
+	private static final String SWF_ENDPOINT_KEY = "swf.endpoint";
+	private static final String SNS_ENDPOINT_KEY = "sns.endpoint";
+	private static final String SNS_TOPIC_KEY = "sns.topic";
+	private static final String S3_BUCKET_KEY = "s3.bucket";
 
-	private static final String S3_ACCESS_ID_KEY = "AWS.Access.ID";
-	private static final String S3_SECRET_KEY_KEY = "AWS.Secret.Key";
-	private static final String S3_BUCKET_NAME_KEY = "S3.Bucket.Name";
+	private String synapseRepoEndpoint;
+	private String synapseAuthEndpoint;
 
-	private String synapseServiceUrl;
+	private String snsEndpoint;
+	private String swfEndpoint;
 
-	private String swfServiceUrl;
-	private String swfAccessId;
-	private String swfSecretKey;
+	private String snsTopic;
+	private String s3Bucket;
 
-	private String s3AccessId;
-	private String s3SecretKey;
-	private String s3BucketName;
+	private String awsAccessId;
+	private String awsSecretKey;
 
 	private volatile static ConfigHelper theInstance = null;
 
-	private ConfigHelper() throws Exception {
+	private ConfigHelper() {
 
 		URL url = ClassLoader.getSystemResource(PROPERTIES_FILENAME);
 		if (null == url) {
-			throw new Exception("unable to find in classpath "
+			throw new Error("unable to find in classpath "
 					+ PROPERTIES_FILENAME);
 		}
 		Properties serviceProperties = new Properties();
-		serviceProperties.load(new FileInputStream(new File(url.getFile())));
+		try {
+			serviceProperties
+					.load(new FileInputStream(new File(url.getFile())));
+		} catch (FileNotFoundException e) {
+			throw new Error(e);
+		} catch (IOException e) {
+			throw new Error(e);
+		}
 
-		this.synapseServiceUrl = serviceProperties
-				.getProperty(SYNAPSE_SERVICE_URL_KEY);
-		this.swfServiceUrl = serviceProperties.getProperty(SWF_SERVICE_URL_KEY);
-		this.swfAccessId = System.getProperty(SWF_ACCESS_ID_KEY); // serviceProperties.getProperty(SWF_ACCESS_ID_KEY);
-		this.swfSecretKey = System.getProperty(SWF_SECRET_KEY_KEY); // serviceProperties.getProperty(SWF_SECRET_KEY_KEY);
+		synapseRepoEndpoint = serviceProperties
+				.getProperty(SYNAPSE_REPO_ENDPOINT_KEY);
+		synapseAuthEndpoint = serviceProperties
+				.getProperty(SYNAPSE_AUTH_ENDPOINT_KEY);
 
-		this.s3AccessId = System.getProperty(SWF_ACCESS_ID_KEY); // serviceProperties.getProperty(S3_ACCESS_ID_KEY);
-		this.s3SecretKey = System.getProperty(SWF_SECRET_KEY_KEY); // serviceProperties.getProperty(S3_SECRET_KEY_KEY);
-		this.s3BucketName = serviceProperties.getProperty(S3_BUCKET_NAME_KEY);
+		snsEndpoint = serviceProperties.getProperty(SNS_ENDPOINT_KEY);
+		swfEndpoint = serviceProperties.getProperty(SWF_ENDPOINT_KEY);
+
+		snsTopic = serviceProperties.getProperty(SNS_TOPIC_KEY);
+		s3Bucket = serviceProperties.getProperty(S3_BUCKET_KEY);
+
+		// These come from the environment, not a config file so that we do not
+		// check credentials into source control
+		awsAccessId = System.getProperty(ACCESS_ID_KEY);
+		awsSecretKey = System.getProperty(SECRET_KEY_KEY);
+
+		if ((null == awsAccessId) || (null == awsSecretKey)) {
+			throw new Error(
+					"AWS credentials are missing, pass them as JVM args -D"
+							+ ACCESS_ID_KEY + "=theAccessKey -D"
+							+ SECRET_KEY_KEY + "=theSecretKey");
+		}
+
 	}
 
-	public static ConfigHelper createConfig() throws Exception {
+	/**
+	 * Factory method for Configuration Helper
+	 * 
+	 * @return the configuration singleton
+	 */
+	public static ConfigHelper createConfig() {
 		if (null == theInstance) {
 			synchronized (ConfigHelper.class) {
 				if (null == theInstance) {
@@ -82,30 +115,69 @@ public class ConfigHelper {
 		return theInstance;
 	}
 
+	/**
+	 * Create a synchronous Simple Workflow Framework (SWF) Client
+	 * 
+	 * @return the SWF client
+	 */
 	public AmazonSimpleWorkflow createSWFClient() {
 		ClientConfiguration config = new ClientConfiguration()
 				.withSocketTimeout(70 * 1000);
-		AWSCredentials awsCredentials = new BasicAWSCredentials(
-				this.swfAccessId, this.swfSecretKey);
+		AWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessId,
+				awsSecretKey);
 		AmazonSimpleWorkflow client = new AmazonSimpleWorkflowClient(
 				awsCredentials, config);
-		client.setEndpoint(this.swfServiceUrl);
+		client.setEndpoint(swfEndpoint);
 		return client;
 	}
 
+	/**
+	 * Create a synchronous Simple Storage Service (S3) client
+	 * 
+	 * @return the S3 Client
+	 */
 	public AmazonS3 createS3Client() {
-		AWSCredentials s3AWSCredentials = new BasicAWSCredentials(
-				this.s3AccessId, this.s3SecretKey);
+		AWSCredentials s3AWSCredentials = new BasicAWSCredentials(awsAccessId,
+				awsSecretKey);
 		AmazonS3 client = new AmazonS3Client(s3AWSCredentials);
 		return client;
 	}
 
-	public Synapse createSynapseClient() {
-		Synapse synapse = new Synapse(this.synapseServiceUrl);
+	/**
+	 * Create a synchronous Simple Notification Service (SNS) client
+	 * 
+	 * @return the SNS Client
+	 */
+	public AmazonSNS createSNSClient() {
+		AWSCredentials snsAWSCredentials = new BasicAWSCredentials(awsAccessId,
+				awsSecretKey);
+		AmazonSNS client = new AmazonSNSClient(snsAWSCredentials);
+		client.setEndpoint(snsEndpoint);
+		return client;
+	}
+
+	/**
+	 * @return the Synapse client
+	 * @throws MalformedURLException 
+	 */
+	public Synapse createSynapseClient() throws MalformedURLException {
+		Synapse synapse = new Synapse();
+		synapse.setRepositoryEndpoint(synapseRepoEndpoint);
+		synapse.setAuthEndpoint(synapseAuthEndpoint);
 		return synapse;
 	}
 
-	public String getS3BucketName() {
-		return s3BucketName;
+	/**
+	 * @return the s3Bucket
+	 */
+	public String getS3Bucket() {
+		return s3Bucket;
+	}
+
+	/**
+	 * @return the snsTopic
+	 */
+	public String getSnsTopic() {
+		return snsTopic;
 	}
 }
--- tools/tcgaWorkflow/src/main/java/org/sagebionetworks/workflow/curation/TcgaWorkflowActivityService.java
+++ tools/tcgaWorkflow/src/main/java/org/sagebionetworks/workflow/curation/TcgaWorkflowActivityService.java
@@ -1,10 +1,3 @@
-// Copyright [2011]-[2011] Amazon.com, Inc. or its affiliates. All Rights Reserved. 
-// Licensed under the Amazon Web Services Customer Agreement (the “License”). 
-// You may not use this file except in compliance with the License. A copy of the License is located at
-//     http://aws.amazon.com/agreement/
-// or in the “license” file accompanying this file. 
-// This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
-// either express or implied. See the License for the specific language governing permissions and limitations under the License.
 package org.sagebionetworks.workflow.curation;
 
 import java.io.IOException;
