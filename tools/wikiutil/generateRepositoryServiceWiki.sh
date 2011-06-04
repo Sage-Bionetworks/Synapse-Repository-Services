50d89087333082b0b0933b571e57559a32ea763d
deflaux <nicole.deflaux@sagebase.org>
2011-06-04 00:39:54 +0000

    Wiki generator now logs into synapse.

--- tools/wikiutil/generateRepositoryServiceWiki.sh
+++ tools/wikiutil/generateRepositoryServiceWiki.sh
@@ -3,11 +3,11 @@
 
 cat src/main/resources/RepositoryWikiPrefix.txt
 
-mvn exec:java -Dexec.mainClass="org.sagebionetworks.repo.ReadOnlyWikiGenerator" -Dexec.args=$1 | iconv -c -f UTF-8 -t UTF-8 | grep -v "\[INFO\]" | grep -v "DEBUG \[httpclient.wire.header\] >> \"" | grep -v "WARN \[org.apache.commons.httpclient.HttpMethodBase\] Going to buffer response body of large or unknown size. Using getResponseBodyAsStream instead is recommended." | sed "s/ INFO \[org.sagebionetworks.repo.WikiGenerator\] //" | sed 's/DEBUG \[httpclient.wire.header\] << "//' | sed 's/\[\\r\]\[\\n\]"//' | uniq 
-
 # only call this on localhost so that we do not accidentally put bad
 # data in prod
-mvn exec:java -Dexec.mainClass="org.sagebionetworks.repo.CRUDWikiGenerator" -Dexec.args=http://localhost:8080/repo/v1 | iconv -c -f UTF-8 -t UTF-8 | grep -v "\[INFO\]" | grep -v "DEBUG \[httpclient.wire.header\] >> \"" | grep -v "WARN \[org.apache.commons.httpclient.HttpMethodBase\] Going to buffer response body of large or unknown size. Using getResponseBodyAsStream instead is recommended." | sed "s/ INFO \[org.sagebionetworks.repo.WikiGenerator\] //" | sed 's/DEBUG \[httpclient.wire.header\] << "//' | sed 's/\[\\r\]\[\\n\]"//' | uniq 
+mvn exec:java -Dexec.mainClass="org.sagebionetworks.repo.CRUDWikiGenerator" -Dexec.args="http://localhost:8080/repo/v1 $2 $3 $4" | iconv -c -f UTF-8 -t UTF-8 | grep -v "\[INFO\]" | grep -v "DEBUG \[httpclient.wire.header\] >> \"" | grep -v "WARN \[org.apache.commons.httpclient.HttpMethodBase\] Going to buffer response body of large or unknown size. Using getResponseBodyAsStream instead is recommended." | sed "s/ INFO \[org.sagebionetworks.repo.WikiGenerator\] //" | sed 's/DEBUG \[httpclient.wire.header\] << "//' | sed 's/\[\\r\]\[\\n\]"//' | uniq 
+
+mvn exec:java -Dexec.mainClass="org.sagebionetworks.repo.ReadOnlyWikiGenerator" -Dexec.args="$1 $2 $3 $4" | iconv -c -f UTF-8 -t UTF-8 | grep -v "\[INFO\]" | grep -v "DEBUG \[httpclient.wire.header\] >> \"" | grep -v "WARN \[org.apache.commons.httpclient.HttpMethodBase\] Going to buffer response body of large or unknown size. Using getResponseBodyAsStream instead is recommended." | sed "s/ INFO \[org.sagebionetworks.repo.WikiGenerator\] //" | sed 's/DEBUG \[httpclient.wire.header\] << "//' | sed 's/\[\\r\]\[\\n\]"//' | uniq 
 
 cat src/main/resources/RepositoryWikiSuffix.txt
 
