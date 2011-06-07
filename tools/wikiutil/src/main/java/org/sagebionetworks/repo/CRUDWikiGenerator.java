3e51fe894b23ac8c948cd4e974e2dbc813aee0a6
deflaux <nicole.deflaux@sagebase.org>
2011-06-07 17:54:47 +0000

    Updated wiki generator for new /location uris

--- tools/wikiutil/src/main/java/org/sagebionetworks/repo/CRUDWikiGenerator.java
+++ tools/wikiutil/src/main/java/org/sagebionetworks/repo/CRUDWikiGenerator.java
@@ -1,5 +1,7 @@
 package org.sagebionetworks.repo;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
@@ -50,9 +52,9 @@ public class CRUDWikiGenerator {
 						new JSONObject(
 								"{\"status\": \"Pending\", \"description\": \"Genetic and epigenetic alterations have been identified that ...\", "
 										+ "\"creator\": \"Charles Sawyers\", \"releaseDate\": \"2008-09-14\", \"version\": \"1.0.0\", \"name\": \"MSKCC Prostate Cancer\"}"),
-						"h3. Create a Dataset", "Note that the request is a POST and the content type of the data we are sending to the service is json");
+						"h3. Create a Dataset",
+						"Note that the request is a POST and the content type of the data we are sending to the service is json");
 
-	
 		dataset.put("status", "Current");
 		wiki
 				.doPut(
@@ -78,11 +80,15 @@ public class CRUDWikiGenerator {
 						+ "Cell. 2010 Jul 13;18(1):11-22.  \"], \"Disease\": [\"Cancer\"], \"Species\": [\"Human\"], \"Internal_Name\": [\"Prostate cancer-MSKCC\"], "
 						+ "\"Tissue_Tumor\": [\"Prostate\"], \"Type\": [\"GCD\"], \"Institution\": [\"Memorial Sloan Kettering Cancer Center\"]}}");
 		// transfer our canned annotations to our annotations object
-		annotations.put("dateAnnotations", cannedAnnotations.getJSONObject("dateAnnotations"));
-		annotations.put("doubleAnnotations", cannedAnnotations.getJSONObject("doubleAnnotations"));
-		annotations.put("longAnnotations", cannedAnnotations.getJSONObject("longAnnotations"));
-		annotations.put("stringAnnotations", cannedAnnotations.getJSONObject("stringAnnotations"));
-		
+		annotations.put("dateAnnotations", cannedAnnotations
+				.getJSONObject("dateAnnotations"));
+		annotations.put("doubleAnnotations", cannedAnnotations
+				.getJSONObject("doubleAnnotations"));
+		annotations.put("longAnnotations", cannedAnnotations
+				.getJSONObject("longAnnotations"));
+		annotations.put("stringAnnotations", cannedAnnotations
+				.getJSONObject("stringAnnotations"));
+
 		wiki
 				.doPut(
 						dataset.getString("annotations"),
@@ -91,39 +97,64 @@ public class CRUDWikiGenerator {
 						"Then you add/modify the annotations of interest and do a PUT. *Note that annotation values must always be arrays even if the "
 								+ "array is only of length one.*");
 
-		JSONObject layer = wiki.doPost(dataset.getString("layer"), new JSONObject("{\"status\": \"curated\", \"name\": \"phenotypes\", \"numSamples\": \"261\", \"platform\": \"\", \"version\": \"1.0.0\", \"type\": \"C\"}"),
-				"h3. Add a Layer to a Dataset",
-				"Note that the dataset id in the uri denotes to which dataset this layer belongs");
+		JSONObject layer = wiki
+				.doPost(
+						dataset.getString("layer"),
+						new JSONObject(
+								"{\"status\": \"curated\", \"name\": \"phenotypes\", \"numSamples\": \"261\", \"platform\": \"\", \"version\": \"1.0.0\", \"type\": \"C\"}"),
+						"h3. Add a Layer to a Dataset",
+						"Note that the dataset id in the uri denotes to which dataset this layer belongs");
 
 		log.info("h3. Add Locations to a Layer");
-		JSONObject locations = wiki.doGet(layer.getJSONArray("locations").getString(0), 
-				"h4. Get the locations",
-				"First get the empty locations for your newly created layer");
 
-		JSONObject cannedLocations = new JSONObject("{\"locations\": [{\"path\": \"/Shares/external-data/DAT_011__sawyers_prostate_cancer\", \"type\": \"sage\"}, "
-				+ "{\"path\": \"mskcc_prostate_cancer.phenotype.zip\", \"md5sum\": \"b513a23fc54b7b0d65312e1a900af5a6\", \"type\": \"awss3\"}]}"); 
-		// transfer our canned locations to our locations object
-		locations.put("locations", cannedLocations.getJSONArray("locations"));
-		
-		wiki.doPut(layer.getJSONArray("locations").getString(0), locations, 
-				"h4. Put the locations",
-				"Then you add/modify the locations of interest and do a PUT.");
+		JSONObject cannedLocation = new JSONObject(
+				"{\"parentId\":\""
+						+ layer.getString("id")
+						+ "\",\"path\": \"mskcc_prostate_cancer.phenotype.zip\", \"md5sum\": \"b513a23fc54b7b0d65312e1a900af5a6\", \"type\": \"awss3\"}");
+
+		JSONObject s3Location = wiki
+				.doPost(
+						"/location",
+						cannedLocation,
+						"h4. Create the location to which to PUT your data",
+						"First get the presigned S3 URL to use for the upload.  You must specify the parentId of the Layer (or Dataset) to which this location belongs.");
+
+		log.info("h4. PUT the data");
+		log
+				.info("Then PUT the data to S3 via an HTTP Client that supports multipart upload.  Note that you must: ");
+		log.info("# Add header {{Content-Type: application/binary}}");
+		log
+				.info("# Add header {{Content-MD5: <the base64 encoded md5 checksum>}}");
+		log.info("# Add header {{x-amz-acl: bucket-owner-full-control}}");
+		log.info("\nHere is an example with curl:{code}");
+
+		// TODO find a more direct way to go from hex to base64
+		byte[] encoded = Base64.encodeBase64(Hex
+				.decodeHex("b513a23fc54b7b0d65312e1a900af5a6".toCharArray()));
+		String base64Md5 = new String(encoded, "ASCII");
+
+		log.info("curl -v -X PUT -H Content-MD5:" + base64Md5
+				+ " -H x-amz-acl:bucket-owner-full-control \\\n"
+				+ " -H Content-Type:application/binary "
+				+ " --data-binary @<localFilepath> \\\n'"
+				+ s3Location.getString("path") + "'{code}\n\n");
 
 		log.info("h3. Add Preview to a Layer");
-		JSONObject preview = wiki.doGet(layer.getString("preview"), 
+		JSONObject preview = wiki.doGet(layer.getString("preview"),
 				"h4. Get the preview",
 				"First get the empty preview for your newly created layer");
 
 		String cannedPreview = "phenotype_id\tsample_type\tmetastatic_site\tethnicity\tpredxbxpsa\tage\tclinical_primary_gleason\tclinical_secondary_gleason\tclinical_gleason_score\tpre_treatment_psa\tclinical_tnm_stage_t\tneoadjradtx\tchemotx\thormtx\tradtxtype\trp_type\tsms\textra_capsular_extension\tseminal_vesicle_invasion\ttnm_stage_n\tnumber_nodes_removed\tnumber_nodes_positive\tpathologic_tnm_stage_t\tpathologic_primary_gleason\tpathologic_secondary_gleason\tpathologic_gleason_score\tbcr_freetime\tbcr_event\tmetsevent\tsurvtime\tevent\tnomogram_pfp_postrp\tnomogram_nomopred_extra_capsular_extension\tnomogram_nomopred_lni\tnomogram_nomopred_ocd\tnomogram_nomopred_seminal_vesicle_invasion\tcopy_number_cluster\texpression_array_tissue_source\r\nPCA0004\tPRIMARY\tNA\tWhite Non Hispanic\t27.5\t68.93\t3\t2\t5\t11.8\tT2B\tNA\tNA\tNA\tNA\tRP\tNegative\tESTABLISHED\tNegative\tNormal_N0\t13\t0\tT3A\t3\t4\t7\t152.55\tNO\tNO\t152.55\tNO\tNA\t37.937846\t3.593974\t55.082939\tNA\t1\tNA\r\nPCA0006\tPRIMARY\tNA\tWhite Non Hispanic\t15.7\t56.64\t3\t3\t6\t8.2\tT2B\tNA\tNA\tNeoadjuvant HORM\tNA\tRP\tNegative\tNONE\tNegative\tNormal_N0\t4\t0\tT2C\t3\t3\t6\t160.96\tNO\tNO\t160.96\tNO\tNA\tNA\tNA\tNA\tNA\t4\tNA\r\nPCA0016\tPRIMARY\tNA\tWhite Non Hispanic\t12\t67.36\t3\t3\t6\t12\tT2B\tNA\tNA\tNeoadjuvant HORM\tNA\tRP\tNegative\tNONE\tNegative\tNormal_N0\t2\t0\tT2C\t4\t4\t8\t74.22\tNO\tNO\t74.22\tNO\t99\tNA\tNA\tNA\t97.11015465\t2\tNA\r\nPCA0019\tPRIMARY\tNA\tWhite Non Hispanic\t6.6\t68.12\t3\t4\t7\t6.6\tT1C\tNA\tNA\tNA\tNA\tRP\tNegative\tNONE\tNegative\tNormal_N0\t1\t0\tT2C\t3\t3\t6\t110.33\tBCR_Algorithm\tNO\t123.67\tNO\tNA\tNA\tNA\tNA\t79.85545652\t2\tNA\r\nPCA0023\tPRIMARY\tNA\tBlack Non Hispanic\t4.3\t60.57\t4\t3\t7\t3.88\tT1C\tNA\tNA\tPostHORM\tNA\tRP\tPositive\tNONE\tNegative\tNormal_N0\t2\t0\tT2C\t4\t5\t9\t10.61\tBCR_Algorithm\tNO\t72.84\tDEATH FROM OTHER CANCER\t79.85546\t19.190208\t2.138938\t77.240045\t99\t4\tNA\r\n";
 		// transfer our canned preview to our preview object
 		preview.put("preview", cannedPreview);
-		
-		wiki.doPut(layer.getString("preview"), preview, 
-				"h4. Put the preview",
+
+		wiki.doPut(layer.getString("preview"), preview, "h4. Put the preview",
 				"Then you add/modify the preview and do a PUT.");
-				
-		
-		wiki.doDelete(dataset.getString("uri"), "h3. Delete a Dataset",
-				"Note that the request is a DELETE and no content is returned.  Also note that this will delete all of the datasets layers, etc.");
+
+		wiki
+				.doDelete(
+						dataset.getString("uri"),
+						"h3. Delete a Dataset",
+						"Note that the request is a DELETE and no content is returned.  Also note that this will delete all of the datasets layers, etc.");
 	}
 }
