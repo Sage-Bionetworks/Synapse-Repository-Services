2cff76a677e3fc1a438ac0e79d1089e3af5c1d7e
deflaux <nicole.deflaux@sagebase.org>
2011-06-09 17:16:55 +0000

    The R client now supports uploads of data to Synapse (S3)

---
+++ client/rSynapseClient/R/synapseUploadFile.R
@@ -0,0 +1,37 @@
+.hexMD5ToBase64MD5 <- function(checksumHex) {
+	checksumBytes <- raw(16)
+	for (i in 1:16)
+	{
+		hex <- paste('0x', substr(checksumHex, ((i*2)-1), (i*2)), sep="")
+		checksumBytes[i] <- as.raw(hex)
+	}
+	checksumBase64 <- base64Encode(checksumBytes)
+	checksumBase64[[1]]
+}
+
+synapseUploadFile <- 
+		function (url, srcfile, checksum, method = "curl", quiet = FALSE, mode = "w", cacheOK = TRUE, cacheDir = synapseCacheDir())
+{
+	
+	if(!grepl(cacheDir, srcfile)) {
+		srcfile <- file.path(cacheDir, srcfile)
+	}
+	
+	if (method == "curl") {
+		extra <- if (quiet)
+					" -s -S "
+				else ""
+		status <- system(paste("curl", extra, " -k ", 
+						" -H Content-Type:application/binary",
+						" -H Content-MD5:", .hexMD5ToBase64MD5(checksum),
+						" -H x-amz-acl:bucket-owner-full-control",
+						" --data-binary ", "@", path.expand(srcfile),
+						" -X PUT ",
+						shQuote(url), sep=""))
+		
+	}else{
+		stop("unsupported method:", method)
+	}
+	
+	invisible(status)
+}
--- client/rSynapseClient/inst/unitTests/test_hexMD5ToBase64MD5.R
+++ client/rSynapseClient/inst/unitTests/test_hexMD5ToBase64MD5.R
@@ -1,18 +1,5 @@
-library(tools)
-
-hexMD5ToBase64MD5 <- function(checksumHex) {
-	checksumBytes <- raw(16)
-	for (i in 1:16)
-	{
-		hex <- paste('0x', substr(checksumHex, ((i*2)-1), (i*2)), sep="")
-		checksumBytes[i] <- as.raw(hex)
-	}
-	checksumBase64 <- base64Encode(checksumBytes)
-	checksumBase64[[1]]
-}
-
 unitTestMd5hexToBase64 <- function() {
-	checkEquals('BP5xIZ38aXAq1sYBz2iDbw==', hexMD5ToBase64MD5('04fe71219dfc69702ad6c601cf68836f')) 
+	checkEquals('BP5xIZ38aXAq1sYBz2iDbw==', .hexMD5ToBase64MD5('04fe71219dfc69702ad6c601cf68836f')) 
 }
 
 
