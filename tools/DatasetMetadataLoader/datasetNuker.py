8928e40f0f4918ee098be37a70e6329af00c66df
deflaux <nicole.deflaux@sagebase.org>
2011-06-23 17:02:06 +0000

    Some bug fixes for the integration tests.

--- tools/DatasetMetadataLoader/datasetNuker.py
+++ tools/DatasetMetadataLoader/datasetNuker.py
@@ -2,7 +2,7 @@
 
 # To debug this, python -m pdb myscript.py
 
-import os, json, re, string, datetime, pwd, urllib, httplib, ConfigParser, argparse, synapse.utils, synapse.client
+import synapse.utils, synapse.client
 
 #-------[ Documentation embedded in Command Line Arguments ]----------------
 parser = synapse.utils.createBasicArgParser('Tool to nuke all datasets currently stored in the repository service.  You might run this prior to datasetCsvLoader.py.')
