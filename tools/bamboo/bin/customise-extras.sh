b9e7e4c38fdaabb6d18a4137c23bbfa6460a55fd
deflaux <nicole.deflaux@sagebase.org>
2011-07-05 14:48:53 +0000

    Updates to the bamboo build to allow the R and Python integration tests to work.

--- tools/bamboo/bin/customise-extras.sh
+++ tools/bamboo/bin/customise-extras.sh
@@ -7,35 +7,13 @@
 # http://confluence.atlassian.com/display/BAMBOO/Configuring+Elastic+Instances+to+use+the+EBS
 # http://confluence.atlassian.com/display/BAMBOO/Populating+your+EBS+volume
 
+echo 'export LD_LIBRARY_PATH=/mnt/bamboo-ebs/lib/:/mnt/bamboo-ebs/R/lib/:/opt/jdk-6/jre/lib/i386/client/$LD_LIBRARY_PATH' >> /etc/profile.d/bamboo
+echo 'export PATH=/mnt/bamboo-ebs/bin:$PATH' >> /etc/profile.d/bamboo
 
-### Setup MySQL
-yum install -y mysql 
-yum install -y mysql-server 
-yum install -y mysql-devel 
-chgrp -R mysql /var/lib/mysql 
-chmod -R 770 /var/lib/mysql 
-service mysqld start 
-/usr/bin/mysqladmin create test2
-/usr/bin/mysqladmin -u root password 'platform' 
+export LD_LIBRARY_PATH=/mnt/bamboo-ebs/lib/:$LD_LIBRARY_PATH
+/usr/sbin/groupadd -g 27 mysql
+/usr/sbin/useradd -u 27 -g 27 mysql
+cd /mnt/bamboo-ebs;./bin/mysqld_safe &
 
-
-### Setup Python2.7, no need to do this everytime if we are sure to
-### install it on the EBS volume
-#mkdir /mnt/bamboo-ebs/systemDependencies
-#cd /mnt/bamboo-ebs/systemDependencies
-#wget http://www.python.org/ftp/python/2.7.1/Python-2.7.1.tar.bz2
-#tar -xvjf Python-2.7.1.tar.bz2
-#cd Python*
-#./configure --prefix=/mnt/bamboo/systemDependencies/python27
-#make
-#make install
-
-### Setup R, no need to do this everytime if we are sure to
-### install it on the EBS volume
-#cd /mnt/bamboo-ebs/systemDependencies
-#wget http://cran.fhcrc.org/src/base/R-2/R-2.13.0.tar.gz
-#tar -xvzf  R-2.13.0.tar.gz
-#cd R-2.13.0*
-#./configure --prefix=/mnt/bamboo/systemDependencies/R
-#make
-#make install
+# To shutdown mysql
+# /mnt/bamboo-ebs/bin/mysqladmin -u root -p shutdown
