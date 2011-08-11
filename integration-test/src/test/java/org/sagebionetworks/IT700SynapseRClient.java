3e4665b3d50c161e345790ab5f9b5b924b7a399e
deflaux <nicole.deflaux@sagebase.org>
2011-08-11 16:19:31 +0000

    (1) now that we have decided its okay to hit S3 from bamboo, none of the tests are "staging only" (2) fixed a bug in a new integration test where a project was created, not deleted, and another was made with the same name

--- integration-test/src/test/java/org/sagebionetworks/IT700SynapseRClient.java
+++ integration-test/src/test/java/org/sagebionetworks/IT700SynapseRClient.java
@@ -100,7 +100,7 @@ public class IT700SynapseRClient {
 						+ "', password='"
 						+ StackConfiguration.getIntegrationTestUserOnePassword() + "')",
 				"-e",
-				"synapseClient:::.integrationTest(testFileRegexp='^test_[^_]*R$')" };
+				"synapseClient:::.integrationTest()" };
 		ExternalProcessResult result = Helpers.runExternalProcess(cmd);
 		assertTrue(0 <= result.getStdout().indexOf(" 0 errors, 0 failures"));
 	}
