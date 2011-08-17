5d3e09b8dba2d8387ad260f2d4258e84e5d49bb9
bruce.hoff <bruce.hoff@sagebase.org>
2011-08-17 00:38:53 +0000

    PLFM-467

--- services/auth/src/test/java/org/sagebionetworks/auth/SerializationTest.java
+++ services/auth/src/test/java/org/sagebionetworks/auth/SerializationTest.java
@@ -1,9 +1,21 @@
 package org.sagebionetworks.auth;
 
 import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.codec.binary.Base64;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.sagebionetworks.StackConfiguration;
+import org.sagebionetworks.StringEncrypter;
 
 public class SerializationTest {
 
@@ -17,11 +29,35 @@ public class SerializationTest {
 	
 	@Test 
 	public void testSerialization() throws Exception {
-		String s = "lkasjfl;asjsdfghuihglfj";
-		String e = SampleConsumer.encryptingSerializer(s);
+		List<Integer> o = new ArrayList<Integer>();
+		o.add(1);
+		o.add(2);
+		o.add(100);
+		o.add(-10000);
+		
+//	    ByteArrayOutputStream out = new ByteArrayOutputStream();
+//	    ObjectOutputStream oos = new ObjectOutputStream(out);
+//	    oos.writeObject(o);
+//	    oos.close();
+//	    byte[] b1 = out.toByteArray();
+//
+//		ByteArrayInputStream bais = new ByteArrayInputStream(b1);
+//		ObjectInputStream ois = new ObjectInputStream(bais);
+//		try {
+//			assertEquals(o, ois.readObject());
+//		} catch (ClassNotFoundException e) {
+//			throw new RuntimeException(e);
+//		}
+
+		String e = SampleConsumer.encryptingSerializer(o);
+
+//	   	StringEncrypter se = new StringEncrypter(StackConfiguration.getEncryptionKey());
+//	   	String serializedAndBase64EncodedDI = se.decrypt(e);
+//	   	String serialized = new String(Base64.decodeBase64(serializedAndBase64EncodedDI.getBytes()));
+
 //		System.err.println(e);
-		String s2 = SampleConsumer.decryptingDeserializer(e);
-		assertEquals(s, s2);
+		ArrayList<Integer> o2 = SampleConsumer.decryptingDeserializer(e);
+		assertEquals(o, o2);
 	}
 
 }
