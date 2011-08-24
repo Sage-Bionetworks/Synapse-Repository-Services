f2d479432169bc48b7bbfa7196bb7526fc057eb8
deflaux <nicole.deflaux@sagebase.org>
2011-08-24 21:08:14 +0000

    bug fix to ensure 2.14 users get the right message

--- tools/rPackageBuildTools/sagebioCRAN.R
+++ tools/rPackageBuildTools/sagebioCRAN.R
@@ -3,10 +3,10 @@ source("http://bioconductor.org/biocLite.R")
 major <- as.numeric(R.Version()$major)
 minor <- as.numeric(R.Version()$minor)
 
-if (major == 2 && (minor >= 13)) {
+if (major == 2 && (minor >= 13 && (minor < 14))) {
   ##ok
 } else {
-  stop("You must be running R version 2.13 or higher")
+  stop("You must be running R version 2.13")
 }
 
 rversion <- paste(R.Version()$major, as.integer(R.Version()$minor), sep=".")
