ff637286d8e2ba233676cbaec025d69666b27705
MattNapsAlot <matt.furia@sagebase.org>
2011-09-09 21:13:30 +0000

    More cosmetic changes to Tk login widget. Made login widget not resizable. Added padding around components. Fixed text alignment.

--- client/rSynapseClient/R/getCredentials.R
+++ client/rSynapseClient/R/getCredentials.R
@@ -78,16 +78,16 @@
 	tcltk::tkgrid(imageLabel, column=0, row=0, columnspan=3)
 	
 	## the first row is for username
-	tcltk::tkgrid(tcltk::tklabel(root,text="Email Address"), column=0, row=1)
-	tcltk::tkgrid(usernameEntryWidget, column=1, row=1, columnspan=2)
+	tcltk::tkgrid(tcltk::tklabel(root,text="Email Address", justify='right'), column=0, row=1, sticky="e", padx=c(0,3), pady=c(12,3))
+	tcltk::tkgrid(usernameEntryWidget, column=1, row=1, columnspan=2, sticky="nsew", padx=c(0,3), pady=c(12,3))
 	
 	## the second row is for password
-	tcltk::tkgrid(tcltk::tklabel(root, text="     Password"), column=0, row=2)
-	tcltk::tkgrid(passwordEntryWidget, column=1, row=2, columnspan=2)
+	tcltk::tkgrid(tcltk::tklabel(root, text="Password", justify='right'), column=0, row=2, sticky="e", padx=c(0,3), pady=c(0,3))
+	tcltk::tkgrid(passwordEntryWidget, column=1, row=2, columnspan=2, sticky="nsew", padx=c(0,3), pady=c(0,8))
 	
 	## the third row is for the Login and Cancel buttons
-	tcltk::tkgrid(cancelButton, column=1, row=3)
-	tcltk::tkgrid(loginButton, column=2, row=3)
+	tcltk::tkgrid(cancelButton, column=1, row=3, pady=c(0,3))
+	tcltk::tkgrid(loginButton, column=2, row=3, pady=c(0,5))
 	
 	## bind the return key to onLogin function when in the passwordEntry widget and to
 	## set focus to the password entry widget when in the ussername entry widget
@@ -95,6 +95,9 @@
 	tcltk::tkbind(passwordEntryWidget, "<Return>", onLogin)
 	tcltk::tkbind(usernameEntryWidget, "<KeyRelease>", checkState)
 	tcltk::tkbind(passwordEntryWidget, "<KeyRelease>", checkState)
+	
+	## fix the size of the login window
+	tcltk::tkwm.resizable(root,FALSE, FALSE)
 	
 	## set the focus to the root widget
 	tcltk::tkgrab.set(root)
