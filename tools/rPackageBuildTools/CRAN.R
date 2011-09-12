3623f12f1e7201d80ec9aa47385a369cf53a9e59
deflaux <nicole.deflaux@sagebase.org>
2011-09-12 22:56:49 +0000

    New CRAN repositories for prod and staging R packages.

---
+++ tools/rPackageBuildTools/CRAN.R
@@ -0,0 +1,20 @@
+source("http://bioconductor.org/biocLite.R")
+
+pkgInstall <- function (package, stack='prod', ...) {
+  major <- as.numeric(R.Version()$major)
+  minor <- as.numeric(R.Version()$minor)
+
+  if (major == 2 && minor >= 13) {
+    ##ok
+  } else {
+    stop("You must be running R version 2.13 or higher")
+  }
+
+  rVersion <- paste(R.Version()$major, as.integer(R.Version()$minor), sep=".")
+  sageRepos <- paste("http://sage.fhcrc.org/CRAN",
+                     stack, rVersion, sep="/")
+  allRepos <- c(sageRepos, biocinstallRepos())
+
+  install.packages(package, repos=allRepos, ...)
+}
+
