595dd7b93a66486bc8335605908d497780bcae82
deflaux <nicole.deflaux@sagebase.org>
2011-09-12 23:20:37 +0000

    Updated script to handle different stacks and R 2.14

--- tools/rPackageBuildTools/deployAllPackages.sh
+++ tools/rPackageBuildTools/deployAllPackages.sh
@@ -1,18 +1,39 @@
-export STAGING_ROOT=/home/sagebio/sagebioRPackages/staging
-export REPO_ROOT=/srv/www/htdocs/Foswiki/sagebioCRAN
-#export REPO_ROOT=/home/ndeflaux/sagebioCRAN2
+#!/bin/bash
 
-## Make the CRAN directories, this is just a one-time task so keeping it
-## commented out
-# mkdir -p $REPO_ROOT/2.13/bin/macosx/leopard/contrib/2.13
-# mkdir -p $REPO_ROOT/2.13/bin/windows/contrib/2.13
-# ln -s $REPO_ROOT/2.13/bin/windows  $REPO_ROOT/2.13/bin/windows64
-# mkdir -p $REPO_ROOT/2.13/src/contrib
+stack=$1
+create=$2
 
-## Copy the packages from staging to the right spots in the CRAN directory tree
-cp $STAGING_ROOT/*zip $REPO_ROOT/2.13/bin/windows/contrib/2.13
-cp $STAGING_ROOT/*tgz $REPO_ROOT/2.13/bin/macosx/leopard/contrib/2.13
-cp $STAGING_ROOT/*tar.gz $REPO_ROOT/2.13/src/contrib
+if [ -z "$stack" ] ; then
+    echo "missing stack name"
+    exit 0
+fi
+
+export PACKAGE_ROOT=/home/sagebio/sagebioRPackages/$stack
+if [ ! -d $PACKAGE_ROOT ] ; then
+    echo "package root $PACKAGE_ROOT does not exist"
+    exit 0
+fi
+
+export REPO_ROOT=/srv/www/htdocs/Foswiki/CRAN/$stack
+if [ ! -d $REPO_ROOT ] ; then
+    echo "repository root $REPO_ROOT does not exist"
+    exit 0
+fi
+
+if [ -n "$create" ] && [ "$create" = "create" ] ; then
+    ## Make the CRAN directories
+    mkdir -p $REPO_ROOT/2.13/bin/macosx/leopard/contrib/2.13
+    mkdir -p $REPO_ROOT/2.13/bin/windows/contrib/2.13
+    ln -s $REPO_ROOT/2.13/bin/windows  $REPO_ROOT/2.13/bin/windows64
+    mkdir -p $REPO_ROOT/2.13/src/contrib
+    ln -s $REPO_ROOT/2.13 $REPO_ROOT/2.14
+fi
+
+## Copy the packages from the deployment area to the right spots in
+## the CRAN directory tree
+cp $PACKAGE_ROOT/*zip $REPO_ROOT/2.13/bin/windows/contrib/2.13
+cp $PACKAGE_ROOT/*tgz $REPO_ROOT/2.13/bin/macosx/leopard/contrib/2.13
+cp $PACKAGE_ROOT/*tar.gz $REPO_ROOT/2.13/src/contrib
 
 ## Make the PACKAGE and PACKAGE.gz files
 cd $REPO_ROOT/2.13/src/contrib ; R -e 'tools:::write_PACKAGES(".", type="source")'
