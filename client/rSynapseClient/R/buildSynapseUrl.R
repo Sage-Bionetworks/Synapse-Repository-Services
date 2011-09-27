4bb9ed4a71cb5580a0a337786887a17c072f8705
MattNapsAlot <matt.furia@sagebase.org>
2011-09-27 04:54:52 +0000

    Changes to Synapse R client: fixed bug in onWeb() function.

--- client/rSynapseClient/R/buildSynapseUrl.R
+++ client/rSynapseClient/R/buildSynapseUrl.R
@@ -6,12 +6,5 @@
 .buildSynapseUrl <- 
 		function(entity)
 {
-	if(grepl("staging", synapseRepoServiceEndpoint())){
-		portal <- kServicePortalUrlMap[["staging"]]
-	}else if(grepl("alpha", synapseRepoServiceEndpoint())){
-		portal <- kServicePortalUrlMap[["alpha"]]
-	}else{
-		return("")
-	}
-	url <- sprintf("%s/#Lookup:%s", portal, entity)
+	url <- sprintf("%s/#Lookup:%s", synapsePortalEndpoint(), entity)
 }
