e7589cbc9b2cd11560568ef6ef95ffe2a822495e
bruce.hoff <bruce.hoff@sagebase.org>
2011-10-06 17:42:41 +0000

    update

--- tools/securityTools/src/test/java/org/sagebionetworks/securitytools/HMACUtilsTest.java
+++ tools/securityTools/src/test/java/org/sagebionetworks/securitytools/HMACUtilsTest.java
@@ -11,16 +11,17 @@ public class HMACUtilsTest {
 	
 	@Test
 	public void testHMACUtils() throws Exception {
-//		DateTime timeStamp = new DateTime();
-//		System.out.println(timeStamp);
+		DateTime timeStamp = new DateTime();
+		System.out.println(timeStamp);
 
 //		String timeStampString = "";
 		String userId="demouser@sagebase.org";
-		String uri="/services-repository-0.7-SNAPSHOT/repo/v1/dataset";
+		String uri="/repo/v1/dataset";
+//		String uri="/services-repository-0.7-SNAPSHOT/repo/v1/dataset";
 		String base64EncodedSecretKey = "nUielh3l3rHuZis4JQ/4sr05N8ounV8OnQsZqmjmHnD2r1ITmJQSkr4WmM37e5Fi81lQ+WdZ794G6KEDMx/NKw==";
 		
 
-		String timeStampString = "2011-09-28T13:31:16.90-0700";
+		String timeStampString = timeStamp.toString(); //"2011-09-28T13:31:16.90-0700";
 
 		
 		String encoded = HMACUtils.generateHMACSHA1Signature(
