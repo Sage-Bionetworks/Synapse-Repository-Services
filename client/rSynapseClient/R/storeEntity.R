00385bc3709125eedd05a11a8876a2c25384029c
deflaux <nicole.deflaux@sagebase.org>
2011-10-07 23:41:45 +0000

    If layer is a media layer and contains only one file, don't zip it.  PLFM-654

--- client/rSynapseClient/R/storeEntity.R
+++ client/rSynapseClient/R/storeEntity.R
@@ -16,7 +16,11 @@ setMethod(
 		definition = function(entity){
 			if(length(list.files(file.path(entity$cacheDir, .getCache("rObjCacheDir")), all.files = TRUE)) > 0 ){
 				storeEntityObjects(entity)
-			}else{
+			} else if(("M" == propertyValue(entity, "type")) && (length(entity$files) == 1)) {
+				# Special case for media layers, don't zip them
+				storeFile(entity, entity$files)
+			}
+			else {
 				storeEntityFiles(entity)
 			}
 		}
