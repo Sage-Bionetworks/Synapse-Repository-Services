49cbd5af0c2c071849fea56cb3dc2827bf49db20
brig.mecham <brig.mecham@sagebase.org>
2011-10-19 23:38:32 +0000

    changed datasetCrawler.R to produce JSON of choice.

--- tools/gePipeline/src/main/resources/datasetCrawler.R
+++ tools/gePipeline/src/main/resources/datasetCrawler.R
@@ -1,2 +1,17 @@
 system("perl getGSEs.pl")
 all.gses <- read.table("all.GSEs.txt",sep="\t",header=TRUE,stringsAsFactors=FALSE,row.names=1)
+
+output <- list()
+lapply(1:nrow(all.gses), function(i) {  
+	b <- list(gpl     =all.gses[i,1],
+						lastUpdate    =all.gses[i,2],
+  					species =all.gses[i,3],
+						summary =all.gses[i,4],
+						hasCelFile=all.gses[i,5],
+						n_sample=all.gses[i,6])
+  b
+}) -> output
+
+names(output) <- rownames(all.gses)
+
+
