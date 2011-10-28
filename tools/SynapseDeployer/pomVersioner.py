7ea81eca6f221dec25f2b4a809a38c7dd002f391
mike.kellen <mike.kellen@sagebase.org>
2011-10-28 04:55:38 +0000

    Script to walk the complete PLFM hierarchy and bring all the pom.xml files up to new minor version

---
+++ tools/SynapseDeployer/pomVersioner.py
@@ -0,0 +1,30 @@
+import os, sys, fileinput
+'''
+Walk the complete PLFM hierarchy and bring all the pom.xml files up to new minor version
+'''
+
+#Old and new minor versions
+oldVersion = '0.8'
+newVersion = '0.9'
+#Path to PLFM on your system
+startPath = 'C:\\dev\\workspace\\Synapse'
+for root, subFolders, files in os.walk(startPath):    
+    for file in files:
+        if file == 'pom.xml': 
+            f = os.path.join(root, file) 
+            print 'updating '+f
+            for line in fileinput.FileInput(f, inplace=1):
+                found = False
+                if line.find('<version>'+oldVersion+'-SNAPSHOT</version>') >= 0 and not found:
+                    line = line.replace(oldVersion,newVersion)
+                    found = True
+                print line.rstrip()
+        if file == 'DESCRIPTION' and root.find('rSynapseClient') >= 0:
+            f = os.path.join(root, file)
+            print 'updating '+f
+            for line in fileinput.FileInput(f, inplace=1):
+                found = False
+                if not found and line.find('Version') >= 0:
+                    line = 'Version: '+newVersion+'-0'
+                    found = True
+                print line.rstrip()
