a0b748ed65e2f6dc9013cde315a946f46fe1cb7e
mike.kellen <mike.kellen@sagebase.org>
2011-11-02 23:30:28 +0000

    Upgraded boto to 2.1.1.  Fixed the zip archiver to include subdirectories.  Added support for creating application versions and updating active beanstalk instances. We now support the use case of an automated upgrade that only involves changes to one or more of the .war files.

--- tools/SynapseDeployer/main.py
+++ tools/SynapseDeployer/main.py
@@ -1,33 +1,36 @@
-import sageArtifactory, synapseAwsEnvironment
+import artifactoryClient, synapseAwsEnvironment, os.path
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
-# Parameters to drive Deployment
+class Component:
+    def __init__(self, environment_name, artifact_name):
+        self.environment_name = environment_name
+        self.artifact_name = artifact_name
 
-# Connection to AWS account DON'T CHECK PASSWORDS IN TO SVN
-account_id = 'xxx'
-secret_key = 'yyyy'
+# Parameters to drive Deployment
+workDir = os.sep+'temp'+os.sep 
+componentsToUpgrade = [Component('DefaultEnvironment', 'services-authentication')]
+version = '0.8'
+isSnapshot = True
+beanstalk_application_name = 'My First Elastic Beanstalk Application'
+description = 'created by Synapse Deployer' 
+environment_name = 'Default-Environment'
 
 # Change to match beanstalk bucket for your account
 deploymentBucket = 'elasticbeanstalk-us-east-1-059816207990'
 
-workDir = '/temp/' 
-warsToUpgrade = sageArtifactory.SUPPORTED_ARTIFACT_NAMES #all of them
-version = '0.8'
-isSnapshot = True
+# Initialize environment
+synapse = synapseAwsEnvironment.SynapseAwsEnvironment(deploymentBucket)
 
-synapse = synapseAwsEnvironment.SynapseAwsEnvironment(account_id, secret_key, deploymentBucket)
-#Testing connection to s3 
-#synapse.getAllBuckets()
-
-for war in warsToUpgrade:
-    artifact = sageArtifactory.downloadArtifact(war, version, isSnapshot, workDir)
-    key = war+'-'+version
-    if isSnapshot: key += '-SNAPSHOT'
-    key += '-'+str(artifact.buildNumber)+'.war'
-    print('uploading '+key)
-    synapse.putFile(key, artifact.fileName)
-
+# For all wars we want to upgrade, download from artifactory, put to S3, create beanstalk versions, and update beanstalk environments
+for component in componentsToUpgrade:
+    artifact = artifactoryClient.downloadArtifact(component.artifact_name, version, isSnapshot, workDir)
+    key = synapse.putWar(artifact.fileName, component.artifact_name, artifact.buildNumber, version, isSnapshot)
+    version_label = key[0:len(key)-4] #strip off .war
+    print 'creating version '+version_label
+    synapse.createApplicationVersion(version_label, beanstalk_application_name, 
+                                     key, description=description)
+    synapse.updateEnvironment(environment_name = environment_name, version_label = version_label)
     
 print('Done')
 
