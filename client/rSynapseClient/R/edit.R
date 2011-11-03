6e09237bca16853f77a80f665659cda7e60dcc8f
MattNapsAlot <matt.furia@sagebase.org>
2011-11-03 07:55:22 +0000

    Finalize demo script.

--- client/rSynapseClient/R/edit.R
+++ client/rSynapseClient/R/edit.R
@@ -5,14 +5,23 @@ setMethod(
 			args <- as.list(substitute(list(...)))[-1L]
 			if("file" %in% names(args))
 				stop("file argument is currently not supported")
-			if(missing(which))
+			if(missing(which)){
+				if(length(name$files) == 0){
+					filename <- tempfile(fileext=".R")
+					file.create(filename)
+					name <- addFile(name, filename, "/code.R")
+					warning("Adding new file to entity named code.R")
+				}
 				which <- 1:length(name$files)
+			}
 			if(!(is.numeric(which)))
 				stop("argument 'which' must be numeric")
+			if(any(which) > length(name$files))
+				stop("Invalid file specified")
 			
 			if(!("file" %in% names(args)))
 				file <- file.path(name$cacheDir, name$files[which])
 			file.edit(file, ...)
-			invisible(name)
+			name
 		}
 )
---
+++ client/rSynapseClient/demo/code.R
@@ -0,0 +1,4 @@
+myHeatmap <- function(x){
+  myName <- "<your name>"
+  heatmap(x, main=sprintf("%s's Custom Heatmap", myName))  
+}
