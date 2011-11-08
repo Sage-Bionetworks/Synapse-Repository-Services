f1f0eb9e5cfc1c829c516bc753b8ec73f0149f92
deflaux <nicole.deflaux@sagebase.org>
2011-11-08 05:00:29 +0000

    modified StepsHome should only show the current user's steps sorted creation date descending, PLFM-744

--- client/web/src/test/java/org/sagebionetworks/web/unitclient/presenter/StepsHomePresenterTest.java
+++ client/web/src/test/java/org/sagebionetworks/web/unitclient/presenter/StepsHomePresenterTest.java
@@ -9,19 +9,22 @@ import org.mockito.Mockito;
 import org.sagebionetworks.web.client.GlobalApplicationState;
 import org.sagebionetworks.web.client.place.StepsHome;
 import org.sagebionetworks.web.client.presenter.StepsHomePresenter;
+import org.sagebionetworks.web.client.security.AuthenticationController;
 import org.sagebionetworks.web.client.view.StepsHomeView;
 
 public class StepsHomePresenterTest {
 
 	StepsHomePresenter stepsHomePresenter;
 	StepsHomeView mockView;
+	AuthenticationController mockAuthenticationController;
 	GlobalApplicationState mockGlobalApplicationState;
 	
 	@Before
 	public void setup(){
 		mockView = mock(StepsHomeView.class);
+		mockAuthenticationController = mock(AuthenticationController.class);
 		mockGlobalApplicationState = mock(GlobalApplicationState.class);
-		stepsHomePresenter = new StepsHomePresenter(mockView, mockGlobalApplicationState);
+		stepsHomePresenter = new StepsHomePresenter(mockView, mockAuthenticationController, mockGlobalApplicationState);
 		
 		verify(mockView).setPresenter(stepsHomePresenter);
 	}	
@@ -32,3 +35,4 @@ public class StepsHomePresenterTest {
 		stepsHomePresenter.setPlace(place);
 	}	
 }
+
