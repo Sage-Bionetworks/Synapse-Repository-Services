266656da20a88e6ee3d259aea332e855f313484b
MattNapsAlot <matt.furia@sagebase.org>
2011-11-09 21:13:08 +0000

    more fixes for unit tests on windows

--- client/rSynapseClient/inst/unitTests/test_now.R
+++ client/rSynapseClient/inst/unitTests/test_now.R
@@ -1,4 +1,4 @@
 unitTestNow <- function() {
-	checkTrue(grepl("\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d.*", .now()))
-	checkTrue(grepl("\\d\\d\\d\\d-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d\\.\\d\\d\\dZ", .nowAsString()))
+	checkTrue(grepl("\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d.*", synapseClient:::.now()))
+	checkTrue(grepl("\\d\\d\\d\\d-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d\\.\\d\\d\\dZ", synapseClient:::.nowAsString()))
 }
