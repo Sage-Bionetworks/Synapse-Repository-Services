63afddf13097739578497b43731fe276298cf9cb
brig.mecham <brig.mecham@sagebase.org>
2011-11-15 20:49:02 +0000

    Changed crawler to provide URLs from GEO database.

--- tools/gePipeline/src/main/resources/datasetCrawler.R
+++ tools/gePipeline/src/main/resources/datasetCrawler.R
@@ -2,21 +2,20 @@ system("perl src/main/resources/getGSEs.pl")
 all.gses <- read.table("all.GSEs.txt",sep="\t",header=TRUE,stringsAsFactors=FALSE,row.names=1)
 all.gses <- all.gses[which(all.gses$Number_of_Samples < 1500),]
 all.gses <- all.gses[order(all.gses$Number_of_Samples),]
-"GSE.ID\tGPL\tLast Update Date\tSpecies\tDescription\tSupplementary_File\tNumber_of_Samples\tInvestigator\tPlatform\n";
+header <- "GSE.ID\tGPL\tLast Update Date\tSpecies\tDescription\tSupplementary_File\tNumber_of_Samples\tInvestigator\tPlatform\n";
 
 output <- list()
 lapply(1:nrow(all.gses), function(i) {  
-			b <- list(gpl     =all.gses[i,1],
-					lastUpdate    =all.gses[i,2],
-					Species =all.gses[i,3],
-					description =all.gses[i,4],
-					hasCelFile=all.gses[i,5],
-					number_of_samples=all.gses[i,6],
-					creator=all.gses[i,7]
-					#,Platform=all.gses[i,8]
-			)
-  b
-}) -> output
+      b <- list(gpl     =all.gses[i,1],
+          lastUpdate    =all.gses[i,2],
+          Species =all.gses[i,3],
+          Description =all.gses[i,4],
+          hasCelFile=all.gses[i,5],
+          Number_of_Samples=all.gses[i,6],
+          creator=all.gses[i,7],
+          Platform=all.gses[i,8],
+          url=paste("ftp://ftp.ncbi.nih.gov/pub/geo/DATA/supplementary/series/",rownames(all.gses)[i],"/",rownames(all.gses)[i],"_RAW.tar",sep=""))
+    }) -> output
 
 names(output) <- rownames(all.gses)
 
