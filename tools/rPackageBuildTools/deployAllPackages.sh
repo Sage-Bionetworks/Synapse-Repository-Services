d93c32020d83ed119323777f61d19561f2155c2e
MattNapsAlot <matt.furia@sagebase.org>
2012-01-21 03:18:01 +0000

    Modified deploy script to handle deployment for multiple R major versions (R2.13 and R2.14).

--- tools/rPackageBuildTools/deployAllPackages.sh
+++ tools/rPackageBuildTools/deployAllPackages.sh
@@ -26,16 +26,29 @@ if [ -n "$create" ] && [ "$create" = "create" ] ; then
     mkdir -p $REPO_ROOT/2.13/bin/windows/contrib/2.13
     ln -s $REPO_ROOT/2.13/bin/windows  $REPO_ROOT/2.13/bin/windows64
     mkdir -p $REPO_ROOT/2.13/src/contrib
-    ln -s $REPO_ROOT/2.13 $REPO_ROOT/2.14
+    
+    mkdir -p $REPO_ROOT/2.14/bin/macosx/leopard/contrib/2.14
+    mkdir -p $REPO_ROOT/2.14/bin/windows/contrib/2.14
+    ln -s $REPO_ROOT/2.14/bin/windows  $REPO_ROOT/2.14/bin/windows64
+    mkdir -p $REPO_ROOT/2.14/src/contrib
+
 fi
 
 ## Copy the packages from the deployment area to the right spots in
 ## the CRAN directory tree
-cp $PACKAGE_ROOT/*zip $REPO_ROOT/2.13/bin/windows/contrib/2.13
-cp $PACKAGE_ROOT/*tgz $REPO_ROOT/2.13/bin/macosx/leopard/contrib/2.13
-cp $PACKAGE_ROOT/*tar.gz $REPO_ROOT/2.13/src/contrib
+cp $PACKAGE_ROOT/2.13/*zip $REPO_ROOT/2.13/bin/windows/contrib/2.13
+cp $PACKAGE_ROOT/2.13/*tgz $REPO_ROOT/2.13/bin/macosx/leopard/contrib/2.13
+cp $PACKAGE_ROOT/2.13/*tar.gz $REPO_ROOT/2.13/src/contrib
+
+cp $PACKAGE_ROOT/2.14/*zip $REPO_ROOT/2.14/bin/windows/contrib/2.14
+cp $PACKAGE_ROOT/2.14/*tgz $REPO_ROOT/2.14/bin/macosx/leopard/contrib/2.14
+cp $PACKAGE_ROOT/2.14/*tar.gz $REPO_ROOT/2.14/src/contrib
 
 ## Make the PACKAGE and PACKAGE.gz files
-cd $REPO_ROOT/2.13/src/contrib ; R -e 'tools:::write_PACKAGES(".", type="source")'
-cd $REPO_ROOT/2.13/bin/macosx/leopard/contrib/2.13; R -e 'tools:::write_PACKAGES(".", type="mac.binary")'
-cd $REPO_ROOT/2.13/bin/windows/contrib/2.13; R -e 'tools:::write_PACKAGES(".", type="win.binary")'
+cd $REPO_ROOT/2.13/src/contrib ; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="source")'
+cd $REPO_ROOT/2.13/bin/macosx/leopard/contrib/2.13; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="mac.binary")'
+cd $REPO_ROOT/2.13/bin/windows/contrib/2.13; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="win.binary")'
+
+cd $REPO_ROOT/2.14/src/contrib ; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="source")'
+cd $REPO_ROOT/2.14/bin/macosx/leopard/contrib/2.14; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="mac.binary")'
+cd $REPO_ROOT/2.14/bin/windows/contrib/2.14; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="win.binary")'
