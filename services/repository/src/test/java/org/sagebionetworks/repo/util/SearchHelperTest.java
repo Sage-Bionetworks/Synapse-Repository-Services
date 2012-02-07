46588294ce2ad73d9e7e0ad9de616f3d2693a36b
deflaux <nicole.deflaux@sagebase.org>
2012-02-07 22:53:47 +0000

    merge of r6096 for Bug fix for PLFM-961 "Search throws NullPointerException on out of bounds date range query"

--- services/repository/src/test/java/org/sagebionetworks/repo/util/SearchHelperTest.java
+++ services/repository/src/test/java/org/sagebionetworks/repo/util/SearchHelperTest.java
@@ -1,12 +1,14 @@
 package org.sagebionetworks.repo.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.net.URLEncoder;
 
 import org.junit.Test;
+import org.sagebionetworks.repo.model.search.SearchResults;
 
 /**
  * @author deflaux
@@ -148,4 +150,29 @@ public class SearchHelperTest {
 		}
 	}
 
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void testParseHits() throws Exception {
+
+		String response = "{\"rank\":\"-text_relevance\",\"match-expr\":\"(label 'prostate')\",\"hits\":{\"found\":260,\"start\":0,\"hit\":[{\"id\":\"4494\",\"data\":{\"name\":\"MSKCC Prostate Cancer\"}},{\"id\":\"4610\",\"data\":{\"name\":\"Prostate Cancer FHCRC\"}},{\"id\":\"4566\",\"data\":{\"name\":\"Prostate Cancer ICGC\"}},{\"id\":\"114535\",\"data\":{\"name\":\"114535\"}},{\"id\":\"115510\",\"data\":{\"name\":\"115510\"}},{\"id\":\"112949\",\"data\":{\"name\":\"GSE11842\"}},{\"id\":\"100287\",\"data\":{\"name\":\"GSE11842\"}},{\"id\":\"112846\",\"data\":{\"name\":\"GSE15580\"}},{\"id\":\"108857\",\"data\":{\"name\":\"GSE17483\"}},{\"id\":\"108942\",\"data\":{\"name\":\"GSE25500\"}}]},\"info\":{\"rid\":\"6ddcaa561c05c4cc85ddb10cb46568af0024f6e4f534231d8e5a4d7098b31e11e39838035983b8cc226dc7099b535033\",\"time-ms\":3,\"cpu-time-ms\":0}}";
+		SearchResults results = SearchHelper.cloudSearchToSynapseSearchResults(response);
+		assertEquals(10, results.getHits().size());
+		assertEquals(0, results.getFacets().size());
+		assertEquals(new Long(260), results.getFound());
+	}
+	
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void testParseNoHits() throws Exception {
+
+		String response = "{\"rank\":\"-text_relevance\",\"match-expr\":\"(and 'prostate,cancer' modified_on:1368973180..1429453180 (or acl:'test-user@sagebase.org' acl:'AUTHENTICATED_USERS' acl:'PUBLIC' acl:'test-group'))\",\"hits\":{\"found\":0,\"start\":0,\"hit\":[]},\"facets\":{},\"info\":{\"rid\":\"6ddcaa561c05c4cc85ddb10cb46568af0024f6e4f534231d657d53613aed2d4ea69ed14f5fdff3d1951b339a661631f4\",\"time-ms\":3,\"cpu-time-ms\":0}}";
+		SearchResults results = SearchHelper.cloudSearchToSynapseSearchResults(response);
+		assertEquals(0, results.getFacets().size());
+		assertEquals(0, results.getHits().size());
+		assertEquals(new Long(0), results.getFound());
+	}
 }
