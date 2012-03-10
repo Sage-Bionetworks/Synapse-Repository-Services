628bbdf2f19630601ab9d74374785b32f0f2046b
deflaux <nicole.deflaux@sagebase.org>
2012-03-10 01:09:14 +0000

    no longer considering it an error if this particular test fails on a windows machine

--- lib/lib-workflow/src/test/java/org/sagebionetworks/workflow/ScriptProcessorTest.java
+++ lib/lib-workflow/src/test/java/org/sagebionetworks/workflow/ScriptProcessorTest.java
@@ -3,8 +3,9 @@ package org.sagebionetworks.workflow;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -23,18 +24,28 @@ public class ScriptProcessorTest {
 		config = new WorkflowTemplatedConfigurationImpl();
 		config.reloadConfiguration();
 	}
-	
+
 	/**
 	 * @throws Exception
 	 */
-	@Ignore // This is not a valid test as it fails on Windows!
 	@Test
 	public void testShellScript() throws Exception {
-		ScriptResult scriptResult = ScriptProcessor.runScript(config,
-				"./src/test/resources/stdoutKeepAlive.sh", null);
-		assertNotNull(scriptResult);
-		assertTrue(0 <= scriptResult.getStdout().indexOf("I am doing real work"));
-		assertTrue(0 <= scriptResult.getStderr().indexOf("Can't locate object method \"bad\" via package \"syntax\""));
+		try {
+			ScriptResult scriptResult = ScriptProcessor.runScript(config,
+					"./src/test/resources/stdoutKeepAlive.sh", null);
+			assertNotNull(scriptResult);
+			assertTrue(0 <= scriptResult.getStdout().indexOf(
+					"I am doing real work"));
+			assertTrue(0 <= scriptResult
+					.getStderr()
+					.indexOf(
+							"Can't locate object method \"bad\" via package \"syntax\""));
+		} catch (IOException e) {
+			// We can consider this test to "pass" if it is run on a windows
+			// machine and throws this particular exception
+			assertTrue(-1 < e.getMessage().indexOf(
+					"is not a valid Win32 application"));
+		}
 	}
 
 	/**
@@ -45,17 +56,19 @@ public class ScriptProcessorTest {
 		ScriptResult scriptResult = ScriptProcessor.runScript(config,
 				"./src/test/resources/simpleScriptThatSucceeds.r", null);
 		assertNotNull(scriptResult);
-		assertTrue(0 <= scriptResult.getStdout().indexOf("Hello World! This should succeed."));
-		assertTrue(0 <= scriptResult.getStderr().indexOf("and here's some stderr"));
+		assertTrue(0 <= scriptResult.getStdout().indexOf(
+				"Hello World! This should succeed."));
+		assertTrue(0 <= scriptResult.getStderr().indexOf(
+				"and here's some stderr"));
 	}
 
 	/**
 	 * @throws Exception
 	 */
-	@Test(expected=UnrecoverableException.class)
+	@Test(expected = UnrecoverableException.class)
 	public void testRScriptThatFails() throws Exception {
 		ScriptProcessor.runScript(config,
 				"./src/test/resources/simpleScriptThatFails.r", null);
 	}
-	
+
 }
