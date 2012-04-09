09661c7714e7b2bb7f1f03b88fc7864bdf6995aa
deflaux <nicole.deflaux@sagebase.org>
2012-04-09 20:57:41 +0000

    Added R 2.15

--- tools/rPackageBuildTools/deployAllPackages.sh
+++ tools/rPackageBuildTools/deployAllPackages.sh
@@ -32,6 +32,11 @@ if [ -n "$create" ] && [ "$create" = "create" ] ; then
     ln -s $REPO_ROOT/2.14/bin/windows  $REPO_ROOT/2.14/bin/windows64
     mkdir -p $REPO_ROOT/2.14/src/contrib
 
+    mkdir -p $REPO_ROOT/2.15/bin/macosx/leopard/contrib/2.15
+    mkdir -p $REPO_ROOT/2.15/bin/windows/contrib/2.15
+    ln -s $REPO_ROOT/2.15/bin/windows  $REPO_ROOT/2.15/bin/windows64
+    mkdir -p $REPO_ROOT/2.15/src/contrib
+
 fi
 
 ## Copy the packages from the deployment area to the right spots in
@@ -44,6 +49,10 @@ cp $PACKAGE_ROOT/2.14/*zip $REPO_ROOT/2.14/bin/windows/contrib/2.14
 cp $PACKAGE_ROOT/2.14/*tgz $REPO_ROOT/2.14/bin/macosx/leopard/contrib/2.14
 cp $PACKAGE_ROOT/2.14/*tar.gz $REPO_ROOT/2.14/src/contrib
 
+cp $PACKAGE_ROOT/2.15/*zip $REPO_ROOT/2.15/bin/windows/contrib/2.15
+cp $PACKAGE_ROOT/2.15/*tgz $REPO_ROOT/2.15/bin/macosx/leopard/contrib/2.15
+cp $PACKAGE_ROOT/2.15/*tar.gz $REPO_ROOT/2.15/src/contrib
+
 ## Make the PACKAGE and PACKAGE.gz files
 cd $REPO_ROOT/2.13/src/contrib ; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="source")'
 cd $REPO_ROOT/2.13/bin/macosx/leopard/contrib/2.13; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="mac.binary")'
@@ -52,3 +61,7 @@ cd $REPO_ROOT/2.13/bin/windows/contrib/2.13; /usr/local/bin/R -e 'tools:::write_
 cd $REPO_ROOT/2.14/src/contrib ; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="source")'
 cd $REPO_ROOT/2.14/bin/macosx/leopard/contrib/2.14; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="mac.binary")'
 cd $REPO_ROOT/2.14/bin/windows/contrib/2.14; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="win.binary")'
+
+cd $REPO_ROOT/2.15/src/contrib ; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="source")'
+cd $REPO_ROOT/2.15/bin/macosx/leopard/contrib/2.15; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="mac.binary")'
+cd $REPO_ROOT/2.15/bin/windows/contrib/2.15; /usr/local/bin/R -e 'tools:::write_PACKAGES(".", type="win.binary")'
