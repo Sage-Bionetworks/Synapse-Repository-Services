ea83b7749460cc3410dd94d47af922f50858c7e3
x.schildwachter <x.schildwachter@sagebase.org>
2012-04-16 21:06:03 +0000

    Partial fix PLFM-1210 + Aliases for xxxdata

--- lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/jdo/NodeDAOImplTest.java
+++ lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/jdo/NodeDAOImplTest.java
@@ -1445,7 +1445,7 @@ public class NodeDAOImplTest {
 		
 		ids = nodeDao.getAllNodeTypesForAlias("layer");
 		assertNotNull(ids);
-		assertEquals(1, ids.size());
+		assertEquals(4, ids.size());
 		assertEquals(new Short(EntityType.layer.getId()), ids.get(0));
 		
 		ids = nodeDao.getAllNodeTypesForAlias("data");
