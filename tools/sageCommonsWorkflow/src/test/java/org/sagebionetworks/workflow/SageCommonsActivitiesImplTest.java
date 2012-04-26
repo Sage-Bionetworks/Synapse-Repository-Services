28d9597526976249875f8c1ea30767f4d8f7d678
deflaux <nicole.deflaux@sagebase.org>
2012-04-26 20:26:10 +0000

    fix for PLFM-1111

--- tools/sageCommonsWorkflow/src/test/java/org/sagebionetworks/workflow/SageCommonsActivitiesImplTest.java
+++ tools/sageCommonsWorkflow/src/test/java/org/sagebionetworks/workflow/SageCommonsActivitiesImplTest.java
@@ -15,11 +15,12 @@ import org.junit.Test;
 
 /**
  * @author deflaux
- *
+ * 
  */
 public class SageCommonsActivitiesImplTest {
-	
-	SageCommonsActivities activities = new SageCommonsActivitiesImpl(new MockSageCommonsChildWorkflowDispatcher());
+
+	SageCommonsActivities activities = new SageCommonsActivitiesImpl(
+			new MockSageCommonsChildWorkflowDispatcher());
 
 	/**
 	 * @throws java.lang.Exception
@@ -41,42 +42,63 @@ public class SageCommonsActivitiesImplTest {
 	@Ignore
 	@Test
 	public void testUrl() throws Exception {
-	String url = "https://s3.amazonaws.com/stagingdata.sagebase.org/160499/160500/tcgaInput_txt.zip?Expires=1331324473&x-amz-security-token=AQoDYXdzEJX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEasALfYKfj6aAqB%2Fe4IOmgX%2Fp%2BabkXk5YcGk3IdxNhs15K6YsyflsZiPUoaeMujYIlIw4NLajQx%2BOD1Cz%2FHcwsYn3bgj5etiIanImEQakd5An91%2BcyY4QDYxZ2rv6KjfOJW0A76ycbYRs7QgmV656z6BqHzUUz6H0Xw0vSMa8Lm2KhVzGahhz2wQJaoCwbbwgf1Oy4td5jCc1L%2BYRjL075RJi2oeOCUN%2BBDXsCGj1yutqvyjmHk%2Fik%2FluqAzj%2BShuy34whSYMNi0BpS8siBpNGfOnSN5R%2BMkFEkUydi4g%2BIcXX4X1Nm%2F9iZiF6NVvD0QfU6%2F0e4E1kuNtTgvrhlO1lzXWgD6Q1fXB4qkyG9ahOuv7%2FPGPAz8%2BqvvIyspmxFC7lTau8fwXPq7f8Rb0nFj51EyuZILmp5PoE&AWSAccessKeyId=ASIAJX7JBMB6ZJWNEXZQ&Signature=zFS5dRSSyakHPeNBKy9NCJJeVOk%3D";
-	int numJobs = activities.processSpreadsheet(url);
-	assertEquals(3, numJobs);
-	
+		// TODO put a test spreadsheet somewhere else and perhaps move this to
+		// the integration test suite
+		String url = "https://s3.amazonaws.com/stagingdata.sagebase.org/160499/160500/tcgaInput_txt.zip?Expires=1331324473&x-amz-security-token=AQoDYXdzEJX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEasALfYKfj6aAqB%2Fe4IOmgX%2Fp%2BabkXk5YcGk3IdxNhs15K6YsyflsZiPUoaeMujYIlIw4NLajQx%2BOD1Cz%2FHcwsYn3bgj5etiIanImEQakd5An91%2BcyY4QDYxZ2rv6KjfOJW0A76ycbYRs7QgmV656z6BqHzUUz6H0Xw0vSMa8Lm2KhVzGahhz2wQJaoCwbbwgf1Oy4td5jCc1L%2BYRjL075RJi2oeOCUN%2BBDXsCGj1yutqvyjmHk%2Fik%2FluqAzj%2BShuy34whSYMNi0BpS8siBpNGfOnSN5R%2BMkFEkUydi4g%2BIcXX4X1Nm%2F9iZiF6NVvD0QfU6%2F0e4E1kuNtTgvrhlO1lzXWgD6Q1fXB4qkyG9ahOuv7%2FPGPAz8%2BqvvIyspmxFC7lTau8fwXPq7f8Rb0nFj51EyuZILmp5PoE&AWSAccessKeyId=ASIAJX7JBMB6ZJWNEXZQ&Signature=zFS5dRSSyakHPeNBKy9NCJJeVOk%3D";
+		int numJobs = activities.processSpreadsheet(url);
+		assertEquals(3, numJobs);
+
 	}
+
 	/**
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	@Test
 	public void testUncompressed() throws Exception {
-		int numJobs = activities.processSpreadsheetContents(new File("./src/test/resources/tcga.txt"));
+		int numJobs = activities.processSpreadsheetContents(new File(
+				"./src/test/resources/tcga.txt"));
 		assertEquals(3, numJobs);
 	}
 
 	/**
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	@Test
 	public void testZip() throws Exception {
-		int numJobs = activities.processSpreadsheetContents(new File("./src/test/resources/tcga.zip"));
+		int numJobs = activities.processSpreadsheetContents(new File(
+				"./src/test/resources/tcga.zip"));
 		assertEquals(3, numJobs);
 	}
 
 	/**
-	 * Test method for {@link org.sagebionetworks.workflow.SageCommonsActivitiesImpl#runRScript(java.lang.String, java.lang.String)}.
-	 * @throws Exception 
+	 * Test method for
+	 * {@link org.sagebionetworks.workflow.SageCommonsActivitiesImpl#runRScript(java.lang.String, java.lang.String)}
+	 * .
+	 * 
+	 * @throws Exception
 	 */
-	@Ignore // John cannot get this to work on windows. See PLFM-1111
 	@Test
 	public void testRunRScript() throws Exception {
 		String spreadsheetData = "foo, bar, baz, bat\n123, friday, http://fun.com/data.tar.gz, 42\n";
-		activities.runRScript("./src/main/resources/simpleScriptThatSucceeds.r", spreadsheetData);
+		try {
+			activities.runRScript(
+					"./src/main/resources/simpleScriptThatSucceeds.r",
+					spreadsheetData);
+		} catch (Exception e) {
+			if (-1 != e.getMessage().indexOf(
+					"The system cannot find the file specified")) {
+				// this happens on window when rscript is not in the path, See
+				// PLFM-1111
+			} else {
+				throw e;
+			}
+		}
 	}
 
 	/**
-	 * Test method for {@link org.sagebionetworks.workflow.SageCommonsActivitiesImpl#formulateNotificationMessage(org.sagebionetworks.repo.model.Layer, java.lang.Integer)}.
+	 * Test method for
+	 * {@link org.sagebionetworks.workflow.SageCommonsActivitiesImpl#formulateNotificationMessage(org.sagebionetworks.repo.model.Layer, java.lang.Integer)}
+	 * .
 	 */
 	@Ignore
 	@Test
@@ -85,7 +107,9 @@ public class SageCommonsActivitiesImplTest {
 	}
 
 	/**
-	 * Test method for {@link org.sagebionetworks.workflow.SageCommonsActivitiesImpl#notifyFollowers(java.lang.String, java.lang.String, java.lang.String)}.
+	 * Test method for
+	 * {@link org.sagebionetworks.workflow.SageCommonsActivitiesImpl#notifyFollowers(java.lang.String, java.lang.String, java.lang.String)}
+	 * .
 	 */
 	@Ignore
 	@Test
