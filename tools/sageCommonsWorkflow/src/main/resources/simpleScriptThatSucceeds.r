ae348952960d6809852191bfb3d9dcea6d6ac261
deflaux <nicole.deflaux@sagebase.org>
2012-05-07 20:26:26 +0000

    Tweaked the welcome message for the common issue of login troubles due to T&C not being signed yet.

--- tools/sageCommonsWorkflow/src/main/resources/simpleScriptThatSucceeds.r
+++ tools/sageCommonsWorkflow/src/main/resources/simpleScriptThatSucceeds.r
@@ -3,3 +3,5 @@
 print("Hello World! This should succeed.")
 message("and here's some stderr")
 
+library(synapseClient)
+setApiCredentials(username=Sys.getenv("SYNAPSE_USERNAME"), secretKey=Sys.getenv("SYNAPSE_SECRET_KEY"))
