486e8db7d24161d331931158ed29f768b6d79e81
john-hill <john.hill@sagebase.org>
2012-05-23 16:54:50 -0700

    created version 0.13.3 and moved the pomVersioner.py to the root

--- pomVersioner.py
+++ pomVersioner.py
@@ -4,27 +4,21 @@ Walk the complete PLFM hierarchy and bring all the pom.xml files up to new minor
 '''
 
 #Old and new minor versions
-oldVersion = '0.13.1-SNAPSHOT'
-newVersion = '0.13.2-SNAPSHOT'
+oldVersion = '0.13.2-SNAPSHOT'
+newVersion = '0.13.3'
 #Path to PLFM on your system
-startPath = 'C:\\sage\\workspace\\trunk'
+startPath = os.getcwd()
+count = 0;
 for root, subFolders, files in os.walk(startPath):    
     for file in files:
         if file == 'pom.xml': 
             f = os.path.join(root, file) 
-            print 'updating '+f
+            print 'checking '+f
             for line in fileinput.FileInput(f, inplace=1):
                 found = False
                 if line.find('<version>'+oldVersion+'</version>') >= 0 and not found:
                     line = line.replace(oldVersion,newVersion)
                     found = True
+                    count += 1
                 print line.rstrip()
-        if file == 'DESCRIPTION' and root.find('rSynapseClient') >= 0:
-            f = os.path.join(root, file)
-            print 'updating '+f
-            for line in fileinput.FileInput(f, inplace=1):
-                found = False
-                if not found and line.find('Version') >= 0:
-                    line = 'Version: '+newVersion+'-0'
-                    found = True
-                print line.rstrip()
+print 'updated: '+str(count)+' pom.xml files'
