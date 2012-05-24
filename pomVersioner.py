3b045b19bce31c0732d6858d794f3f5653324e7b
john-hill <john.hill@sagebase.org>
2012-05-23 17:45:27 -0700

    created the 0.13.4-SNAPSHOT

--- pomVersioner.py
+++ pomVersioner.py
@@ -4,8 +4,8 @@ Walk the complete PLFM hierarchy and bring all the pom.xml files up to new minor
 '''
 
 #Old and new minor versions
-oldVersion = '0.13.2-SNAPSHOT'
-newVersion = '0.13.3'
+oldVersion = '0.13.3'
+newVersion = '0.13.4-SNAPSHOT'
 #Path to PLFM on your system
 startPath = os.getcwd()
 count = 0;
