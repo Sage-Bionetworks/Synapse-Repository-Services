ad4579ac95f8fbd7ab5b69d6d1d289bcd8006d1a
David Burdick <dburdick@gmail.com>
2012-05-25 14:30:28 -0700

    rev to 0.13.6-SNAPSHOT

--- pomVersioner.py
+++ pomVersioner.py
@@ -4,8 +4,8 @@ Walk the complete PLFM hierarchy and bring all the pom.xml files up to new minor
 '''
 
 #Old and new minor versions
-oldVersion = '0.13.5-SNAPSHOT'
-newVersion = '0.13.5'
+oldVersion = '0.13.5'
+newVersion = '0.13.6-SNAPSHOT'
 #Path to PLFM on your system
 startPath = os.getcwd()
 count = 0;
