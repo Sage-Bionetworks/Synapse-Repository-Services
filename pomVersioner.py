49de66a4686111435ba00aac7428dfaa9d25f7b2
brucehoff <bruce.hoff@sagebase.org>
2012-06-07 16:34:59 -0700

    incremented POM to 0.13.7.1

--- pomVersioner.py
+++ pomVersioner.py
@@ -4,8 +4,8 @@ Walk the complete PLFM hierarchy and bring all the pom.xml files up to new minor
 '''
 
 #Old and new minor versions
-oldVersion = '0.13.6-SNAPSHOT'
-newVersion = '0.13.6'
+oldVersion = '0.13.7-SNAPSHOT'
+newVersion = '0.13.7.1'
 #Path to PLFM on your system
 startPath = os.getcwd()
 count = 0;
