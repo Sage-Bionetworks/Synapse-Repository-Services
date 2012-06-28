0b36bb8deaa15951b8388dc0715536e66314c907
jay-hodgson <jay.hodgson@sagebase.org>
2012-06-28 09:33:48 -0700

    Add AttachmentData to UserProfile. Move authentication away from the shared S3TokenManager, to the calling managers (UserProfileManager, and EntityManager for now), and move associated tests too (oh, and implement new auth tests for User Profile attachment).  Expose service calls for getting a user profile S3 attachment token, and pre-signed url. And add calls to the Java client to expose this functionality. Note that a preview version for the user profile attachment is generated when the profile is updated.

---
+++ services/repository/src/test/java/org/sagebionetworks/repo/manager/EntityManagerImplTest.java
@@ -0,0 +1,117 @@
+package org.sagebionetworks.repo.manager;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.sagebionetworks.ids.IdGenerator;
+import org.sagebionetworks.repo.model.ACCESS_TYPE;
+import org.sagebionetworks.repo.model.DatastoreException;
+import org.sagebionetworks.repo.model.InvalidModelException;
+import org.sagebionetworks.repo.model.UnauthorizedException;
+import org.sagebionetworks.repo.model.User;
+import org.sagebionetworks.repo.model.UserInfo;
+import org.sagebionetworks.repo.model.attachment.PresignedUrl;
+import org.sagebionetworks.repo.model.attachment.S3AttachmentToken;
+import org.sagebionetworks.repo.util.LocationHelper;
+import org.sagebionetworks.repo.web.NotFoundException;
+import org.springframework.http.HttpMethod;
+
+import com.amazonaws.services.securitytoken.model.Credentials;
+
+public class EntityManagerImplTest {
+
+	private UserManager mockUserManager;
+	private PermissionsManager mockPermissionsManager;
+	private UserInfo mockUser;
+	private EntityManagerImpl entityManager;
+	private NodeManager mockNodeManager;
+	private S3TokenManager mockS3TokenManager;
+	private IdGenerator mocIdGenerator;
+	private LocationHelper mocKLocationHelper;
+	String userId = "007";
+	
+	@Before
+	public void before(){
+		// Create the mocks
+		mockPermissionsManager = Mockito.mock(PermissionsManager.class);
+		mockUserManager = Mockito.mock(UserManager.class);
+		mockNodeManager = Mockito.mock(NodeManager.class);
+		mockS3TokenManager = Mockito.mock(S3TokenManager.class);
+		mocIdGenerator = Mockito.mock(IdGenerator.class);
+		mocKLocationHelper = Mockito.mock(LocationHelper.class);
+		mockUser = new UserInfo(false);
+		mockUser.setUser(new User());
+		mockUser.getUser().setId(userId);
+		entityManager = new EntityManagerImpl(mockNodeManager, mockS3TokenManager, mockPermissionsManager, mockUserManager);
+	}
+
+	@Test (expected=UnauthorizedException.class)
+	public void testValidateReadAccessFail() throws DatastoreException, NotFoundException, UnauthorizedException{
+		String userId = "123456";
+		String entityId = "abc";
+		// return the mock user.
+		when(mockUserManager.getUserInfo(userId)).thenReturn(mockUser);
+		// Say now to this
+		when(mockPermissionsManager.hasAccess(entityId, ACCESS_TYPE.READ, mockUser)).thenReturn(false);
+		when(mockPermissionsManager.hasAccess(entityId, ACCESS_TYPE.UPDATE, mockUser)).thenThrow(new IllegalArgumentException("Read and not update should have been checked"));
+		entityManager.validateReadAccess(mockUser, entityId);
+		
+	}
+	
+	@Test 
+	public void testValidateReadAccessPass() throws DatastoreException, NotFoundException, UnauthorizedException{
+		String userId = "123456";
+		String entityId = "abc";
+		// return the mock user.
+		when(mockUserManager.getUserInfo(userId)).thenReturn(mockUser);
+		// Say now to this
+		when(mockPermissionsManager.hasAccess(entityId, ACCESS_TYPE.READ, mockUser)).thenReturn(true);
+		when(mockPermissionsManager.hasAccess(entityId, ACCESS_TYPE.UPDATE, mockUser)).thenThrow(new IllegalArgumentException("Read and not update should have been checked"));
+		entityManager.validateReadAccess(mockUser, entityId);
+	}
+	
+
+	@Test (expected=UnauthorizedException.class)
+	public void testGetAttachmentUrlNoReadAccess() throws Exception{
+		Long tokenId = new Long(456);
+		String entityId = "132";
+		String userId = "007";
+		String expectedPath = S3TokenManagerImpl.createAttachmentPathSlash(entityId, tokenId.toString());
+		String expectePreSigneUrl = "I am a presigned url! whooot!";
+		when(mockUserManager.getUserInfo(userId)).thenReturn(mockUser);
+		// Simulate a 
+		when(mockPermissionsManager.hasAccess(entityId, ACCESS_TYPE.READ, mockUser)).thenReturn(false);
+		when(mocKLocationHelper.presignS3GETUrlShortLived(userId, expectedPath)).thenReturn(expectePreSigneUrl);
+		// Make the actual call
+		PresignedUrl url = entityManager.getAttachmentUrl(userId, entityId, tokenId.toString());
+		assertNotNull(url);
+		assertEquals(expectePreSigneUrl, url.getPresignedUrl());
+	}
+	
+	@Test (expected=UnauthorizedException.class)
+	public void testCreateS3AttachmentTokenNoUpdateAccess() throws NumberFormatException, DatastoreException, NotFoundException, UnauthorizedException, InvalidModelException{
+		S3AttachmentToken startToken = new S3AttachmentToken();
+		startToken.setFileName(null);
+		String almostMd5 = "79054025255fb1a26e4bc422aef54eb4";
+		startToken.setMd5(almostMd5);
+		Long tokenId = new Long(456);
+		String entityId = "132";
+		String userId = "007";
+		String expectedPath = entityId+"/"+tokenId.toString();
+		String expectePreSigneUrl = "I am a presigned url! whooot!";
+		when(mocIdGenerator.generateNewId()).thenReturn(tokenId);
+		Credentials mockCreds = Mockito.mock(Credentials.class);
+		when(mockUserManager.getUserInfo(userId)).thenReturn(mockUser);
+		when(mockPermissionsManager.hasAccess(entityId, ACCESS_TYPE.UPDATE, mockUser)).thenReturn(false);
+		when(mocKLocationHelper.createFederationTokenForS3(userId,HttpMethod.PUT,expectedPath)).thenReturn(mockCreds);
+		when(mocKLocationHelper.presignS3PUTUrl(mockCreds, expectedPath, almostMd5, "image/jpeg")).thenReturn(expectePreSigneUrl);
+		// Make the actual call
+		S3AttachmentToken endToken = entityManager.createS3AttachmentToken(userId, entityId, startToken);
+		assertNotNull(endToken);
+		assertEquals(expectePreSigneUrl, endToken.getPresignedUrl());
+	}
+
+}
