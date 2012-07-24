770924e876599a94058efbef5aa909f87e6b037a
Xavier Schildwachter <x.schildwachter@sagebase.org>
2012-07-23 19:25:51 -0700

    Trying to force new tag

--- pomSnapshotToLastGitTag.sh
+++ pomSnapshotToLastGitTag.sh
@@ -12,3 +12,5 @@ for f in `find -name "pom.xml"` ; do
      #We are using sed to replaces all 'develop-SNAPSHOT' verions with the new
      sed "s|<version>develop-SNAPSHOT</version>|$newVersion|g" -i $f
 done
+
+
