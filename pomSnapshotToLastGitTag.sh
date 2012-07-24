cff89ab74dba6487140faca53040af4c0c60a03f
Xavier Schildwachter <x.schildwachter@sagebase.org>
2012-07-23 18:55:35 -0700

    Added script

---
+++ pomSnapshotToLastGitTag.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+#This script will change the vesion of all pom.xml files from:
+#<version>develop-SNAPSHOT</version>
+# to
+#<version>GitTag</version>
+# The new version number is the last Git tag
+
+newVersion="<version>"`git describe --tags`"</version>"
+echo "Changing all pom.xml to version=$newVersion"
+for f in `find -name "pom.xml"` ; do
+     echo "Changing version of $f"
+     #We are using sed to replaces all 'develop-SNAPSHOT' verions with the new
+     sed "s|<version>develop-SNAPSHOT</version>|$newVersion|g" -i $f
+done
