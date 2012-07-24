901dadc6363a1e73fb0f615df429232fbf30f859
MattNapsAlot <matt.furia@sagebase.org>
2012-07-24 14:38:20 -0700

    removed build number from pom version

--- pomSnapshotToLastGitTag.sh
+++ pomSnapshotToLastGitTag.sh
@@ -11,7 +11,7 @@ echo "Changing all pom.xml to version=$newVersion"
 for f in `find . -name "pom.xml"` ; do
      echo "Changing version of $f"
      #We are using sed to replaces all 'develop-SNAPSHOT' verions with the new
-     sed "s|<version>develop-SNAPSHOT</version>|<version>${newVersion}_\${buildNumber}</version>|g" -i $f
+     sed "s|<version>develop-SNAPSHOT</version>|<version>${newVersion}</version>|g" -i $f
 done
 
 
