01d966733f52df06a4857bc971b2bd3f3814e885
Eric Wu <eric.wu@sagebase.org>
2012-09-24 16:57:59 -0700

    Separate out NodeQueryService from EntityService

---
+++ services/repository/src/main/java/org/sagebionetworks/repo/web/service/NodeQueryService.java
@@ -0,0 +1,22 @@
+package org.sagebionetworks.repo.web.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.sagebionetworks.repo.model.DatastoreException;
+import org.sagebionetworks.repo.model.QueryResults;
+import org.sagebionetworks.repo.model.UnauthorizedException;
+import org.sagebionetworks.repo.model.query.BasicQuery;
+import org.sagebionetworks.repo.queryparser.ParseException;
+import org.sagebionetworks.repo.web.NotFoundException;
+
+public interface NodeQueryService {
+
+	QueryResults query(String userId, String query, HttpServletRequest request)
+			throws DatastoreException, ParseException, NotFoundException, UnauthorizedException;
+
+	/**
+	 * Executes a query and includes the annotations for each entity.
+	 */
+	QueryResults executeQueryWithAnnotations(String userId, BasicQuery query, HttpServletRequest request)
+			throws DatastoreException, NotFoundException, UnauthorizedException;
+}
