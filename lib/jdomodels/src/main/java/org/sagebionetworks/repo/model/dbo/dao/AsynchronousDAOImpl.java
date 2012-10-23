ad9135cdb2bbc6b45417366aa53416365ef2d35f
john-hill <john.hill@sagebase.org>
2012-10-23 16:08:32 -0700

    added missing files

---
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/dao/AsynchronousDAOImpl.java
@@ -0,0 +1,113 @@
+package org.sagebionetworks.repo.model.dbo.dao;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import java.util.Set;
+
+import org.sagebionetworks.repo.model.Annotations;
+import org.sagebionetworks.repo.model.AsynchronousDAO;
+import org.sagebionetworks.repo.model.DatastoreException;
+import org.sagebionetworks.repo.model.NamedAnnotations;
+import org.sagebionetworks.repo.model.NodeDAO;
+import org.sagebionetworks.repo.model.Reference;
+import org.sagebionetworks.repo.model.StorageLocationDAO;
+import org.sagebionetworks.repo.model.StorageLocations;
+import org.sagebionetworks.repo.model.jdo.JDOSecondaryPropertyUtils;
+import org.sagebionetworks.repo.model.jdo.KeyFactory;
+import org.sagebionetworks.repo.web.NotFoundException;
+import org.sagebionetworks.schema.adapter.JSONObjectAdapterException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Basic implementation of the AsynchronousDAO.
+ * 
+ * @author jmhill
+ * 
+ */
+public class AsynchronousDAOImpl implements AsynchronousDAO {
+
+	@Autowired
+	private NodeDAO nodeDao;
+	@Autowired
+	private DBOReferenceDao dboReferenceDao;
+	@Autowired
+	private DBOAnnotationsDao dboAnnotationsDao;
+	@Autowired
+	private StorageLocationDAO storageLocationDao;
+	
+	/**
+	 * This constructor is used by unit tests.
+	 * @param nodeDao
+	 * @param dboReferenceDao
+	 * @param dboAnnotationsDao
+	 * @param storageLocationDao
+	 */
+	public AsynchronousDAOImpl(NodeDAO nodeDao,
+			DBOReferenceDao dboReferenceDao,
+			DBOAnnotationsDao dboAnnotationsDao,
+			StorageLocationDAO storageLocationDao) {
+		super();
+		this.nodeDao = nodeDao;
+		this.dboReferenceDao = dboReferenceDao;
+		this.dboAnnotationsDao = dboAnnotationsDao;
+		this.storageLocationDao = storageLocationDao;
+	}
+	
+	/**
+	 * Used by Spring's IoC
+	 */
+	public AsynchronousDAOImpl(){
+		
+	}
+
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+	@Override
+	public void createEntity(String id) throws DatastoreException, NotFoundException {
+		// Replace all
+		replaceAll(id);
+	}
+
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+	@Override
+	public void updateEntity(String id) throws NotFoundException {
+		// Replace all
+		replaceAll(id);
+	}
+	
+	/**
+	 * Replace all of the data in the database tables.
+	 * 
+	 * @param id
+	 * @throws NotFoundException
+	 */
+	public void replaceAll(String id) throws NotFoundException {
+		if(id == null) throw new IllegalArgumentException("Id cannot be null");
+		Long nodeId = KeyFactory.stringToKey(id);
+		// When an entity is created we need to update all daos.
+		Map<String, Set<Reference>> references = nodeDao.getNodeReferences(id);
+		if(references != null){
+			dboReferenceDao.replaceReferences(nodeId, references);
+		}
+		try {
+			// Storage locations
+			NamedAnnotations namedAnnos = nodeDao.getAnnotations(id);
+			StorageLocations sl = JDOSecondaryPropertyUtils.getStorageLocations(namedAnnos, nodeId, namedAnnos.getCreatedBy());
+			storageLocationDao.replaceLocationData(sl);
+			// Annotations
+			Annotations forDb = JDOSecondaryPropertyUtils.prepareAnnotationsForDBReplacement(namedAnnos, id);
+			dboAnnotationsDao.replaceAnnotations(forDb);
+		} catch (UnsupportedEncodingException e) {
+			throw new DatastoreException(e);
+		} catch (JSONObjectAdapterException e) {
+			throw new DatastoreException(e);
+		}
+	}
+
+	@Override
+	public void deleteEntity(String id) {
+		// Currently we are using cascade deletes at the database level.
+	}
+
+}
---
+++ lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/dao/AsynchronousDAOImplTest.java
@@ -0,0 +1,89 @@
+package org.sagebionetworks.repo.model.dbo.dao;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import org.mockito.Mockito;
+import org.sagebionetworks.repo.model.Annotations;
+import org.sagebionetworks.repo.model.DatastoreException;
+import org.sagebionetworks.repo.model.NamedAnnotations;
+import org.sagebionetworks.repo.model.NodeDAO;
+import org.sagebionetworks.repo.model.Reference;
+import org.sagebionetworks.repo.model.StorageLocationDAO;
+import org.sagebionetworks.repo.model.StorageLocations;
+import org.sagebionetworks.repo.model.jdo.JDOSecondaryPropertyUtils;
+import org.sagebionetworks.repo.model.jdo.KeyFactory;
+import org.sagebionetworks.repo.web.NotFoundException;
+import org.sagebionetworks.schema.adapter.JSONObjectAdapterException;
+/**
+ * A unit test for AsynchronousDAOImpl.
+ * @author John
+ *
+ */
+public class AsynchronousDAOImplTest {
+	
+	Long nodeId = new Long(123);
+	String nodeIdString = KeyFactory.keyToString(nodeId);
+	
+	NodeDAO mockNodeDao;
+	DBOReferenceDao mockReferenceDao;
+	DBOAnnotationsDao mockAnnotationsDao;
+	StorageLocationDAO mockStorageLocationDao;
+	AsynchronousDAOImpl testDao;
+	NamedAnnotations annos;
+	StorageLocations sl;
+	Annotations forDb;
+	Map<String, Set<Reference>> references;
+
+	@Before
+	public void before() throws DatastoreException, NotFoundException, UnsupportedEncodingException, JSONObjectAdapterException{
+		// Build up the mocks
+		mockNodeDao = Mockito.mock(NodeDAO.class);
+		mockReferenceDao = Mockito.mock(DBOReferenceDao.class);
+		mockAnnotationsDao = Mockito.mock(DBOAnnotationsDao.class);
+		mockStorageLocationDao = Mockito.mock(StorageLocationDAO.class);
+		// Setup the references
+		references = new HashMap<String, Set<Reference>>();
+		Set<Reference> set = new HashSet<Reference>();
+		Reference ref = new Reference();
+		ref.setTargetId("syn444");
+		ref.setTargetVersionNumber(1l);
+		set.add(ref);
+		references.put("some_key", set);
+		// The annotations
+		annos = new NamedAnnotations();
+		annos.setId(nodeIdString);
+		annos.setEtag("456");
+		annos.setCreatedBy(999l);
+		annos.setCreationDate(new Date(123));
+		sl = JDOSecondaryPropertyUtils.getStorageLocations(annos, nodeId, annos.getCreatedBy());
+		forDb = JDOSecondaryPropertyUtils.prepareAnnotationsForDBReplacement(annos, nodeIdString);
+		// Mock the node dao.
+		when(mockNodeDao.getNodeReferences(nodeIdString)).thenReturn(references);
+		when(mockNodeDao.getAnnotations(nodeIdString)).thenReturn(annos);
+		
+		testDao = new AsynchronousDAOImpl(mockNodeDao, mockReferenceDao, mockAnnotationsDao, mockStorageLocationDao);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testReplaceAllNull() throws NotFoundException{
+		// Test replace add
+		testDao.replaceAll(null);
+	}
+	
+	@Test
+	public void testReplaceAll() throws NotFoundException{
+		// Make the call
+		testDao.replaceAll(nodeIdString);
+		// verify
+		verify(mockReferenceDao, times(1)).replaceReferences(nodeId, references);
+		verify(mockStorageLocationDao, times(1)).replaceLocationData(sl);
+		verify(mockAnnotationsDao, times(1)).replaceAnnotations(forDb);
+	}
+}
