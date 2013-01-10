d5fb71b4cec53b33842499f38b2cb42883996be8
eric-wu <eric.wu@sagebase.org>
2013-01-09 17:36:15 -0800

    Unit test better name

--- lib/lib-dynamo/src/test/java/org/sagebionetworks/dynamo/dao/LineagePairPutTest.java
+++ lib/lib-dynamo/src/test/java/org/sagebionetworks/dynamo/dao/LineagePairPutTest.java
@@ -14,7 +14,7 @@ import com.amazonaws.services.dynamodb.datamodeling.DynamoDBMapper;
 public class LineagePairPutTest {
 
 	@Test
-	public void testWriteOrder() {
+	public void testPutOrder() {
 
 		DboNodeLineage d2a = mock(DboNodeLineage.class);
 		DboNodeLineage a2d = mock(DboNodeLineage.class);
