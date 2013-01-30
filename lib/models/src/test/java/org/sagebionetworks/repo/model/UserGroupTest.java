4d7b1f8f344ad5b1d8e6ca829f5843201fbd6018
eric-wu <eric.wu@sagebase.org>
2013-01-29 19:21:32 -0800

    Add InvalidUserException -- test part

--- lib/models/src/test/java/org/sagebionetworks/repo/model/UserGroupTest.java
+++ lib/models/src/test/java/org/sagebionetworks/repo/model/UserGroupTest.java
@@ -6,31 +6,31 @@ import org.junit.Test;
 import org.sagebionetworks.repo.model.util.UserGroupUtil;
 
 public class UserGroupTest {
-	
+
 	@Test
 	public  void testIsEmailAddress(){
 		assertTrue(UserGroupUtil.isEmailAddress("something@gmail.com"));
 		assertFalse(UserGroupUtil.isEmailAddress("PUBLIC"));
 	}
-	
+
 	@Test (expected=IllegalArgumentException.class)
 	public void validateNull(){
 		UserGroupUtil.validate(null);
 	}
-	
-	@Test (expected=IllegalArgumentException.class)
+
+	@Test (expected=InvalidUserException.class)
 	public void validateNullId(){
 		UserGroup userGroup = new UserGroup();
 		UserGroupUtil.validate(userGroup);
 	}
-	
-	@Test (expected=IllegalArgumentException.class)
+
+	@Test (expected=InvalidUserException.class)
 	public void validateNullName(){
 		UserGroup userGroup = new UserGroup();
 		userGroup.setId("99");
 		UserGroupUtil.validate(userGroup);
 	}
-	
+
 	@Test
 	public void validateValid(){
 		UserGroup userGroup = new UserGroup();
@@ -39,8 +39,8 @@ public class UserGroupTest {
 		userGroup.setIsIndividual(true);
 		UserGroupUtil.validate(userGroup);
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
+
+	@Test(expected=InvalidUserException.class)
 	public void validateGroupWithEmailAddressName(){
 		UserGroup userGroup = new UserGroup();
 		userGroup.setId("99");
@@ -48,8 +48,8 @@ public class UserGroupTest {
 		userGroup.setIsIndividual(false);
 		UserGroupUtil.validate(userGroup);
 	}
-	
-	@Test(expected=IllegalArgumentException.class)
+
+	@Test(expected=InvalidUserException.class)
 	public void validateUserWithoutEmailAddressName(){
 		UserGroup userGroup = new UserGroup();
 		userGroup.setId("99");
@@ -57,5 +57,4 @@ public class UserGroupTest {
 		userGroup.setIsIndividual(true);
 		UserGroupUtil.validate(userGroup);
 	}
-
 }
--- lib/models/src/test/java/org/sagebionetworks/repo/model/UserTest.java
+++ lib/models/src/test/java/org/sagebionetworks/repo/model/UserTest.java
@@ -3,26 +3,25 @@ package org.sagebionetworks.repo.model;
 import org.junit.Test;
 
 public class UserTest {
-	
+
 	@Test (expected=IllegalArgumentException.class)
 	public void testValidateNull(){
 		User.validateUser(null);
 	}
 
-	@Test (expected=IllegalArgumentException.class)
+	@Test (expected=InvalidUserException.class)
 	public void testValidateNullId(){
 		User user = new User();
 		User.validateUser(user);
 	}
-	
-	@Test (expected=IllegalArgumentException.class)
+
+	@Test (expected=InvalidUserException.class)
 	public void testValidateNullUserId(){
 		User user = new User();
 		user.setId("1010");
 		User.validateUser(user);
 	}
 
-	
 	@Test
 	public void testValidateNotNull(){
 		User user = new User();
