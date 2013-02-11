9d060099674d8324f3f32b7d5319c02e50a3d575
eric-wu <eric.wu@sagebase.org>
2013-02-11 13:28:00 -0800

    Add trash service

---
+++ services/repository/src/main/java/org/sagebionetworks/repo/web/service/TrashService.java
@@ -0,0 +1,35 @@
+package org.sagebionetworks.repo.web.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.sagebionetworks.repo.model.DatastoreException;
+import org.sagebionetworks.repo.model.PaginatedResults;
+import org.sagebionetworks.repo.model.TrashedEntity;
+import org.sagebionetworks.repo.model.UnauthorizedException;
+import org.sagebionetworks.repo.web.NotFoundException;
+
+/**
+ * A service for entities in the trash can.
+ *
+ * @author Eric Wu
+ */
+public interface TrashService {
+
+	/**
+	 * Moves an entity and its descendants to the trash can.
+	 */
+	void moveToTrash(String userId, String entityId)
+			throws NotFoundException, DatastoreException, UnauthorizedException;
+
+	/**
+	 * Moves an entity and its descendants out of the trash can.
+	 */
+	void restoreFromTrash(String userId, String entityId, String newParentId)
+			throws NotFoundException, DatastoreException, UnauthorizedException;
+
+	/**
+	 * Retrieves entities (in the trash can) deleted by the specified user.
+	 */
+	PaginatedResults<TrashedEntity> viewTrash(String userId, Integer offset, Integer limit,
+			HttpServletRequest request) throws DatastoreException, NotFoundException;
+}
