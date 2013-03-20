9bf45bef1b73afcaf44899db272e1a26550c2618
eric-wu <eric.wu@sagebase.org>
2013-03-20 15:34:28 -0700

    Add mocked test for retry

--- lib/lib-doi/src/test/java/org/sagebionetworks/doi/EzidClientTest.java
+++ lib/lib-doi/src/test/java/org/sagebionetworks/doi/EzidClientTest.java
@@ -2,17 +2,34 @@ package org.sagebionetworks.doi;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URI;
 import java.util.Calendar;
 import java.util.Random;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.Credentials;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.params.AuthPolicy;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.CoreConnectionPNames;
 import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.protocol.HTTP;
 import org.junit.Test;
 import org.sagebionetworks.StackConfiguration;
 
@@ -40,8 +57,7 @@ public class EzidClientTest {
 	public void testCreate() throws Exception {
 		final EzidMetadata metadata = new EzidMetadata();
 		// Create a random ID in the EZID test "domain"
-		Random r = new Random();
-		String id = Integer.toHexString(r.nextInt());
+		String id = Integer.toHexString(random.nextInt());
 		final String doi = "doi:10.5072/FK2." + id;
 		metadata.setDoi(doi);
 		final String target = "https://synapse.sagebase.org/";
@@ -62,8 +78,7 @@ public class EzidClientTest {
 	@Test(expected=RuntimeException.class)
 	public void testCreateWithException() throws Exception {
 		final EzidMetadata metadata = new EzidMetadata();
-		Random r = new Random();
-		String id = Integer.toHexString(r.nextInt());
+		String id = Integer.toHexString(random.nextInt());
 		// Invalid doi
 		final String doi = "doi:10.99999/test.invalid." + id;
 		metadata.setDoi(doi);
@@ -81,4 +96,62 @@ public class EzidClientTest {
 		DoiClient client = new EzidClient();
 		client.create(metadata);
 	}
+
+	@Test
+	public void testCreateRetry() throws Exception {
+
+		// Create test data
+		final EzidMetadata metadata = new EzidMetadata();
+		String id = Integer.toHexString(random.nextInt());
+		final String doi = "doi:10.5072/FK2." + id;
+		metadata.setDoi(doi);
+		final String target = "https://synapse.sagebase.org/";
+		metadata.setTarget(target);
+		final String creator = "Test, Something";
+		metadata.setCreator(creator);
+		final String title = "This is a test";
+		metadata.setTitle(title);
+		final String publisher = "Sage Bionetworks";
+		metadata.setPublisher(publisher);
+		final int year = Calendar.getInstance().get(Calendar.YEAR);
+		metadata.setPublicationYear(year);
+
+		// Create request from the test data
+		URI uri = URI.create(StackConfiguration.getEzidUrl() + "id/" + metadata.getDoi());
+		HttpPut httpPut = new HttpPut(uri);
+		StringEntity requestEntity = new StringEntity(metadata.getMetadataAsString(), HTTP.PLAIN_TEXT_TYPE, "UTF-8");
+		httpPut.setEntity(requestEntity);
+
+		// Mock 503
+		StatusLine mockStatus = mock(StatusLine.class);
+		when(mockStatus.getStatusCode()).thenReturn(HttpStatus.SC_SERVICE_UNAVAILABLE);
+		HttpResponse mockResponse = mock(HttpResponse.class);
+		when(mockResponse.getStatusLine()).thenReturn(mockStatus);
+		HttpEntity httpEntity = new StringEntity("503 error");
+		when(mockResponse.getEntity()).thenReturn(httpEntity);
+		HttpClient mockClient = mock(HttpClient.class);
+		when(mockClient.execute(httpPut)).thenReturn(mockResponse);
+
+		// "Inject" the mock client
+		DoiClient doiClient = new EzidClient();
+		assertNotNull(doiClient);
+		Field field = EzidClient.class.getDeclaredField("client");
+		assertNotNull(field);
+		field.setAccessible(true);
+		field.set(doiClient, mockClient);
+		Method method = EzidClient.class.getDeclaredMethod("executeWithRetry", HttpUriRequest.class);
+		method.setAccessible(true);
+
+		try {
+			method.invoke(doiClient, httpPut);
+		} catch (InvocationTargetException e) {
+			Throwable cause = e.getCause();
+			assertTrue(cause instanceof RuntimeException);
+			assertTrue(cause.getMessage().contains("503"));
+		}
+
+		verify(mockClient.execute(httpPut), times(3));
+	}
+
+	private final Random random = new Random();
 }
