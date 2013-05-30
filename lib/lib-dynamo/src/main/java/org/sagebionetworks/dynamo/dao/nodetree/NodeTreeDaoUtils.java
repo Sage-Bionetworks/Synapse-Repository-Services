583522013fe387b898bde819f2c9e728a974d810
eric-wu <eric.wu@sagebase.org>
2013-05-29 18:23:03 -0700

    Split NodeTreeDaoNodeLineageImpl into NodeTreeQueryDaoImpl and NodeTreeUpdateDaoImpl

---
+++ lib/lib-dynamo/src/main/java/org/sagebionetworks/dynamo/dao/nodetree/NodeTreeDaoUtils.java
@@ -0,0 +1,193 @@
+package org.sagebionetworks.dynamo.dao.nodetree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.amazonaws.services.dynamodb.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodb.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodb.model.AttributeValue;
+import com.amazonaws.services.dynamodb.model.ComparisonOperator;
+import com.amazonaws.services.dynamodb.model.Condition;
+
+/**
+ * Utility class that provides raw operations on node lineage trees in DynamoDB.
+ */
+class NodeTreeDaoUtils {
+
+	/** The maximum number of items to retrieve per page. */
+	private static final int MAX_PAGE_SIZE = 100000;
+
+	/**
+	 * Gets the path from the root (inclusive) to this node (exclusive). The path must be complete.
+	 * That is to say the depth must start with depth 0 (the root) and must be consecutive all the
+	 * way to the depth of this node. The dummy root is NOT included in the list. An empty list is
+	 * returned if this node is the actual root.
+	 *
+	 * @throws IncompletePathException When the path is incomplete
+	 */
+	static List<NodeLineage> getCompletePathFromRoot(final String node, final DynamoDBMapper mapper)
+			throws IncompletePathException {
+
+		// The list of ancestors in the order of distance from this node. The list includes
+		// the dummy root if and only if this node is the actual root.
+		final List<DboNodeLineage> dboList = query(node, LineageType.ANCESTOR, -1, mapper);
+
+		if (dboList == null) {
+			throw new NoAncestorException("Null list of ancestors returned from DynamoDB for node " + node);
+		}
+
+		final int dboListSize = dboList.size();
+		if (dboListSize == 0) {
+			// We should at least have the actual root or the dummy ROOT.
+			throw new NoAncestorException("Empty list of ancestors returned from DynamoDB for node " + node);
+		}
+
+		// Start from checking the root
+		final List<NodeLineage> rootList = getRootLineage(mapper);
+		final NodeLineage lastLineage = new NodeLineage(dboList.get(dboListSize - 1));
+		final String lastNode = lastLineage.getAncestorOrDescendantId();
+		boolean lastIsRoot = false;
+		for (NodeLineage rootLineage : rootList) {
+			final String root = rootLineage.getAncestorOrDescendantId();
+			if (DboNodeLineage.ROOT.equals(lastNode)) {
+				// If the last node is the dummy node, this node must be the actual root.
+				// No other node points to the dummy node except the actual root.
+				if (root.equals(node)) {
+					lastIsRoot = true;
+					return new ArrayList<NodeLineage>(0);
+				}
+			} else if (root.equals(lastNode)) {
+				lastIsRoot = true;
+				break;
+			}
+		}
+		if (!lastIsRoot) {
+			throw new IncompletePathException("The list of ancestors for node " + node + " is missing the root.");
+		}
+
+		// Check the rest one by one and make sure we get all the nodes on the path
+		final List<NodeLineage> path = new ArrayList<NodeLineage>(dboListSize);
+		for (int i = dboListSize - 1; i >= 0; i--) {
+			DboNodeLineage dbo = dboList.get(i);
+			NodeLineage lineage = new NodeLineage(dbo);
+			int dist = lineage.getDistance();
+			// The list is sorted by distance. The first ancestor at index 0 is the parent
+			// whose distance is 1. If we have a complete path, the distance of any ancestor
+			// in the list should be exactly i+1
+			if (dist > (i + 1)) {
+				throw new IncompletePathException("Missing ancestor at distance " + (i + 1)
+						+ " in the ancestor path for node " + node);
+			} else if (dist < (i + 1)) {
+				throw new MultipleInheritanceException("Node " + node
+						+ " has more than 1 ancestor of a particular generation.");
+			}
+			path.add(lineage);
+		}
+
+		return path;
+	}
+
+	/**
+	 * Whether the node is a root.
+	 */
+	static boolean isRoot(final String nodeId, final DynamoDBMapper mapper) {
+		List<NodeLineage> rootList = getRootLineage(mapper);
+		for (NodeLineage rootLineage : rootList) {
+			String root = rootLineage.getAncestorOrDescendantId();
+			if (root.equals(nodeId)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Gets the lineage from the dummy ROOT to all the root nodes. These are essentially
+	 * the pointers from the dummy ROOT to the actual roots.
+	 */
+	static List<NodeLineage> getRootLineage(final DynamoDBMapper mapper) {
+		// Use the dummy ROOT to locate the actual root
+		// The actual root is directly below the dummy ROOT
+		List<DboNodeLineage> results = query(DboNodeLineage.ROOT, LineageType.DESCENDANT, 1, mapper);
+		List<NodeLineage> roots = new ArrayList<NodeLineage>();
+		for (DboNodeLineage dbo : results) {
+			roots.add(new NodeLineage(dbo));
+		}
+		return roots;
+	}
+
+	/**
+	 * Whether the node exists in dynamo.
+	 */
+	static boolean exists(final String nodeId, final DynamoDBMapper mapper) {
+		NodeLineage parentLineage = getParentLineage(nodeId, mapper);
+		return (parentLineage != null);
+	}
+
+	/**
+	 * Gets the lineage to the parent node. Returns null if the child does not exist yet
+	 * in DynamoDB. The root will return the dummy ROOT as the parent.
+	 */
+	static NodeLineage getParentLineage(final String child, final DynamoDBMapper mapper) {
+		// Parent is exactly 1 node away
+		List<DboNodeLineage> dboList = query(child, LineageType.ANCESTOR, 1, mapper);
+		if (dboList == null || dboList.isEmpty()) {
+			return null;
+		}
+		if (dboList.size() > 1) {
+			throw new MultipleInheritanceException(child + " fetches back more than 1 parent.");
+		}
+		NodeLineage parent = new NodeLineage(dboList.get(0));
+		return parent;
+	}
+
+	/**
+	 * Finds all the descendants. Use this internally for updating only.
+	 */
+	static List<NodeLineage> getDescendants(final String nodeId, final DynamoDBMapper mapper) {
+		List<DboNodeLineage> dboList = query(nodeId, LineageType.DESCENDANT, -1, mapper);
+		final List<NodeLineage> descList = new ArrayList<NodeLineage>(dboList.size());
+		int count = 0;
+		for (DboNodeLineage dbo : dboList) {
+			count++;
+			// The query method handles paging internally. It returns a 'lazy-loaded' collection.
+			// That is, it initially returns only one page of results, and then makes a service
+			// call for the next page if needed. Iterating over the list fetches the whole
+			// collection page by page.
+			descList.add(new NodeLineage(dbo));
+			if (count > MAX_PAGE_SIZE) {
+				// Are we querying the root for its descendants?
+				throw new RuntimeException("Max page size, " + MAX_PAGE_SIZE
+						+ ", reached for the descendants of node " + nodeId);
+			}
+		}
+		return descList;
+	}
+
+	/**
+	 * Performs a simple query on the specified node.
+	 *
+	 * @param nodeId        The node to perform query on
+	 * @param distance      The distance to the node, the range key, supply a value <= 0 to ignore the range
+	 * @param lineageType   The type of query, whether it is ancestor or descendant
+	 * @param dynamoMapper  The DynamoDB mapper used to perform the query
+	 */
+	static List<DboNodeLineage> query(final String nodeId, final LineageType lineageType,
+			final int distance, final DynamoDBMapper dynamoMapper) {
+
+		final String hashKey = DboNodeLineage.createHashKey(nodeId, lineageType);
+		AttributeValue hashKeyAttr = new AttributeValue().withS(hashKey);
+		DynamoDBQueryExpression queryExpression = new DynamoDBQueryExpression(hashKeyAttr);
+
+		if (distance > 0) {
+			String rangeKeyStart = DboNodeLineage.createRangeKey(distance, "");
+			Condition rangeKeyCondition = new Condition()
+					.withComparisonOperator(ComparisonOperator.BEGINS_WITH)
+					.withAttributeValueList(new AttributeValue().withS(rangeKeyStart));
+			queryExpression.setRangeKeyCondition(rangeKeyCondition);
+		}
+
+		List<DboNodeLineage> dboList = dynamoMapper.query(DboNodeLineage.class, queryExpression);
+		return dboList;
+	}
+}
--- lib/lib-dynamo/src/test/java/org/sagebionetworks/dynamo/dao/nodetree/NodeTreeDaoImplPreconditionTest.java
+++ lib/lib-dynamo/src/test/java/org/sagebionetworks/dynamo/dao/nodetree/NodeTreeDaoImplPreconditionTest.java
@@ -8,83 +8,85 @@ import org.mockito.Mockito;
 
 import com.amazonaws.services.dynamodb.AmazonDynamoDB;
 
-public class NodeTreeDaoNodeLineageImplPreconditionTest {
+public class NodeTreeDaoImplPreconditionTest {
 
-	private NodeTreeDaoNodeLineageImpl nodeTreeDao;
+	private NodeTreeUpdateDao updateDao;
+	private NodeTreeQueryDao queryDao;
 
 	@Before
 	public void before() {
 		AmazonDynamoDB client = Mockito.mock(AmazonDynamoDB.class);
-		this.nodeTreeDao = new NodeTreeDaoNodeLineageImpl(client);
+		updateDao = new NodeTreeUpdateDaoImpl(client);
+		queryDao = new NodeTreeQueryDaoImpl(client);
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testCreateIllegalArgumentException1() {
-		nodeTreeDao.create(null, "parent", new Date());
+		updateDao.create(null, "parent", new Date());
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testCreateIllegalArgumentException2() {
-		nodeTreeDao.create("child", null, new Date());
+		updateDao.create("child", null, new Date());
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testCreateIllegalArgumentException3() {
-		nodeTreeDao.create("child", "parent", null);
+		updateDao.create("child", "parent", null);
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testUpdateIllegalArgumentException1() {
-		nodeTreeDao.update(null, "parent", new Date());
+		updateDao.update(null, "parent", new Date());
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testUpdateIllegalArgumentException2() {
-		nodeTreeDao.update("child", null, new Date());
+		updateDao.update("child", null, new Date());
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testUpdateIllegalArgumentException3() {
-		nodeTreeDao.update("child", "parent", null);
+		updateDao.update("child", "parent", null);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void testDeleteIllegalArgumentException1() {
-		nodeTreeDao.delete(null, new Date());
+		updateDao.delete(null, new Date());
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testDeleteIllegalArgumentException2() {
-		nodeTreeDao.delete("child", null);
+		updateDao.delete("child", null);
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testGetAncestorsIllegalArgumentException() {
-		nodeTreeDao.getAncestors(null);
+		queryDao.getAncestors(null);
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testGetParentIllegalArgumentException() {
-		nodeTreeDao.getParent(null);
+		queryDao.getParent(null);
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testGetDescendantsIllegalArgumentException1() {
-		nodeTreeDao.getDescendants(null, 3, null);
+		queryDao.getDescendants(null, 3, null);
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testGetDescendantsIllegalArgumentException2() {
-		nodeTreeDao.getDescendants(null, 1, 3, null);
+		queryDao.getDescendants(null, 1, 3, null);
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testGetDescendantsIllegalArgumentException3() {
-		nodeTreeDao.getDescendants("child", 0, null);
+		queryDao.getDescendants("child", 0, null);
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testGetDescendantsIllegalArgumentException4() {
-		nodeTreeDao.getDescendants("child", 0, 3, null);
+		queryDao.getDescendants("child", 0, 3, null);
 	}
 }
