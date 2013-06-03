541b2adad66068b12898e7593f6a9f6ce5c9622c
brucehoff <bruce.hoff@sagebase.org>
2013-06-03 09:55:12 -0700

    PLFM-1945

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/AccessRequirementManagerImplAutoWiredTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/AccessRequirementManagerImplAutoWiredTest.java
@@ -272,7 +272,7 @@ public class AccessRequirementManagerImplAutoWiredTest {
 		assertEquals(0, ars.getResults().size());
 	}
 	
-	// evaluation owner never has unmet access requirements
+	// evaluation owner gets no special treatment
 	@Test
 	public void testGetUnmetEvaluationAccessRequirementsOwner() throws Exception {
 		ar = newEvaluationAccessRequirement(evaluation.getId());
@@ -281,8 +281,8 @@ public class AccessRequirementManagerImplAutoWiredTest {
 		rod.setId(evaluation.getId());
 		rod.setType(RestrictableObjectType.EVALUATION);
 		QueryResults<AccessRequirement> ars = accessRequirementManager.getUnmetAccessRequirements(testUserProvider.getTestUserInfo(), rod);
-		assertEquals(0L, ars.getTotalNumberOfResults());
-		assertEquals(0, ars.getResults().size());
+		assertEquals(1L, ars.getTotalNumberOfResults());
+		assertEquals(1, ars.getResults().size());
 	}
 	
 	@Test
