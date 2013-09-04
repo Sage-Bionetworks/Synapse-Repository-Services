e96945b3c0b97c26174237cbe60f1b6a21174615
john-hill <john.hill@sagebase.org>
2013-09-04 14:43:17 -0700

    logging directory is discoverd from Log4J now

--- lib/logging/src/main/java/org/sagebionetworks/logging/s3/LogSweeperFactoryImpl.java
+++ lib/logging/src/main/java/org/sagebionetworks/logging/s3/LogSweeperFactoryImpl.java
@@ -1,10 +1,16 @@
 package org.sagebionetworks.logging.s3;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.appender.RollingFileAppender;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -15,25 +21,48 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class LogSweeperFactoryImpl implements LogSweeperFactory {
 	
-	Logger log = LogManager.getLogger(LogSweeperFactoryImpl.class);
+	Logger log = (Logger) LogManager.getLogger(LogSweeperFactoryImpl.class);
 	
 	@Autowired
 	private LogDAO logDAO;
-	private File logDirectory;
-	private long lockExpiresMs;
+	private long lockExpiresMs = 1000*30;
+	private List<File> logDirectories;
 
 	/**
 	 * Injected via Spring
-	 * @param logDirectory
+	 * 
+	 * @param appendersToSweep
 	 */
-	public void setLogDirectory(String logDirectory) {
-		log.debug("Setting logging directory to: "+logDirectory);
-		this.logDirectory = new File(logDirectory);
-		this.logDirectory.mkdirs();
-		if(!this.logDirectory.isDirectory()){
-			throw new IllegalArgumentException("The logging directory must be a directory");
+	public void setAppendersToSweep(List<String> appendersToSweep) {
+		if(appendersToSweep == null) throw new IllegalArgumentException("Appends cannot be null");
+		log.debug("appenders: "+appendersToSweep);
+		// find the sweep directory
+		Map<String, Appender<?>> map = log.getAppenders();
+		// Find each appender
+		logDirectories = new LinkedList<File>();
+		Set<String> directorySet = new HashSet<String>();
+		for(String appenderName: appendersToSweep){
+			Appender appender = map.get(appenderName);
+			if(appender == null){
+				throw new IllegalArgumentException("Cannot find the appender named: "+appenderName);
+			}
+			if(!(appender instanceof RollingFileAppender)){
+				throw new IllegalArgumentException("Expected a RollingFileAppender for appender name: "+appenderName+" but found "+appender.getClass().getName());
+			}
+			RollingFileAppender rolling = (RollingFileAppender) appender;
+			File logFile = new File(rolling.getFileName());
+			File dir = logFile.getParentFile();
+			if(directorySet.add(dir.getAbsolutePath())){
+				logDirectories.add(dir);
+			}
 		}
 	}
+
+	@Override
+	public List<File> getLogDirectories() {
+		return logDirectories;
+	}
+
 
 	/**
 	 * Injected via Spring
@@ -49,15 +78,17 @@ public class LogSweeperFactoryImpl implements LogSweeperFactory {
 	 */
 	@Override
 	public List<String> sweepLogs() {
+		if(logDirectories == null) throw new IllegalStateException("Must call setAppendersToSweep() to set valid appender names to sweep.");
+		// This will contain all of the keys of any file created.
+		List<String> keys = new LinkedList<String>();
+		// Sweep each directory
+		for(File directoryToSweep: logDirectories){
+			// Sweep this directory
+			LogSweeper sweeper =  new LogSweeper(directoryToSweep, lockExpiresMs, logDAO);
+			keys.addAll(sweeper.sweepAllfiles());
+		}
 		// Create the sweeper and run it.
-		LogSweeper sweeper =  new LogSweeper(logDirectory, lockExpiresMs, logDAO);
-		return sweeper.sweepAllfiles();
-	}
-
-
-	@Override
-	public File getLoggingDirectory() {
-		return this.logDirectory;
+		return keys;
 	}
 
 }
