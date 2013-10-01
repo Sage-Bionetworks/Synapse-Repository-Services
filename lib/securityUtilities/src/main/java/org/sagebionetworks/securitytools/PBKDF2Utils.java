824db86e79205f8f262c625ace9f953d85fdb320
Joseph Wu <kaysoky88@gmail.com>
2013-10-01 15:19:15 -0700

    Added utility for password hashing

---
+++ lib/securityUtilities/src/main/java/org/sagebionetworks/securitytools/PBKDF2Utils.java
@@ -0,0 +1,68 @@
+package org.sagebionetworks.securitytools;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class PBKDF2Utils {
+	private static final String HASH_PREFIX = "{PKCS5S2}";
+	private static final String HASHING_ALGORITHM = "PBKDF2WithHmacSHA1";
+	private static final int HASHING_ITERATIONS = 10000;
+	private static final int HASHING_SALTLENGTH = 16; // bytes
+	private static final int HASHING_KEYLENGTH = 32; // bytes
+	private static final int PBEKEYSPEC_KEYLENGTH = HASHING_KEYLENGTH * 8; // bits
+	
+	/**
+	 * Returns the salt used to generate the password hash
+	 * @param passHash Must be exactly 73 characters long (9 for the prefix, 64 for the salt and checksum)
+	 */
+	public static byte[] extractSalt(String passHash) {
+		if (passHash == null 
+				|| passHash.length() != (HASH_PREFIX.length() + (HASHING_SALTLENGTH + HASHING_KEYLENGTH) * 4 / 3)
+				|| !passHash.startsWith(HASH_PREFIX)) {
+			throw new IllegalArgumentException("Password hash format is unrecognized"); 
+		}
+		String hashBody = passHash.substring(HASH_PREFIX.length());
+		byte[] hashBytes = Base64.decodeBase64(hashBody.getBytes());
+		return Arrays.copyOfRange(hashBytes, 0, HASHING_SALTLENGTH);
+	}
+	
+	/**
+	 * Hashes the password with the given salt
+	 * @return A string with LDAP password format {PREFIX}[data]
+	 *   where [data] is a 64 character, base64 encoded string containing a salt and password checksum 
+	 */
+	public static String hashPassword(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		if (password == null) {
+			// Note: the hashing scheme allows null passwords
+			throw new IllegalArgumentException("Password may not be null");
+		}
+		if (salt.length != HASHING_SALTLENGTH) {
+			throw new IllegalArgumentException("Salt must be 16 bytes");
+		}
+		
+		SecretKeyFactory factory = SecretKeyFactory.getInstance(HASHING_ALGORITHM);
+		KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, HASHING_ITERATIONS, PBEKEYSPEC_KEYLENGTH);
+		SecretKey hash = factory.generateSecret(spec);
+		
+		// Append the password hash to the salt
+		byte[] checksum = hash.getEncoded();
+		byte[] mashup = new byte[HASHING_SALTLENGTH + HASHING_KEYLENGTH];
+		for (int i = 0; i < HASHING_SALTLENGTH; i++) {
+			mashup[i] = salt[i];
+		}
+		for (int i = 0; i < HASHING_KEYLENGTH; i++) {
+			mashup[HASHING_SALTLENGTH + i] = checksum[i];
+		}
+		
+		// Convert to a string and add the prefix
+		return HASH_PREFIX + new String(Base64.encodeBase64(mashup));
+	}
+}
---
+++ lib/securityUtilities/src/test/java/org/sagebionetworks/securitytools/PBKDF2UtilsTest.java
@@ -0,0 +1,41 @@
+package org.sagebionetworks.securitytools;
+
+import org.apache.commons.codec.binary.Base64;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for the password hashing scheme
+ */
+public class PBKDF2UtilsTest {
+	
+	@Test
+	public void testPBKDF2Utils() throws Exception {
+		String passHash = "{PKCS5S2}cnDeuXJkUW+sQwdTw4YlBaV0PMYvZQKc69lHAamznecCeEX9IPqpp7TjhEdJlNkV";
+		
+		byte[] salt = PBKDF2Utils.extractSalt(passHash);
+		byte[] expectedSalt = Base64.decodeBase64("cnDeuXJkUW+sQwdTw4YlBQ==".getBytes());
+		Assert.assertArrayEquals(expectedSalt, salt);
+		
+		String rehashed = PBKDF2Utils.hashPassword("password", expectedSalt);
+		Assert.assertEquals(passHash, rehashed);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testBadHash_Short() throws Exception {
+		String passHash = "{PKCS5S2}I'm too short";
+		PBKDF2Utils.extractSalt(passHash);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testBadHash_Long() throws Exception {
+		String passHash = "{PKCS5S2}WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaayOutaHere";
+		PBKDF2Utils.extractSalt(passHash);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testBadHash_Prefix() throws Exception {
+		String passHash = "Oops, I forgot to add the prefix to this invalid password hash..........."; // right length though
+		PBKDF2Utils.extractSalt(passHash);
+	}
+}
