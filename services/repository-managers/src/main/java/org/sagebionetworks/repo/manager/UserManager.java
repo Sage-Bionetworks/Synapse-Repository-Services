235311b74996881881c7146e9f65c72b52b13518
Joseph Wu <kaysoky88@gmail.com>
2013-10-09 16:30:23 -0700

    Cleanup of UserManager

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/UserManager.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/UserManager.java
@@ -1,18 +1,14 @@
 package org.sagebionetworks.repo.manager;
 
-import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import javax.xml.xpath.XPathExpressionException;
-
-import org.sagebionetworks.authutil.AuthenticationException;
 import org.sagebionetworks.repo.model.AuthorizationConstants.DEFAULT_GROUPS;
 import org.sagebionetworks.repo.model.DatastoreException;
 import org.sagebionetworks.repo.model.UnauthorizedException;
-import org.sagebionetworks.repo.model.UserDAO;
 import org.sagebionetworks.repo.model.UserGroup;
 import org.sagebionetworks.repo.model.UserInfo;
+import org.sagebionetworks.repo.model.auth.NewUser;
 import org.sagebionetworks.repo.web.NotFoundException;
 
 public interface UserManager {
@@ -25,54 +21,35 @@ public interface UserManager {
 	 */
 	public UserInfo getUserInfo(String userName) throws DatastoreException, NotFoundException;
 	
-	// for testing
-	public void setUserDAO(UserDAO userDAO);
-	
 	/**
-	 * Get a default group.
-	 * @param group
-	 * @return
-	 * @throws DatastoreException
+	 * Get a default group
 	 */
 	public UserGroup getDefaultUserGroup(DEFAULT_GROUPS group) throws DatastoreException;
-	
-	/**
-	 * Delete a user.
-	 * @param id
-	 * @throws NotFoundException 
-	 * @throws DatastoreException 
-	 */
-	public void deleteUser(String id) throws DatastoreException, NotFoundException;
-	
 
 	/**
-	 * Find a group.
-	 * @param name
-	 * @param b
-	 * @return
-	 * @throws DatastoreException 
+	 * Find a group
 	 */
 	public UserGroup findGroup(String name, boolean b) throws DatastoreException;
 	
 	/**
-	 * Create a user
-	 * @param toCreate
-	 * @return
-	 * @throws DatastoreException 
+	 * Creates a new user
 	 */
+	public void createUser(NewUser user);
+	
+	/**
+	 * Creates a new user
+	 * To be replaced with createUser
+	 */
+	@Deprecated
 	public String createPrincipal(String name, boolean isIndividual) throws DatastoreException;
 	
 	/**
 	 * Does a principal with this name exist?
-	 * @param name
-	 * @return
 	 */
 	public boolean doesPrincipalExist(String name);
 	
 	/**
 	 * Delete a principal by name
-	 * @param name
-	 * @return
 	 */
 	public boolean deletePrincipal(String name);
 
@@ -80,26 +57,31 @@ public interface UserManager {
 	 * @param principalId
 	 * @return for a group, returns the group name, for a user returns the display name in the user's profile
 	 */
-	public String getDisplayName(Long principalId) throws NotFoundException, DatastoreException;
+	public String getDisplayName(Long principalId) throws NotFoundException;
 	
+	/**
+	 * Returns the group name
+	 */
 	public String getGroupName(String principalId) throws NotFoundException;
 
-	public void updateEmail(UserInfo userInfo, String newEmail) throws DatastoreException, NotFoundException, IOException, AuthenticationException, XPathExpressionException;
-	
+	/**
+	 * To be removed soon
+	 */
+	@Deprecated
 	public void clearCache();
+	
+	/**
+	 * Changes the user's email
+	 */
+	public void updateEmail(UserInfo userInfo, String newEmail) throws DatastoreException, NotFoundException;
 
 	/**
 	 * Get all non-individual user groups, including Public.
-	 * 
-	 * @return
-	 * @throws DatastoreException
-	 * @throws UnauthorizedException
 	 */
 	public Collection<UserGroup> getGroups() throws DatastoreException;
 
 	/**
-	 * get non-individual user groups (including Public) in range
-	 * 
+	 * Get non-individual user groups (including Public) in range
 	 **/
 	public List<UserGroup> getGroupsInRange(UserInfo userInfo, long startIncl, long endExcl, String sort, boolean ascending) throws DatastoreException, UnauthorizedException;
 }
