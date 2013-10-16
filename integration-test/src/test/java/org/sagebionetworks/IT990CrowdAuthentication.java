5a89be8110b8b30817efe8ecb29778822205d379
Joseph Wu <kaysoky88@gmail.com>
2013-10-15 17:13:24 -0700

    Silly mistake

--- integration-test/src/test/java/org/sagebionetworks/IT990CrowdAuthentication.java
+++ integration-test/src/test/java/org/sagebionetworks/IT990CrowdAuthentication.java
@@ -51,7 +51,7 @@ public class IT990CrowdAuthentication {
 	}
 	
 	@Before
-	public static void setup() throws Exception {
+	public void setup() throws Exception {
 		synapse.login(username, password);
 	}
 
