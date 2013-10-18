105a2537a293a122fc99c64ff521b28a5a487fd5
brucehoff <bruce.hoff@sagebase.org>
2013-10-17 20:12:27 -0700

    PLFM-2117

--- services/repository/src/test/java/org/sagebionetworks/repo/web/service/TeamServiceTest.java
+++ services/repository/src/test/java/org/sagebionetworks/repo/web/service/TeamServiceTest.java
@@ -2,7 +2,8 @@ package org.sagebionetworks.repo.web.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Arrays;
