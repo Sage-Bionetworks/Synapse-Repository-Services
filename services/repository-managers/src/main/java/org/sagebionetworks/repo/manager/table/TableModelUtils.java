ebe8425d7a9dd96896058edaf3726cac736f3e96
John Hill <john.hill@sagebase.org>
2013-11-01 17:18:11 -0700

    a break point

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/table/TableModelUtils.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/table/TableModelUtils.java
@@ -1,38 +1,120 @@
 package org.sagebionetworks.repo.manager.table;
 
-import org.sagebionetworks.repo.model.table.TableEntity;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.sagebionetworks.repo.model.table.ColumnType;
+import org.sagebionetworks.repo.model.table.Row;
+import org.sagebionetworks.repo.model.table.ColumnModel;
+import org.sagebionetworks.repo.model.table.RowSet;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+/**
+ * Utilities for working with Tables and Row data.
+ * 
+ * @author jmhill
+ *
+ */
 public class TableModelUtils {
 	
+	private static final String INVALID_VALUE_TEMPLATE = "Value at [%1$s,%2$s] was not a valid %3$s. %4$s";
 	/**
-	 * Validate the passed table model.
-	 * @param toValidate
+	 * Sets the maxiumn string length for a string value in a table.
 	 */
-	public static void validateNewTableModel(TableEntity toValidate){
-		if(toValidate == null) throw new IllegalArgumentException("TableEntity cannot be null");
-		// Issue an ID for each column
-//		List<ColumnModel> models = toValidate.getColumns();
-//		if(models != null){
-//			for(int i=0; i<models.size();i++){
-//				ColumnModel mod = models.get(i);
-//				// Use the index as the ID for all new models
-//				mod.setId(""+i);	
-//			}
-//		}
-	}
-	
+	public static final int MAX_STRING_LENGTH = 2000;
+
 	/**
-	 * Validate the new TableEntity meets all of the requirements.
-	 * @param newEntity
-	 * @param currentEntity
+	 * This utility will validate and convert the passed RowSet to an output CSV.
+	 * @param models
+	 * @param set
+	 */
+	public static void validateAndWriteToCSV(List<ColumnModel> models, RowSet set, CSVWriter out){
+		if(models == null) throw new IllegalArgumentException("Models cannot be null");
+		if(set == null) throw new IllegalArgumentException("RowSet cannot be null");;
+		if(set.getHeaders() == null) throw new IllegalArgumentException("RowSet.headers cannot be null");
+		if(set.getRows() == null) throw new IllegalArgumentException("RowSet.rows cannot be null");
+		if(set.getRows().size() < 1) throw new IllegalArgumentException("RowSet.rows must contain at least one row.");
+		if(out == null) throw new IllegalArgumentException("CSVWriter cannot be null");
+		if(models.size() != set.getHeaders().size()) throw new IllegalArgumentException("RowSet.headers size must be equal to the number of columns in the table.  The table has :"+models.size()+" columns and the passed RowSet.headers has: "+set.getHeaders().size());
+		// Now map the index of each column
+		Map<String, Integer> columnIndexMap  = new HashMap<String, Integer>();
+		int index = 0;
+		for(String header: set.getHeaders()){
+			columnIndexMap.put(header, index);
+			index++;
+		}
+		// Process each row
+		int count = 0;
+		for(Row row: set.getRows()){
+			// First convert the values to
+			if(row.getValues() == null) throw new IllegalArgumentException("Row "+count+" has null list of values");
+			if(models.size() !=row.getValues().size()) throw new IllegalArgumentException("Row.value size must be equal to the number of columns in the table.  The table has :"+models.size()+" columns and the passed Row.value has: "+row.getValues().size()+" for row number: "+count);
+			// Convert the values to an array for quick lookup
+			String[] values = row.getValues().toArray(new String[row.getValues().size()]);
+			// Prepare the final array which includes the ID and version number as the first two columns.
+			String[] finalRow = new String[values.length+2];
+			if(row.getRowId() == null) throw new IllegalArgumentException("Row.rowId cannot be null for row number: "+count);
+			if(row.getVersionNumber() == null) throw new IllegalArgumentException("Row.versionNumber cannot be null for row number: "+count);
+			finalRow[0] = row.getRowId().toString();
+			finalRow[1] = row.getVersionNumber().toString();
+			// Now process all of the columns as defined by the schema
+			for(int i=0; i<models.size(); i++){
+				ColumnModel cm = models.get(i);
+				Integer valueIndex = columnIndexMap.get(cm.getId());
+				if(valueIndex == null) throw new IllegalArgumentException("The Table's ColumnModels includes: name="+cm.getName()+" with id="+cm.getId()+" but "+cm.getId()+" was not found in the headers of the RowResults");
+				// Get the value
+				String value = values[valueIndex];
+				// Validate the value against the model
+				value = validateRowValue(value, cm, i, valueIndex);
+				// Add the value to the final
+				finalRow[i+2] = value;
+			}
+			out.writeNext(finalRow);
+			count++;
+		}
+	}
+	/**
+	 * Validate a value
+	 * @param value
+	 * @param cm
+	 * @param rowIndex
+	 * @param columnIndex
 	 * @return
 	 */
-	public static boolean validateTableModelUpdate(TableEntity newEntity, TableEntity currentEntity){
-		if(newEntity == null) throw new IllegalArgumentException("new TableEntity cannot be null");
-		if(currentEntity == null) throw new IllegalArgumentException("current TableEntity cannot be null");
-		// first map the IDs to the column model
-		
-		return true;
+	public static String validateRowValue(String value, ColumnModel cm, int rowIndex, int columnIndex){
+		if(cm == null) throw new IllegalArgumentException("ColumnModel cannot be null");
+		if(cm.getColumnType() == null) throw new IllegalArgumentException("ColumnModel.columnType cannot be null");
+		// Validate non-null values
+		if(value != null){
+			
+			try {
+				if (ColumnType.BOOLEAN.equals(cm.getColumnType())) {
+					boolean boolValue = Boolean.parseBoolean(value);
+					return Boolean.toString(boolValue);
+				} else if (ColumnType.LONG.equals(cm.getColumnType()) || ColumnType.FILEHANDLEID.equals(cm.getColumnType())) {
+					long lv =Long.parseLong(value);
+					return Long.toString(lv);
+				} else if (ColumnType.DOUBLE.equals(cm.getColumnType())) {
+					double dv = Double.parseDouble(value);
+					return Double.toString(dv);
+				} else if (ColumnType.STRING.equals(cm.getColumnType())) {
+					if(value.length() > MAX_STRING_LENGTH) throw new IllegalArgumentException("String exceeds the maximum length of "+MAX_STRING_LENGTH+" characters. Consider using a FileHandle to store large strings.");
+					return value;
+				} else {
+					throw new IllegalArgumentException(
+							"Unknown ColumModel type: " + cm.getColumnType());
+				}
+			} catch (Exception e) {
+				throw new IllegalArgumentException(String.format(INVALID_VALUE_TEMPLATE, rowIndex, columnIndex, cm.getColumnType(), e.getLocalizedMessage() ));
+			}
+		}else{
+			// the value is null so apply the default if provided
+			if(cm.getDefaultValue() != null){
+				value = cm.getDefaultValue();
+			}
+			return value;
+		}
 	}
-
 }
--- services/repository-managers/src/test/java/org/sagebionetworks/repo/model/table/TableModelUtilsTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/model/table/TableModelUtilsTest.java
@@ -1,26 +1,267 @@
 package org.sagebionetworks.repo.model.table;
 
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.sagebionetworks.repo.manager.table.TableModelUtils;
 
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+
+/**
+ * 
+ * @author jmhill
+ *
+ */
 public class TableModelUtilsTest {
+	
+	List<ColumnModel> validModel;
+	RowSet validRowSet;
+	StringWriter outWritter;
+	CSVWriter out;
+	
+	@Before
+	public void before(){
+		validModel = new LinkedList<ColumnModel>();
+		ColumnModel cm = new ColumnModel();
+		cm.setName("one");
+		cm.setId("1");
+		cm.setColumnType(ColumnType.BOOLEAN);
+		validModel.add(cm);
+		cm = new ColumnModel();
+		cm.setName("two");
+		cm.setId("2");
+		cm.setColumnType(ColumnType.LONG);
+		validModel.add(cm);
+		
+		validRowSet = new RowSet();
+		List<String> headers = new LinkedList<String>();
+		headers.add("2");
+		headers.add("1");
+		validRowSet.setHeaders(headers);
+		List<Row> rows = new LinkedList<Row>();
+		// row one
+		Row row = new Row();
+		row.setRowId(new Long(456));
+		row.setVersionNumber(new Long(2));
+		List<String> values = new LinkedList<String>();
+		values.add("9999");
+		values.add("true");
+		row.setValues(values);
+		rows.add(row);
+		// row two
+		row = new Row();
+		row.setRowId(new Long(457));
+		row.setVersionNumber(new Long(2));
+		values = new LinkedList<String>();
+		values.add("0");
+		values.add("anything that is not 'true' is treated as false so this is false");
+		row.setValues(values);
+		rows.add(row);
+		validRowSet.setRows(rows);
+		
+		
+		outWritter = new StringWriter();
+		out = new CSVWriter(outWritter);
+	}
 
 	@Test (expected=IllegalArgumentException.class)
-	public void testValidateNewTableModelNull(){
-		TableModelUtils.validateNewTableModel(null);
+	public void testValidateAndWriteToCSVNullModel(){
+		TableModelUtils.validateAndWriteToCSV(null, validRowSet, out);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testValidateAndWriteToCSVNullRowSet(){
+		TableModelUtils.validateAndWriteToCSV(validModel, null, out);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testValidateAndWriteToCSVNullOut(){
+		TableModelUtils.validateAndWriteToCSV(validModel, validRowSet, null);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testValidateAndWriteToCSVNullHeaders(){
+		validRowSet.setHeaders(null);
+		TableModelUtils.validateAndWriteToCSV(validModel, validRowSet, out);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testValidateAndWriteToCSVNullRows(){
+		validRowSet.setRows(null);
+		TableModelUtils.validateAndWriteToCSV(validModel, validRowSet, out);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testValidateAndWriteToCSVRowsEmpty(){
+		validRowSet.setRows(new LinkedList<Row>());
+		TableModelUtils.validateAndWriteToCSV(validModel, validRowSet, out);
+	}
+	
+	@Test (expected=IllegalArgumentException.class)
+	public void testValidateAndWriteToCSVModelsEmpty(){
+		TableModelUtils.validateAndWriteToCSV(new LinkedList<ColumnModel>(), validRowSet, out);
 	}
 	
 	@Test
-	public void testValidateNewTableModelEmpty(){
-		TableModelUtils.validateNewTableModel(new TableEntity());
+	public void testValidateAndWriteToCSVOneNullRow(){
+		try{
+			validRowSet.getRows().get(0).setValues(null);
+			TableModelUtils.validateAndWriteToCSV(validModel, validRowSet, out);
+			fail("Should have failed");
+		}catch (IllegalArgumentException e){
+			assertEquals("Row 0 has null list of values", e.getMessage());
+		}
 	}
 	
 	@Test
-	public void testValidateNewTableModel(){
-		TableEntity table = new TableEntity();		
-		TableModelUtils.validateNewTableModel(table);
+	public void testValidateAndWriteToCSVWrongValueSize(){
+		try{
+			validRowSet.getRows().get(0).getValues().add("too many");
+			TableModelUtils.validateAndWriteToCSV(validModel, validRowSet, out);
+			fail("Should have failed");
+		}catch (IllegalArgumentException e){
+			assertEquals("Row.value size must be equal to the number of columns in the table.  The table has :2 columns and the passed Row.value has: 3 for row number: 0", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testValidateAndWriteToCSVRowIdNull(){
+		try{
+			validRowSet.getRows().get(0).setRowId(null);
+			TableModelUtils.validateAndWriteToCSV(validModel, validRowSet, out);
+			fail("Should have failed");
+		}catch (IllegalArgumentException e){
+			assertEquals("Row.rowId cannot be null for row number: 0", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testValidateAndWriteToCSVRowVersionNull(){
+		try{
+			validRowSet.getRows().get(1).setVersionNumber(null);
+			TableModelUtils.validateAndWriteToCSV(validModel, validRowSet, out);
+			fail("Should have failed");
+		}catch (IllegalArgumentException e){
+			assertEquals("Row.versionNumber cannot be null for row number: 1", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testValidateAndWriteToCSVHeaderMissmatch(){
+		try{
+			validRowSet.getHeaders().remove(0);
+			validRowSet.getHeaders().add(0, "3");
+			TableModelUtils.validateAndWriteToCSV(validModel, validRowSet, out);
+			fail("Should have failed");
+		}catch (IllegalArgumentException e){
+			assertEquals("The Table's ColumnModels includes: name=two with id=2 but 2 was not found in the headers of the RowResults", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testHappyCase() throws IOException{
+		// Write the following data
+		TableModelUtils.validateAndWriteToCSV(validModel, validRowSet, out);
+		String csv = outWritter.toString();
+		System.out.println(csv);
+		StringReader reader = new StringReader(csv);
+		// There should be two rows
+		CSVReader in = new CSVReader(reader);
+		List<String[]> results = in.readAll();
+		assertNotNull(results);
+		assertEquals(2, results.size());
+		assertTrue(Arrays.equals(new String[]{"456","2","true","9999"}, results.get(0)));
+		assertTrue(Arrays.equals(new String[]{"457","2","false","0"}, results.get(1)));
+	}
+	
+	@Test
+	public void testValidateBoolean(){
+		ColumnModel cm = new ColumnModel();
+		cm.setColumnType(ColumnType.BOOLEAN);
+		assertEquals(Boolean.FALSE.toString(), TableModelUtils.validateRowValue("some string", cm, 0, 0));
+		assertEquals(Boolean.TRUE.toString(), TableModelUtils.validateRowValue("true", cm, 1, 1));
+		assertEquals(null, TableModelUtils.validateRowValue(null, cm, 2, 2));
+		// Set the default to boolean
+		cm.setDefaultValue(Boolean.TRUE.toString());
+		assertEquals(Boolean.TRUE.toString(), TableModelUtils.validateRowValue(null, cm, 2, 2));
+	}
+	
+	@Test
+	public void testValidateLong(){
+		ColumnModel cm = new ColumnModel();
+		cm.setColumnType(ColumnType.LONG);
+		assertEquals("123", TableModelUtils.validateRowValue("123", cm, 0, 0));
+		try {
+			TableModelUtils.validateRowValue("true", cm, 1, 3);
+			fail("should have failed");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Value at [1,3] was not a valid LONG. For input string: \"true\"", e.getMessage());
+		}
+		assertEquals(null, TableModelUtils.validateRowValue(null, cm, 2, 2));
+		// Set the default to boolean
+		cm.setDefaultValue("890");
+		assertEquals("890", TableModelUtils.validateRowValue(null, cm, 2, 3));
+	}
+	
+	@Test
+	public void testValidateFileHandleId(){
+		ColumnModel cm = new ColumnModel();
+		cm.setColumnType(ColumnType.FILEHANDLEID);
+		assertEquals("123", TableModelUtils.validateRowValue("123", cm, 0, 0));
+		try {
+			TableModelUtils.validateRowValue("true", cm, 1, 3);
+			fail("should have failed");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Value at [1,3] was not a valid FILEHANDLEID. For input string: \"true\"", e.getMessage());
+		}
+		assertEquals(null, TableModelUtils.validateRowValue(null, cm, 2, 2));
+		// Set the default to boolean
+		cm.setDefaultValue("890");
+		assertEquals("890", TableModelUtils.validateRowValue(null, cm, 2, 3));
+	}
+	
+	@Test
+	public void testValidateDouble(){
+		ColumnModel cm = new ColumnModel();
+		cm.setColumnType(ColumnType.DOUBLE);
+		assertEquals("123.1", TableModelUtils.validateRowValue("123.1", cm, 0, 0));
+		try {
+			TableModelUtils.validateRowValue("true", cm, 1, 3);
+			fail("should have failed");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Value at [1,3] was not a valid DOUBLE. For input string: \"true\"", e.getMessage());
+		}
+		assertEquals(null, TableModelUtils.validateRowValue(null, cm, 2, 2));
+		// Set the default to boolean
+		cm.setDefaultValue("-89.3e12");
+		assertEquals("-89.3e12", TableModelUtils.validateRowValue(null, cm, 2, 3));
+	}
+	
+	@Test
+	public void testValidateString(){
+		ColumnModel cm = new ColumnModel();
+		cm.setColumnType(ColumnType.STRING);
+		assertEquals("some string", TableModelUtils.validateRowValue("some string", cm, 0, 0));
+		try {
+			char[] tooLarge = new char[TableModelUtils.MAX_STRING_LENGTH+1];
+			Arrays.fill(tooLarge, 'b');
+			TableModelUtils.validateRowValue(new String(tooLarge), cm, 1, 4);
+			fail("should have failed");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Value at [1,4] was not a valid STRING. String exceeds the maximum length of 2000 characters. Consider using a FileHandle to store large strings.", e.getMessage());
+		}
+		assertEquals(null, TableModelUtils.validateRowValue(null, cm, 2, 2));
+		// Set the default to boolean
+		cm.setDefaultValue("-89.3e12");
+		assertEquals("-89.3e12", TableModelUtils.validateRowValue(null, cm, 2, 3));
 	}
 }
