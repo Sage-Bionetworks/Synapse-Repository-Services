b15bd44746f808cc0651391fda3cb4c17118d655
Heidi <heidi.so@sagebase.org>
2013-11-15 13:42:14 -0800

    Fix weird indentation

--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/v2/dao/V2DBOWikiPageDaoImpl.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/v2/dao/V2DBOWikiPageDaoImpl.java
@@ -240,16 +240,16 @@ public class V2DBOWikiPageDaoImpl implements V2WikiPageDao {
 			}
 		}
 
-		 long currentTime = System.currentTimeMillis();
+		long currentTime = System.currentTimeMillis();
 
-         V2DBOWikiPage oldDbo = getWikiPageDBO(ownerId, ownerType, wikiPage.getId());
-         Long incrementedVersion = oldDbo.getMarkdownVersion() + 1;
+		V2DBOWikiPage oldDbo = getWikiPageDBO(ownerId, ownerType, wikiPage.getId());
+		Long incrementedVersion = oldDbo.getMarkdownVersion() + 1;
 
-         // Update this wiki's entry in the WikiPage database (update version)
-         V2DBOWikiPage newDbo = V2WikiTranslationUtils.createDBOFromDTO(wikiPage);
-         // Set the modifiedon to current.
-         newDbo.setModifiedOn(currentTime);
-         newDbo.setMarkdownVersion(incrementedVersion);
+	    // Update this wiki's entry in the WikiPage database (update version)
+	    V2DBOWikiPage newDbo = V2WikiTranslationUtils.createDBOFromDTO(wikiPage);
+	    // Set the modifiedon to current.
+	    newDbo.setModifiedOn(currentTime);
+	    newDbo.setMarkdownVersion(incrementedVersion);
 		
 		update(ownerType, ownerIdLong, newDbo);
 
