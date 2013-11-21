8cf91df15a2a7654387a5c6f7680eb4cfb767b54
Joseph Wu <kaysoky88@gmail.com>
2013-11-20 17:23:28 -0800

    Worker simply calls the manager, which handles the transactional boundary and such.

---
+++ services/workers/src/main/java/org/sagebionetworks/message/workers/MessageToUserWorker.java
@@ -0,0 +1,76 @@
+package org.sagebionetworks.message.workers;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.sagebionetworks.asynchronous.workers.sqs.MessageUtils;
+import org.sagebionetworks.repo.manager.MessageManager;
+import org.sagebionetworks.repo.model.ObjectType;
+import org.sagebionetworks.repo.model.message.ChangeMessage;
+import org.sagebionetworks.repo.model.message.ChangeType;
+import org.sagebionetworks.repo.web.NotFoundException;
+
+import com.amazonaws.services.sqs.model.Message;
+
+/**
+ * The worker that processes messages sending messages to users
+ * 
+ */
+public class MessageToUserWorker implements Callable<List<Message>> {
+
+	static private Logger log = LogManager.getLogger(MessageToUserWorker.class);
+
+	private List<Message> messages;
+	private MessageManager messageManager;
+
+	public MessageToUserWorker(List<Message> messages,
+			MessageManager messageManager) {
+		if (messages == null) {
+			throw new IllegalArgumentException("Messages cannot be null");
+		}
+		if (messageManager == null) {
+			throw new IllegalArgumentException("MessageManager cannot be null");
+		}
+		this.messages = messages;
+		this.messageManager = messageManager;
+	}
+
+	@Override
+	public List<Message> call() throws Exception {
+		List<Message> processedMessages = new LinkedList<Message>();
+		for (Message message : messages) {
+			// Extract the ChangeMessage
+			ChangeMessage change = MessageUtils.extractMessageBody(message);
+			
+			// We only care about MESSAGE messages here
+			if (ObjectType.MESSAGE == change.getObjectType()) {
+				try {
+					switch (change.getChangeType()) {
+					case CREATE:
+						messageManager.sendMessage(change.getObjectId());
+						break;
+					default:
+						throw new IllegalArgumentException("Unknown change type: " + change.getChangeType());
+					}
+					
+					// This message was processed
+					processedMessages.add(message);
+				} catch (NotFoundException e) {
+					log.info("NotFound: " + e.getMessage() + ". The message will be returned as processed and removed from the queue");
+					processedMessages.add(message);
+				} catch (Throwable e) {
+					// Something went wrong and we did not process the message
+					log.error("Failed to process message", e);
+				}
+			} else {
+				// Non-MESSAGE messages must be returned so they can be removed from the queue.
+				processedMessages.add(message);
+			}
+		}
+		return processedMessages;
+	}
+
+}
