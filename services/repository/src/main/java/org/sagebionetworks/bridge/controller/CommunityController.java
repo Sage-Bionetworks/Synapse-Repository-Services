a9ec5e3ed63982ffe925177438f1114d38a96c21
Marcel <marcel.blonk@sagebase.org>
2013-12-02 12:05:16 -0800

    build fix

--- services/repository/src/main/java/org/sagebionetworks/bridge/controller/CommunityController.java
+++ services/repository/src/main/java/org/sagebionetworks/bridge/controller/CommunityController.java
@@ -209,10 +209,10 @@ public class CommunityController extends BridgeBaseController {
 	 */
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = BridgeUrlHelpers.ADD_COMMUNITY_ADMIN, method = RequestMethod.GET)
-	public void addCommunityAdmin(@PathVariable String id, @PathVariable String memberId,
+	public void addCommunityAdmin(@PathVariable String id, @PathVariable String member,
 			@RequestParam(value = AuthorizationConstants.USER_ID_PARAM, required = false) String userId) throws DatastoreException,
 			NotFoundException {
-		serviceProvider.getCommunityService().addCommunityAdmin(userId, id, memberId);
+		serviceProvider.getCommunityService().addCommunityAdmin(userId, id, member);
 	}
 
 	/**
@@ -226,9 +226,9 @@ public class CommunityController extends BridgeBaseController {
 	 */
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = BridgeUrlHelpers.REMOVE_COMMUNITY_ADMIN, method = RequestMethod.GET)
-	public void removeCommunityAdmin(@PathVariable String id, @PathVariable String memberId,
+	public void removeCommunityAdmin(@PathVariable String id, @PathVariable String member,
 			@RequestParam(value = AuthorizationConstants.USER_ID_PARAM, required = false) String userId) throws DatastoreException,
 			NotFoundException {
-		serviceProvider.getCommunityService().removeCommunityAdmin(userId, id, memberId);
+		serviceProvider.getCommunityService().removeCommunityAdmin(userId, id, member);
 	}
 }
