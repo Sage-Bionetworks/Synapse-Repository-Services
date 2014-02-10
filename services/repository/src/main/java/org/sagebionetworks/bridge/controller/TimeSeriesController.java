a1f82bbb6ebb5d65a5c5bcaad734069960a2eaaf
Marcel <marcel.blonk@sagebase.org>
2014-02-10 11:27:09 -0800

    Creating timeseries with new pojos that align better with client needs

--- services/repository/src/main/java/org/sagebionetworks/bridge/controller/TimeSeriesController.java
+++ services/repository/src/main/java/org/sagebionetworks/bridge/controller/TimeSeriesController.java
@@ -5,16 +5,13 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.sagebionetworks.bridge.BridgeUrlHelpers;
-import org.sagebionetworks.bridge.model.timeseries.TimeSeries;
-import org.sagebionetworks.bridge.model.timeseries.TimeSeriesCollection;
+import org.sagebionetworks.bridge.model.timeseries.TimeSeriesTable;
 import org.sagebionetworks.bridge.service.BridgeServiceProvider;
 import org.sagebionetworks.repo.model.AuthorizationConstants;
-import org.sagebionetworks.repo.web.UrlHelpers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,17 +30,9 @@ public class TimeSeriesController {
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = BridgeUrlHelpers.TIME_SERIES, method = RequestMethod.GET)
 	public @ResponseBody
-	TimeSeriesCollection getTimeSeries(@RequestParam(value = AuthorizationConstants.USER_ID_PARAM, required = false) Long userId,
+	TimeSeriesTable getTimeSeries(@RequestParam(value = AuthorizationConstants.USER_ID_PARAM, required = false) Long userId,
 			@RequestParam(value = BridgeUrlHelpers.COLUMNT_NAME, required = false) List<String> columnNames,
 			@PathVariable String participantDataDescriptorId) throws Exception {
 		return serviceProvider.getTimeSeriesService().getTimeSeries(userId, participantDataDescriptorId, columnNames);
-	}
-
-	@ResponseStatus(HttpStatus.OK)
-	@RequestMapping(value = BridgeUrlHelpers.TIME_SERIES_COLUMN_ALIGNED, method = RequestMethod.GET)
-	public @ResponseBody
-	TimeSeriesCollection getTimeSeries(@RequestParam(value = AuthorizationConstants.USER_ID_PARAM, required = false) Long userId,
-			@PathVariable String participantDataDescriptorId, @PathVariable String columnName, @PathVariable String alignBy) throws Exception {
-		return serviceProvider.getTimeSeriesService().getTimeSeries(userId, participantDataDescriptorId, columnName, alignBy);
 	}
 }
--- services/repository/src/main/java/org/sagebionetworks/bridge/service/TimeSeriesService.java
+++ services/repository/src/main/java/org/sagebionetworks/bridge/service/TimeSeriesService.java
@@ -4,26 +4,11 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
-import org.sagebionetworks.bridge.model.data.ParticipantDataCurrentRow;
-import org.sagebionetworks.bridge.model.data.ParticipantDataColumnDescriptor;
-import org.sagebionetworks.bridge.model.data.ParticipantDataDescriptor;
-import org.sagebionetworks.bridge.model.data.ParticipantDataRow;
-import org.sagebionetworks.bridge.model.data.ParticipantDataStatus;
-import org.sagebionetworks.bridge.model.timeseries.TimeSeries;
-import org.sagebionetworks.bridge.model.timeseries.TimeSeriesCollection;
-import org.sagebionetworks.repo.model.AuthorizationConstants;
+import org.sagebionetworks.bridge.model.timeseries.TimeSeriesTable;
 import org.sagebionetworks.repo.model.DatastoreException;
-import org.sagebionetworks.repo.model.PaginatedResults;
-import org.sagebionetworks.repo.model.IdList;
 import org.sagebionetworks.repo.web.NotFoundException;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public interface TimeSeriesService {
-	public TimeSeriesCollection getTimeSeries(Long userId, String participantDataId, List<String> columnNames) throws DatastoreException,
+	public TimeSeriesTable getTimeSeries(Long userId, String participantDataId, List<String> columnNames) throws DatastoreException,
 			NotFoundException, IOException, GeneralSecurityException;
-
-	public TimeSeriesCollection getTimeSeries(Long userId, String participantDataId, String columnName, String alignBy)
-			throws DatastoreException, NotFoundException, IOException, GeneralSecurityException;
 }
