9c2b8fc1bc75ed4a6bd7622fb6ffad1a8a6936a9
john-hill <john.hill@sagebase.org>
2014-02-20 18:08:56 -0800

    check

--- lib/lib-table-query/src/test/java/org/sagebionetworks/table/query/TableQueryParserTest.java
+++ lib/lib-table-query/src/test/java/org/sagebionetworks/table/query/TableQueryParserTest.java
@@ -1,11 +1,39 @@
 package org.sagebionetworks.table.query;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.sagebionetworks.repo.model.table.query.TableQueryModel;
+import org.sagebionetworks.table.query.model.SQLExample;
+import org.sagebionetworks.table.query.model.SQLExampleProvider;
 
 public class TableQueryParserTest {
+	
+	/**
+	 * We must be able to pares all of the example SQL.
+	 */
+	@Test
+	public void testAllExamples(){
+		List<SQLExample> exampleList = SQLExampleProvider.getSQLExamples();
+		assertNotNull(exampleList);
+		assertTrue(exampleList.size() > 2);
+		// Test each example
+		for(SQLExample example: exampleList){
+
+			try{
+				// Make sure we can parse the SQL
+				System.out.println("Parsing: "+example.getSql());
+				TableQueryParser.parserQuery(example.getSql());
+			} catch(ParseException e){
+				e.printStackTrace();
+				fail("Failed to parse: '"+example.getSql()+"' Error: "+e.getMessage());
+			}
+		}
+	}
 	
 	@Test
 	public void testSelectStar() throws ParseException{
