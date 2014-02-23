13e268fc2cdbd282c231d3f26fa838c2e04379d7
xavier <xschildwachter@hotmail.com>
2014-02-22 23:25:21 -0800

    Restored test.

--- integration-test/src/test/java/org/sagebionetworks/IT520SynapseJavaClientEvaluationTest.java
+++ integration-test/src/test/java/org/sagebionetworks/IT520SynapseJavaClientEvaluationTest.java
@@ -21,7 +21,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.Ignore;
 
 import org.sagebionetworks.client.SynapseAdminClient;
 import org.sagebionetworks.client.SynapseAdminClientImpl;
@@ -784,7 +783,6 @@ public class IT520SynapseJavaClientEvaluationTest {
 		assertFalse(uep2.getCanView());
 	}
 	
-	@Ignore
 	@Test
 	public void testAnnotationsQuery() throws SynapseException, InterruptedException, JSONObjectAdapterException {
 		// set up objects
