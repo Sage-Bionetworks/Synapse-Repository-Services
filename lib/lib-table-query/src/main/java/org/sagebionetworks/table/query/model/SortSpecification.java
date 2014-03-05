1f1baedcbce096f549a311adf6367a0354de3021
john-hill <john.hill@sagebase.org>
2014-03-05 09:35:29 -0800

    check

--- lib/lib-table-query/src/main/java/org/sagebionetworks/table/query/model/SortSpecification.java
+++ lib/lib-table-query/src/main/java/org/sagebionetworks/table/query/model/SortSpecification.java
@@ -1,6 +1,5 @@
 package org.sagebionetworks.table.query.model;
 
-import org.sagebionetworks.table.query.OrderingSpecification;
 /**
  * This matches &ltsort specification&gt   in: <a href="http://savage.net.au/SQL/sql-92.bnf">SQL-92</a>
  */
