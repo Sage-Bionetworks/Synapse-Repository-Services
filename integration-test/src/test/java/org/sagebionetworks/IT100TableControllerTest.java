96510487f5ba2c83555f6d306e483d38d4999ef4
john-hill <john.hill@sagebase.org>
2014-03-11 17:37:05 -0700

    java client

--- integration-test/src/test/java/org/sagebionetworks/IT100TableControllerTest.java
+++ integration-test/src/test/java/org/sagebionetworks/IT100TableControllerTest.java
@@ -13,6 +13,7 @@ import javax.servlet.ServletException;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -21,6 +22,7 @@ import org.sagebionetworks.client.SynapseAdminClientImpl;
 import org.sagebionetworks.client.SynapseClient;
 import org.sagebionetworks.client.SynapseClientImpl;
 import org.sagebionetworks.client.exceptions.SynapseException;
+import org.sagebionetworks.client.exceptions.SynapseTableUnavilableException;
 import org.sagebionetworks.repo.model.Entity;
 import org.sagebionetworks.repo.model.Project;
 import org.sagebionetworks.repo.model.dbo.dao.table.TableModelUtils;
@@ -47,8 +49,12 @@ public class IT100TableControllerTest {
 	private List<Entity> entitiesToDelete;
 	private List<TableEntity> tablesToDelete;
 	
+	private static long MAX_QUERY_TIMEOUT_MS = 1000*60*5;
+	
 	@BeforeClass 
 	public static void beforeClass() throws Exception {
+		// Only run this test if the table feature is enabled.
+		Assume.assumeTrue(new StackConfiguration().getTableEnabled());
 		// Create a user
 		adminSynapse = new SynapseAdminClientImpl();
 		SynapseClientHelper.setEndpoints(adminSynapse);
@@ -100,7 +106,7 @@ public class IT100TableControllerTest {
 	}
 	
 	@Test
-	public void testCreateTableEntity() throws SynapseException{
+	public void testCreateTableEntity() throws SynapseException, InterruptedException{
 		// Create a few columns to add to a table entity
 		ColumnModel one = new ColumnModel();
 		one.setName("one");
@@ -152,8 +158,61 @@ public class IT100TableControllerTest {
 		assertEquals(2, results.getRows().size());
 		assertEquals(table.getId(), results.getTableId());
 		assertEquals(TableModelUtils.getHeaders(columns), results.getHeaders());
+		
+		// Now attempt to query for the table results
+		boolean isConsistent = true;
+		boolean countOnly = false;
+		RowSet queryResults = waitForQueryResults("select * from "+table.getId()+" limit 2", isConsistent, countOnly);
+		assertNotNull(queryResults);
+		assertNotNull(queryResults.getEtag());
+		assertEquals(table.getId(), queryResults.getEtag());
+		assertNotNull(queryResults.getRows());
+		assertEquals(2, queryResults.getRows().size());
+		// Now use these results to update the table. By setting the row IDs to null, they should be treated as additions
+		for(Row row: queryResults.getRows()){
+			row.setRowId(null);
+			row.setVersionNumber(null);
+		}
+		results = synapse.appendRowsToTable(queryResults);
+		assertNotNull(results);
+		assertNotNull(results.getRows());
+		// run the query again, but this time get the counts
+		countOnly = true;
+		queryResults = waitForQueryResults("select * from "+table.getId()+" limit 2", isConsistent, countOnly);
+		assertNotNull(queryResults);
+		assertNotNull(queryResults.getEtag());
+		assertEquals(table.getId(), queryResults.getEtag());
+		assertNotNull(queryResults.getRows());
+		assertEquals(1, queryResults.getRows().size());
+		Row onlyRow = queryResults.getRows().get(0);
+		assertNotNull(onlyRow.getValues());
+		assertEquals(1, onlyRow.getValues());
+		assertEquals("There should be 4 rows in this table", "4", onlyRow.getValues().get(0));
 	}
 
+	/**
+	 * Wait for a consistent query results.
+	 * 
+	 * @param sql
+	 * @return
+	 * @throws InterruptedException
+	 * @throws SynapseException
+	 */
+	public RowSet waitForQueryResults(String sql, boolean isConsistent, boolean countOnly) throws InterruptedException, SynapseException{
+		long start = System.currentTimeMillis();
+		while(true){
+			try {
+				RowSet queryResutls = synapse.queryTableEntity(sql);
+				return queryResutls;
+			} catch (SynapseTableUnavilableException e) {
+				// The table is not ready yet
+				System.out.println("Waiting for table index to be available: "+e.getStatus());
+				Thread.sleep(2000);
+				assertTrue("Timed out waiting for query results for sql: "+sql,System.currentTimeMillis()-start < MAX_QUERY_TIMEOUT_MS);
+			}
+		}
+	}
+	
 	@Test
 	public void testListColumnModels() throws ServletException, Exception{
 		ColumnModel one = new ColumnModel();
