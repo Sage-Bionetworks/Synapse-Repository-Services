930e2f37f2bc2e310f4a1c19b25de3d6747a37e9
brucehoff <bruce.hoff@sagebase.org>
2014-03-14 10:01:07 -0700

    PLFM-2592

---
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/team/TeamConstants.java
@@ -0,0 +1,7 @@
+package org.sagebionetworks.repo.manager.team;
+
+public class TeamConstants {
+	public static final Long ADMINISTRATORS_TEAM_ID=2L;
+	public static final Long ACT_TEAM_ID=464532L;
+
+}
