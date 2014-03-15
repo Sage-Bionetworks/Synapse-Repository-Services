44e082fc22baa7122dc051ddfa00e874f49297b4
John Hill <john.hill@sagebase.org>
2014-03-14 18:23:16 -0700

    check

---
+++ services/repository/src/main/java/org/sagebionetworks/repo/web/controller/TableExamples.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.repo.web.controller;
+
+import org.sagebionetworks.repo.web.rest.doc.CSVGeneratedExample;
+
+/**
+ * The @CSVGeneratedExample annotation will trigger the creation of a static example javadoc page
+ * using the given csv file as input.  A link to the generated page can be added using the 
+ * full class name of this class.
+ */
+@CSVGeneratedExample(csvFileName="SQLSpecification.csv")
+public class TableExamples {
+
+}
