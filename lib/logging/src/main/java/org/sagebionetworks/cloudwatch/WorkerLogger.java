6a8943ff0d256779ba9c6ea8e1c90ceaf5dab9ac
brucehoff <bruce.hoff@sagebase.org>
2014-03-19 09:13:21 -0700

    PLFM-2590

--- lib/logging/src/main/java/org/sagebionetworks/cloudwatch/WorkerLogger.java
+++ lib/logging/src/main/java/org/sagebionetworks/cloudwatch/WorkerLogger.java
@@ -2,6 +2,12 @@ package org.sagebionetworks.cloudwatch;
 
 import org.sagebionetworks.repo.model.message.ChangeMessage;
 
+/**
+ * Interface for logging to CloudWatch failures of asynchronous workers
+ * 
+ * @author brucehoff
+ *
+ */
 public interface WorkerLogger {
 	public void setShouldProfile(boolean shouldProfile);
 	
