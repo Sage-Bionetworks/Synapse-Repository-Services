23d2393dba0025dd31815f1244521c1fa8c038c6
brucehoff <bruce.hoff@sagebase.org>
2014-03-28 10:33:53 -0700

    PLFM-2479

--- services/repository/src/main/java/org/sagebionetworks/repo/web/controller/CertifiedUserController.java
+++ services/repository/src/main/java/org/sagebionetworks/repo/web/controller/CertifiedUserController.java
@@ -37,6 +37,10 @@ public class CertifiedUserController {
 	@Autowired
 	ServiceProvider serviceProvider;
 	
+	/**
+	 * Get the test to become a Certified User.
+	 * @return
+	 */
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = UrlHelpers.CERTIFIED_USER_TEST, method = RequestMethod.GET)
 	public @ResponseBody
@@ -44,7 +48,14 @@ public class CertifiedUserController {
 		return serviceProvider.getCertifiedUserService().getCertificationQuestionnaire();
 	}
 
-
+	/**
+	 * Submit a response to the Certified User test.
+	 * 
+	 * @param userId
+	 * @param response
+	 * @return
+	 * @throws NotFoundException
+	 */
 	@ResponseStatus(HttpStatus.CREATED)
 	@RequestMapping(value = UrlHelpers.CERTIFIED_USER_TEST_RESPONSE, method = RequestMethod.POST)
 	public @ResponseBody
@@ -56,6 +67,16 @@ public class CertifiedUserController {
 				submitCertificationQuestionnaireResponse(userId, response);
 	}
 	
+	/**
+	 * Get the Certified User test responses in the system, optionally filtered by the one who took the test.
+	 * Note:  This service is available to Synapse administrators only.
+	 * @param userId
+	 * @param principalId
+	 * @param limit
+	 * @param offset
+	 * @return
+	 * @throws NotFoundException
+	 */
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = UrlHelpers.CERTIFIED_USER_TEST_RESPONSE, method = RequestMethod.GET)
 	public @ResponseBody 
@@ -68,6 +89,12 @@ public class CertifiedUserController {
 		return serviceProvider.getCertifiedUserService().getQuestionnaireResponses(userId, principalId, limit, offset);
 	}
 	
+	/**
+	 * Delete a test response.  Note:  This service is available to Synapse administrators only.
+	 * @param userId
+	 * @param responseId
+	 * @throws NotFoundException
+	 */
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = UrlHelpers.CERTIFIED_USER_TEST_RESPONSE_WITH_ID, method = RequestMethod.DELETE)
 	public void deleteQuestionnaireResponse(
@@ -77,6 +104,13 @@ public class CertifiedUserController {
 		serviceProvider.getCertifiedUserService().deleteQuestionnaireResponse(userId, responseId);	
 	}
 	
+	/**
+	 * Retrieve the Passing Record on the User Certification test for the given user.
+	 * @param userId
+	 * @param principalId
+	 * @return
+	 * @throws NotFoundException
+	 */
 	@ResponseStatus(HttpStatus.OK)
 	@RequestMapping(value = UrlHelpers.CERTIFIED_USER_PASSING_RECORD_WITH_ID, method = RequestMethod.GET)
 	public PassingRecord getPassingRecord(
