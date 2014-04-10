8eabdf374df4d51258d2734cca7b0144a752ccff
brucehoff <bruce.hoff@sagebase.org>
2014-04-09 20:02:33 -0700

    PLFM-2479

--- services/repository/src/main/java/org/sagebionetworks/repo/web/controller/CertifiedUserController.java
+++ services/repository/src/main/java/org/sagebionetworks/repo/web/controller/CertifiedUserController.java
@@ -33,6 +33,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  */
 @ControllerInfo(displayName="Team Services", path="repo/v1")
 @Controller
+@RequestMapping(UrlHelpers.REPO_PATH)
 public class CertifiedUserController {
 	@Autowired
 	ServiceProvider serviceProvider;
