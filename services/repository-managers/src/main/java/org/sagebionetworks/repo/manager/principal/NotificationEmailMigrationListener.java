63be5f5c4b8732445ae522181c12d78a6126016a
brucehoff <bruce.hoff@sagebase.org>
2014-05-27 15:45:04 -0700

    PLFM-2792

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/principal/NotificationEmailMigrationListener.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/principal/NotificationEmailMigrationListener.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.sagebionetworks.repo.manager.migration.MigrationTypeListener;
 import org.sagebionetworks.repo.model.dao.NotificationEmailDAO;
 import org.sagebionetworks.repo.model.dbo.DatabaseObject;
+import org.sagebionetworks.repo.model.dbo.principal.DBOPrincipalAlias;
 import org.sagebionetworks.repo.model.migration.MigrationType;
+import org.sagebionetworks.repo.model.principal.AliasEnum;
 import org.sagebionetworks.repo.model.principal.AliasType;
 import org.sagebionetworks.repo.model.principal.PrincipalAlias;
 import org.sagebionetworks.repo.web.NotFoundException;
@@ -22,13 +24,18 @@ public class NotificationEmailMigrationListener implements
 			MigrationType type, List<D> delta) {
 		if (!type.equals(MigrationType.PRINCIPAL_ALIAS)) return;
 		for (D dbo : delta) {
-			if (!(dbo instanceof PrincipalAlias)) continue;
-			PrincipalAlias pa = (PrincipalAlias)dbo;
-			if (!pa.getType().equals(AliasType.USER_EMAIL)) continue;
+			if (!(dbo instanceof DBOPrincipalAlias)) continue;
+			DBOPrincipalAlias pa = (DBOPrincipalAlias)dbo;
+			if (!pa.getAliasType().equals(AliasEnum.USER_EMAIL)) continue;
 			try {
 				notificationEmailDao.getNotificationEmailForPrincipal(pa.getPrincipalId());
 			} catch (NotFoundException e) {
-				notificationEmailDao.create(pa);
+				PrincipalAlias dto = new PrincipalAlias();
+				dto.setAlias(pa.getAliasDisplay());
+				dto.setAliasId(pa.getId());
+				dto.setPrincipalId(pa.getPrincipalId());
+				dto.setType(AliasType.USER_EMAIL);
+				notificationEmailDao.create(dto);
 			}
 		}
 	}
