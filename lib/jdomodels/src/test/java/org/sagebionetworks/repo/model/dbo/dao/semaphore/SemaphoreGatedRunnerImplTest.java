71f611b2b8f4ece72122bc47b9e601b61b730f29
Marcel <marcel.blonk@sagebase.org>
2014-05-27 15:10:57 -0700

    lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/dao/semaphore/SemaphoreGatedRunnerImplTest.java

--- lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/dao/semaphore/SemaphoreGatedRunnerImplTest.java
+++ lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/dao/semaphore/SemaphoreGatedRunnerImplTest.java
@@ -114,7 +114,6 @@ public class SemaphoreGatedRunnerImplTest {
 		semaphoreGatedRunner.setSemaphoreKey(key);
 	}
 	
-	@Ignore // See PLFM-2796
 	@Test
 	public void testDuplicateKey(){
 		SemaphoreGatedRunnerImpl gateOne = new SemaphoreGatedRunnerImpl();
