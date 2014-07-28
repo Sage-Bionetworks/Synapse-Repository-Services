d5caf858ffff26580f1cb0b9ae8aea0d0ce8ad97
toddis <toddis@uw.edu>
2014-07-28 14:56:50 -0700

    parent in trash can exception.

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/trash/ParentInTrashCanException.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/trash/ParentInTrashCanException.java
@@ -3,7 +3,8 @@ package org.sagebionetworks.repo.manager.trash;
 public class ParentInTrashCanException extends RuntimeException {
 
 	private static final long serialVersionUID = -4218320662080445999L;
-
+	
+	//
 	public ParentInTrashCanException(String message) {
 		super(message);
 	}
