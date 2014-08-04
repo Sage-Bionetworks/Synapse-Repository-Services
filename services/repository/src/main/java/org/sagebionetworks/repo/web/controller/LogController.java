c9d1e01a9aabd3f2e05fe3d2447e62c35f626766
Marcel <marcel.blonk@sagebase.org>
2014-08-04 15:58:51 -0700

    log user agent and client defined label

--- services/repository/src/main/java/org/sagebionetworks/repo/web/controller/LogController.java
+++ services/repository/src/main/java/org/sagebionetworks/repo/web/controller/LogController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,7 +43,7 @@ public class LogController extends BaseController {
 	@RequestMapping(value = UrlHelpers.LOG, method = RequestMethod.POST)
 	public @ResponseBody
 	void log(@RequestParam(value = AuthorizationConstants.USER_ID_PARAM) Long userId, @RequestBody LogEntry logEntry,
-			HttpServletRequest request) {
-		serviceProvider.getLogService().log(logEntry);
+			HttpServletRequest request, @RequestHeader("User-Agent") String userAgent) {
+		serviceProvider.getLogService().log(logEntry, userAgent);
 	}
 }
--- services/repository/src/main/java/org/sagebionetworks/repo/web/service/LogServiceImpl.java
+++ services/repository/src/main/java/org/sagebionetworks/repo/web/service/LogServiceImpl.java
@@ -2,54 +2,57 @@ package org.sagebionetworks.repo.web.service;
 
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.Date;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.sagebionetworks.cloudwatch.Consumer;
+import org.sagebionetworks.cloudwatch.ProfileData;
 import org.sagebionetworks.repo.model.LogEntry;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 
 /**
  * Basic implementation.
  * 
  * @author John
- *
+ * 
  */
 public class LogServiceImpl implements LogService {
 
 	private static final Logger log = LogManager.getLogger(LogService.class);
 
+	@Autowired
+	Consumer consumer;
+
 	@Override
-	public void log(final LogEntry logEntry) {
-		Level level = Level.ERROR;
-		switch (logEntry.getLevel()) {
-		case INFO:
-			level = Level.INFO;
-			break;
-		case WARN:
-			level = Level.WARN;
-			break;
-		case ERROR:
-			level = Level.ERROR;
-			break;
-		}
-		Throwable t = new Throwable(logEntry.getMessage()) {
-			private static final long serialVersionUID = 1L;
+	public void log(final LogEntry logEntry, String userAgent) {
+		log.error(logEntry.getLabel() + " - " + userAgent + ": " + logEntry.getMessage()
+				+ (logEntry.getStacktrace() == null ? "" : (System.lineSeparator() + logEntry.getStacktrace())));
 
-			@Override
-			public void printStackTrace() {
-				printStackTrace(System.err);
-			}
+		// log twice, once with just the label
+		ProfileData logEvent = new ProfileData();
+		logEvent.setNamespace(LogService.class.getName());
+		logEvent.setName("error");
+		logEvent.setValue(1.0);
+		logEvent.setUnit("Count");
+		logEvent.setTimestamp(new Date());
+		logEvent.setDimension(Collections.singletonMap("Label", logEntry.getLabel()));
+		consumer.addProfileData(logEvent);
 
-			@Override
-			public void printStackTrace(PrintStream s) {
-				s.print(logEntry.getStacktrace());
-			}
-
-			@Override
-			public void printStackTrace(PrintWriter s) {
-				s.print(logEntry.getStacktrace());
-			}
-		};
-		log.log(level, logEntry.getMessage(), t);
+		// once with the label and the user agent
+		logEvent = new ProfileData();
+		logEvent.setNamespace(LogService.class.getName());
+		logEvent.setName("error");
+		logEvent.setValue(1.0);
+		logEvent.setUnit("Count");
+		logEvent.setTimestamp(new Date());
+		logEvent.setDimension(ImmutableMap.<String, String> builder().put("Label", logEntry.getLabel()).put("UserAgent", userAgent).build());
+		consumer.addProfileData(logEvent);
 	}
 }
