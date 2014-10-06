721eb70d430331439d16f1b15fc9c9f8b905e396
brucehoff <bruce.hoff@sagebase.org>
2014-10-06 12:40:19 -0700

    PLFM-3027

---
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/AuthorizationManagerUtil.java
@@ -0,0 +1,22 @@
+package org.sagebionetworks.repo.manager;
+
+import org.sagebionetworks.repo.model.UnauthorizedException;
+import org.sagebionetworks.util.Pair;
+
+public class AuthorizationManagerUtil {
+
+	public static final Pair<Boolean,String> AUTHORIZED = new Pair<Boolean,String>(true, "");
+
+	public AuthorizationManagerUtil() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void checkAuthorizationAndThrowException(Pair<Boolean,String> auth) throws UnauthorizedException {
+		if (!auth.getFirst()) throw new UnauthorizedException(auth.getSecond());
+	}
+
+	public static Pair<Boolean,String> accessDenied(String reason) {
+		return new Pair<Boolean,String>(false, reason);
+	}
+
+}
