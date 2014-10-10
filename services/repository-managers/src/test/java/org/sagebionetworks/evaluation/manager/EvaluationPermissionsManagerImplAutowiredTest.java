19a73600eed02c4b433f3becd765a5f3468329be
brucehoff <bruce.hoff@sagebase.org>
2014-10-09 19:20:51 -0700

    PLFM-2682

--- services/repository-managers/src/test/java/org/sagebionetworks/evaluation/manager/EvaluationPermissionsManagerImplAutowiredTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/evaluation/manager/EvaluationPermissionsManagerImplAutowiredTest.java
@@ -87,6 +87,7 @@ public class EvaluationPermissionsManagerImplAutowiredTest {
 		user.setEmail(UUID.randomUUID().toString() + "@test.com");
 		user.setUserName(UUID.randomUUID().toString());
 		userInfo = userManager.getUserInfo(userManager.createUser(user));
+		userInfo.getGroups().add(BOOTSTRAP_PRINCIPAL.CERTIFIED_USERS.getPrincipalId());
 		adminUserInfo = userManager.getUserInfo(BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId());
 
 		aclsToDelete = new ArrayList<String>();
