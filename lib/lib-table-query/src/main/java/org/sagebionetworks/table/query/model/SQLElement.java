68cff2a65bdd93056d839a78a866dc328d9c034b
Marcel <marcel.blonk@sagebase.org>
2014-10-27 10:22:11 -0700

    Minor fixes from code review and make test more robust

--- lib/lib-table-query/src/main/java/org/sagebionetworks/table/query/model/SQLElement.java
+++ lib/lib-table-query/src/main/java/org/sagebionetworks/table/query/model/SQLElement.java
@@ -10,18 +10,59 @@ package org.sagebionetworks.table.query.model;
 public abstract class SQLElement {
 
 	public interface ColumnConvertor {
+		/**
+		 * Convert table name from sql to actual table name in index
+		 * 
+		 * @param tableName
+		 * @param builder
+		 */
 		void convertTableName(String tableName, StringBuilder builder);
 
+		/**
+		 * Convert column name from sql to actual column name in index table
+		 * 
+		 * @param columnReference
+		 * @param builder
+		 */
 		void convertColumn(ColumnReference columnReference, StringBuilder builder);
 
+		/**
+		 * Set the lhs column if valid, so the rhs can know that type to convert to. Always set back to null when lhs
+		 * goes out of scope
+		 * 
+		 * @param columnReferenceLHS
+		 */
 		void setLHSColumn(ColumnReference columnReferenceLHS);
 
+		/**
+		 * New AS column alias encountered. Call this to notify convertor that this new name now exists
+		 * 
+		 * @param columnName
+		 */
 		void addAsColumn(ColumnName columnName);
 
+		/**
+		 * parameterize a number
+		 * 
+		 * @param param
+		 * @param builder
+		 */
 		void convertParam(Number param, StringBuilder builder);
 
+		/**
+		 * parameterize a param that is known to be a number
+		 * 
+		 * @param param
+		 * @param builder
+		 */
 		void convertNumberParam(String param, StringBuilder builder);
 
+		/**
+		 * parameterize a string and convert if lhs is known
+		 * 
+		 * @param signedNumericLiteral
+		 * @param builder
+		 */
 		void convertParam(String signedNumericLiteral, StringBuilder builder);
 	}
 
