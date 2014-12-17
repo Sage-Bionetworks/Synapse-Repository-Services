c6e767653f795dcd06fff8f976c4ea705410a083
brucehoff <bruce.hoff@sagebase.org>
2014-12-16 16:27:07 -0800

    PLFM-2920

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/principal/PrincipalManagerImpl.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/principal/PrincipalManagerImpl.java
@@ -247,8 +247,8 @@ public class PrincipalManagerImpl implements PrincipalManager {
 			}
 			fieldValues.put(EmailUtils.TEMPLATE_KEY_ORIGIN_CLIENT, domainString);
 			fieldValues.put(EmailUtils.TEMPLATE_KEY_WEB_LINK, urlString);
-			String messageBody = EmailUtils.readMailTemplate("message/CreateAccountTemplate.txt", fieldValues);
-			SendEmailRequest sendEmailRequest = EmailUtils.createEmailRequest(user.getEmail(), subject, messageBody, false, null);
+			String messageBody = EmailUtils.readMailTemplate("message/CreateAccountTemplate.html", fieldValues);
+			SendEmailRequest sendEmailRequest = EmailUtils.createEmailRequest(user.getEmail(), subject, messageBody, true, null);
 			sesClient.sendEmail(sendEmailRequest);
 		} else {
 			throw new IllegalArgumentException("Unexpected Domain: "+domain);
@@ -383,8 +383,8 @@ public class PrincipalManagerImpl implements PrincipalManager {
 			fieldValues.put(EmailUtils.TEMPLATE_KEY_EMAIL, email.getEmail());
 			fieldValues.put(EmailUtils.TEMPLATE_KEY_ORIGIN_CLIENT, domain.name());
 			fieldValues.put(EmailUtils.TEMPLATE_KEY_USERNAME, principalAliasDAO.getUserName(userInfo.getId()));
-			String messageBody = EmailUtils.readMailTemplate("message/AdditionalEmailTemplate.txt", fieldValues);
-			SendEmailRequest sendEmailRequest = EmailUtils.createEmailRequest(email.getEmail(), subject, messageBody, false, null);
+			String messageBody = EmailUtils.readMailTemplate("message/AdditionalEmailTemplate.html", fieldValues);
+			SendEmailRequest sendEmailRequest = EmailUtils.createEmailRequest(email.getEmail(), subject, messageBody, true, null);
 			sesClient.sendEmail(sendEmailRequest);
 		} else {
 			throw new IllegalArgumentException("Unexpected Domain: "+domain);
