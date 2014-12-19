18d753f429f93cecc44cd8cb87efc3899c9bbe38
Kimyen <kimyen0816@gmail.com>
2014-12-18 21:06:23 -0800

    add test for ResourceAccessRecordDAOImpl

--- lib/lib-audit/src/test/java/org/sagebionetworks/audit/AclRecordDAOImplTest.java
+++ lib/lib-audit/src/test/java/org/sagebionetworks/audit/AclRecordDAOImplTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import org.junit.Before;
 import org.junit.Test;
---
+++ lib/lib-audit/src/test/java/org/sagebionetworks/audit/ResourceAccessRecordDAOImplTest.java
@@ -0,0 +1,61 @@
+package org.sagebionetworks.audit;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sagebionetworks.audit.dao.ResourceAccessRecordDAO;
+import org.sagebionetworks.repo.model.ACCESS_TYPE;
+import org.sagebionetworks.repo.model.audit.ResourceAccessRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:audit-dao.spb.xml" })
+public class ResourceAccessRecordDAOImplTest {
+
+	@Autowired
+	private ResourceAccessRecordDAO resourceAccessRecordDao;
+	
+	@Autowired
+	private AmazonS3Client s3Client;
+	
+	private String BUCKET_NAME = "dev.resource.access.record.sagebase.org";
+	
+	@Before
+	public void before(){
+		assertNotNull(resourceAccessRecordDao);
+		assertNotNull(s3Client);
+	}
+	
+	@Test
+	public void test() throws IOException{
+		List<ResourceAccessRecord> records = createResourceAccessRecordList(5);
+		for (ResourceAccessRecord record : records) {
+			String fileName = resourceAccessRecordDao.write(record);
+			assertNotNull(s3Client.getObject(BUCKET_NAME, fileName));
+		}
+ 		
+	}
+
+	private List<ResourceAccessRecord> createResourceAccessRecordList(int numberOfRecords) {
+		List<ResourceAccessRecord> list = new ArrayList<ResourceAccessRecord>();
+		for (int i = 0; i < numberOfRecords; i++) {
+			ResourceAccessRecord newRecord = new ResourceAccessRecord();
+			newRecord.setAccessType(ACCESS_TYPE.CREATE);
+			newRecord.setChangeNumber(-1L);
+			newRecord.setPrincipalId(-1L);
+
+			list.add(newRecord);
+		}
+		return list;
+	}
+}
