721b29e740388d9e7bb9e79ee38f09f2e79744b0
john-hill <john.hill@sagebase.org>
2015-01-05 13:35:58 -0800

    alt

--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/v2/dao/V2DBOWikiPageDaoImpl.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/v2/dao/V2DBOWikiPageDaoImpl.java
@@ -433,7 +433,7 @@ public class V2DBOWikiPageDaoImpl implements V2WikiPageDao {
 		// Create the root owner entry
 		V2DBOWikiOwner ownerEntry = new V2DBOWikiOwner();
 		ownerEntry.setOwnerId(new Long(ownerId));
-		ownerEntry.setOwnerTypeEnum(ownerType);
+		ownerEntry.setOwnerType(ownerType);
 		ownerEntry.setRootWikiId(rootWikiId);
 		ownerEntry.setEtag(UUID.randomUUID().toString());
 		
