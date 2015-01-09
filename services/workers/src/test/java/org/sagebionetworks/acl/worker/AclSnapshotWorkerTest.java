7186e52fc331c3262a7d0e4a278c487a146245ba
Kimyen <kimyen0816@gmail.com>
2015-01-08 19:24:10 -0800

    organize import

--- services/workers/src/test/java/org/sagebionetworks/acl/worker/AclSnapshotWorkerTest.java
+++ services/workers/src/test/java/org/sagebionetworks/acl/worker/AclSnapshotWorkerTest.java
@@ -2,7 +2,6 @@ package org.sagebionetworks.acl.worker;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
