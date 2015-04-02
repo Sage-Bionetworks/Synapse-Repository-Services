2b9b3181567bbca8cc552d2dd47b0241318dfbde
john-hill <john.hill@sagebase.org>
2015-04-02 14:43:54 -0700

    dao ready

--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/principal/PrincipalPrefixDAO.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/principal/PrincipalPrefixDAO.java
@@ -3,67 +3,74 @@ package org.sagebionetworks.repo.model.dbo.principal;
 import java.util.List;
 
 /**
- * Abstraction for a Dao used to lookup principals using a prefix.
+ * Abstraction for a DAO used to lookup principals using a prefix.
+ * 
  * @author John
- *
+ * 
  */
 public interface PrincipalPrefixDAO {
-	
 
 	/**
-	 * Add a single principal alias for a given principal, for example, a team name or a username.
+	 * Add a single principal alias for a given principal, for example, a team
+	 * name or a username.
 	 * 
 	 * @param alias
-	 * @param principalId
+	 * @param principalId The principal ID of the user or team.
 	 */
-	public boolean addPrincipalAlias(String alias, Long principalId);
-	
+	public void addPrincipalAlias(String alias, Long principalId);
+
 	/**
-	 * Set the first and last name for a principal.
-	 * @param firstName
-	 * @param lastName
-	 * @param principalId
+	 * Set the first and last name for a principal. A concatenation of both
+	 * first-last and last-first will be added to the table.
+	 * 
+	 * @param firstName user's first name
+	 * @param lastName user's last name
+	 * @param principalId user's principal ID.
 	 */
-	public boolean addPrincipalName(String firstName, String lastName, Long principalId);
-	
+	public void addPrincipalName(String firstName, String lastName,
+			Long principalId);
+
 	/**
 	 * Clear all data for a principal.
 	 * 
-	 * @param principalId
+	 * @param principalId The principal ID of the user or team.
 	 */
 	public void clearPrincipal(Long principalId);
-	
+
 	/**
-	 * List a single page of user that match a given prefix.
+	 * List a single page of users or teams that match a given prefix.
+	 * 
+	 * @param prefix Prefix to filter principals by.
+	 * @param limit Pagination parameter.
+	 * @param offset Pagination parameter.
+	 * @return List of principal IDs that match the query ordered alphabetically.
+	 */
+	List<Long> listPrincipalsForPrefix(String prefix, Long limit, Long offset);
+
+	/**
+	 * Count the number of users and teams that match the given prefix.
 	 * 
 	 * @param prefix
-	 * @param limit
-	 * @param offset
 	 * @return
 	 */
-	List<Long> listUsersForPrefix(String prefix, Long limit, Long offset);
-	
+	Long countPrincipalsForPrefix(String prefix);
+
 	/**
-	 * Count the nubmer of users that match this prefix
-	 * @param prefix
-	 * @return
-	 */
-	Long countUsersForPrefix(String prefix);
-	
-	/**
-	 * List all team members with a given prefix for a given team name.
+	 * For a given team, list all members that share the given prefix.
 	 * 
-	 * @param prefix
-	 * @param teamId
-	 * @param limit
-	 * @param offset
-	 * @return
+	 * @param prefix Prefix to filter by.
+	 * @param teamId The results will only include members of this team.
+	 * @param limit Pagination parameter.
+	 * @param offset Pagination parameter.
+	 * @return List of principal IDs that match the query ordered alphabetically.
 	 */
-	List<Long> listTeamMembersForPrefix(String prefix, Long teamId, Long limit, Long offset);
-	
+	List<Long> listTeamMembersForPrefix(String prefix, Long teamId, Long limit,
+			Long offset);
+
 	/**
-	 * Count the number of users that have the given prefix and belong to the given team.
-	 * @param prefix
+	 * For a given team, count all members that share the given prefix.
+	 * 
+	 * @param prefix Prefix to filter by.
 	 * @param teamId
 	 * @return
 	 */
@@ -73,6 +80,5 @@ public interface PrincipalPrefixDAO {
 	 * Delete all data in the table.
 	 */
 	public void truncateTable();
-
 
 }
