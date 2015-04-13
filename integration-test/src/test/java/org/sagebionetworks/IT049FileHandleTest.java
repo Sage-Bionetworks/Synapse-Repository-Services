f765d33a94edeb32bc63b858b96bddd894d4feed
Marcel <marcel.blonk@sagebase.org>
2015-04-13 12:49:47 -0700

    forgotten import again

--- integration-test/src/test/java/org/sagebionetworks/IT049FileHandleTest.java
+++ integration-test/src/test/java/org/sagebionetworks/IT049FileHandleTest.java
@@ -1,6 +1,7 @@
 package org.sagebionetworks;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
