6bc58265587af1a9155a33fdbe474733c3c7fa0a
bhoff <bruce.hoff@sagebase.org>
2015-05-07 10:48:17 -0700

    PLFM-3363

---
+++ lib/lib-utils/src/test/java/org/sagebionetworks/util/JSONEntitySample.java
@@ -0,0 +1,124 @@
+package org.sagebionetworks.util;
+
+import org.sagebionetworks.schema.ObjectValidator;
+import org.sagebionetworks.schema.adapter.JSONEntity;
+import org.sagebionetworks.schema.adapter.JSONObjectAdapter;
+import org.sagebionetworks.schema.adapter.JSONObjectAdapterException;
+
+public class JSONEntitySample implements JSONEntity {
+
+	public JSONEntitySample() {}
+
+	public String getHmac() {
+		return hmac;
+	}
+	public void setHmac(String hmac) {
+		this.hmac = hmac;
+	}
+	public String getStringField() {
+		return stringField;
+	}
+	public void setStringField(String stringField) {
+		this.stringField = stringField;
+	}
+	private String hmac;
+	private String stringField;
+
+    public final static String EFFECTIVE_SCHEMA = "{\"id\":\"org.sagebionetworks.repo.util.JSONEntitySample\",\"title\":\"JSONEntitySample\",\"properties\":{\"stringField\":{\"description\":\"User's last name\",\"type\":\"string\"},\"hmac\":{\"description\":\"The hash message authentication code for the message.\",\"type\":\"string\"},\"firstName\":{\"description\":\"User's first name\",\"type\":\"string\"}},\"type\":\"object\"}";
+
+    public String getJSONSchema() {
+		return EFFECTIVE_SCHEMA;
+	}
+
+	/**
+	 * @see JSONEntity#initializeFromJSONObject(JSONObjectAdapter)
+	 * @see JSONEntity#writeToJSONObject(JSONObjectAdapter)
+	 * 
+	 * @param adapter
+	 * @throws JSONObjectAdapterException
+	 */
+	@Override
+	public JSONObjectAdapter initializeFromJSONObject(JSONObjectAdapter adapter)
+			throws JSONObjectAdapterException
+	{
+		if (adapter == null) {
+			throw new IllegalArgumentException("org.sagebionetworks.schema.adapter.JSONObjectAdapter cannot be null");
+		}
+		ObjectValidator.validateEntity(JSONEntitySample.EFFECTIVE_SCHEMA, adapter, JSONEntitySample.class);
+		if (!adapter.isNull("hmac")) {
+			hmac = adapter.getString("hmac");
+		} else {
+			hmac = null;
+		}
+		if (!adapter.isNull("stringField")) {
+			stringField = adapter.getString("stringField");
+		} else {
+			stringField = null;
+		}
+		return adapter;
+	}
+
+	/**
+	 * @see JSONEntity#initializeFromJSONObject(JSONObjectAdapter)
+	 * @see JSONEntity#writeToJSONObject(JSONObjectAdapter)
+	 * 
+	 * @param adapter
+	 * @throws JSONObjectAdapterException
+	 */
+	@Override
+	public JSONObjectAdapter writeToJSONObject(JSONObjectAdapter adapter)
+			throws JSONObjectAdapterException
+	{
+		if (adapter == null) {
+			throw new IllegalArgumentException("org.sagebionetworks.schema.adapter.JSONObjectAdapter cannot be null");
+		}
+		if (hmac!= null) {
+			adapter.put("hmac", hmac);
+		}
+		if (stringField!= null) {
+			adapter.put("stringField", stringField);
+		}
+		return adapter;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((hmac == null) ? 0 : hmac.hashCode());
+		result = prime * result
+				+ ((stringField == null) ? 0 : stringField.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		JSONEntitySample other = (JSONEntitySample) obj;
+		if (hmac == null) {
+			if (other.hmac != null)
+				return false;
+		} else if (!hmac.equals(other.hmac))
+			return false;
+		if (stringField == null) {
+			if (other.stringField != null)
+				return false;
+		} else if (!stringField.equals(other.stringField))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "JSONEntitySample [hmac=" + hmac + ", stringField="
+				+ stringField + "]";
+	}
+	
+	
+}
+
