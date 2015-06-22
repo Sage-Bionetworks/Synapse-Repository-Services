c1d8c6135db2dc330b4268fe5aa147853e34f9f4
john-hill <john.hill@sagebase.org>
2015-06-22 16:56:52 -0700

    unused code

--- lib/lib-worker/src/main/java/org/sagebionetworks/asynchronous/workers/timed/TimedWorker.java
+++
@@ -1,8 +0,0 @@
-package org.sagebionetworks.asynchronous.workers.timed;
-
-import org.sagebionetworks.util.ProgressCallback;
-
-public abstract class TimedWorker {
-
-	public abstract void run(ProgressCallback<Void> progressCallback) throws Exception;
-}
