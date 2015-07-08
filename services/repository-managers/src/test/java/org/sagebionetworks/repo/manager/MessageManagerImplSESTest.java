a32b32e6d5f6304113e84baecdd74b13a2437efb
bhoff <bruce.hoff@sagebase.org>
2015-07-08 14:33:51 -0700

    PLFM-3465

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/MessageManagerImplSESTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/MessageManagerImplSESTest.java
@@ -4,7 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
 import java.util.List;
