9abf73ccc5d20833b94e0bb3284bbc40f5bb26ff
Kimyen <kimyen0816@gmail.com>
2015-07-20 23:29:53 -0700

    project setting object record builder

---
+++ services/workers/src/main/java/org/sagebionetworks/object/snapshot/worker/utils/ProjectSettingObjectRecordBuilder.java
@@ -0,0 +1,42 @@
+package org.sagebionetworks.object.snapshot.worker.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.sagebionetworks.audit.utils.ObjectRecordBuilderUtils;
+import org.sagebionetworks.repo.model.ObjectType;
+import org.sagebionetworks.repo.model.ProjectSettingsDAO;
+import org.sagebionetworks.repo.model.audit.ObjectRecord;
+import org.sagebionetworks.repo.model.message.ChangeMessage;
+import org.sagebionetworks.repo.model.message.ChangeType;
+import org.sagebionetworks.repo.model.project.ProjectSetting;
+import org.sagebionetworks.repo.web.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProjectSettingObjectRecordBuilder implements ObjectRecordBuilder {
+	private static Logger log = LogManager.getLogger(ProjectSettingObjectRecordBuilder.class);
+
+	@Autowired
+	private ProjectSettingsDAO projectSettingsDao;
+
+	ProjectSettingObjectRecordBuilder() {}
+
+	// for test only
+	ProjectSettingObjectRecordBuilder(ProjectSettingsDAO projectSettingsDao) {
+		this.projectSettingsDao = projectSettingsDao;
+	}
+
+	@Override
+	public ObjectRecord build(ChangeMessage message) {
+		if (message.getObjectType() != ObjectType.PROJECT_SETTING || message.getChangeType() == ChangeType.DELETE) {
+			throw new IllegalArgumentException();
+		}
+		try {
+			ProjectSetting projectSetting = projectSettingsDao.get(message.getObjectId());
+			return ObjectRecordBuilderUtils.buildObjectRecord(projectSetting, message.getTimestamp().getTime());
+		} catch (NotFoundException e) {
+			log.error("Cannot find ProjectSetting for a " + message.getChangeType() + " message: " + message.toString()) ;
+			return null;
+		}
+	}
+
+}
