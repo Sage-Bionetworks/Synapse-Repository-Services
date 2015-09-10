ff8db9db334a72600b501fa01a1c972abca8db73
john-hill <john.hill@sagebase.org>
2015-09-10 13:53:41 -0700

    single thread pool

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/migration/TableFileAssociationMigrationListener.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/migration/TableFileAssociationMigrationListener.java
@@ -1,7 +1,11 @@
 package org.sagebionetworks.repo.manager.migration;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.sagebionetworks.repo.model.dbo.DatabaseObject;
 import org.sagebionetworks.repo.model.dbo.persistence.table.DBOTableRowChange;
 import org.sagebionetworks.repo.model.migration.MigrationType;
@@ -10,8 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class TableFileAssociationMigrationListener implements
 		MigrationTypeListener {
 	
+	private static final Logger log = LogManager
+			.getLogger(TableFileAssociationMigrationListener.class);
+	
 	@Autowired
 	TableFileMigration tableFileMigration;
+	
+	ScheduledExecutorService threadPool = Executors.newSingleThreadScheduledExecutor();
 
 	@Override
 	public <D extends DatabaseObject<?>> void afterCreateOrUpdate(
@@ -20,12 +29,22 @@ public class TableFileAssociationMigrationListener implements
 		if(MigrationType.TABLE_CHANGE == type){
 			for(D row: delta){
 				if(row instanceof DBOTableRowChange){
-					DBOTableRowChange change = (DBOTableRowChange) row;
-					tableFileMigration.attemptTableFileMigration(change);
+					final DBOTableRowChange change = (DBOTableRowChange) row;
+					// schedule the migration of this change in a separate thread.
+					threadPool.execute(new Runnable() {
+						@Override
+						public void run() {
+							try {
+								// Migrate this table.
+								tableFileMigration.attemptTableFileMigration(change);
+							} catch (Exception e) {
+								log.error("Outer exception: " + change, e);
+							}
+						}
+					});
 				}
 			}
 		}
-
 	}
 
 	@Override
