e88c13421f4299a2b6af613be55ab3c1d30da82a
Kimyen <kimyen0816@gmail.com>
2015-11-11 14:19:00 -0800

    add auto wired test

---
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/discussion/ForumManagerImplAutowiredTest.java
@@ -0,0 +1,77 @@
+package org.sagebionetworks.repo.manager.discussion;
+
+import static org.junit.Assert.*;
+
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sagebionetworks.repo.manager.NodeManager;
+import org.sagebionetworks.repo.manager.UserManager;
+import org.sagebionetworks.repo.model.DomainType;
+import org.sagebionetworks.repo.model.EntityType;
+import org.sagebionetworks.repo.model.Node;
+import org.sagebionetworks.repo.model.UserInfo;
+import org.sagebionetworks.repo.model.AuthorizationConstants.BOOTSTRAP_PRINCIPAL;
+import org.sagebionetworks.repo.model.auth.NewUser;
+import org.sagebionetworks.repo.model.dbo.persistence.DBOCredential;
+import org.sagebionetworks.repo.model.dbo.persistence.DBOTermsOfUseAgreement;
+import org.sagebionetworks.repo.model.discussion.Forum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:test-context.xml" })
+public class ForumManagerImplAutowiredTest {
+	@Autowired
+	public NodeManager nodeManager;
+	@Autowired
+	public UserManager userManager;
+	@Autowired
+	public ForumManager forumManager;
+
+	private UserInfo adminUserInfo;
+	private UserInfo userInfo;
+	private String projectId;
+
+	@Before
+	public void before() {
+		DBOTermsOfUseAgreement tou = new DBOTermsOfUseAgreement();
+		tou.setDomain(DomainType.SYNAPSE);
+		tou.setAgreesToTermsOfUse(Boolean.TRUE);
+
+		adminUserInfo = userManager.getUserInfo(BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId());
+		UserInfo.validateUserInfo(adminUserInfo);
+
+		DBOCredential cred = new DBOCredential();
+		cred.setSecretKey("");
+		NewUser nu = new NewUser();
+		nu.setEmail(UUID.randomUUID().toString() + "@test.com");
+		nu.setUserName(UUID.randomUUID().toString());
+		userInfo = userManager.createUser(adminUserInfo, nu, cred, tou);
+		userInfo.getGroups().add(BOOTSTRAP_PRINCIPAL.CERTIFIED_USERS.getPrincipalId());
+
+		Node newNode = new Node();
+		newNode.setName("project");
+		newNode.setNodeType(EntityType.project);
+		projectId = nodeManager.createNewNode(newNode, userInfo);
+	}
+
+	@After
+	public void cleanup() {
+		nodeManager.delete(adminUserInfo, projectId);
+		userManager.deletePrincipal(adminUserInfo, userInfo.getId());
+	}
+
+	@Test
+	public void testGetNonExistingForum() {
+		Forum dto = forumManager.getForumMetadata(userInfo, projectId);
+		assertNotNull(dto);
+		assertNotNull(dto.getId());
+		assertEquals(dto.getProjectId(), projectId);
+	}
+
+}
