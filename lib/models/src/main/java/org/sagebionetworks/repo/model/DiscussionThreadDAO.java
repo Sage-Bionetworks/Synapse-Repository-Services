9fbbefecc7ff8593a5edfb91610b352b2723416c
Kimyen <kimyen0816@gmail.com>
2015-11-17 16:29:29 -0800

    dao interface

--- lib/models/src/main/java/org/sagebionetworks/repo/model/DiscussionThreadDAO.java
+++ lib/models/src/main/java/org/sagebionetworks/repo/model/DiscussionThreadDAO.java
@@ -1,5 +1,7 @@
 package org.sagebionetworks.repo.model;
 
+import java.util.List;
+
 import org.sagebionetworks.reflection.model.PaginatedResults;
 import org.sagebionetworks.repo.model.discussion.DiscussionOrder;
 import org.sagebionetworks.repo.model.discussion.DiscussionThreadBundle;
@@ -16,15 +18,16 @@ public interface DiscussionThreadDAO {
 	 * @return
 	 */
 	public DiscussionThreadBundle createThread(String forumId, String title,
-			String messageUrl, Long userId);
+			String messageUrl, long userId);
 
 	/**
 	 * Get a discussion thread
 	 * 
 	 * @param threadId
+	 * @param userId
 	 * @return
 	 */
-	public DiscussionThreadBundle getThread(long threadId);
+	public DiscussionThreadBundle getThread(long threadId, long userId);
 
 	/**
 	 * Get the number of discussion thread in a given forum
@@ -42,10 +45,11 @@ public interface DiscussionThreadDAO {
 	 * @param order
 	 * @param limit
 	 * @param offset
+	 * @param userId
 	 * @return
 	 */
 	public PaginatedResults<DiscussionThreadBundle> getThreads(long forumId,
-			DiscussionOrder order, Integer limit, Integer offset);
+			DiscussionOrder order, Integer limit, Integer offset, long userId);
 
 	/**
 	 * Mark a discussion thread as deleted
@@ -69,4 +73,28 @@ public interface DiscussionThreadDAO {
 	 * @param title
 	 */
 	public DiscussionThreadBundle updateTitle(long threadId, String title);
+
+	/**
+	 * update number of views for the given thread
+	 * 
+	 * @param threadId
+	 * @param numberOfViews
+	 */
+	public void setNumberOfViews(long threadId, long numberOfViews);
+
+	/**
+	 * update number of replies for the given thread
+	 * 
+	 * @param threadId
+	 * @param numberOfReplies
+	 */
+	public void setNumberOfReplies(long threadId, long numberOfReplies);
+
+	/**
+	 * update active authors for the given thread
+	 * 
+	 * @param threadId
+	 * @param activeAuthors - the top 5 active authors
+	 */
+	public void setActiveAuthors(long threadId, List<Long> activeAuthors);
 }
