686c1eccc1eec445236cec53f1838cfaa90a7e05
Kimyen <kimyen0816@gmail.com>
2015-11-19 15:10:26 -0800

    remove unused import

--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/persistence/discussion/DBODiscussionThread.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/persistence/discussion/DBODiscussionThread.java
@@ -6,7 +6,6 @@ import java.sql.Blob;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import org.sagebionetworks.repo.model.dbo.FieldColumn;
