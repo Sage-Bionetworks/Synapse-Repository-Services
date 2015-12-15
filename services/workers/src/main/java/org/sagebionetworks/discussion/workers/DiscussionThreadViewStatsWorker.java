fc9bb5a030c6ce6644e2c3357fbc1379b1d8a072
Kimyen <kimyen0816@gmail.com>
2015-12-15 00:20:30 -0800

    thread stat workers

---
+++ services/workers/src/main/java/org/sagebionetworks/discussion/workers/DiscussionThreadViewStatsWorker.java
@@ -0,0 +1,33 @@
+package org.sagebionetworks.discussion.workers;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.sagebionetworks.common.util.progress.ProgressCallback;
+import org.sagebionetworks.common.util.progress.ProgressingRunner;
+import org.sagebionetworks.repo.model.dao.discussion.DiscussionThreadDAO;
+import org.sagebionetworks.repo.model.discussion.DiscussionThreadViewStat;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DiscussionThreadViewStatsWorker implements ProgressingRunner<Void>{
+
+	@Autowired
+	private DiscussionThreadDAO threadDao;
+
+	private final Logger logger = LogManager.getLogger(DiscussionThreadViewStatsWorker.class);
+	public static final Long LIMIT = 100L;
+
+	@Override
+	public void run(ProgressCallback<Void> progressCallback) throws Exception {
+		logger.trace("Updating thread view stats");
+		Long offset = 0L;
+		List<DiscussionThreadViewStat> viewStats = threadDao.getThreadViewStat(LIMIT, offset);
+		while (!viewStats.isEmpty()) {
+			threadDao.updateThreadViewStat(viewStats);
+			offset += LIMIT;
+			viewStats = threadDao.getThreadViewStat(LIMIT, offset);
+		}
+	}
+
+}
---
+++ services/workers/src/test/java/org/sagebionetworks/discussion/workers/DiscussionThreadReplyStatsWorkerTest.java
@@ -0,0 +1,65 @@
+package org.sagebionetworks.discussion.workers;
+
+import static org.sagebionetworks.discussion.workers.DiscussionThreadReplyStatsWorker.LIMIT;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.sagebionetworks.common.util.progress.ProgressCallback;
+import org.sagebionetworks.repo.model.dao.discussion.DiscussionReplyDAO;
+import org.sagebionetworks.repo.model.dao.discussion.DiscussionThreadDAO;
+import org.sagebionetworks.repo.model.discussion.DiscussionThreadReplyStat;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class DiscussionThreadReplyStatsWorkerTest {
+
+	@Mock
+	private DiscussionThreadDAO mockThreadDao;
+	@Mock
+	private DiscussionReplyDAO mockReplyDao;
+	@Mock
+	private ProgressCallback<Void> mockCallback;
+
+	private DiscussionThreadReplyStatsWorker worker;
+
+
+	@Before
+	public void before() {
+		MockitoAnnotations.initMocks(this);
+
+		worker = new DiscussionThreadReplyStatsWorker();
+		ReflectionTestUtils.setField(worker, "threadDao", mockThreadDao);
+		ReflectionTestUtils.setField(worker, "replyDao", mockReplyDao);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testNoThreadsHaveBeenViewed() throws Exception {
+		Mockito.when(mockReplyDao.getThreadReplyStat(Mockito.anyLong(), Mockito.anyLong()))
+				.thenReturn(new ArrayList<DiscussionThreadReplyStat>());
+		worker.run(mockCallback);
+		Mockito.verify(mockReplyDao).getThreadReplyStat(LIMIT, 0L);
+		Mockito.verify(mockReplyDao, Mockito.never()).getThreadReplyStat(LIMIT, LIMIT);
+		Mockito.verify(mockThreadDao, Mockito.never()).updateThreadReplyStat(Mockito.anyList());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testSomeThreadsHaveBeenViewed() throws Exception {
+		DiscussionThreadReplyStat stat = new DiscussionThreadReplyStat();
+		List<DiscussionThreadReplyStat> stats = new ArrayList<DiscussionThreadReplyStat>();
+		stats.add(stat);
+		Mockito.when(mockReplyDao.getThreadReplyStat(Mockito.anyLong(), Mockito.anyLong()))
+				.thenReturn(stats, new ArrayList<DiscussionThreadReplyStat>());
+		worker.run(mockCallback);
+		Mockito.verify(mockReplyDao).getThreadReplyStat(LIMIT, 0L);
+		Mockito.verify(mockReplyDao).getThreadReplyStat(LIMIT, LIMIT);
+		Mockito.verify(mockReplyDao, Mockito.never()).getThreadReplyStat(LIMIT, 2*LIMIT);
+		Mockito.verify(mockThreadDao).updateThreadReplyStat(stats);
+	}
+}
