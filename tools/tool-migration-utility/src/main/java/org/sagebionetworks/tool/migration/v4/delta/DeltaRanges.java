4b61b1f18a59d704b98b2edfe8c1cffb9ce3d6a4
xavier <xschildwachter@hotmail.com>
2016-01-08 09:15:27 -0800

    xfer

---
+++ tools/tool-migration-utility/src/main/java/org/sagebionetworks/tool/migration/v4/delta/DeltaRanges.java
@@ -0,0 +1,53 @@
+package org.sagebionetworks.tool.migration.v4.delta;
+
+import java.util.List;
+
+import org.sagebionetworks.repo.model.migration.MigrationType;
+
+/**
+ * Container for list of ranges where there is a difference between prod and staging
+ *
+ */
+public class DeltaRanges {
+	
+	private MigrationType migrationType;
+	private List<IdRange> insRanges;
+	private List<IdRange> updRanges;
+	private List<IdRange> delRanges;
+	
+	public DeltaRanges() {
+	}
+
+	public MigrationType getMigrationType() {
+		return migrationType;
+	}
+
+	public void setMigrationType(MigrationType migrationType) {
+		this.migrationType = migrationType;
+	}
+
+	public List<IdRange> getInsRanges() {
+		return insRanges;
+	}
+
+	public void setInsRanges(List<IdRange> insRanges) {
+		this.insRanges = insRanges;
+	}
+
+	public List<IdRange> getUpdRanges() {
+		return updRanges;
+	}
+
+	public void setUpdRanges(List<IdRange> updRanges) {
+		this.updRanges = updRanges;
+	}
+
+	public List<IdRange> getDelRanges() {
+		return delRanges;
+	}
+
+	public void setDelRanges(List<IdRange> delRanges) {
+		this.delRanges = delRanges;
+	}
+
+}
