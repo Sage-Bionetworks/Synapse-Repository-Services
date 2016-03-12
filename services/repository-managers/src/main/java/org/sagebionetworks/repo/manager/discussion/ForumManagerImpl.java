61f9b25cc6b598b094e19189c9a18d89d57a208c
Kimyen <kimyen0816@gmail.com>
2016-03-11 17:56:23 -0800

    keep creating forum for old project as user retrieve the forum

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/discussion/ForumManagerImpl.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/discussion/ForumManagerImpl.java
@@ -9,7 +9,7 @@ import org.sagebionetworks.repo.model.UserInfo;
 import org.sagebionetworks.repo.model.dao.discussion.ForumDAO;
 import org.sagebionetworks.repo.model.discussion.Forum;
 import org.sagebionetworks.repo.model.jdo.KeyFactory;
-import org.sagebionetworks.repo.transactions.WriteTransaction;
+import org.sagebionetworks.repo.transactions.WriteTransactionReadCommitted;
 import org.sagebionetworks.repo.web.NotFoundException;
 import org.sagebionetworks.util.ValidateArgument;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +22,7 @@ public class ForumManagerImpl implements ForumManager {
 	@Autowired
 	private AuthorizationManager authorizationManager;
 
-	@WriteTransaction
+	@WriteTransactionReadCommitted
 	@Override
 	public Forum createForum(UserInfo user, String projectId) {
 		validateProjectIdAndThrowException(projectId);
@@ -39,13 +39,18 @@ public class ForumManagerImpl implements ForumManager {
 		}
 	}
 
+	@WriteTransactionReadCommitted
 	@Override
 	public Forum getForumByProjectId(UserInfo user, String projectId) {
 		validateProjectIdAndThrowException(projectId);
 		UserInfo.validateUserInfo(user);
 		AuthorizationManagerUtil.checkAuthorizationAndThrowException(
 				authorizationManager.canAccess(user, projectId, ObjectType.ENTITY, ACCESS_TYPE.READ));
-		return forumDao.getForumByProjectId(projectId);
+		try {
+				return forumDao.getForumByProjectId(projectId);
+		} catch (NotFoundException e) {
+			return createForum(user, projectId);
+		}
 	}
 
 	@Override
--- services/repository/src/test/java/org/sagebionetworks/BeanTest.java
+++ services/repository/src/test/java/org/sagebionetworks/BeanTest.java
@@ -69,7 +69,7 @@ public class BeanTest implements ApplicationContextAware {
 	}
 
 	private static final List<String> readMethodPrefixes = Lists.newArrayList("check", "get");
-	private static final List<String> exceptions = Lists.newArrayList("checkSessionToken", "getSessionToken", "getEtagForUpdate");
+	private static final List<String> exceptions = Lists.newArrayList("checkSessionToken", "getSessionToken", "getEtagForUpdate", "getForumByProjectId");
 
 	@Test
 	public void testNoGetterWriteTransactions() {
