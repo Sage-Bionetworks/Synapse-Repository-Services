820f7cc377860aba855eed37050a2288fbed23e6
Kimyen <kimyen0816@gmail.com>
2016-03-23 14:34:05 -0700

    organize imports

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/message/ReplyMessageBuilderFactoryTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/message/ReplyMessageBuilderFactoryTest.java
@@ -1,9 +1,9 @@
 package org.sagebionetworks.repo.manager.message;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -13,7 +13,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.sagebionetworks.repo.model.EntityHeader;
 import org.sagebionetworks.repo.model.NodeDAO;
-import org.sagebionetworks.repo.model.ProjectHeader;
 import org.sagebionetworks.repo.model.dao.discussion.DiscussionReplyDAO;
 import org.sagebionetworks.repo.model.dao.discussion.DiscussionThreadDAO;
 import org.sagebionetworks.repo.model.discussion.DiscussionFilter;
