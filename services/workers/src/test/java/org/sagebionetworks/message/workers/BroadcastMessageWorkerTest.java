1b43ede77826997b93645d217c00a1ad032132f6
Kimyen <kimyen0816@gmail.com>
2016-03-23 08:51:51 -0700

    update test

--- services/workers/src/test/java/org/sagebionetworks/message/workers/BroadcastMessageWorkerTest.java
+++ services/workers/src/test/java/org/sagebionetworks/message/workers/BroadcastMessageWorkerTest.java
@@ -10,8 +10,10 @@ import org.sagebionetworks.common.util.progress.ProgressCallback;
 import org.sagebionetworks.repo.manager.UserManager;
 import org.sagebionetworks.repo.manager.message.BroadcastMessageManager;
 import org.sagebionetworks.repo.model.AuthorizationConstants.BOOTSTRAP_PRINCIPAL;
+import org.sagebionetworks.repo.model.ObjectType;
 import org.sagebionetworks.repo.model.UserInfo;
 import org.sagebionetworks.repo.model.message.ChangeMessage;
+import org.sagebionetworks.repo.model.message.ChangeType;
 import org.sagebionetworks.workers.util.aws.message.RecoverableMessageException;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -35,7 +37,9 @@ public class BroadcastMessageWorkerTest {
 
 	@Test
 	public void test() throws RecoverableMessageException, Exception {
-		ChangeMessage fakeMessage = null;
+		ChangeMessage fakeMessage = new ChangeMessage();
+		fakeMessage.setChangeType(ChangeType.CREATE);
+		fakeMessage.setObjectType(ObjectType.THREAD);
 		worker.run(mockCallback, fakeMessage);
 		verify(mockUserManager).getUserInfo(BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId());
 		verify(mockBroadcastManager).broadcastMessage(any(UserInfo.class), eq(mockCallback), eq(fakeMessage));
