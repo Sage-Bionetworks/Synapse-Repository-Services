5458819f3219bc1c429fef362b89d3540870379b
Kimyen <kimyen0816@gmail.com>
2016-07-05 18:17:51 -0700

    refactor model objects

---
+++ lib/models/src/main/java/org/sagebionetworks/repo/model/broadcast/UserNotificationInfo.java
@@ -0,0 +1,95 @@
+package org.sagebionetworks.repo.model.broadcast;
+
+/**
+ * The model object represents a single user and their synapse notification info.
+ */
+public class UserNotificationInfo {
+
+	private String userId;
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String notificationEmail;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((notificationEmail == null) ? 0 : notificationEmail.hashCode());
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserNotificationInfo other = (UserNotificationInfo) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (notificationEmail == null) {
+			if (other.notificationEmail != null)
+				return false;
+		} else if (!notificationEmail.equals(other.notificationEmail))
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "UserNotificationInfo [userId=" + userId + ", username=" + username + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", notificationEmail=" + notificationEmail + "]";
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getNotificationEmail() {
+		return notificationEmail;
+	}
+	public void setNotificationEmail(String notificationEmail) {
+		this.notificationEmail = notificationEmail;
+	}
+}
---
+++ lib/models/src/main/java/org/sagebionetworks/repo/model/dao/subscription/Subscriber.java
@@ -0,0 +1,109 @@
+package org.sagebionetworks.repo.model.dao.subscription;
+
+/**
+ * The model object represents a single subscriber to a subscription.
+ */
+public class Subscriber {
+
+	private String subscriptionId;
+	private String subscriberId;
+	private String username;
+	private String firstName;
+	private String lastname;
+	private String notificationEmail;
+	public String getSubscriptionId() {
+		return subscriptionId;
+	}
+	public void setSubscriptionId(String subscriptionId) {
+		this.subscriptionId = subscriptionId;
+	}
+	public String getSubscriberId() {
+		return subscriberId;
+	}
+	public void setSubscriberId(String subscriberId) {
+		this.subscriberId = subscriberId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getNotificationEmail() {
+		return notificationEmail;
+	}
+	public void setNotificationEmail(String notificationEmail) {
+		this.notificationEmail = notificationEmail;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + ((notificationEmail == null) ? 0 : notificationEmail.hashCode());
+		result = prime * result + ((subscriberId == null) ? 0 : subscriberId.hashCode());
+		result = prime * result + ((subscriptionId == null) ? 0 : subscriptionId.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Subscriber other = (Subscriber) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (notificationEmail == null) {
+			if (other.notificationEmail != null)
+				return false;
+		} else if (!notificationEmail.equals(other.notificationEmail))
+			return false;
+		if (subscriberId == null) {
+			if (other.subscriberId != null)
+				return false;
+		} else if (!subscriberId.equals(other.subscriberId))
+			return false;
+		if (subscriptionId == null) {
+			if (other.subscriptionId != null)
+				return false;
+		} else if (!subscriptionId.equals(other.subscriptionId))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Subscriber [subscriptionId=" + subscriptionId + ", subscriberId=" + subscriberId + ", username="
+				+ username + ", firstName=" + firstName + ", lastname=" + lastname + ", notificationEmail="
+				+ notificationEmail + "]";
+	}
+}
