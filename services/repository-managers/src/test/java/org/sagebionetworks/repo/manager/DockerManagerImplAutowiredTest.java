75ddbdead03973bdc244dace71a7f24c8bdd9cd5
bhoff <bruce.hoff@sagebase.org>
2016-07-21 05:25:56 -0700

    PLFM-3922

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/DockerManagerImplAutowiredTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/DockerManagerImplAutowiredTest.java
@@ -28,10 +28,10 @@ public class DockerManagerImplAutowiredTest {
 
 	private static final String SERVICE = "docker.synapse.org";
 	private static final String TYPE = "repository";
-	private String repositoryPath;
 	private static final String TAG = "lastest";
 	private static final String DIGEST = "sha256:10010101";
 
+	private String repositoryPath;
 	
 	@Autowired
 	private DockerManager dockerManager;
@@ -42,36 +42,30 @@ public class DockerManagerImplAutowiredTest {
 	@Autowired
 	private UserManager userManager;
 
-	private UserInfo userInfo;
+	private UserInfo adminUserInfo;
 	private String projectId;
 	
 	@Before
 	public void setUp() throws Exception {
-		NewUser user = new NewUser();
-		String username = UUID.randomUUID().toString();
-		user.setEmail(username + "@test.com");
-		user.setUserName(username);
-		userInfo = userManager.getUserInfo(userManager.createUser(user));
-		userInfo.getGroups().add(BOOTSTRAP_PRINCIPAL.CERTIFIED_USERS.getPrincipalId());
+		adminUserInfo = userManager.getUserInfo(BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId());
 
 		Project project = new Project();
 		project.setName("project" + RandomStringUtils.randomAlphanumeric(10));
-		projectId = entityManager.createEntity(userInfo, project, null);
+		projectId = entityManager.createEntity(adminUserInfo, project, null);
 		repositoryPath = projectId+"/path";
 	}
 	
 	@After
 	public void tearDown() throws Exception {
-		UserInfo adminUserInfo = userManager.getUserInfo(BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId());
 		entityManager.deleteEntity(adminUserInfo, projectId);
-		userManager.deletePrincipal(adminUserInfo, Long.parseLong(userInfo.getId().toString()));
+		userManager.deletePrincipal(adminUserInfo, Long.parseLong(adminUserInfo.getId().toString()));
 	}
 
 	@Test
 	public void testAuthorizeDockerAccess() {
 		// test to see if we can push to the project.  Answer should be yes!
 		String scope =TYPE+":"+repositoryPath+":push";
-		DockerAuthorizationToken token = dockerManager.authorizeDockerAccess(userInfo, SERVICE, scope);
+		DockerAuthorizationToken token = dockerManager.authorizeDockerAccess(adminUserInfo, SERVICE, scope);
 		assertNotNull(token.getToken());
 	}
 	
@@ -79,7 +73,7 @@ public class DockerManagerImplAutowiredTest {
 	public void testDockerRegistryNotification() {
 		DockerRegistryEventList events = 
 				DockerRegistryEventUtil.createDockerRegistryEvent(
-						RegistryEventAction.push, SERVICE, userInfo.getId(), repositoryPath, TAG, DIGEST);
+						RegistryEventAction.push, SERVICE, adminUserInfo.getId(), repositoryPath, TAG, DIGEST);
 		dockerManager.dockerRegistryNotification(events);
 	}
 
