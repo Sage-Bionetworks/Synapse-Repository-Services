4564af26287c35da0ba434848da3bec4d9c0fcb7
Ziming Dong <zimingd@gmail.com>
2016-08-09 15:08:35 -0700

    changed SemaphoreMangerTest to include checks for calls to MemoryTimeBlockCountingSemaphore

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/SemaphoreManagerImplTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/SemaphoreManagerImplTest.java
@@ -11,21 +11,25 @@ import org.sagebionetworks.database.semaphore.CountingSemaphore;
 import org.sagebionetworks.repo.model.UnauthorizedException;
 import org.sagebionetworks.repo.model.UserInfo;
 import org.sagebionetworks.repo.model.semaphore.MemoryCountingSemaphore;
+import org.sagebionetworks.repo.model.semaphore.MemoryTimeBlockCountingSemaphore;
 import org.springframework.test.util.ReflectionTestUtils;
 
 public class SemaphoreManagerImplTest {
 	
 	CountingSemaphore mockSemaphoreDao;
 	MemoryCountingSemaphore mockMemoryCountingSemaphore;
-	SemaphoreManagerImpl manager;
+	MemoryTimeBlockCountingSemaphore mockMemoryTimeBlockCountingSemaphore;
+	SemaphoreManager manager;
 	
 	@Before
 	public void before(){
 		mockSemaphoreDao = Mockito.mock(CountingSemaphore.class);
 		mockMemoryCountingSemaphore = Mockito.mock(MemoryCountingSemaphore.class);
+		mockMemoryTimeBlockCountingSemaphore = Mockito.mock(MemoryTimeBlockCountingSemaphore.class);
 		manager = new SemaphoreManagerImpl();
 		ReflectionTestUtils.setField(manager,"countingSemaphore", mockSemaphoreDao);
 		ReflectionTestUtils.setField(manager,"userThrottleMemoryCountingSemaphore", mockMemoryCountingSemaphore);
+		ReflectionTestUtils.setField(manager,"userThrottleMemoryTimeBlockSemaphore", mockMemoryTimeBlockCountingSemaphore);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
@@ -38,6 +42,7 @@ public class SemaphoreManagerImplTest {
 		manager.releaseAllLocksAsAdmin(new UserInfo(true));
 		verify(mockSemaphoreDao, times(1)).releaseAllLocks();
 		verify(mockMemoryCountingSemaphore, times(1)).releaseAllLocks();
+		verify(mockMemoryTimeBlockCountingSemaphore, times(1)).releaseAllLocks();
 	}
 	
 	@Test (expected=UnauthorizedException.class)
@@ -45,6 +50,7 @@ public class SemaphoreManagerImplTest {
 		manager.releaseAllLocksAsAdmin(new UserInfo(false));
 		verify(mockSemaphoreDao, never()).releaseAllLocks();
 		verify(mockMemoryCountingSemaphore, never()).releaseAllLocks();
+		verify(mockMemoryTimeBlockCountingSemaphore, never()).releaseAllLocks();
 	}
 
 }
