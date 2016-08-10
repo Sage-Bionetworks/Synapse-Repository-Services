fbeeb1e01686bc3a673968ccad1fec5782339459
Ziming Dong <zimingd@gmail.com>
2016-08-10 12:40:54 -0700

    made MemoryTimeBlockCountingSemaphoreTest more robust

--- lib/models/src/test/java/org/sagebionetworks/repo/model/semaphore/MempryTimeBlockCountingSemaphoreTest.java
+++ lib/models/src/test/java/org/sagebionetworks/repo/model/semaphore/MempryTimeBlockCountingSemaphoreTest.java
@@ -1,66 +1,123 @@
 package org.sagebionetworks.repo.model.semaphore;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.eq;
 
+import java.util.Map;
+import java.util.concurrent.Semaphore;
+
+import org.joda.time.chrono.LimitChronology;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MempryTimeBlockCountingSemaphoreTest {
 	
-	MemoryTimeBlockCountingSemaphore memoryTimeBlockCountingSemaphore;
+	private MemoryTimeBlockCountingSemaphore memoryTimeBlockCountingSemaphore;
+	
+	@Mock
+	private Map<String, SimpleSemaphore> keySemaphoreMap;
+	
+	@Mock
+	private SimpleSemaphore mockSemaphore;
 	
 	private static String key = "some key";
+	
+	private static final int limit = 1;
+	
+	private static final int timeoutSec = 2;
 	
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
-
 		memoryTimeBlockCountingSemaphore = Mockito.spy(new MemoryTimeBlockCountingSemaphoreImpl());
+		ReflectionTestUtils.setField(memoryTimeBlockCountingSemaphore, "keySemaphoreMap", keySemaphoreMap);
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
 	public void testAcquireLockNullKey(){
-		memoryTimeBlockCountingSemaphore.attemptToAcquireLock(null, 1, 1);
+		memoryTimeBlockCountingSemaphore.attemptToAcquireLock(null, timeoutSec, limit);
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
 	public void testAcquireLockNegativeTimeoutSec(){
-		memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, -1, 1);
+		memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, -1, limit);
 		
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
 	public void testAcquireLockNegativeMaxLock(){
-		memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, 1, -1);
+		memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, timeoutSec, -1);
 	}
 
 	@Test
 	public void testAcquireLockNoExistentSemaphore() {
-		boolean gotLock = memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, 1, 1);
-		assertTrue(gotLock);
+		when(keySemaphoreMap.get(key)).thenReturn(null);
+		
+		assertTrue(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, timeoutSec, limit));
+		
+		verify(memoryTimeBlockCountingSemaphore).attemptToAcquireLock(key, timeoutSec, limit);
+		verify(keySemaphoreMap).get(key);
+		verify(keySemaphoreMap).put(eq(key), any(SimpleSemaphore.class));
+		verifyNoMoreInteractions(memoryTimeBlockCountingSemaphore, keySemaphoreMap);
 	}
 	
 	@Test
 	public void testAcquireLockExpiredSemaphore() throws InterruptedException {
-		assertTrue(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, 1, 1));
-		assertFalse(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, 1, 1));
-		Thread.sleep(2 * 1000);
-		assertTrue(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, 1, 1));
+		when(keySemaphoreMap.get(key)).thenReturn(mockSemaphore);
+		when(mockSemaphore.isExpired()).thenReturn(true);
+		
+		assertTrue(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, timeoutSec, limit));
+		
+		verify(memoryTimeBlockCountingSemaphore).attemptToAcquireLock(key, timeoutSec, limit);
+		verify(keySemaphoreMap).get(key);
+		verify(mockSemaphore).isExpired();
+		verify(mockSemaphore).setExpiration(any(Long.class));
+		verify(mockSemaphore).resetCount();
+		verify(mockSemaphore).increment();
+		
+		verifyNoMoreInteractions(memoryTimeBlockCountingSemaphore, keySemaphoreMap,  mockSemaphore);
 	}
 	
 	@Test
 	public void testAcquireLockOverCountLimit(){
-		assertTrue(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, 1, 1));
-		assertFalse(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, 1, 1));
+		when(keySemaphoreMap.get(key)).thenReturn(mockSemaphore);
+		when(mockSemaphore.isExpired()).thenReturn(false);
+		when(mockSemaphore.getCount()).thenReturn(limit);
+		
+		assertFalse(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, timeoutSec, limit));
+		
+		verify(memoryTimeBlockCountingSemaphore).attemptToAcquireLock(key, timeoutSec, limit);
+		verify(keySemaphoreMap).get(key);
+		verify(mockSemaphore).isExpired();
+		verify(mockSemaphore).getCount();
+		verifyNoMoreInteractions(memoryTimeBlockCountingSemaphore, keySemaphoreMap,  mockSemaphore);
 	}
 	
 	@Test
 	public void testAcquireLockUnderCountLimit(){
-		assertTrue(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, 1, 2));
-		assertTrue(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, 1, 2));
+		when(keySemaphoreMap.get(key)).thenReturn(mockSemaphore);
+		when(mockSemaphore.isExpired()).thenReturn(false);
+		when(mockSemaphore.getCount()).thenReturn(limit - 1);
+		
+		assertTrue(memoryTimeBlockCountingSemaphore.attemptToAcquireLock(key, timeoutSec, limit));
+		
+		verify(memoryTimeBlockCountingSemaphore).attemptToAcquireLock(key, timeoutSec, limit);
+		verify(keySemaphoreMap).get(key);
+		verify(mockSemaphore).isExpired();
+		verify(mockSemaphore).getCount();
+		verify(mockSemaphore).increment();
+		verifyNoMoreInteractions(memoryTimeBlockCountingSemaphore, keySemaphoreMap,  mockSemaphore);
 
 	}
 
