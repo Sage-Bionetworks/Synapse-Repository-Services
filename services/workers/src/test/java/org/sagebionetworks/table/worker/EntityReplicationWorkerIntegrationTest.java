f27239c4474d5ef9a4e14b2358df6fd614f7eee9
john-hill <john.hill@sagebase.org>
2016-08-24 18:51:50 -0700

    add integration test

---
+++ services/workers/src/test/java/org/sagebionetworks/table/worker/EntityReplicationWorkerIntegrationTest.java
@@ -0,0 +1,12 @@
+package org.sagebionetworks.table.worker;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:test-context.xml" })
+public class EntityReplicationWorkerIntegrationTest {
+
+	
+}
