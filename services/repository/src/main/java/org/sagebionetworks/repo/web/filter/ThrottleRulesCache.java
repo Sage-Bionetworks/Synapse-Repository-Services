fa4804481bb44ba768f72f4ad83f2e401dbff9db
Ziming Dong <zimingd@gmail.com>
2016-08-26 10:49:52 -0700

    improved thread behavior in ThrottleRulesCache. Now only spawns 1 thread every time an update is needed

--- services/repository/src/main/java/org/sagebionetworks/repo/web/filter/ThrottleRulesCache.java
+++ services/repository/src/main/java/org/sagebionetworks/repo/web/filter/ThrottleRulesCache.java
@@ -16,56 +16,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
 public class ThrottleRulesCache {
+	private static long UPDATE_PERIOD_MILISECONDS = 5 * 60 * 1000; //update every 5 minutes
+	
 	private Map<String, ThrottleLimit> throttleRulesMap = new HashMap<String, ThrottleLimit>();
 	private long lastUpdated = 0; //last time in miliseconds the cache was updated
-	private static long UPDATE_PERIOD_MILISECONDS = 5 * 60 * 1000; //update every 5 minutes
-	private boolean currentlyUpdating = false; //used to ensure multiple threads do not update at the same time
+	private boolean currentlyUpdating = false; //used to ensure only one thread created for update
+	private Object lockObj = new Object(); //used as a lock to synchronize on
 	
 	@Autowired
 	private ThrottleRulesDAO throttleRulesDao;
 	
 	public ThrottleLimit getThrottleLimit(String normalizedUri){
-			if(shouldUpdate()){
+		synchronized (lockObj) {
+			//make sure it is time to update and no thread is currently updating already
+			if(System.currentTimeMillis() - lastUpdated >= UPDATE_PERIOD_MILISECONDS && !currentlyUpdating){
 				//spin up new thread to do update
+				currentlyUpdating = true;
 				(new Thread(){
 					@Override
-					public void run(){
-						updateRules();
+					public void run(){ //updates the map
+						//put rules in a new map
+						Map<String, ThrottleLimit> updatedRulesMap = new HashMap<String, ThrottleLimit>();
+						List<ThrottleRule> rules = throttleRulesDao.getAllThrottles();
+						for(ThrottleRule rule : rules){
+							updatedRulesMap.put(rule.getNormalizedUri(), new ThrottleLimit(rule.getMaxCalls(), rule.getCallPeriodSec()));
+						}
+						
+						//change the reference to the new map and flag that the thread finished updating
+						throttleRulesMap = updatedRulesMap;
+						lastUpdated = System.currentTimeMillis();
+						currentlyUpdating = false;
 					}
 				}).start();
-				
 			}
-			return throttleRulesMap.get(normalizedUri);
-	}
-	
-	
-	/**
-	 * updates the throttle rules Map
-	 */
-	private synchronized void updateRules(){
-		//checks condition again to make so that extra threads that were created do not update.
-		if(shouldUpdate()){
-			
-			//flag that this thread is updating and start updating
-			currentlyUpdating = true;
-			
-			//put rules in a new map
-			Map<String, ThrottleLimit> updatedRulesMap = new HashMap<String, ThrottleLimit>();
-			List<ThrottleRule> rules = throttleRulesDao.getAllThrottles();
-			for(ThrottleRule rule : rules){
-				updatedRulesMap.put(rule.getNormalizedUri(), new ThrottleLimit(rule.getMaxCalls(), rule.getCallPeriodSec()));
-			}
-			
-			//change the reference to the new map and flag that the thread finished updating
-			throttleRulesMap = updatedRulesMap;
-			lastUpdated = System.currentTimeMillis();
-			currentlyUpdating = false;
 		}
-	}
-	
-	private boolean shouldUpdate(){
-		//make sure it is time to update and no thread is currently updating already
-		return System.currentTimeMillis() - lastUpdated >= UPDATE_PERIOD_MILISECONDS && !currentlyUpdating;
+		
+		return throttleRulesMap.get(normalizedUri);
 	}
 
 }
