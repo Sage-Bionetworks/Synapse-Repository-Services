cb7514a1b1b9187027207c01e888a6147796362d
Xavier Schildwachter <xchildwachter@gmail.com>
2016-11-17 21:02:17 -0800

    When clean does not mean clean...

--- client/synapseJavaClient/src/main/java/org/sagebionetworks/client/SynapseClientImpl.java
+++ client/synapseJavaClient/src/main/java/org/sagebionetworks/client/SynapseClientImpl.java
@@ -240,7 +240,6 @@ import org.sagebionetworks.repo.model.verification.VerificationSubmission;
 import org.sagebionetworks.repo.model.versionInfo.SynapseVersionInfo;
 import org.sagebionetworks.repo.model.wiki.WikiHeader;
 import org.sagebionetworks.repo.model.wiki.WikiPage;
-import org.sagebionetworks.repo.model.wiki.WikiVersionsList;
 import org.sagebionetworks.repo.web.NotFoundException;
 import org.sagebionetworks.schema.adapter.JSONEntity;
 import org.sagebionetworks.schema.adapter.JSONObjectAdapter;
--- integration-test/src/test/java/org/sagebionetworks/IT055WikiPageTest.java
+++ integration-test/src/test/java/org/sagebionetworks/IT055WikiPageTest.java
@@ -40,7 +40,6 @@ import org.sagebionetworks.repo.model.v2.wiki.V2WikiHistorySnapshot;
 import org.sagebionetworks.repo.model.v2.wiki.V2WikiPage;
 import org.sagebionetworks.repo.model.wiki.WikiHeader;
 import org.sagebionetworks.repo.model.wiki.WikiPage;
-import org.sagebionetworks.repo.model.wiki.WikiVersionsList;
 import org.sagebionetworks.utils.MD5ChecksumHelper;
 
 public class IT055WikiPageTest {
