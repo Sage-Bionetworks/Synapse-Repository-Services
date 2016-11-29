0bdab8071fc2072d4db39278550e1f63788e7b3c
Ziming Dong <ziming.dong@sagebase.org>
2016-11-29 12:20:10 -0800

    refactored out common code in test into the setup

--- services/repository-managers/src/test/java/org/sagebionetworks/evaluation/manager/SubmissionManagerAutowiredTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/evaluation/manager/SubmissionManagerAutowiredTest.java
@@ -3,6 +3,8 @@ package org.sagebionetworks.evaluation.manager;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -10,12 +12,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.sagebionetworks.evaluation.model.Evaluation;
 import org.sagebionetworks.evaluation.model.EvaluationStatus;
@@ -35,9 +34,10 @@ import org.sagebionetworks.repo.model.auth.NewUser;
 import org.sagebionetworks.repo.model.docker.DockerCommit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:test-context.xml" })
 public class SubmissionManagerAutowiredTest {
 
@@ -59,14 +59,16 @@ public class SubmissionManagerAutowiredTest {
 	@Autowired
 	private TeamManager teamManager;
 	
-	@Rule
-	public ExpectedException expectedException = ExpectedException.none();
-	
 	private UserInfo adminUserInfo;
 	private UserInfo userInfo;
 	private List<String> evalsToDelete;
 	private List<String> nodesToDelete;
 	private String teamId;
+	private String nodeId;
+	private String evalId;
+	private Submission submission;
+	private Node retrievedNode;
+	private EntityBundle bundle;
 	
 	private Node createNode(String name, EntityType type, UserInfo userInfo) throws Exception {
 		final long principalId = Long.parseLong(userInfo.getId().toString());
@@ -111,6 +113,30 @@ public class SubmissionManagerAutowiredTest {
 		Team team = new Team();
 		team.setName("teamName");		
 		teamId = teamManager.create(adminUserInfo, team).getId();
+		
+		
+		//setup for submission
+		nodeId = createNode("repo", EntityType.dockerrepo, adminUserInfo).getId();
+		evalId = evalsToDelete.get(0);
+		
+		DockerCommit commit = new DockerCommit();
+		commit.setTag("foo");
+		commit.setDigest(DOCKER_DIGEST);
+		commit.setCreatedOn(new Date());
+		dockerCommitDao.createDockerCommit(nodeId, adminUserInfo.getId(), commit);
+		
+		retrievedNode = nodeManager.get(adminUserInfo, nodeId);
+
+		// set up a submission
+		submission = new Submission();
+		submission.setDockerDigest(DOCKER_DIGEST);
+		submission.setEntityId(nodeId);
+		submission.setEvaluationId(evalId);
+		submission.setUserId(""+adminUserInfo.getId());
+		submission.setVersionNumber(retrievedNode.getVersionNumber());
+		bundle = new EntityBundle();
+		bundle.setFileHandles(Collections.EMPTY_LIST);
+		assertNotNull(retrievedNode.getETag());
 	}
 
 	@After
@@ -130,27 +156,6 @@ public class SubmissionManagerAutowiredTest {
 	@Test
 	public void testDockerRepoSubmissionCreateAndRead() throws Exception {
 		// create a docker repository
-		String nodeId = createNode("repo", EntityType.dockerrepo, adminUserInfo).getId();
-		String evalId = evalsToDelete.get(0);
-		
-		DockerCommit commit = new DockerCommit();
-		commit.setTag("foo");
-		commit.setDigest(DOCKER_DIGEST);
-		commit.setCreatedOn(new Date());
-		dockerCommitDao.createDockerCommit(nodeId, adminUserInfo.getId(), commit);
-		
-		Node retrievedNode = nodeManager.get(adminUserInfo, nodeId);
-
-		// create a submission
-		Submission submission = new Submission();
-		submission.setDockerDigest(DOCKER_DIGEST);
-		submission.setEntityId(nodeId);
-		submission.setEvaluationId(evalId);
-		submission.setUserId(""+adminUserInfo.getId());
-		submission.setVersionNumber(retrievedNode.getVersionNumber());
-		EntityBundle bundle = new EntityBundle();
-		bundle.setFileHandles(Collections.EMPTY_LIST);
-		assertNotNull(retrievedNode.getETag());
 		submission = submissionManager.createSubmission(adminUserInfo, submission, 
 				retrievedNode.getETag(), null, bundle);
 		
@@ -171,42 +176,24 @@ public class SubmissionManagerAutowiredTest {
 		assertEquals(DOCKER_DIGEST, retrieved.getDockerDigest());
 	}
 	
-	@Test //(expected = UnauthorizedException.class)
+	@Test
 	public void testCreateSubmissionIndividualSubmissionAfterTeamSubmission() throws Exception{
-		//this is similar to the (expect=SomeException.class) annotation, but also allows the verification of the exception message
-		expectedException.expect(UnauthorizedException.class);
-		expectedException.expectMessage("Submitter may not submit as an individual when having submitted as part of a Team");
-		
-		// create a docker repository
-		String nodeId = createNode("repo", EntityType.dockerrepo, adminUserInfo).getId();
-		String evalId = evalsToDelete.get(0);
-		
-		DockerCommit commit = new DockerCommit();
-		commit.setTag("foo");
-		commit.setDigest(DOCKER_DIGEST);
-		commit.setCreatedOn(new Date());
-		dockerCommitDao.createDockerCommit(nodeId, adminUserInfo.getId(), commit);
-		
-		Node retrievedNode = nodeManager.get(adminUserInfo, nodeId);		
-
-		// create a submission
-		Submission submission = new Submission();
-		submission.setDockerDigest(DOCKER_DIGEST);
-		submission.setEntityId(nodeId);
-		submission.setEvaluationId(evalId);
-		submission.setUserId(""+adminUserInfo.getId());
-		submission.setVersionNumber(retrievedNode.getVersionNumber());
+		// create a submission for team
 		submission.setTeamId(teamId);
-		EntityBundle bundle = new EntityBundle();
-		bundle.setFileHandles(Collections.EMPTY_LIST);
-		assertNotNull(retrievedNode.getETag());
-		String submissionEligiblityHash = "" +SubmissionEligibilityManagerImpl.computeTeamSubmissionEligibilityHash(evaluationManager.getTeamSubmissionEligibility(adminUserInfo, evalId, teamId));
+		String submissionEligiblityHash = evaluationManager.getTeamSubmissionEligibility(adminUserInfo, evalId, teamId).getEligibilityStateHash().toString();
 		submissionManager.createSubmission(adminUserInfo, submission, 
 				retrievedNode.getETag(), submissionEligiblityHash, bundle);
 		
+		//create second submission as individual
 		submission.setTeamId(null);
-		submissionManager.createSubmission(adminUserInfo, submission, 
+		try{
+			submissionManager.createSubmission(adminUserInfo, submission, 
 				retrievedNode.getETag(), null, bundle);
-		
+		}catch(UnauthorizedException e){
+			//verify the error message because there are many UnauthorizedExceptions that could be thrown
+			assertTrue(e.getMessage().equals("Submitter may not submit as an individual when having submitted as part of a Team."));
+			return;
+		}
+		fail("No exception was thrown even though it was expected");
 	}
 }
