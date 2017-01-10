b25da5690f2869b0442e534e61f1d2cad615f3f5
john-hill <john.hill@sagebase.org>
2017-01-09 17:14:00 -0800

    added missing files

---
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/NextPageTokenTest.java
@@ -0,0 +1,45 @@
+package org.sagebionetworks.repo.manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NextPageTokenTest {
+
+	@Test
+	public void testNextPage(){
+		long limit = 101;
+		long offset = 51;
+		NextPageToken token = new NextPageToken(limit, offset);
+		assertEquals(limit, token.getLimit());
+		assertEquals(offset, token.getOffset());
+		String tokenString = token.toToken();
+		NextPageToken clone = new NextPageToken(tokenString);
+		assertEquals(limit, clone.getLimit());
+		assertEquals(offset, clone.getOffset());
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testNextPageNull(){
+		String tokenString = null;
+		NextPageToken clone = new NextPageToken(tokenString);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testNextPageBadToken(){
+		String tokenString = "notatoken";
+		NextPageToken clone = new NextPageToken(tokenString);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testNextPageBadTokenTwo(){
+		String tokenString = "1a";
+		NextPageToken clone = new NextPageToken(tokenString);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testNextPageBadTokenThree(){
+		String tokenString = "a2";
+		NextPageToken clone = new NextPageToken(tokenString);
+	}
+}
