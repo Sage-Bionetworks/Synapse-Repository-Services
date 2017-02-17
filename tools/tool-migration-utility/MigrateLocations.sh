1c8996e3c7916427c9c2d0562ccfbf0cf12ceee9
xschildw <x.schildwachter@sagebase.org>
2017-02-17 00:54:33 -0800

    Remove tool-migration-utility.

--- tools/tool-migration-utility/MigrateLocations.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Migrate all locations.
-java -cp target/tool-migration-utility-0.10-SNAPSHOT-jar-with-dependencies.jar org.sagebionetworks.tool.migration.MigrateLocations "$@"
--- tools/tool-migration-utility/run_migrator.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-# usage: ./run_migrator <MIGRATER JAR> <MIGRATER PROPERTY FILE>
-java -Xms256m -Xmx2g -cp $1 org.sagebionetworks.tool.migration.gui.MigrationConsoleUI $2
