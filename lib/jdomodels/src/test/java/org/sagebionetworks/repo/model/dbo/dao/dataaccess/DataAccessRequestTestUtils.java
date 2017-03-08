e8e46e7b1b2534ff77dae98cd5ab1cd618b9135c
Kimyen <kimyen0816@gmail.com>
2017-03-07 19:01:05 -0800

    add concreteType

--- lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/dao/dataaccess/DataAccessRequestTestUtils.java
+++ lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/dao/dataaccess/DataAccessRequestTestUtils.java
@@ -4,8 +4,27 @@ import java.util.Arrays;
 import java.util.Date;
 
 import org.sagebionetworks.repo.model.dataaccess.DataAccessRenewal;
+import org.sagebionetworks.repo.model.dataaccess.DataAccessRequest;
 
 public class DataAccessRequestTestUtils {
+
+	public static DataAccessRequest createNewDataAccessRequest() {
+		DataAccessRequest dto = new DataAccessRequest();
+		dto.setId("1");
+		dto.setAccessRequirementId("2");
+		dto.setResearchProjectId("3");
+		dto.setCreatedBy("4");
+		dto.setCreatedOn(new Date());
+		dto.setModifiedBy("5");
+		dto.setModifiedOn(new Date());
+		dto.setEtag("etag");
+		dto.setAccessors(Arrays.asList("6", "7", "8"));
+		dto.setDucFileHandleId("9");
+		dto.setIrbFileHandleId("10");
+		dto.setAttachments(Arrays.asList("11", "12"));
+		dto.setConcreteType(DataAccessRequest.class.getName());
+		return dto;
+	}
 
 	public static DataAccessRenewal createNewDataAccessRenewal() {
 		DataAccessRenewal dto = new DataAccessRenewal();
@@ -23,6 +42,7 @@ public class DataAccessRequestTestUtils {
 		dto.setAttachments(Arrays.asList("11", "12"));
 		dto.setPublication("publication");
 		dto.setSummaryOfUse("summaryOfUse");
+		dto.setConcreteType(DataAccessRenewal.class.getName());
 		return dto;
 	}
 
