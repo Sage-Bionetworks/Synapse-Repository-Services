fe2832780460800bcd02bcd9d7e980a54b90f998
Kimyen <kimyen0816@gmail.com>
2017-03-17 17:16:37 -0700

    refactor

--- lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/dao/dataaccess/DataAccessSubmissionUtilsTest.java
+++ lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/dao/dataaccess/DataAccessSubmissionUtilsTest.java
@@ -2,8 +2,6 @@ package org.sagebionetworks.repo.model.dbo.dao.dataaccess;
 
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,13 +9,12 @@ import java.util.Set;
 import org.junit.Test;
 import org.sagebionetworks.repo.model.dataaccess.DataAccessSubmission;
 import org.sagebionetworks.repo.model.dataaccess.DataAccessSubmissionState;
-import org.sagebionetworks.repo.model.dataaccess.ResearchProject;
 
 public class DataAccessSubmissionUtilsTest {
 
 	@Test
 	public void testRoundTrip() {
-		DataAccessSubmission dto = createSubmission();
+		DataAccessSubmission dto = DataAccessSubmissionTestUtils.createSubmission();
 		DBODataAccessSubmission dbo = new DBODataAccessSubmission();
 		DataAccessSubmissionUtils.copyDtoToDbo(dto, dbo);
 		DBODataAccessSubmissionStatus status = DataAccessSubmissionUtils.getDBOStatus(dto);
@@ -37,40 +34,6 @@ public class DataAccessSubmissionUtilsTest {
 		newDto = DataAccessSubmissionUtils.copyDboToDto(dbo, status);
 		dto.setState(DataAccessSubmissionState.APPROVED);
 		assertEquals(dto, newDto);
-	}
-
-	public DataAccessSubmission createSubmission() {
-		DataAccessSubmission dto = new DataAccessSubmission();
-		dto.setId("1");
-		dto.setAccessRequirementId("2");
-		dto.setDataAccessRequestId("3");
-		ResearchProject researchProjectSnapshot = new ResearchProject();
-		researchProjectSnapshot.setId("4");
-		researchProjectSnapshot.setAccessRequirementId("2");
-		researchProjectSnapshot.setInstitution("Sage");
-		researchProjectSnapshot.setProjectLead("projectLead");
-		researchProjectSnapshot.setIntendedDataUseStatement("intendedDataUseStatement");
-		researchProjectSnapshot.setCreatedBy("5");
-		researchProjectSnapshot.setCreatedOn(new Date());
-		researchProjectSnapshot.setModifiedBy("5");
-		researchProjectSnapshot.setModifiedOn(new Date());
-		researchProjectSnapshot.setEtag("etag1");
-		dto.setResearchProjectSnapshot(researchProjectSnapshot);
-		dto.setAccessors(Arrays.asList("6", "7", "8"));
-		dto.setAttachments(Arrays.asList("9"));
-		dto.setDucFileHandleId("0");
-		dto.setIrbFileHandleId("10");
-		dto.setEtag("etag2");
-		dto.setIsRenewalSubmission(true);
-		dto.setPublication("publication");
-		dto.setSummaryOfUse("summaryOfUse");
-		dto.setModifiedBy("5");
-		dto.setModifiedOn(new Date());
-		dto.setSubmittedBy("5");
-		dto.setSubmittedOn(new Date());
-		dto.setState(DataAccessSubmissionState.SUBMITTED);
-		dto.setRejectedReason("no reasons");
-		return dto;
 	}
 
 }
