38675ec147d2ddf5f161099066aa1aef7acd9594
Kimyen <kimyen0816@gmail.com>
2017-05-11 11:09:42 -0700

    add current version column

--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/query/jdo/SqlConstants.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/query/jdo/SqlConstants.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.sagebionetworks.repo.model.Node;
 import org.sagebionetworks.repo.model.NodeConstants;
 import org.sagebionetworks.repo.model.jdo.BasicIdentifierFactory;
-import org.sagebionetworks.repo.model.query.Comparator;
  
 @SuppressWarnings("rawtypes")
 public class SqlConstants {
@@ -101,17 +100,18 @@ public class SqlConstants {
 	public static final String COL_PRINCIPAL_PREFIX_PRINCIPAL_ID 	= "PRINCIPAL_ID";
 
 	// The ACCESS_REQUIREMENT table
-	public static final String TABLE_ACCESS_REQUIREMENT				= "ACCESS_REQUIREMENT";
-	public static final String COL_ACCESS_REQUIREMENT_ID			= "ID";
-	public static final String COL_ACCESS_REQUIREMENT_ETAG			= "ETAG";
-	public static final String COL_ACCESS_REQUIREMENT_CREATED_BY	= "CREATED_BY";
-	public static final String COL_ACCESS_REQUIREMENT_CREATED_ON	= "CREATED_ON";
-	public static final String COL_ACCESS_REQUIREMENT_MODIFIED_BY	= "MODIFIED_BY";
-	public static final String COL_ACCESS_REQUIREMENT_MODIFIED_ON	= "MODIFIED_ON";
-	public static final String COL_ACCESS_REQUIREMENT_ACCESS_TYPE	= "ACCESS_TYPE";
-	public static final String COL_ACCESS_REQUIREMENT_CONCRETE_TYPE	= "CONCRETE_TYPE";
-	public static final String COL_ACCESS_REQUIREMENT_SERIALIZED_ENTITY	= "SERIALIZED_ENTITY";
-	public static final String DDL_FILE_ACCESS_REQUIREMENT			= "schema/AccessRequirement-ddl.sql";
+	public static final String TABLE_ACCESS_REQUIREMENT							= "ACCESS_REQUIREMENT";
+	public static final String COL_ACCESS_REQUIREMENT_ID						= "ID";
+	public static final String COL_ACCESS_REQUIREMENT_ETAG						= "ETAG";
+	public static final String COL_ACCESS_REQUIREMENT_CURRENT_REVISION_NUMBER	= "CURRENT_REV_NUM";
+	public static final String COL_ACCESS_REQUIREMENT_CREATED_BY				= "CREATED_BY";
+	public static final String COL_ACCESS_REQUIREMENT_CREATED_ON				= "CREATED_ON";
+	public static final String COL_ACCESS_REQUIREMENT_MODIFIED_BY				= "MODIFIED_BY";
+	public static final String COL_ACCESS_REQUIREMENT_MODIFIED_ON				= "MODIFIED_ON";
+	public static final String COL_ACCESS_REQUIREMENT_ACCESS_TYPE				= "ACCESS_TYPE";
+	public static final String COL_ACCESS_REQUIREMENT_CONCRETE_TYPE				= "CONCRETE_TYPE";
+	public static final String COL_ACCESS_REQUIREMENT_SERIALIZED_ENTITY			= "SERIALIZED_ENTITY";
+	public static final String DDL_FILE_ACCESS_REQUIREMENT						= "schema/AccessRequirement-ddl.sql";
 
 	// The SUBJECT_ACCESS_REQUIREMENT table (a join table linking the ENTITY or EVALUTION and ACCESS_REQUIREMENT tables
 	// !!! Note: The table name should be SUBJECT_ACCESS_REQUIREMENT, but migration issues prevent
