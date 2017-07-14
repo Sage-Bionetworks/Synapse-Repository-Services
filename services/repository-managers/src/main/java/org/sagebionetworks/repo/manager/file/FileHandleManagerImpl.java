993f4789e6f70599cfe4e6e02e1d8692efd3ea19
Bruce Hoff <bruce.hoff@sagebase.org>
2017-07-14 09:53:46 -0700

    Update FileHandleManagerImpl.java

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/file/FileHandleManagerImpl.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/file/FileHandleManagerImpl.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
