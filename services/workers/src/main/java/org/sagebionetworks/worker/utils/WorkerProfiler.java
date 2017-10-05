c42da3e9736ad11e01dfe84e2d4ade52344c3b85
john-hill <john.hill@sagebase.org>
2017-10-04 18:57:55 -0700

    runtime works but not counts

--- services/workers/src/main/java/org/sagebionetworks/worker/utils/WorkerProfiler.java
+++ services/workers/src/main/java/org/sagebionetworks/worker/utils/WorkerProfiler.java
@@ -1,15 +1,20 @@
 package org.sagebionetworks.worker.utils;
 
+import java.util.Collections;
+import java.util.Date;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.sagebionetworks.StackConfiguration;
 import org.sagebionetworks.cloudwatch.Consumer;
+import org.sagebionetworks.cloudwatch.ProfileData;
 import org.sagebionetworks.common.util.progress.ProgressCallback;
-import org.sagebionetworks.repo.manager.asynch.AsynchJobStatusManager;
-import org.sagebionetworks.repo.model.asynch.AsynchronousJobStatus;
+import org.sagebionetworks.common.util.progress.ProgressListener;
 import org.sagebionetworks.workers.util.aws.message.MessageDrivenRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.amazonaws.services.cloudwatch.model.StandardUnit;
 import com.amazonaws.services.sqs.model.Message;
 
 /**
@@ -19,12 +24,19 @@ import com.amazonaws.services.sqs.model.Message;
 @Aspect
 public class WorkerProfiler {
 	
+	public static final String COUNT = "count";
+	public static final String RUNTIME = "runtime";
+	public static final String WORKER_NAME = "name";
+	public static final Double ONE = new Double(1);
+	
+	public static final String WORKER_COUNT_NAMESPACE = "Worker-Count-2"
+			+ StackConfiguration.getStackInstance();
+	public static final String WORKER_RUNTIME_NAMESPACE = "Worker-Runtime-2"
+			+ StackConfiguration.getStackInstance();
+	
 	@Autowired
 	Consumer consumer;
 	
-	@Autowired
-	AsynchJobStatusManager asynchJobStatusManager;
-
 	
 	/**
 	 * Profile any {@link MessageDrivenRunner#run(ProgressCallback, Message)} call.
@@ -37,19 +49,63 @@ public class WorkerProfiler {
 			+ ".run(org.sagebionetworks.common.util.progress.ProgressCallback, com.amazonaws.services.sqs.model.Message))"
 			+ " && args(progressCallback, message)")
 	public Object profileMessageDrivenRunner(ProceedingJoinPoint pjp, ProgressCallback progressCallback, Message message) throws Throwable{
-		// Lookup the job type.
-		AsynchronousJobStatus status = asynchJobStatusManager.lookupJobStatus(message.getBody());
-		System.out.println(status.getRequestBody().getClass().getName());
+		final String targetClassName = pjp.getTarget().getClass().getSimpleName();
 		Object retVal = null;
+		// add count metric before the start
 		long startTime = System.currentTimeMillis();
+		ProgressListener progressListener = new ProgressListener() {
+			@Override
+			public void progressMade() {
+				// add count metric as progress is made
+				fireWorkerCountMetric(targetClassName);
+			}
+		};
+		progressCallback.addProgressListener(progressListener);
 		try{
 			// the actual call.
 			retVal = pjp.proceed();
 		}finally{
-			long elapse = System.currentTimeMillis() - startTime;
-			System.out.println("Elapse: "+elapse);
+			// unconditionally remove the progress listener
+			progressCallback.removeProgressListener(progressListener);
+			long elapseTimeMS = System.currentTimeMillis() - startTime;
+			// add a runtime metric
+			fireWorkerRuntimeMetric(targetClassName, elapseTimeMS);
+			// add count metric at the end.
+			fireWorkerCountMetric(targetClassName);
 		}
 		return retVal;
 	}
-
+	
+	/**
+	 * Fire a worker count metric
+	 * @param workerName
+	 */
+	private void fireWorkerCountMetric(String workerName){
+		ProfileData pd = new ProfileData();
+		pd.setNamespace(WORKER_COUNT_NAMESPACE);
+		pd.setTimestamp(new Date(System.currentTimeMillis()));
+		pd.setName(COUNT);
+		pd.setUnit(StandardUnit.Count.name());
+		pd.setValue(ONE);
+		pd.setDimension(Collections.singletonMap(WORKER_NAME, workerName));
+		consumer.addProfileData(pd);
+	}
+	
+	/**
+	 * Fire a worker runtime metric.
+	 * @param workerName
+	 * @param elapseTimeMS
+	 */
+	private void fireWorkerRuntimeMetric(String workerName, long elapseTimeMS){
+		ProfileData pd = new ProfileData();
+		pd.setNamespace(WORKER_RUNTIME_NAMESPACE);
+		pd.setTimestamp(new Date(System.currentTimeMillis()));
+		pd.setName(RUNTIME);
+		pd.setUnit(StandardUnit.Milliseconds.name());
+		pd.setValue(new Double(elapseTimeMS));
+		pd.setDimension(Collections.singletonMap(WORKER_NAME, workerName));
+		consumer.addProfileData(pd);
+	}
+	
+	
 }
