36d0b80fbbdaa3c141ae01062b632104d747f673
john-hill <john.hill@sagebase.org>
2017-11-28 19:00:12 -0800

    util to convert schemas

---
+++ lib/lib-auto-generated/src/test/java/org/sagebionetworks/UpgradeEnumerations.java
@@ -0,0 +1,83 @@
+package org.sagebionetworks;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.commons.io.FileUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.sagebionetworks.schema.EnumValue;
+import org.sagebionetworks.schema.ObjectSchema;
+import org.sagebionetworks.schema.adapter.JSONArrayAdapter;
+import org.sagebionetworks.schema.adapter.JSONObjectAdapter;
+import org.sagebionetworks.schema.adapter.JSONObjectAdapterException;
+import org.sagebionetworks.schema.adapter.org.json.JSONObjectAdapterImpl;
+
+/**
+ * Utility to convert old enumeration to the new enumerations with for all
+ * schemas.
+ *
+ */
+public class UpgradeEnumerations {
+	
+	public static void main(String[] args) throws IOException, JSONObjectAdapterException, JSONException {
+
+		File dir = new File("src/main/resources");
+		Iterator<?> itr = FileUtils.iterateFiles(dir, new String[] { "json" }, true);
+
+		while (itr.hasNext()) {
+			File file = (File) itr.next();
+			JSONObjectAdapterImpl adapter = null;
+			try {
+				String json = readFileToString(file.getAbsolutePath());
+				adapter = new JSONObjectAdapterImpl(json);
+				ObjectSchema schema = new ObjectSchema(adapter);
+			} catch (JSONObjectAdapterException e) {
+				if(e.getMessage().contains("JSONArray[0] is not a JSONObject")) {
+					System.out.println("Need to translate: "+file.getAbsolutePath());
+					if(adapter.has(ObjectSchema.JSON_ENUM)) {
+						JSONArrayAdapter array = adapter.getJSONArray(ObjectSchema.JSON_ENUM);
+						JSONArrayAdapter newArray = translateOldToNew(array);
+						// Replace the old array with a new array
+						adapter.put(ObjectSchema.JSON_ENUM, newArray);
+						JSONObject object = new JSONObject(adapter.toJSONString());
+						System.out.println(object.toString(4));
+					}
+				}
+			}
+		}
+	}
+	
+	private static JSONArrayAdapter translateOldToNew(JSONArrayAdapter array) throws JSONObjectAdapterException {
+		JSONArrayAdapter newArray = array.createNewArray();
+		for(int i=0; i<array.length(); i++) {
+			String name = array.getString(i);
+			EnumValue value = new EnumValue(name, "TODO Auto-generated description");
+			JSONObjectAdapter newValueAdapter = array.createNew();
+			value.writeToJSONObject(newValueAdapter);
+			newArray.put(i, newValueAdapter);
+		}
+		return newArray;
+	}
+
+	private static String readFileToString(String filePath) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		try {
+			String line = null;
+			StringBuilder sb = new StringBuilder();
+			String ls = System.getProperty("line.separator");
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+				sb.append(ls);
+			}
+			return sb.toString();
+		} finally {
+			reader.close();
+		}
+	}
+	
+	
+}
