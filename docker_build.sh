3b00ad6b75664126d147cf592eff2f2f064c9098
Ziming Dong <ziming.dong@sagebase.org>
2018-01-12 13:03:55 -0800

    temporarily change build script so all tests are run

--- docker_build.sh
+++ docker_build.sh
@@ -84,8 +84,7 @@ docker run -i --rm --name ${build_container_name} \
 -w /repo \
 maven:3-jdk-8 \
 #TODO: UNDO THIS CHANGE BEFORE MERGING
-bash -c "mvn clean install \
---fail-never \
+bash -c "mvn clean install -fn\
 -Dorg.sagebionetworks.repository.database.connection.url=jdbc:mysql://${rds_container_name}/${rds_user_name} \
 -Dorg.sagebionetworks.id.generator.database.connection.url=jdbc:mysql://${rds_container_name}/${rds_user_name} \
 -Dorg.sagebionetworks.stackEncryptionKey=${org_sagebionetworks_stackEncryptionKey} \
