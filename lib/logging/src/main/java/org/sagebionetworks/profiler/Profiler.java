00fe7515de73f37d762d41a0fd59bb7ab348888a
Ziming Dong <ziming.dong@sagebase.org>
2018-02-23 14:56:26 -0800

    refactored out the stack management logic from Profiler

--- lib/logging/src/main/java/org/sagebionetworks/profiler/Profiler.java
+++ lib/logging/src/main/java/org/sagebionetworks/profiler/Profiler.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * This is a Profiler that logs the results.
@@ -16,50 +17,16 @@ import org.aspectj.lang.annotation.Aspect;
  */
 @Aspect
 public class Profiler {
-	// Each thread gets its own stack.
-	private static ThreadLocal<Stack<Frame>> threadFrameStack = ThreadLocal.withInitial(Stack::new);
-
-	private List<ProfileHandler> handlers = null;
-
-	public List<ProfileHandler> getHandlers() {
-		return handlers;
-	}
-
-	/**
-	 * Injected via Spring.
-	 * 
-	 * @param handlers
-	 */
-	public void setHandlers(List<ProfileHandler> handlers) {
-		this.handlers = handlers;
-	}
-
-
-	/**
-	 * Should we even profile.
-	 * 
-	 * @param args
-	 * @return
-	 */
-	boolean shouldCaptureData(Object[] args) {
-		if (handlers == null) {
-			return false;
-		}
-		for (ProfileHandler handler : this.handlers) {
-			if (handler.shouldCaptureProfile(args)) {
-				return true;
-			}
-		}
-		return false;
-	}
+	@Autowired
+	private ProfilerFrameStackManager frameStackManager;
 
 	// execution(* org.sagebionetworks..*.*(..)) means profile any bean in the
 	// package org.sagebionetworks or any sub-packages
 	@Around("execution(* org.sagebionetworks..*.*(..)) && !within(org.sagebionetworks.profiler.*)")
 	public Object doBasicProfiling(ProceedingJoinPoint pjp) throws Throwable {
-
 		// Do nothing if logging is not on
-		if (!shouldCaptureData(pjp.getArgs())) {
+		Object[] profiledMethodArgs = pjp.getArgs();
+		if (!frameStackManager.shouldCaptureData(profiledMethodArgs)) {
 			// Just proceed if logging is off.
 			return pjp.proceed();
 		}
@@ -68,52 +35,14 @@ public class Profiler {
 		Class<?> declaring = pjp.getTarget().getClass();
 		String methodName = declaring.getName() + "." + signature.getName();
 
-		Stack<Frame> parentFramesStack = threadFrameStack.get();
-
-		Frame currentFrame = getCurrentFrame(methodName);
-
-		//add the current frame to the stack before proceeding
-		parentFramesStack.push(currentFrame);
 		long startTime = System.nanoTime();
-
-		// Now start the method.
 		try {
+			frameStackManager.startProfiling(methodName, profiledMethodArgs);
 			return pjp.proceed();
 		} finally {
 			long endTime = System.nanoTime();
-			currentFrame.addElapsedTime((endTime - startTime) / 1000000);
-
-			// now that the method finished pop the current frame off the stack
-			parentFramesStack.pop();
-
-			// If this is the first frame, log the profiling data
-			if (parentFramesStack.isEmpty()) {
-				doFireProfile(currentFrame);
-			}
+			frameStackManager.endProfiling(methodName, profiledMethodArgs, (endTime - startTime) / 1000000);
 		}
 	}
 
-	Frame getCurrentFrame(String methodName) {
-		Stack<Frame> parentFramesStack = threadFrameStack.get();
-		Frame currentFrame;
-		if (parentFramesStack.isEmpty()) {
-			// There are no parent frames, so create a new frame
-			currentFrame = new Frame(methodName);
-		} else{
-			// A parent frame exists so add it to the parent frame
-			Frame parentFrame = parentFramesStack.peek();
-			currentFrame = parentFrame.addChildFrameIfAbsent(methodName);
-		}
-		return currentFrame;
-	}
-
-	void doFireProfile(Frame frame) {
-		if (handlers != null) {
-			for (ProfileHandler handler : this.handlers) {
-				if (handler.shouldCaptureProfile(null)) {
-					handler.fireProfile(frame);
-				}
-			}
-		}
-	}
 }
--- lib/logging/src/test/java/org/sagebionetworks/profiler/ProfilerTest.java
+++ lib/logging/src/test/java/org/sagebionetworks/profiler/ProfilerTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -21,6 +22,7 @@ import java.util.Stack;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,135 +36,47 @@ public class ProfilerTest {
 	@Mock
 	ProceedingJoinPoint mockProceedingJoinPoint;
 
+	@Mock
+	ProfilerFrameStackManager mockProfilerFrameStackManager;
+
 	Profiler spyProfiler;
 
-	@Mock
-	ProfileHandler mockProfileHandler1;
-
-	@Mock
-	ProfileHandler mockProfileHandler2;
-
-	List<ProfileHandler> profileHandlers;
-
-	@Mock
-	ThreadLocal<Stack<Frame>> mockThreadLocalStack;
-
-	@Mock
-	Stack<Frame> mockFrameStack;
-
-	@Mock
-	Frame mockFrame;
-
+	Object[] profiledMethodArgs;
 	@Before
 	public void setUp(){
-		spyProfiler = spy(new Profiler());
-		profileHandlers = Arrays.asList(mockProfileHandler1, mockProfileHandler2);
-		when(mockProfileHandler1.shouldCaptureProfile(any())).thenReturn(false);
-		when(mockProfileHandler2.shouldCaptureProfile(any())).thenReturn(true);
+		profiledMethodArgs = new Object[]{};
 
-		when(mockThreadLocalStack.get()).thenReturn(mockFrameStack);
-		ReflectionTestUtils.setField(Profiler.class,"threadFrameStack", mockThreadLocalStack);
-		when(mockFrameStack.peek()).thenReturn(mockFrame);
+		Profiler profiler = new Profiler();
+		ReflectionTestUtils.setField(profiler, "frameStackManager", mockProfilerFrameStackManager);
+
+		spyProfiler = spy(profiler);
+
+		when(mockProceedingJoinPoint.getArgs()).thenReturn(profiledMethodArgs);
 	}
 
-	@Test
-	public void testShouldCaptureData_nullHandlers(){
-		spyProfiler.setHandlers(null);
-		assertNull(spyProfiler.getHandlers());
-
-		assertFalse(spyProfiler.shouldCaptureData(new Object[]{}));
+	@After
+	public void noMoreInteractions(){
+		verifyNoMoreInteractions(mockProceedingJoinPoint);
+		verifyNoMoreInteractions(mockProfilerFrameStackManager);
+		verifyNoMoreInteractions(spyProfiler);
 	}
 
-	@Test
-	public void testShouldCaptureData_emptyHandlers(){
-		spyProfiler.setHandlers(Collections.emptyList());
-
-		assertFalse(spyProfiler.shouldCaptureData(new Object[]{}));
-	}
-
-	@Test
-	public void testShouldCaptureData_multipleHandlers(){
-		spyProfiler.setHandlers(profileHandlers);
-
-		assertTrue(spyProfiler.shouldCaptureData(new Object[]{}));
-	}
-
-	@Test
-	public void testDoFireProfile_nullHandlers(){
-		spyProfiler.setHandlers(null);
-		assertNull(spyProfiler.getHandlers());
-
-		spyProfiler.doFireProfile(mockFrame);
-
-		verify(mockProfileHandler1, never()).shouldCaptureProfile(any());
-		verify(mockProfileHandler2, never()).shouldCaptureProfile(any());
-		verify(mockProfileHandler1, never()).fireProfile(any());
-		verify(mockProfileHandler2, never()).fireProfile(any());
-	}
-
-	@Test
-	public void testDoFireProfile_emptyHandlers(){
-		spyProfiler.setHandlers(Collections.emptyList());
-
-		spyProfiler.doFireProfile(mockFrame);
-
-		verify(mockProfileHandler1, never()).shouldCaptureProfile(any());
-		verify(mockProfileHandler2, never()).shouldCaptureProfile(any());
-		verify(mockProfileHandler1, never()).fireProfile(any());
-		verify(mockProfileHandler2, never()).fireProfile(any());
-	}
-
-	@Test
-	public void testDoFireProfile_multipleHandlers(){
-		spyProfiler.setHandlers(profileHandlers);
-
-		spyProfiler.doFireProfile(mockFrame);
-
-		verify(mockProfileHandler1).shouldCaptureProfile(null);
-		verify(mockProfileHandler2).shouldCaptureProfile(null);
-		verify(mockProfileHandler1, never()).fireProfile(any());
-		verify(mockProfileHandler2).fireProfile(mockFrame);
-	}
-
-	@Test
-	public void testGetCurrentFrame__emptyStack(){
-		String methodName = "asdf";
-		when(mockFrameStack.isEmpty()).thenReturn(true);
-		Frame currentFrame = spyProfiler.getCurrentFrame(methodName);
-
-		assertEquals(methodName, currentFrame.getName());
-	}
-
-
-	@Test
-	public void testGetCurrentFrame__nonEmptyStack(){
-		String methodName = "asdf";
-
-		when(mockFrame.addChildFrameIfAbsent(methodName)).thenReturn(new Frame(methodName));
-
-		Frame currentFrame = spyProfiler.getCurrentFrame(methodName);
-
-		assertEquals(methodName, currentFrame.getName());
-		verify(mockFrame).addChildFrameIfAbsent(methodName);
-	}
 
 	@Test
 	public void testDoBasicProfiling_shouldNotCaptureData() throws Throwable{
-		doReturn(false).when(spyProfiler).shouldCaptureData(any());
+		doReturn(false).when(mockProfilerFrameStackManager).shouldCaptureData(any());
 
 		spyProfiler.doBasicProfiling(mockProceedingJoinPoint);
 
 		verify(spyProfiler).doBasicProfiling(mockProceedingJoinPoint);
-		verify(spyProfiler).shouldCaptureData(any());
+		verify(mockProfilerFrameStackManager).shouldCaptureData(any());
 		verify(mockProceedingJoinPoint).getArgs();
 		verify(mockProceedingJoinPoint).proceed();
-		verifyNoMoreInteractions(mockProceedingJoinPoint);
-		verifyNoMoreInteractions(spyProfiler);
 	}
 
 	@Test
 	public void testDoBasicProfiling_shouldCaptureData() throws Throwable{
-		doReturn(true).when(spyProfiler).shouldCaptureData(any());
+		doReturn(true).when(mockProfilerFrameStackManager).shouldCaptureData(any());
 		Object target = new Object();
 		String signatureName = "fakename()";
 		Signature mockSignature = mock(Signature.class);
@@ -170,32 +84,21 @@ public class ProfilerTest {
 		String expectedMethodName = target.getClass().getName() + "." + signatureName;
 		when(mockProceedingJoinPoint.getTarget()).thenReturn(target);
 		when(mockProceedingJoinPoint.getSignature()).thenReturn(mockSignature);
-		doReturn(mockFrame).when(spyProfiler).getCurrentFrame(expectedMethodName);
 
 
 		spyProfiler.doBasicProfiling(mockProceedingJoinPoint);
 
+		verify(mockProfilerFrameStackManager).shouldCaptureData(any());
 		verify(spyProfiler).doBasicProfiling(mockProceedingJoinPoint);
-		verify(spyProfiler).shouldCaptureData(any());
 		verify(mockProceedingJoinPoint).getArgs();
 		verify(mockProceedingJoinPoint).getSignature();
 		verify(mockProceedingJoinPoint).getTarget();
 		verify(mockSignature).getName();
-		verify(mockThreadLocalStack).get();
-		verify(spyProfiler).getCurrentFrame(expectedMethodName);
-		verify(mockFrameStack).push(mockFrame);
+		verify(mockProfilerFrameStackManager).startProfiling(expectedMethodName, profiledMethodArgs);
 		verify(mockProceedingJoinPoint).proceed();
-		verify(mockFrame).addElapsedTime(anyLong());
-		verify(mockFrameStack).pop();
-		verify(mockFrameStack).isEmpty();
+		verify(mockProfilerFrameStackManager).endProfiling(eq(expectedMethodName), eq(profiledMethodArgs), anyLong());
 
 
-
-		verifyNoMoreInteractions(mockProceedingJoinPoint);
-		verifyNoMoreInteractions(spyProfiler);
 		verifyNoMoreInteractions(mockSignature);
-		verifyNoMoreInteractions(mockFrame);
-		verifyNoMoreInteractions(mockThreadLocalStack);
-		verifyNoMoreInteractions(mockFrameStack);
 	}
 }
