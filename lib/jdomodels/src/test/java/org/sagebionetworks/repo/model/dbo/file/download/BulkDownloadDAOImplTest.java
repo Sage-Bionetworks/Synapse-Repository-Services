3cd797ece96b9c22e4c374f00950cfd5cf058bfe
john-hill <john.hill@sagebase.org>
2018-09-07 11:39:41 -0700

    fixed test

--- lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/file/download/BulkDownloadDAOImplTest.java
+++ lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/file/download/BulkDownloadDAOImplTest.java
@@ -211,7 +211,7 @@ public class BulkDownloadDAOImplTest {
 		assertNotNull(list);
 		assertNotNull(list.getUpdatedOn());
 		assertNotNull(list.getEtag());
-		assertEquals(null, list.getFilesToDownload());
+		assertNotNull(list.getFilesToDownload());
 		assertEquals(this.userOneId, list.getOwnerId());
 	}
 	
