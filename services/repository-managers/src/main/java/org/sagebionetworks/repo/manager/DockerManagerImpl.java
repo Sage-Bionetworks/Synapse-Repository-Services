9b655fbe5806abda57f22d191c91888db9721ccb
john-hill <john.hill@sagebase.org>
2018-10-16 13:20:08 -0700

    for test fixes

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/DockerManagerImpl.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/DockerManagerImpl.java
@@ -9,8 +9,6 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.sagebionetworks.StackConfiguration;
-import org.sagebionetworks.ids.IdGenerator;
-import org.sagebionetworks.ids.IdType;
 import org.sagebionetworks.reflection.model.PaginatedResults;
 import org.sagebionetworks.repo.model.ACCESS_TYPE;
 import org.sagebionetworks.repo.model.DockerCommitDao;
@@ -26,7 +24,6 @@ import org.sagebionetworks.repo.model.docker.DockerRegistryEvent;
 import org.sagebionetworks.repo.model.docker.DockerRegistryEventList;
 import org.sagebionetworks.repo.model.docker.DockerRepository;
 import org.sagebionetworks.repo.model.docker.RegistryEventAction;
-import org.sagebionetworks.repo.model.jdo.KeyFactory;
 import org.sagebionetworks.repo.model.message.ChangeType;
 import org.sagebionetworks.repo.model.message.TransactionalMessenger;
 import org.sagebionetworks.repo.model.util.DockerNameUtil;
@@ -43,9 +40,6 @@ public class DockerManagerImpl implements DockerManager {
 	
 	@Autowired
 	private DockerNodeDao dockerNodeDao;
-
-	@Autowired
-	IdGenerator idGenerator;
 
 	@Autowired
 	UserManager userManager;
