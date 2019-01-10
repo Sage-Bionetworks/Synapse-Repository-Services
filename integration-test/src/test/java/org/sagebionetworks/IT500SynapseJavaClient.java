ca270a4ae3cbb8e53ecf28a3f95bf3d0a39e5cd0
Nick Grosenbacher <nick.grosenbacher@sagebase.org>
2019-01-10 11:47:01 -0500

    add bootstrapped team to client test bootstrap

--- integration-test/src/test/java/org/sagebionetworks/IT500SynapseJavaClient.java
+++ integration-test/src/test/java/org/sagebionetworks/IT500SynapseJavaClient.java
@@ -140,6 +140,7 @@ public class IT500SynapseJavaClient {
 		bootstrappedTeams.add("2"); // Administrators
 		bootstrappedTeams.add("464532"); // Access and Compliance Team
 		bootstrappedTeams.add("4"); // Trusted message senders
+		bootstrappedTeams.add("5"); // Synapse reports team
 	}
 	
 	private long getBootstrapCountPlus(long number) {
