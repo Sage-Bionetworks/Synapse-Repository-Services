e6fd6abf68e839b2feaeada8298508d966ca6e0e
john-hill <john.hill@sagebase.org>
2019-02-05 17:01:58 -0800

    only annotation strings too large for view columns will throw a translated exception

--- lib/lib-table-cluster/src/test/java/org/sagebionetworks/table/cluster/SQLUtilsTest.java
+++ lib/lib-table-cluster/src/test/java/org/sagebionetworks/table/cluster/SQLUtilsTest.java
@@ -2043,7 +2043,16 @@ public class SQLUtilsTest {
 	
 	
 	/**
-	 * This is a test for PLFM-4260 where a boolean column was mapped to a string annotation.
+	 * The original fix to PLFM-4260 involved throwing an exception when trying to map a string
+	 * annotation to a boolean column.  However, the fix for PLFM-4864 involved changing
+	 * how we copy data from the ANNOTATION_REPLICATION table to views. We now only copy
+	 * data from ANNOTATION_REPLICATION from columns that match the view column type.
+	 * As a result, the only type of exception that can occur is when a string annotation
+	 * is too large of for a string column.
+	 * This means the condition in PLFM-4260 no longer result in an error so we should 
+	 * not throw an exception for this case.
+	 * 
+	 * This is part of the fix for PLFM-5348.
 	 */
 	@Test
 	public void testDetermineCauseOfExceptionWrongType() {
@@ -2056,18 +2065,8 @@ public class SQLUtilsTest {
 		annotationModel.setName("foo");
 		annotationModel.setColumnType(ColumnType.STRING);
 		annotationModel.setMaximumSize(11L);
-		try {
-			// call under test
-			SQLUtils.determineCauseOfException(oringal, columnModel,
-					annotationModel);
-			fail("Should have failed.");
-		} catch (IllegalArgumentException expected) {
-			assertEquals(
-					"Cannot insert an annotation value of type STRING into column 'foo' which is of type BOOLEAN.",
-					expected.getMessage());
-			// the cause should be kept
-			assertEquals(oringal, expected.getCause());
-		}
+		// call under test - this should not throw an exception
+		SQLUtils.determineCauseOfException(oringal, columnModel, annotationModel);
 	}
 	
 	@Test
@@ -2120,6 +2119,7 @@ public class SQLUtilsTest {
 			assertEquals(oringal, expected.getCause());
 		}
 	}
+	
 	
 	@Test
 	public void testGetViewScopeFilterColumnForType() {
