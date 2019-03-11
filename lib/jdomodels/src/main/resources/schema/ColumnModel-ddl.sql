53e01912d9afa3d2a9864931559b3b140ee4ced8
john-hill <john.hill@sagebase.org>
2019-03-11 16:27:45 -0700

    switch all columns to utf-8

--- lib/jdomodels/src/main/resources/schema/ColumnModel-ddl.sql
+++ lib/jdomodels/src/main/resources/schema/ColumnModel-ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `COLUMN_MODEL` (
   `ID` bigint(20) NOT NULL,
-  `NAME` varchar(256) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
-  `HASH` varchar(256) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+  `NAME` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `HASH` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `BYTES` mediumblob,
   PRIMARY KEY (`ID`),
   KEY `CM_NAME_INDEX` (`NAME`),
