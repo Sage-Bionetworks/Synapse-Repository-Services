5b4cd20e93751228a5dfbf4fc598531b0e46aa9f
john-hill <john.hill@sagebase.org>
2019-03-15 16:57:59 -0700

    comments

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/migration/TableTransactionBackfillMigrationListener.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/migration/TableTransactionBackfillMigrationListener.java
@@ -1,6 +1,5 @@
 package org.sagebionetworks.repo.manager.migration;
 
-import java.sql.Timestamp;
 import java.util.List;
 
 import org.sagebionetworks.repo.model.dbo.DatabaseObject;
@@ -13,68 +12,81 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
+/**
+ * This is a one-time listener used to create and associated table transaction
+ * with existing table changes. Changes from the same same users on the same
+ * table that occur within one minute of each other will be group together in a
+ * new transaction. The start date for the new transaction will match the date
+ * from the earliest user change in the group.
+ *
+ */
 public class TableTransactionBackfillMigrationListener implements MigrationTypeListener {
-	
+
 	@Autowired
 	TableTransactionDao tableTransactionDao;
-	
+
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
-	public static long ONE_MINUTE = 1000L*60L;
-	
+
+	public static long ONE_MINUTE = 1000L * 60L;
+
 	RowMapper<DBOTableTransaction> TRANSACTION_MAPPER = new DBOTableTransaction().getTableMapping();
 
 	@Override
 	public <D extends DatabaseObject<?>> void afterCreateOrUpdate(MigrationType type, List<D> delta) {
 		// only watch table changes.
-		if(MigrationType.TABLE_CHANGE.equals(type)) {
+		if (MigrationType.TABLE_CHANGE.equals(type)) {
 			// disable foreign keys
 			setGlobalForeignKeyChecks(false);
 			try {
-				for(D dto: delta) {
-					if(!(dto instanceof DBOTableRowChange)) {
-						throw new IllegalArgumentException("Unexpected DTO: "+dto.getClass().getName());
+				for (D dto : delta) {
+					if (!(dto instanceof DBOTableRowChange)) {
+						throw new IllegalArgumentException("Unexpected DTO: " + dto.getClass().getName());
 					}
 					DBOTableRowChange rowChange = (DBOTableRowChange) dto;
-					if(rowChange.getTransactionId() == null) {
+					if (rowChange.getTransactionId() == null) {
 						// only need to back-fill changes without a transaction
 						findOrCreateTransaction(rowChange);
 					}
 				}
-			}finally {
+			} finally {
 				// enable foreign keys
 				setGlobalForeignKeyChecks(true);
 			}
 		}
 	}
+
 	/**
-	 * Find an existing transaction to match the provided change.  If 
-	 * a match cannot be found create a new transaction.
+	 * Find an existing transaction to match the provided change. If a match cannot
+	 * be found create a new transaction.
+	 * 
 	 * @param rowChange
 	 */
 	public void findOrCreateTransaction(DBOTableRowChange rowChange) {
 		// Attempt to match the previous change's transaction to this change.
 		Long transactionId = getPreviousTransactionIdIfMatches(rowChange);
-		if(transactionId == null) {
+		if (transactionId == null) {
 			// no match found so start a new transaction for this change
 			transactionId = startTransactionForChange(rowChange);
 		}
 		// Assign the transaction ID to this change.
 		setChangeTransactionId(rowChange, transactionId);
 	}
-	
+
 	/**
 	 * Start a new transaction for the given table
+	 * 
 	 * @param rowChange
 	 * @return
 	 */
 	public long startTransactionForChange(DBOTableRowChange rowChange) {
-		return tableTransactionDao.startTransaction(""+rowChange.getTableId(), rowChange.getCreatedBy(), rowChange.getCreatedOn());
+		return tableTransactionDao.startTransaction("" + rowChange.getTableId(), rowChange.getCreatedBy(),
+				rowChange.getCreatedOn());
 	}
-	
+
 	/**
 	 * Lookup the previous transaction for this table.
+	 * 
 	 * @param rowChange
 	 * @return
 	 */
@@ -82,21 +94,24 @@ public class TableTransactionBackfillMigrationListener implements MigrationTypeL
 		try {
 			// Find the previous row version
 			Long previousRowVersion = jdbcTemplate.queryForObject(
-					"SELECT MAX(ROW_VERSION) FROM TABLE_ROW_CHANGE WHERE TABLE_ID = ? AND ROW_VERSION < ?",
-					Long.class, rowChange.getTableId(), rowChange.getRowVersion());
-			if(previousRowVersion == null) {
+					"SELECT MAX(ROW_VERSION) FROM TABLE_ROW_CHANGE WHERE TABLE_ID = ? AND ROW_VERSION < ?", Long.class,
+					rowChange.getTableId(), rowChange.getRowVersion());
+			if (previousRowVersion == null) {
 				return null;
 			}
-			// Lookup the transaction for the previous change if it matches this user and is within an hour
-			return jdbcTemplate.queryForObject("SELECT T.TRX_ID FROM TABLE_TRANSACTION T"
-					+ " JOIN TABLE_ROW_CHANGE C ON (T.TABLE_ID = C.TABLE_ID AND T.TRX_ID = C.TRX_ID)"
-					+ " WHERE C.TABLE_ID = ? AND C.ROW_VERSION = ? AND C.CREATED_BY = ? AND C.CREATED_ON > ?", Long.class,
-					rowChange.getTableId(), previousRowVersion, rowChange.getCreatedBy(), rowChange.getCreatedOn() - ONE_MINUTE);
+			// Lookup the transaction for the previous change if it matches this user and is
+			// within an hour
+			return jdbcTemplate.queryForObject(
+					"SELECT T.TRX_ID FROM TABLE_TRANSACTION T"
+							+ " JOIN TABLE_ROW_CHANGE C ON (T.TABLE_ID = C.TABLE_ID AND T.TRX_ID = C.TRX_ID)"
+							+ " WHERE C.TABLE_ID = ? AND C.ROW_VERSION = ? AND C.CREATED_BY = ? AND C.CREATED_ON > ?",
+					Long.class, rowChange.getTableId(), previousRowVersion, rowChange.getCreatedBy(),
+					rowChange.getCreatedOn() - ONE_MINUTE);
 		} catch (EmptyResultDataAccessException e) {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Set the transaction ID for the given table change.
 	 * 
@@ -107,15 +122,16 @@ public class TableTransactionBackfillMigrationListener implements MigrationTypeL
 		jdbcTemplate.update("UPDATE TABLE_ROW_CHANGE SET TRX_ID = ? WHERE TABLE_ID = ? AND ROW_VERSION = ?",
 				transactionId, rowChange.getTableId(), rowChange.getRowVersion());
 	}
-	
+
 	/**
 	 * Helper to enable/disable foreign keys.
+	 * 
 	 * @param enabled
 	 */
 	private void setGlobalForeignKeyChecks(boolean enabled) {
 		int value;
-		if(enabled){
-			// trun it on.
+		if (enabled) {
+			// turn it on.
 			value = 1;
 		} else {
 			// turn it off
--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/migration/TransactionBackfillTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/migration/TransactionBackfillTest.java
@@ -5,60 +5,55 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.sagebionetworks.repo.manager.UserManager;
+import org.sagebionetworks.repo.model.AuthorizationConstants.BOOTSTRAP_PRINCIPAL;
 import org.sagebionetworks.repo.model.UserInfo;
 import org.sagebionetworks.repo.model.daemon.BackupAliasType;
 import org.sagebionetworks.repo.model.migration.MigrationType;
-import org.sagebionetworks.repo.model.migration.RestoreTypeRequest;
 import org.sagebionetworks.repo.model.migration.RestoreTypeResponse;
-import org.sagebionetworks.repo.model.AuthorizationConstants.BOOTSTRAP_PRINCIPAL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ * This is a temporarily test of the TableTransactionBackfillMigrationListener.
+ * This test will be removed when the listener is removed.
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:test-context.xml" })
 public class TransactionBackfillTest {
-	
+
 	@Autowired
 	private MigrationManager migrationManager;
 	@Autowired
 	private UserManager userManager;
-	
+
 	UserInfo adminUser;
-	
+
 	@Before
 	public void before() {
 		adminUser = userManager.getUserInfo(BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId());
 	}
 	
-	@Ignore
-	@Test
-	public void testBackFill() throws IOException {
-		RestoreTypeRequest restoreRequest = new RestoreTypeRequest();
-		restoreRequest.setAliasType(BackupAliasType.MIGRATION_TYPE_NAME);
-		restoreRequest.setBackupFileKey("prod-255-TABLE_SEQUENCE-e36f4bd3-f0ce-4b2d-ac0e-762fff3acbae.zip");
-		restoreRequest.setBatchSize(50000L);
-		restoreRequest.setMinimumRowId(0L);
-		restoreRequest.setMaximumRowId(Long.MAX_VALUE);
-		restoreRequest.setMigrationType(MigrationType.TABLE_SEQUENCE);
-		// call under test
-		migrationManager.restoreRequest(adminUser, restoreRequest);
-	}
-	
+	/**
+	 * This test is run using a locally provided backup actual data from production to ensure
+	 * all transactions are created and bound as expected.
+	 * 
+	 * @throws IOException
+	 */
 	@Test
 	public void testBackFillLocal() throws IOException {
-		String localFile = "C:/Users/John/Downloads/prod-255-TABLE_SEQUENCE-e36f4bd3-f0ce-4b2d-ac0e-762fff3acbae.zip";
-		try(FileInputStream input = new FileInputStream(new File(localFile))){
+		String localFile = "C:/Users/jhill/Downloads/prod-255-TABLE_SEQUENCE-e36f4bd3-f0ce-4b2d-ac0e-762fff3acbae.zip";
+		try (FileInputStream input = new FileInputStream(new File(localFile))) {
 			MigrationType primaryType = MigrationType.TABLE_SEQUENCE;
 			BackupAliasType backupAliasType = BackupAliasType.MIGRATION_TYPE_NAME;
 			Long batchSize = 50000L;
 			// call under test
-			RestoreTypeResponse response = migrationManager.restoreStream(input, primaryType, backupAliasType, batchSize);
-			System.out.println("Rows restored: "+response.getRestoredRowCount());
+			RestoreTypeResponse response = migrationManager.restoreStream(input, primaryType, backupAliasType,
+					batchSize);
+			System.out.println("Rows restored: " + response.getRestoredRowCount());
 		}
 	}
 
