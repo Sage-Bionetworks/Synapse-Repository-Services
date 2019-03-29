808c1b19acefb8c60655532e742b3e77c5b9dd59
bhoff <bruce.hoff@sagebase.org>
2019-03-29 10:29:23 -0700

    PLFM-5040

--- lib/stackConfiguration/src/test/java/org/sagebionetworks/SynapseS3ClientImplTest.java
+++ lib/stackConfiguration/src/test/java/org/sagebionetworks/SynapseS3ClientImplTest.java
@@ -1,31 +1,41 @@
 package org.sagebionetworks;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.sagebionetworks.aws.AwsClientFactory;
 import org.sagebionetworks.aws.SynapseS3ClientImpl;
 
 import com.amazonaws.services.s3.model.Region;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SynapseS3ClientImplTest {
 	
-	private static final SynapseS3ClientImpl synapseS3Client = (SynapseS3ClientImpl)AwsClientFactory.createAmazonS3Client();;
+	private static SynapseS3ClientImpl synapseS3Client;
 	
 	private static final String S3_BUCKET_NAME = StackConfigurationSingleton.singleton().getS3Bucket();
 
+	
+	@BeforeClass
+	public static void beforeClass() {
+		synapseS3Client = (SynapseS3ClientImpl)AwsClientFactory.createAmazonS3Client();
+	}
+
 	@Test
-	void testGetRegionForBucket() {
+	public void testGetRegionForBucket() {
 		assertEquals(Region.US_Standard, synapseS3Client.getRegionForBucket(S3_BUCKET_NAME));
 	}
 
 	@Test
-	void testGetS3ClientForBucket() {
+	public void testGetS3ClientForBucket() {
 		assertEquals(Region.US_Standard, synapseS3Client.getS3ClientForBucket(S3_BUCKET_NAME).getRegion());
 	}
 	
 	@Test
-	public void getStandardRegionClient() {
+	public void testGetStandardRegionClient() {
 		assertEquals(Region.US_Standard, synapseS3Client.getUSStandardAmazonClient().getRegion());
 	}
 
---
+++ lib/stackConfiguration/src/test/java/org/sagebionetworks/SynapseS3ClientImplUnitTest.java
@@ -0,0 +1,67 @@
+package org.sagebionetworks;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.sagebionetworks.aws.SynapseS3ClientImpl;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.Region;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SynapseS3ClientImplUnitTest {
+	
+	@Mock
+	private AmazonS3 mockAmazonUSStandardClient;
+	
+	@Mock
+	private AmazonS3 mockAmazonClient;
+	
+	private SynapseS3ClientImpl client;
+	
+	private static final String BUCKET_NAME = "bucket-name";
+	private static final Region BUCKET_REGION_US_STANDARD = Region.US_Standard;
+	
+	private static final Region BUCKET_REGION = Region.US_West;
+	private static final String BUCKET_REGION_ID = BUCKET_REGION.getFirstRegionId();
+	
+	private static final String OBJECT_KEY = "s3-object-key";
+
+	@Before
+	public void before() {
+		Map<Region, AmazonS3> regionSpecificClients = new HashMap<Region, AmazonS3>();
+		regionSpecificClients.put(BUCKET_REGION_US_STANDARD, mockAmazonUSStandardClient);
+		regionSpecificClients.put(BUCKET_REGION, mockAmazonClient);
+		client = new SynapseS3ClientImpl(regionSpecificClients);
+		
+		when(mockAmazonUSStandardClient.getBucketLocation(BUCKET_NAME)).thenReturn(BUCKET_REGION_ID);
+	}
+	
+	@Test
+	public void testDeleteObject() {
+		// method under test
+		client.deleteObject(BUCKET_NAME, OBJECT_KEY);
+		
+		verify(mockAmazonClient).deleteObject(BUCKET_NAME, OBJECT_KEY);
+	}
+
+	@Test
+	public void testDeleteObjects() {		
+		DeleteObjectsRequest deleteObjectsRequest = new DeleteObjectsRequest(BUCKET_NAME);
+
+		// method under test
+		client.deleteObjects(deleteObjectsRequest);
+		
+		verify(mockAmazonClient).deleteObjects(deleteObjectsRequest);
+	}
+
+}
