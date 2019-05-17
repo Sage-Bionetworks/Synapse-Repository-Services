d08707dc1908408ded1a668780118d692f91ef71
Ziming Dong <ziming.dong@sagebase.org>
2019-05-16 20:13:59 -0700

    i love command line

--- docker_build_remote.sh
+++ docker_build_remote.sh
@@ -61,8 +61,8 @@ if [ ${SETTINGS_XML} ]; then
 fi
 
 
-mysql -u ${rds_user_name} -p ${rds_password} -h ${org_sagebionetworks_repo_db_endpoint} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};set global log_bin_trust_function_creators=1;"
-mysql -u ${rds_user_name} -p ${rds_password} -h ${org_sagebionetworks_user_tables_db_endpoint} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};set global log_bin_trust_function_creators=1;"
+mysql -u${rds_user_name} -p${rds_password} -h ${org_sagebionetworks_repo_db_endpoint} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};set global log_bin_trust_function_creators=1;"
+mysql -u${rds_user_name} -p${rds_password} -h ${org_sagebionetworks_user_tables_db_endpoint} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};set global log_bin_trust_function_creators=1;"
 
 
 # create build container and run build
