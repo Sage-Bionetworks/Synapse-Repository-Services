54caeb430ae55d3f32f7d8cc1c77b5d6b797ab39
Nick Grosenbacher <nick.grosenbacher@sagebase.org>
2019-06-14 12:46:58 -0700

    add google cloud metadata type for filehandles

--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/persistence/DBOFileHandle.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/persistence/DBOFileHandle.java
@@ -47,6 +47,7 @@ public class DBOFileHandle implements MigratableDatabaseObject<DBOFileHandle, Fi
 	 */
 	public static enum MetadataType {
 		S3,
+		GOOGLECLOUDSTORAGE,
 		EXTERNAL,
 		PREVIEW,
 		PROXY,
