c703af64810b1a1b7aa9574431f0e4c9db65623f
KimYen Ladia <kimyen0816@gmail.com>
2019-06-21 11:55:53 -0700

    remove unused imports

--- integration-test/src/test/java/org/sagebionetworks/IT500SynapseJavaClient.java
+++ integration-test/src/test/java/org/sagebionetworks/IT500SynapseJavaClient.java
@@ -27,7 +27,6 @@ import java.util.UUID;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
