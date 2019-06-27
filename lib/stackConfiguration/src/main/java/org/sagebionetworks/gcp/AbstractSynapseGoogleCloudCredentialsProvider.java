23d376bb3f03e09096fd41ed0e082ef32d4ba744
Nick Grosenbacher <nick.grosenbacher@sagebase.org>
2019-06-27 14:37:14 -0700

    use four keys instead of one

--- lib/stackConfiguration/src/main/java/org/sagebionetworks/gcp/AbstractSynapseGoogleCloudCredentialsProvider.java
+++ lib/stackConfiguration/src/main/java/org/sagebionetworks/gcp/AbstractSynapseGoogleCloudCredentialsProvider.java
@@ -1,7 +1,5 @@
 package org.sagebionetworks.gcp;
 
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 import com.amazonaws.util.StringUtils;
@@ -12,7 +10,10 @@ import com.google.auth.oauth2.ServiceAccountCredentials;
 public abstract class AbstractSynapseGoogleCloudCredentialsProvider implements CredentialsProvider {
 
 	public static final String GOOGLE_CLOUD_CREDENTIALS_WERE_NOT_FOUND = "Google Cloud credentials were not found.";
-	public static final String ORG_SAGEBIONETWORKS_STACK_GCP_SVC_ACCOUNT_KEY = "org.sagebionetworks.gcp.key";
+	public static final String ORG_SAGEBIONETWORKS_GOOGLE_CLOUD_CLIENT_ID = "org.sagebionetworks.google.cloud.client.id";
+	public static final String ORG_SAGEBIONETWORKS_GOOGLE_CLOUD_CLIENT_EMAIL = "org.sagebionetworks.google.cloud.client.email";
+	public static final String ORG_SAGEBIONETWORKS_GOOGLE_CLOUD_CLIENT_PRIVATE_KEY = "org.sagebionetworks.google.cloud.key";
+	public static final String ORG_SAGEBIONETWORKS_GOOGLE_CLOUD_CLIENT_PRIVATE_KEY_ID = "org.sagebionetworks.google.cloud.key.id";
 
 	final Properties properties;
 
@@ -27,9 +28,12 @@ public abstract class AbstractSynapseGoogleCloudCredentialsProvider implements C
 		try {
 			Properties properties = getProperties();
 			if (properties != null) {
-				String accessKey = StringUtils.trim(properties.getProperty(ORG_SAGEBIONETWORKS_STACK_GCP_SVC_ACCOUNT_KEY));
-				if (accessKey != null) {
-					return ServiceAccountCredentials.fromStream(new ByteArrayInputStream(accessKey.getBytes(StandardCharsets.UTF_8)));
+				String clientId = StringUtils.trim(properties.getProperty(ORG_SAGEBIONETWORKS_GOOGLE_CLOUD_CLIENT_ID));
+				String clientEmail = StringUtils.trim(properties.getProperty(ORG_SAGEBIONETWORKS_GOOGLE_CLOUD_CLIENT_EMAIL));
+				String privateKeyPkcs8 = StringUtils.trim(properties.getProperty(ORG_SAGEBIONETWORKS_GOOGLE_CLOUD_CLIENT_PRIVATE_KEY));
+				String privateKeyId = StringUtils.trim(properties.getProperty(ORG_SAGEBIONETWORKS_GOOGLE_CLOUD_CLIENT_PRIVATE_KEY_ID));;
+				if (clientId != null && clientEmail != null && privateKeyPkcs8 != null && privateKeyId != null) {
+					return ServiceAccountCredentials.fromPkcs8(clientId, clientEmail, privateKeyPkcs8, privateKeyId, null);
 				}
 			}
 			throw new IllegalStateException(GOOGLE_CLOUD_CREDENTIALS_WERE_NOT_FOUND);
