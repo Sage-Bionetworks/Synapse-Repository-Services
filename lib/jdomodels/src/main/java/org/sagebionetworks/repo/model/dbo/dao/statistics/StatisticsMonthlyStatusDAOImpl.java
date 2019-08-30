204797684648cc855ae0e93f6e27f02dc3a94afb
Marco Marasca <marco.marasca@sagebase.org>
2019-08-29 19:54:42 -0700

    PLFM-5767: Added DAO for monthly stats status

---
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/dao/statistics/StatisticsMonthlyStatusDAOImpl.java
@@ -0,0 +1,157 @@
+package org.sagebionetworks.repo.model.dbo.dao.statistics;
+
+import static org.sagebionetworks.repo.model.query.jdo.SqlConstants.COL_STATISTICS_MONTHLY_MONTH;
+import static org.sagebionetworks.repo.model.query.jdo.SqlConstants.COL_STATISTICS_MONTHLY_OBJECT_TYPE;
+import static org.sagebionetworks.repo.model.query.jdo.SqlConstants.TABLE_STATISTICS_MONTHLY;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Optional;
+
+import org.sagebionetworks.repo.model.dao.statistics.StatisticsMonthlyStatusDAO;
+import org.sagebionetworks.repo.model.dbo.DBOBasicDao;
+import org.sagebionetworks.repo.model.dbo.persistence.statistics.monthly.DBOMonthlyStatisticsStatus;
+import org.sagebionetworks.repo.model.statistics.StatisticsMonthlyStatus;
+import org.sagebionetworks.repo.model.statistics.StatisticsMonthlyUtils;
+import org.sagebionetworks.repo.model.statistics.StatisticsObjectType;
+import org.sagebionetworks.repo.model.statistics.StatisticsStatus;
+import org.sagebionetworks.repo.transactions.WriteTransaction;
+import org.sagebionetworks.util.ValidateArgument;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StatisticsMonthlyStatusDAOImpl implements StatisticsMonthlyStatusDAO {
+
+	private static final String PARAM_OBJECT_TYPE = "objectType";
+	private static final String PARAM_MONTH = "month";
+	private static final String PARAM_FROM = "from";
+	private static final String PARAM_TO = "to";
+
+	private static final String SQL_DELETE_ALL = "DELETE FROM " + TABLE_STATISTICS_MONTHLY;
+	private static final String SQL_SELECT_IN_RANGE = "SELECT * FROM " + TABLE_STATISTICS_MONTHLY + " WHERE "
+			+ COL_STATISTICS_MONTHLY_OBJECT_TYPE + " = :" + PARAM_OBJECT_TYPE + " AND " + COL_STATISTICS_MONTHLY_MONTH + " BETWEEN :"
+			+ PARAM_FROM + " AND :" + PARAM_TO;
+
+	private static final RowMapper<DBOMonthlyStatisticsStatus> DBO_MAPPER = new DBOMonthlyStatisticsStatus().getTableMapping();
+
+	private static final RowMapper<StatisticsMonthlyStatus> ROW_MAPPER = new RowMapper<StatisticsMonthlyStatus>() {
+
+		@Override
+		public StatisticsMonthlyStatus mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return map(DBO_MAPPER.mapRow(rs, rowNum));
+		}
+	};
+
+	private DBOBasicDao basicDao;
+	private NamedParameterJdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public StatisticsMonthlyStatusDAOImpl(DBOBasicDao basicDao, NamedParameterJdbcTemplate jdbcTemplate) {
+		this.basicDao = basicDao;
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	@WriteTransaction
+	public StatisticsMonthlyStatus setAvailable(StatisticsObjectType objectType, YearMonth month) {
+		return createOrUpdate(objectType, month, StatisticsStatus.AVAILABLE, System.currentTimeMillis(), null);
+	}
+
+	@Override
+	@WriteTransaction
+	public StatisticsMonthlyStatus setProcessingFailed(StatisticsObjectType objectType, YearMonth month) {
+		return createOrUpdate(objectType, month, StatisticsStatus.PROCESSING_FAILED, null, System.currentTimeMillis());
+	}
+
+	@Override
+	@WriteTransaction
+	public StatisticsMonthlyStatus setProcessing(StatisticsObjectType objectType, YearMonth month) {
+		return createOrUpdate(objectType, month, StatisticsStatus.PROCESSING, null, null);
+	}
+
+	@Override
+	public Optional<StatisticsMonthlyStatus> getStatus(StatisticsObjectType objectType, YearMonth month) {
+		ValidateArgument.required(objectType, "objectType");
+		ValidateArgument.required(month, "month");
+
+		SqlParameterSource params = getPrimaryKeyParams(objectType, month);
+
+		DBOMonthlyStatisticsStatus dbo = basicDao.getObjectByPrimaryKeyIfExists(DBOMonthlyStatisticsStatus.class, params);
+
+		if (dbo == null) {
+			return Optional.empty();
+		}
+
+		return Optional.of(map(dbo));
+	}
+
+	@Override
+	public List<StatisticsMonthlyStatus> getAvailableStatusInRange(StatisticsObjectType objectType, YearMonth from, YearMonth to) {
+		ValidateArgument.required(objectType, "objectType");
+		ValidateArgument.required(from, "from");
+		ValidateArgument.required(to, "to");
+		ValidateArgument.requirement(from.isBefore(to), "The start of the range should be before the end");
+
+		MapSqlParameterSource params = new MapSqlParameterSource();
+
+		params.addValue(PARAM_OBJECT_TYPE, objectType.toString());
+		params.addValue(PARAM_FROM, StatisticsMonthlyUtils.toDate(from));
+		params.addValue(PARAM_TO, StatisticsMonthlyUtils.toDate(to));
+
+		return jdbcTemplate.query(SQL_SELECT_IN_RANGE, params, ROW_MAPPER);
+	}
+
+	@Override
+	@WriteTransaction
+	public void clear() {
+		jdbcTemplate.update(SQL_DELETE_ALL, EmptySqlParameterSource.INSTANCE);
+	}
+
+	private StatisticsMonthlyStatus createOrUpdate(StatisticsObjectType objectType, YearMonth month, StatisticsStatus status,
+			Long lastSucceededAt, Long lastFailedAt) {
+		ValidateArgument.required(objectType, "objectType");
+		ValidateArgument.required(month, "month");
+		ValidateArgument.required(status, "status");
+
+		DBOMonthlyStatisticsStatus dbo = new DBOMonthlyStatisticsStatus();
+
+		dbo.setObjectType(objectType.toString());
+		dbo.setMonth(StatisticsMonthlyUtils.toDate(month));
+		dbo.setStatus(status.toString());
+		dbo.setLastSucceededAt(lastSucceededAt);
+		dbo.setLastFailedAt(lastFailedAt);
+
+		return map(basicDao.createOrUpdate(dbo));
+	}
+
+	private SqlParameterSource getPrimaryKeyParams(StatisticsObjectType objectType, YearMonth month) {
+		MapSqlParameterSource params = new MapSqlParameterSource();
+
+		params.addValue(PARAM_OBJECT_TYPE, objectType.toString());
+		params.addValue(PARAM_MONTH, StatisticsMonthlyUtils.toDate(month));
+
+		return params;
+	}
+
+	private static StatisticsMonthlyStatus map(DBOMonthlyStatisticsStatus dbo) {
+		StatisticsMonthlyStatus dto = new StatisticsMonthlyStatus();
+
+		dto.setObjectType(StatisticsObjectType.valueOf(dbo.getObjectType()));
+		dto.setStatus(StatisticsStatus.valueOf(dbo.getStatus()));
+		dto.setMonth(YearMonth.from(dbo.getMonth()));
+		dto.setLastSucceededAt(dbo.getLastSucceededAt());
+		dto.setLastFailedAt(dbo.getLastFailedAt());
+
+		return dto;
+
+	}
+
+}
--- lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/dao/statistics/StatisticsMonthlyStatusDAOTest.java
+++ lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/dbo/dao/statistics/StatisticsMonthlyStatusDAOTest.java
@@ -1,6 +1,24 @@
 package org.sagebionetworks.repo.model.dbo.dao.statistics;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.sagebionetworks.repo.model.dao.statistics.StatisticsMonthlyStatusDAO;
+import org.sagebionetworks.repo.model.statistics.StatisticsMonthlyStatus;
+import org.sagebionetworks.repo.model.statistics.StatisticsObjectType;
+import org.sagebionetworks.repo.model.statistics.StatisticsStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -9,8 +27,237 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ContextConfiguration(locations = { "classpath:jdomodels-test-context.xml" })
 public class StatisticsMonthlyStatusDAOTest {
 
+	private static final StatisticsObjectType OBJECT_TYPE = StatisticsObjectType.PROJECT;
+
 	@Autowired
-	private StatisticsMonthlyDAOImpl dao;
+	private StatisticsMonthlyStatusDAO dao;
+
+	@BeforeEach
+	public void before() {
+		dao.clear();
+	}
+
+	@AfterEach
+	public void after() {
+		dao.clear();
+	}
+
+	@Test
+	public void testSetOnCreate() {
+		int year = 2019;
+		int month = 8;
+
+		YearMonth yearMonth = YearMonth.of(year, month);
+
+		// Makes sure that it does not exist
+		assertFalse(dao.getStatus(OBJECT_TYPE, yearMonth).isPresent());
+
+		// Call under test
+		StatisticsMonthlyStatus status = dao.setAvailable(OBJECT_TYPE, yearMonth);
+
+		assertEquals(yearMonth, status.getMonth());
+		assertEquals(StatisticsStatus.AVAILABLE, status.getStatus());
+	}
+
+	@Test
+	public void testSetOnUpdate() {
+		int year = 2019;
+		int month = 8;
+
+		YearMonth yearMonth = YearMonth.of(year, month);
+
+		// First set a status
+		dao.setProcessing(OBJECT_TYPE, yearMonth);
+
+		// Makes sure that it does exist
+		Optional<StatisticsMonthlyStatus> getStatusResult = dao.getStatus(OBJECT_TYPE, yearMonth);
+
+		assertTrue(getStatusResult.isPresent());
+
+		StatisticsMonthlyStatus status = getStatusResult.get();
+
+		assertEquals(StatisticsStatus.PROCESSING, status.getStatus());
+
+		// Call under test
+		StatisticsMonthlyStatus updated = dao.setAvailable(OBJECT_TYPE, yearMonth);
+
+		assertEquals(yearMonth, updated.getMonth());
+		assertEquals(StatisticsStatus.AVAILABLE, updated.getStatus());
+	}
 	
+	@Test
+	public void testSetFailedWithEmptyType() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth yearMonth = YearMonth.of(2019, 8);
+			dao.setProcessingFailed(null, yearMonth);
+		});
+	}
+	
+	@Test
+	public void testSetFailedWithEmptyMonth() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth yearMonth = null;
+			dao.setProcessingFailed(OBJECT_TYPE, yearMonth);
+		});
+	}
+	
+	@Test
+	public void testSetAvailableWithEmptyType() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth yearMonth = YearMonth.of(2019, 8);
+			dao.setAvailable(null, yearMonth);
+		});
+	}
+	
+	@Test
+	public void testSetAvailableWithEmptyMonth() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth yearMonth = null;
+			dao.setAvailable(OBJECT_TYPE, yearMonth);
+		});
+	}
+	
+	@Test
+	public void testSetProcessingWithEmptyType() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth yearMonth = YearMonth.of(2019, 8);
+			dao.setProcessing(null, yearMonth);
+		});
+	}
+	
+	@Test
+	public void testSetProcessingWithEmptyMonth() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth yearMonth = null;
+			dao.setProcessing(OBJECT_TYPE, yearMonth);
+		});
+	}
+
+	@Test
+	public void testSetAvailableTimestamp() {
+		int year = 2019;
+		int month = 8;
+
+		YearMonth yearMonth = YearMonth.of(year, month);
+
+		// Call under test
+		StatisticsMonthlyStatus status = dao.setAvailable(OBJECT_TYPE, yearMonth);
+
+		assertEquals(yearMonth, status.getMonth());
+		assertEquals(StatisticsStatus.AVAILABLE, status.getStatus());
+		assertNotNull(status.getLastSucceededAt());
+		assertNull(status.getLastFailedAt());
+	}
+
+	@Test
+	public void testSetFailedTimestamp() {
+		int year = 2019;
+		int month = 8;
+
+		YearMonth yearMonth = YearMonth.of(year, month);
+
+		// Call under test
+		StatisticsMonthlyStatus status = dao.setProcessingFailed(OBJECT_TYPE, yearMonth);
+
+		assertEquals(yearMonth, status.getMonth());
+		assertEquals(StatisticsStatus.PROCESSING_FAILED, status.getStatus());
+		assertNull(status.getLastSucceededAt());
+		assertNotNull(status.getLastFailedAt());
+	}
+
+	@Test
+	public void testSetProcessing() {
+		int year = 2019;
+		int month = 8;
+
+		YearMonth yearMonth = YearMonth.of(year, month);
+
+		// Call under test
+		StatisticsMonthlyStatus status = dao.setProcessing(OBJECT_TYPE, yearMonth);
+
+		assertEquals(yearMonth, status.getMonth());
+		assertEquals(StatisticsStatus.PROCESSING, status.getStatus());
+		assertNull(status.getLastSucceededAt());
+		assertNull(status.getLastFailedAt());
+	}
+	
+	@Test
+	public void testGetStatusWithEmptyType() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth yearMonth = YearMonth.of(2019, 8);
+			// Call under test
+			dao.getStatus(null, yearMonth);
+		});
+	}
+	
+	@Test
+	public void testGetStatusWithEmptyMonth() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth yearMonth = null;
+			// Call under test
+			dao.getStatus(OBJECT_TYPE, yearMonth);
+		});
+	}
+	
+	@Test
+	public void testGetStatusEmpty() {
+		int year = 2019;
+		int month = 8;
+
+		YearMonth yearMonth = YearMonth.of(year, month);
+		
+		Optional<StatisticsMonthlyStatus> getStatusResult = dao.getStatus(OBJECT_TYPE, yearMonth);
+		
+		assertFalse(getStatusResult.isPresent());
+	}
+	
+	@Test
+	public void testGetAvailableStatusInRangeWithIllegalRange() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth from = YearMonth.of(2019, 8);
+			YearMonth to = YearMonth.of(2019, 8);
+			// Call under test
+			dao.getAvailableStatusInRange(OBJECT_TYPE, from, to);
+		});
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth from = YearMonth.of(2019, 9);
+			YearMonth to = YearMonth.of(2019, 8);
+			// Call under test
+			dao.getAvailableStatusInRange(OBJECT_TYPE, from, to);
+		});
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth from = null;
+			YearMonth to = YearMonth.of(2019, 8);
+			// Call under test
+			dao.getAvailableStatusInRange(OBJECT_TYPE, from, to);
+		});
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth from = YearMonth.of(2019, 7);
+			YearMonth to = null;
+			// Call under test
+			dao.getAvailableStatusInRange(OBJECT_TYPE, from, to);
+		});
+	}
+	
+	@Test
+	public void testGetAvailableStatusInRangeWithEmptyType() {
+		assertThrows(IllegalArgumentException.class, ()-> {
+			YearMonth from = YearMonth.of(2019, 7);
+			YearMonth to = YearMonth.of(2019, 8);
+			// Call under test
+			dao.getAvailableStatusInRange(null, from, to);
+		});
+	}
+	
+	@Test
+	public void testGetAvailableStatusInRangeEmpty() {
+		YearMonth from = YearMonth.of(2019, 7);
+		YearMonth to = YearMonth.of(2019, 8);
+		
+		// Call under test
+		List<StatisticsMonthlyStatus> result = dao.getAvailableStatusInRange(OBJECT_TYPE, from, to);
+		
+		assertTrue(result.isEmpty());
+	}
 	
 }
