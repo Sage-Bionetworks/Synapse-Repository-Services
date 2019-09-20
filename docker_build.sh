549d4864c17e92667652dda95ad2e5da9d9d6472
Ziming Dong <ziming.dong@sagebase.org>
2019-09-20 16:32:23 -0700

    force update mvn snapshots

--- docker_build.sh
+++ docker_build.sh
@@ -82,7 +82,7 @@ docker run -i --rm --name ${build_container_name} \
 -e MAVEN_OPTS="-Xms256m -Xmx2048m -XX:MaxPermSize=512m" \
 -w /repo \
 maven:3-jdk-8 \
-bash -c "mvn clean ${MVN_GOAL} \
+bash -c "mvn clean ${MVN_GOAL} -U \
 -Dorg.sagebionetworks.repository.database.connection.url=jdbc:mysql://${org_sagebionetworks_repository_database_connection_url}/${db_name} \
 -Dorg.sagebionetworks.id.generator.database.connection.url=jdbc:mysql://${org_sagebionetworks_repository_database_connection_url}/${db_name} \
 -Dorg.sagebionetworks.repository.database.username=${rds_user_name} \
