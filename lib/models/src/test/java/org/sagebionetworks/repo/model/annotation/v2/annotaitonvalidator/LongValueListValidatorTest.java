39fdc62829ae6c62487dfe8f4dabbdfbf2a43077
Ziming Dong <ziming.dong@sagebase.org>
2019-10-03 18:37:10 -0700

    fix annotation values validation for strings

---
+++ lib/models/src/test/java/org/sagebionetworks/repo/model/annotation/v2/annotaitonvalidator/LongValueListValidatorTest.java
@@ -0,0 +1,58 @@
+package org.sagebionetworks.repo.model.annotation.v2.annotaitonvalidator;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.sagebionetworks.repo.model.annotation.v2.AnnotationsValueType;
+
+class LongValueListValidatorTest {
+
+	LongValueListValidator valueValidator = new LongValueListValidator(AnnotationsValueType.LONG);
+	String key = "myKey";
+
+	@Test
+	void testValidate_valid() {
+		valueValidator.validate(key, "-123");
+		valueValidator.validate(key, "0");
+		valueValidator.validate(key, "123");
+	}
+
+	@Test
+	void testValidate_invalid() {
+		assertThrows(IllegalArgumentException.class, ()->{
+			valueValidator.validate(key, (String) null);
+		});
+		assertThrows(IllegalArgumentException.class, ()->{
+			valueValidator.validate(key, "-123.1");
+		});
+		assertThrows(IllegalArgumentException.class, ()->{
+			valueValidator.validate(key, "0.0");
+		});
+		assertThrows(IllegalArgumentException.class, ()->{
+			valueValidator.validate(key, "123.0");
+		});
+		assertThrows(IllegalArgumentException.class, ()->{
+			valueValidator.validate(key, "asdf");
+		});
+		assertThrows(IllegalArgumentException.class, ()->{
+			valueValidator.validate(key, "");
+		});
+	}
+
+
+	@Test
+	public void testValidateList_allValid(){
+		valueValidator.validate(key , Arrays.asList("123", "456", "678999787978"));
+	}
+
+	@Test
+	public void testValidateList_containsInValid(){
+		assertThrows(IllegalArgumentException.class, () -> {
+			valueValidator.validate(key, Arrays.asList("123", "nope", "23131"));
+		});
+	}
+}
---
+++ lib/models/src/test/java/org/sagebionetworks/repo/model/annotation/v2/annotaitonvalidator/StringValueListValidatorTest.java
@@ -0,0 +1,56 @@
+package org.sagebionetworks.repo.model.annotation.v2.annotaitonvalidator;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import com.google.common.base.Strings;
+import org.junit.jupiter.api.Test;
+import org.sagebionetworks.repo.model.annotation.v2.AnnotationsValueType;
+
+class StringValueListValidatorTest {
+
+	StringValueListValidator valueValidator = new StringValueListValidator(AnnotationsValueType.STRING);
+	String key = "myKey";
+
+	@Test
+	void testValidate_valid() {
+		//anything except values exceeding the limit should return true
+		valueValidator.validate(key, Collections.singletonList(""));
+		valueValidator.validate(key, Collections.singletonList("asdf"));
+		valueValidator.validate(key, Collections.singletonList("123"));
+		valueValidator.validate(key, Collections.singletonList("123.456"));
+		valueValidator.validate(key, Collections.singletonList(Strings.repeat("a", StringValueListValidator.MAX_STRING_SIZE)));
+		int secondStringSize = 42;
+		valueValidator.validate(key, Arrays.asList(Strings.repeat("a", StringValueListValidator.MAX_STRING_SIZE - secondStringSize ),
+				Strings.repeat("a", secondStringSize )));
+	}
+
+	@Test
+	void testValidate_containsNull(){
+		assertThrows(IllegalArgumentException.class, ()->{
+			valueValidator.validate(key, Collections.singletonList(null));
+		});
+		assertThrows(IllegalArgumentException.class, ()->{
+			valueValidator.validate(key, Arrays.asList("asdf", null, "asdf"));
+		});
+
+	}
+
+	@Test
+	void testValidate_exceedLength() {
+		assertThrows(IllegalArgumentException.class, ()->{
+			valueValidator.validate(key, Collections.singletonList(Strings.repeat("a", StringValueListValidator.MAX_STRING_SIZE + 1)));
+		});
+
+		int secondStringSize = 42;
+		assertThrows(IllegalArgumentException.class, ()->{
+			valueValidator.validate(key, Arrays.asList(Strings.repeat("a", StringValueListValidator.MAX_STRING_SIZE - secondStringSize ),
+					Strings.repeat("a", secondStringSize ),
+					"a"));
+		});
+	}
+
+}
