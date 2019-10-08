6d8ec9005023d797a061a2c11e16aadc083bfd23
john-hill <john.hill@sagebase.org>
2019-10-08 12:33:26 -0700

    viewSnapshotDao tests

--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/dao/table/ViewSnapshotDao.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/dao/table/ViewSnapshotDao.java
@@ -23,4 +23,9 @@ public interface ViewSnapshotDao {
 	 */
 	ViewSnapshot getSnapshot(IdAndVersion idAndVersion);
 
+	/**
+	 * Truncate all snapshots.
+	 */
+	void truncateAll();
+
 }
--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/dao/table/ViewSnapshotDaoImpl.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/dao/table/ViewSnapshotDaoImpl.java
@@ -1,43 +1,62 @@
 package org.sagebionetworks.repo.model.dbo.dao.table;
 
 import static org.sagebionetworks.repo.model.query.jdo.SqlConstants.COL_VIEW_SNAPSHOT_VERSION;
-import static org.sagebionetworks.repo.model.query.jdo.SqlConstants.COL_VIEW_SNAPSHOT_VIEW_ID;
+import static org.sagebionetworks.repo.model.query.jdo.SqlConstants.*;
 import static org.sagebionetworks.repo.model.query.jdo.SqlConstants.TABLE_VIEW_SNAPSHOT;
 
 import java.sql.Timestamp;
 
 import org.sagebionetworks.ids.IdGenerator;
 import org.sagebionetworks.ids.IdType;
+import org.sagebionetworks.repo.model.DatastoreException;
 import org.sagebionetworks.repo.model.dbo.DBOBasicDao;
 import org.sagebionetworks.repo.model.entity.IdAndVersion;
+import org.sagebionetworks.repo.transactions.WriteTransaction;
+import org.sagebionetworks.repo.web.NotFoundException;
 import org.sagebionetworks.util.ValidateArgument;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import com.amazonaws.services.apigatewayv2.model.NotFoundException;
-
 @Repository
 public class ViewSnapshotDaoImpl implements ViewSnapshotDao {
-	
+
 	@Autowired
 	IdGenerator idGenerator;
 	@Autowired
 	DBOBasicDao basicDao;
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
+
 	private static RowMapper<DBOViewSnapshot> MAPPER = new DBOViewSnapshot().getTableMapping();
 
+	@WriteTransaction
 	@Override
 	public ViewSnapshot createSnapshot(ViewSnapshot snapshot) {
+		ValidateArgument.required(snapshot, "snapshot");
+		ValidateArgument.required(snapshot.getViewId(), "snapshot.viewId");
+		ValidateArgument.required(snapshot.getVersion(), "snapshot.version");
+		ValidateArgument.required(snapshot.getCreatedBy(), "snapshot.createdBy");
+		ValidateArgument.required(snapshot.getCreatedOn(), "snapshot.createdOn");
+		ValidateArgument.required(snapshot.getBucket(), "snapshot.bucket");
+		ValidateArgument.required(snapshot.getKey(), "snapshot.key");
 		DBOViewSnapshot dbo = translate(snapshot);
 		dbo.setSnapshotId(idGenerator.generateNewId(IdType.VIEW_SNAPSHOT_ID));
-		return translate(basicDao.createNew(dbo));
+		try {
+			return translate(basicDao.createNew(dbo));
+		} catch (IllegalArgumentException e) {
+			if(DuplicateKeyException.class.equals(e.getCause().getClass())) {
+				throw new IllegalArgumentException("Snapshot already exists for: " + IdAndVersion.newBuilder()
+				.setId(snapshot.getViewId()).setVersion(snapshot.getVersion()).build().toString(), e);
+			}else {
+				throw e;
+			}
+		}
 	}
-	
+
 	@Override
 	public ViewSnapshot getSnapshot(IdAndVersion idAndVersion) {
 		ValidateArgument.required(idAndVersion, "idAndVersion");
@@ -48,10 +67,10 @@ public class ViewSnapshotDaoImpl implements ViewSnapshotDao {
 							+ COL_VIEW_SNAPSHOT_VERSION + " = ?",
 					MAPPER, idAndVersion.getId(), idAndVersion.getVersion().get()));
 		} catch (EmptyResultDataAccessException e) {
-			throw new NotFoundException("Snapshot not found for: "+idAndVersion.toString());
+			throw new NotFoundException("Snapshot not found for: " + idAndVersion.toString(), e);
 		}
 	}
-	
+
 	static DBOViewSnapshot translate(ViewSnapshot dto) {
 		DBOViewSnapshot dbo = new DBOViewSnapshot();
 		dbo.setSnapshotId(dto.getSnapshotId());
@@ -63,16 +82,17 @@ public class ViewSnapshotDaoImpl implements ViewSnapshotDao {
 		dbo.setKey(dto.getKey());
 		return dbo;
 	}
-	
+
 	static ViewSnapshot translate(DBOViewSnapshot in) {
-		return new ViewSnapshot()
-				.withSnapshotId(in.getSnapshotId())
-				.withViewId(in.getViewId())
-				.withVersion(in.getVersion())
-				.withCreatedBy(in.getCreatedBy())
-				.withCreatedOn(in.getCreatedOn())
-				.withBucket(in.getBucket())
-				.withKey(in.getKey());
+		return new ViewSnapshot().withSnapshotId(in.getSnapshotId()).withViewId(in.getViewId())
+				.withVersion(in.getVersion()).withCreatedBy(in.getCreatedBy()).withCreatedOn(in.getCreatedOn())
+				.withBucket(in.getBucket()).withKey(in.getKey());
+	}
+
+	@WriteTransaction
+	@Override
+	public void truncateAll() {
+		jdbcTemplate.update("DELETE FROM " + TABLE_VIEW_SNAPSHOT + " WHERE " + COL_VIEW_SNAPSHOT_ID + " > 0");
 	}
 
 }
