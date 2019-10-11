1934ab272982d8bc5400a96fa9c291f86852bfbf
Ziming Dong <ziming.dong@sagebase.org>
2019-10-10 22:30:49 -0700

    start test for translationreferencelookup

---
+++ lib/lib-table-cluster/src/test/java/org/sagebionetworks/table/cluster/columntranslation/ColumnTranslationReferenceLookupTest.java
@@ -0,0 +1,14 @@
+package org.sagebionetworks.table.cluster.columntranslation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.sagebionetworks.repo.model.table.ColumnModel;
+
+class ColumnTranslationReferenceLookupTest {
+
+	ColumnModel fooModel;
+	ColumnModel barModel;
+
+
+
+}
