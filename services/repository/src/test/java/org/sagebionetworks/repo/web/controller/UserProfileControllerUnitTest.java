3d8d3e4bc6c05a7c9b3fe4ea8c9ac20fa7c58994
bhoff <bruce.hoff@sagebase.org>
2019-10-21 19:12:32 -0700

    PLFM-5751

--- services/repository/src/test/java/org/sagebionetworks/repo/web/controller/UserProfileControllerUnitTest.java
+++ services/repository/src/test/java/org/sagebionetworks/repo/web/controller/UserProfileControllerUnitTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
