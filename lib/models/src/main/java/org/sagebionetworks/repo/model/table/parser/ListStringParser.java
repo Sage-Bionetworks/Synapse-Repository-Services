7268cc9d737465de03ca63d88d9c62b20f0ff239
Ziming Dong <ziming.dong@sagebase.org>
2019-10-24 19:53:46 -0700

    switching to usage of columntype for lists

---
+++ lib/models/src/main/java/org/sagebionetworks/repo/model/table/parser/ListStringParser.java
@@ -0,0 +1,50 @@
+package org.sagebionetworks.repo.model.table.parser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.sagebionetworks.repo.model.table.ValueParser;
+
+/**
+ * Parser for JSON List (e.g. ["a","b", "c"] or [1,2,3] )
+ */
+public class ListStringParser extends AbstractValueParser{
+	private ValueParser individualElementParser;
+
+	/**
+	 *
+	 * @param individualElementParser parser that will be applied to each element in the list
+	 */
+	public ListStringParser(ValueParser individualElementParser){
+		this.individualElementParser = individualElementParser;
+	}
+
+	@Override
+	public Object parseValueForDatabaseWrite(String value) throws IllegalArgumentException {//TODO: test
+		try {
+			JSONArray parsed = new JSONArray(value);
+			JSONArray toDatabase = new JSONArray();
+			for(int i = 0; i < parsed.length(); i++){
+				if(parsed.isNull(i)){
+					throw new IllegalArgumentException("null value is not allowed");
+				}
+
+				Object parsedObject = individualElementParser.parseValueForDatabaseWrite(parsed.getString(i));
+
+				//nan and inf are written as string
+				if(parsedObject instanceof Double && (((Double) parsedObject).isInfinite() || ((Double) parsedObject).isNaN())){
+					parsedObject = parsedObject.toString();
+				}
+				toDatabase.put(parsedObject);
+			}
+			return toDatabase.toString();
+		} catch (JSONException e){
+			throw new IllegalArgumentException("not a JSON Array:" + value);
+		}
+	}
+
+	@Override
+	public String parseValueForDatabaseRead(String value) throws IllegalArgumentException {
+		// the value should already have be a JSON string when retrieved from the database so nothing left to do
+		return value;
+	}
+}
