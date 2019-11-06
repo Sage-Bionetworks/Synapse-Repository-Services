0f242c3df11f576a2413e021d18533f31664ad8f
bhoff <bruce.hoff@sagebase.org>
2019-11-05 18:52:26 -0800

    PLFM-5934

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/oauth/claimprovider/EmailClaimProvider.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/oauth/claimprovider/EmailClaimProvider.java
@@ -1,16 +1,14 @@
 package org.sagebionetworks.repo.manager.oauth.claimprovider;
 
-import java.util.List;
-
-import org.sagebionetworks.repo.manager.UserProfileManager;
-import org.sagebionetworks.repo.model.UserProfile;
+import org.sagebionetworks.repo.model.dao.NotificationEmailDAO;
 import org.sagebionetworks.repo.model.oauth.OIDCClaimName;
 import org.sagebionetworks.repo.model.oauth.OIDCClaimsRequestDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class EmailClaimProvider implements OIDCClaimProvider {
 	@Autowired
-	private UserProfileManager userProfileManager;
+	private NotificationEmailDAO notificationEmailDao;
+	
 
 	@Override
 	public OIDCClaimName getName() {
@@ -19,17 +17,12 @@ public class EmailClaimProvider implements OIDCClaimProvider {
 
 	@Override
 	public String getDescription() {
-		return "Your email address";
+		return "Your primary email address";
 	}
 
 	@Override
 	public Object getClaim(String userId, OIDCClaimsRequestDetails details) {
-		UserProfile userProfile = userProfileManager.getUserProfile(userId);
-		List<String> emails = userProfile.getEmails();
-		if (emails==null || emails.isEmpty()) {
-			return null;
-		}
-		return emails.get(0);
+		return notificationEmailDao.getNotificationEmailForPrincipal(Long.parseLong(userId));
 	}
 
 }
--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/oauth/claimprovider/EmailClaimProviderTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/oauth/claimprovider/EmailClaimProviderTest.java
@@ -5,44 +5,32 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 
-import java.util.Collections;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.sagebionetworks.repo.manager.UserProfileManager;
-import org.sagebionetworks.repo.model.UserProfile;
+import org.sagebionetworks.repo.model.dao.NotificationEmailDAO;
 import org.sagebionetworks.repo.model.oauth.OIDCClaimName;
-
-import com.google.common.collect.ImmutableList;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EmailClaimProviderTest {
 	
 	@Mock
-	private UserProfileManager mockUserProfileManager;
+	private NotificationEmailDAO mockNotificationEmailDao;
 	
 	@InjectMocks
 	private EmailClaimProvider claimProvider;
 	
 	private static final String USER_ID = "101";
+	private static final long USER_ID_LONG = Long.parseLong(USER_ID);
 	
 	private static final String EMAIL = "my@email.com";
 	
-	private UserProfile userProfile;
-	
-	@Before
-	public void setUp() {
-		userProfile = new UserProfile();
-		when(mockUserProfileManager.getUserProfile(USER_ID)).thenReturn(userProfile);
-		userProfile.setEmails(ImmutableList.of(EMAIL, "secondary email"));
-	}
-
 	@Test
 	public void testEmailClaim() {
+		when(mockNotificationEmailDao.getNotificationEmailForPrincipal(USER_ID_LONG)).thenReturn(EMAIL);
 		// method under test
 		assertEquals(OIDCClaimName.email, claimProvider.getName());
 		// method under test
@@ -53,11 +41,7 @@ public class EmailClaimProviderTest {
 
 	@Test
 	public void testEmailClaimMissingEmail() {
-		userProfile.setEmails(null);
-		// method under test
-		assertNull(claimProvider.getClaim(USER_ID, null));
-
-		userProfile.setEmails(Collections.EMPTY_LIST);
+		when(mockNotificationEmailDao.getNotificationEmailForPrincipal(USER_ID_LONG)).thenReturn(null);
 		// method under test
 		assertNull(claimProvider.getClaim(USER_ID, null));
 	}
