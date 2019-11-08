95df5718178523eef050ba80733c6220fa609dbb
Ziming Dong <ziming.dong@sagebase.org>
2019-11-07 18:46:26 -0800

    unused import

--- lib/lib-table-cluster/src/main/java/org/sagebionetworks/table/cluster/utils/ColumnConstants.java
+++ lib/lib-table-cluster/src/main/java/org/sagebionetworks/table/cluster/utils/ColumnConstants.java
@@ -6,8 +6,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.sagebionetworks.repo.model.table.parser.ListStringParser;
-
 public class ColumnConstants {
 
 	/**
