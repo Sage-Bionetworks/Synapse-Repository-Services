4dbd26cd8a57d4f45980b60f71f4766bc5f4267c
john-hill <john.hill@sagebase.org>
2019-12-09 19:04:34 -0800

    test for cases where the csv contains the UTF-8 BOM maker as a prefix of the file

--- services/workers/src/test/java/org/sagebionetworks/table/worker/UploadPreviewBuilderTest.java
+++ services/workers/src/test/java/org/sagebionetworks/table/worker/UploadPreviewBuilderTest.java
@@ -9,12 +9,16 @@ import static org.sagebionetworks.repo.model.table.TableConstants.ROW_ID;
 import static org.sagebionetworks.repo.model.table.TableConstants.ROW_VERSION;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -27,6 +31,7 @@ import org.sagebionetworks.repo.model.table.CsvTableDescriptor;
 import org.sagebionetworks.repo.model.table.Row;
 import org.sagebionetworks.repo.model.table.UploadToTablePreviewRequest;
 import org.sagebionetworks.repo.model.table.UploadToTablePreviewResult;
+import org.sagebionetworks.table.cluster.utils.CSVUtils;
 
 import au.com.bytecode.opencsv.CSVReader;
 
@@ -837,6 +842,42 @@ public class UploadPreviewBuilderTest {
 		assertEquals(null, UploadPreviewBuilder.parseAsLong(null));
 		assertEquals(null, UploadPreviewBuilder.parseAsLong(""));
 		assertEquals(new Long(123), UploadPreviewBuilder.parseAsLong("123"));
+	}
+	
+	@Test
+	public void testPLFM_5989() throws IOException {
+		String fileName = "testPLFM_5989.csv";
+		String csvString = null;
+		try(InputStream in =UploadPreviewBuilderTest.class.getClassLoader().getResourceAsStream(fileName)){
+			assertNotNull("Cannot find: "+fileName+" on classpath", in);
+			csvString = IOUtils.toString(in, StandardCharsets.UTF_8);
+		}
+		
+		CsvTableDescriptor descriptor = new CsvTableDescriptor();
+		descriptor.setIsFirstLineHeader(true);
+		UploadToTablePreviewRequest request = new UploadToTablePreviewRequest();
+		request.setCsvTableDescriptor(descriptor);
+		
+		StringReader sReader = new StringReader(csvString);
+		Long linesToSkip = 0L;
+		CSVReader reader = CSVUtils.createCSVReader(sReader, descriptor, linesToSkip);
+		
+		UploadPreviewBuilder builder = new UploadPreviewBuilder(reader, mockProgressCallback, request);
+		// call under test
+		UploadToTablePreviewResult result = builder.buildResult();
+		assertNotNull(result);
+		assertNotNull(result.getSuggestedColumns());
+		assertEquals(2, result.getSuggestedColumns().size());
+		// one
+		ColumnModel cm = result.getSuggestedColumns().get(0);
+		assertEquals(ColumnType.STRING, cm.getColumnType());
+		assertEquals("column1", cm.getName());
+		assertEquals(new Long(5), cm.getMaximumSize());
+		// two
+		cm = result.getSuggestedColumns().get(1);
+		assertEquals(ColumnType.DOUBLE, cm.getColumnType());
+		assertEquals("column2", cm.getName());
+		assertEquals(new Long(5), cm.getMaximumSize());
 	}
 	
 	public static List<String> getColumnNames(List<ColumnModel> models){
