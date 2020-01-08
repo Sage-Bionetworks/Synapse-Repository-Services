861fe4da45739aa17c8f6d428587aabd4a24d15d
Ziming Dong <ziming.dong@sagebase.org>
2020-01-07 17:57:50 -0800

    accept base64 md5

--- lib/communicationUtilities/src/main/java/org/sagebionetworks/utils/MD5ChecksumHelper.java
+++ lib/communicationUtilities/src/main/java/org/sagebionetworks/utils/MD5ChecksumHelper.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Base64;
 import java.util.regex.Pattern;
 
 import org.apache.commons.codec.digest.DigestUtils;
@@ -17,14 +18,34 @@ public class MD5ChecksumHelper {
 	// Exactly 32 characters, hexadecimal.
 	private static final Pattern VALID_MD5_REGEX = Pattern.compile("^[0-9a-fA-F]{32}$");
 
+	// each hex value is log2(16)=4 bits or 1/2 of a byte
+	// 32 hex values contains a total of 32/2=16 bytes
+	private static final int NUM_BYTES_IN_MD5 = 16;
+
+	// According to base64 encoding, data is split into blocks of 3 bytes resulting in ceil(16/3)=6 blocks
+	// Each blocks converts into a 4 character base64 string, resulting in 6*4=24 characters total
+	private static final int BASE_64_MD5_STRING_LEN = 24;
+
 	/**
 	 * Determines if a hexadecimal string is a valid MD5 digest (i.e. is exactly 32 hexadecimal characters).
 	 * @param hexDigest The hexadecimal string
-	 * @return true iff the string is a valid MD5 digest
+	 * @throws IllegalArgumentException if the MD5 digest is invalid
 	 */
 	public static boolean isValidMd5Digest(String hexDigest) {
 		if (hexDigest == null) throw new IllegalArgumentException("The MD5 digest may not be null");
-		return VALID_MD5_REGEX.matcher(hexDigest).matches();
+		if(!VALID_MD5_REGEX.matcher(hexDigest).matches()){
+			//if not a hex digest, see if it is a base64 encoded MD5
+			if(hexDigest.length() == BASE_64_MD5_STRING_LEN) {
+				try {
+					byte[] decoded = Base64.getDecoder().decode(hexDigest);
+					return decoded.length == NUM_BYTES_IN_MD5;
+				} catch (IllegalArgumentException e) { //not a base64 string
+					return false;
+				}
+			}
+			return false;
+		}
+		return true;
 	}
 
 	/**
--- lib/communicationUtilities/src/test/java/org/sagebionetworks/utils/MD5ChecksumHelperTest.java
+++ lib/communicationUtilities/src/test/java/org/sagebionetworks/utils/MD5ChecksumHelperTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
@@ -30,6 +31,7 @@ class MD5ChecksumHelperTest {
 	public void checkInvalidMD5TooLong() {
 		assertFalse(MD5ChecksumHelper.isValidMd5Digest("b7241f813875de12517c11394add7ae4a"));
 	}
+
 	@Test
 	public void checkInvalidMD5NonHexadecimal() {
 		// starts with 'g'
@@ -46,6 +48,29 @@ class MD5ChecksumHelperTest {
 		assertThrows(IllegalArgumentException.class, () -> MD5ChecksumHelper.isValidMd5Digest(null));
 	}
 
+	@Test
+	public void checkValidMD5_Base64() {
+		assertTrue(MD5ChecksumHelper.isValidMd5Digest("+XHdk3AQ65DR96cC0n7N4g=="));
+	}
+
+	@Test
+	public void checkInValidMD5_Base64_IncorrectStringLength() {
+		assertFalse(MD5ChecksumHelper.isValidMd5Digest("k3AQ65DR96cC0n7N4g=="));
+		assertFalse(MD5ChecksumHelper.isValidMd5Digest("7N4g+XHdk3AQ65DR96cC0n7N4g=="));
+	}
+
+	@Test
+	public void checkInValidMD5_Base64_notBase64String() {
+		//string that is size 24 but not in base64 character set
+		assertFalse(MD5ChecksumHelper.isValidMd5Digest(String.join("", Collections.nCopies(24, "?"))));
+	}
+
+	@Test
+	public void checkInValidMD5_Base64_IncorrectDecodedByteLength() {
+		// "=" is a padding string which means it does not convert into any bits.
+		// this string has an additional value replaced with a padding character.
+		assertFalse(MD5ChecksumHelper.isValidMd5Digest("+XHdk3AQ65DR96cC0n7N4==="));
+	}
 
 	@Test
 	public void testGetMD5Checksum() throws Exception {
