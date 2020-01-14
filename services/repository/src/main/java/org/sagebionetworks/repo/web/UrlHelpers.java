d1935b15326ebe8c37ae18dc8c70f2eec4e1c28e
bhoff <bruce.hoff@sagebase.org>
2020-01-14 14:25:42 -0800

    PLFM-4294

--- services/repository/src/main/java/org/sagebionetworks/repo/web/UrlHelpers.java
+++ services/repository/src/main/java/org/sagebionetworks/repo/web/UrlHelpers.java
@@ -504,7 +504,6 @@ public class UrlHelpers {
 
 	public static final String ACCESS_APPROVAL = "/accessApproval";
 	public static final String ACCESS_APPROVALS = "/accessApprovals";
-	public static final String ACCESS_APPROVAL_WITH_ENTITY_ID = ENTITY_ID+ACCESS_APPROVAL;
 	public static final String ACCESS_APPROVAL_WITH_APPROVAL_ID = ACCESS_APPROVAL+"/{approvalId}";
 	public static final String ACCESS_APPROVAL_GROUP = ACCESS_APPROVAL+"/group";
 	public static final String ACCESS_APPROVAL_GROUP_REVOKE = ACCESS_APPROVAL_GROUP+"/revoke";
@@ -876,7 +875,6 @@ public class UrlHelpers {
 	public static final String TEAM_ID_ACL = TEAM_ID+"/acl";
 	
 	public static final String ACCESS_REQUIREMENT_WITH_TEAM_ID = TEAM_ID+ACCESS_REQUIREMENT;
-	public static final String ACCESS_APPROVAL_WITH_TEAM_ID = TEAM_ID+ACCESS_APPROVAL;
 
 	// membership invitation
 	public static final String MEMBERSHIP_INVITATION = "/membershipInvitation";
