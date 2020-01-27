a62b0c79d425ba44437878e59b6ed6a3c319a379
john-hill <john.hill@sagebase.org>
2020-01-27 14:19:15 -0800

    moved wait for view to util for test fix

--- services/workers/src/test/java/org/sagebionetworks/AsynchronousJobWorkerHelperImpl.java
+++ services/workers/src/test/java/org/sagebionetworks/AsynchronousJobWorkerHelperImpl.java
@@ -4,12 +4,16 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import org.sagebionetworks.repo.manager.EntityManager;
 import org.sagebionetworks.repo.manager.asynch.AsynchJobStatusManager;
 import org.sagebionetworks.repo.manager.table.TableManagerSupport;
 import org.sagebionetworks.repo.manager.table.TableViewManager;
+import org.sagebionetworks.repo.model.AsynchJobFailedException;
 import org.sagebionetworks.repo.model.Entity;
+import org.sagebionetworks.repo.model.EntityType;
 import org.sagebionetworks.repo.model.UserInfo;
 import org.sagebionetworks.repo.model.asynch.AsynchJobState;
 import org.sagebionetworks.repo.model.asynch.AsynchronousJobStatus;
@@ -20,6 +24,9 @@ import org.sagebionetworks.repo.model.jdo.KeyFactory;
 import org.sagebionetworks.repo.model.table.ColumnModel;
 import org.sagebionetworks.repo.model.table.EntityDTO;
 import org.sagebionetworks.repo.model.table.EntityView;
+import org.sagebionetworks.repo.model.table.TableFailedException;
+import org.sagebionetworks.repo.model.table.TableState;
+import org.sagebionetworks.repo.model.table.TableStatus;
 import org.sagebionetworks.repo.model.table.ViewScope;
 import org.sagebionetworks.table.cluster.ConnectionFactory;
 import org.sagebionetworks.table.cluster.TableIndexDAO;
@@ -110,6 +117,52 @@ public class AsynchronousJobWorkerHelperImpl implements AsynchronousJobWorkerHel
 		viewScope.setViewTypeMask(viewTypeMask);
 		tableViewManager.setViewSchemaAndScope(user, view.getColumnIds(), viewScope, viewId);
 		return view;
+	}
+	
+	/**
+	 * An expensive call to determine if a view is up-to-date with the entity replication data.
+	 * 
+	 * @param tableId
+	 * @return Optional<Boolean> A non-empty result is only returned if the ID belongs view
+	 * with a status of available.
+	 * @throws TableFailedException 
+	 */
+	@Override
+	public Optional<Boolean> isViewAvailableAndUpToDate(IdAndVersion tableId) throws TableFailedException {
+		EntityType type = tableMangerSupport.getTableEntityType(tableId);
+		if(!EntityType.entityview.equals(type)) {
+			// not a view
+			return Optional.empty();
+		}
+		TableStatus status = tableMangerSupport.getTableStatusOrCreateIfNotExists(tableId);
+		if(!TableState.AVAILABLE.equals(status.getState())) {
+			return Optional.empty();
+		}
+		TableIndexDAO indexDao = tableConnectionFactory.getConnection(tableId);
+		Long viewTypeMask = tableMangerSupport.getViewTypeMask(tableId);
+		Set<Long> allContainersInScope = tableMangerSupport.getAllContainerIdsForViewScope(tableId, viewTypeMask);
+		long limit = 1L;
+		Set<Long> changes = indexDao.getOutOfDateRowsForView(tableId, viewTypeMask, allContainersInScope,  limit);
+		return Optional.of(changes.isEmpty());
+	}
+	
+	/**
+	 * Helper to wait for a view to be up-to-date
+	 * @param user
+	 * @param viewId
+	 * @throws InterruptedException
+	 * @throws AsynchJobFailedException
+	 * @throws TableFailedException 
+	 */
+	@Override
+	public void waitForViewToBeUpToDate(IdAndVersion viewId, long maxWaitMS) throws InterruptedException, AsynchJobFailedException, TableFailedException {
+		long start = System.currentTimeMillis();
+		// only wait if the view is available but out-of-date.
+		while(!isViewAvailableAndUpToDate(viewId).orElse(true)) {
+			assertTrue("Timed out waiting for table view to be up-to-date.", (System.currentTimeMillis()-start) <  maxWaitMS);
+			System.out.println("Waiting for view "+viewId+" to be up-to-date");
+			Thread.sleep(1000);
+		}
 	}
 
 }
