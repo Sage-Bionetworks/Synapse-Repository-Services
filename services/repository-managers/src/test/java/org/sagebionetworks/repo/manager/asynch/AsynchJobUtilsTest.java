793bb03078631db1f71bfdc7f08715cb89dde5fc
john-hill <john.hill@sagebase.org>
2020-02-06 15:23:48 -0800

    added unit test for extracted util

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/asynch/AsynchJobUtilsTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/asynch/AsynchJobUtilsTest.java
@@ -1,9 +1,11 @@
 package org.sagebionetworks.repo.manager.asynch;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.sagebionetworks.repo.model.AsynchJobFailedException;
+import org.sagebionetworks.repo.model.asynch.AsynchJobState;
 import org.sagebionetworks.repo.model.asynch.AsynchronousJobStatus;
 import org.sagebionetworks.repo.model.file.BulkFileDownloadRequest;
 import org.sagebionetworks.repo.model.table.DownloadFromTableRequest;
@@ -20,26 +22,87 @@ public class AsynchJobUtilsTest {
 		assertEquals(status.getRequestBody(), body);
 	}
 	
-	@Test (expected=IllegalArgumentException.class)
+	@Test
 	public void testExtractRequestBodyNullStatus(){
 		AsynchronousJobStatus status = null;
-		// call under test.
-		AsynchJobUtils.extractRequestBody(status, DownloadFromTableRequest.class);
+		assertThrows(IllegalArgumentException.class, ()->{
+			// call under test.
+			AsynchJobUtils.extractRequestBody(status, DownloadFromTableRequest.class);
+		});
 	}
 	
-	@Test (expected=IllegalArgumentException.class)
+	@Test
 	public void testExtractRequestBodyNullBody(){
 		AsynchronousJobStatus status = new AsynchronousJobStatus();
 		status.setRequestBody(null);
-		// call under test.
-		AsynchJobUtils.extractRequestBody(status, DownloadFromTableRequest.class);
+		assertThrows(IllegalArgumentException.class, ()->{
+			// call under test.
+			AsynchJobUtils.extractRequestBody(status, DownloadFromTableRequest.class);
+		});
 	}
 	
-	@Test (expected=IllegalArgumentException.class)
+	@Test
 	public void testExtractRequestBodyWrongType(){
 		AsynchronousJobStatus status = new AsynchronousJobStatus();
 		status.setRequestBody(null);
-		// call under test.
-		AsynchJobUtils.extractRequestBody(status, BulkFileDownloadRequest.class);
+		assertThrows(IllegalArgumentException.class, ()->{
+			// call under test.
+			AsynchJobUtils.extractRequestBody(status, BulkFileDownloadRequest.class);
+		});
+	}
+	
+	@Test
+	public void testThrowExceptionIfFailed_Complete() throws Throwable {
+		AsynchronousJobStatus jobStatus = new AsynchronousJobStatus();
+		jobStatus.setJobState(AsynchJobState.COMPLETE);
+		// call under test
+		AsynchJobUtils.throwExceptionIfFailed(jobStatus);
+	}
+	
+	@Test
+	public void testThrowExceptionIfFailed_Processing() throws Throwable {
+		AsynchronousJobStatus jobStatus = new AsynchronousJobStatus();
+		jobStatus.setJobState(AsynchJobState.PROCESSING);
+		// call under test
+		AsynchJobUtils.throwExceptionIfFailed(jobStatus);
+	}
+	
+	@Test
+	public void testThrowExceptionIfFailed_Failed() throws Throwable {
+		IllegalArgumentException exception = new IllegalArgumentException("some exception");
+		AsynchronousJobStatus jobStatus = new AsynchronousJobStatus();
+		jobStatus.setJobState(AsynchJobState.FAILED);
+		jobStatus.setException(exception.getClass().getName());
+		jobStatus.setErrorDetails(exception.getMessage());
+		jobStatus.setErrorMessage(exception.getMessage());
+		IllegalArgumentException result = assertThrows(IllegalArgumentException.class, ()->{
+			// call under test
+			AsynchJobUtils.throwExceptionIfFailed(jobStatus);
+		});
+		assertEquals(result.getMessage(), exception.getMessage());
+	}
+	
+	@Test
+	public void testThrowExceptionIfFailed_FailedNoException() throws Throwable {
+		AsynchronousJobStatus jobStatus = new AsynchronousJobStatus();
+		jobStatus.setJobState(AsynchJobState.FAILED);
+		jobStatus.setException(null);
+		AsynchJobFailedException result = assertThrows(AsynchJobFailedException.class, ()->{
+			// call under test
+			AsynchJobUtils.throwExceptionIfFailed(jobStatus);
+		});
+		assertEquals(result.getStatus(), jobStatus);
+	}
+	
+	@Test
+	public void testThrowExceptionIfFailed_FailedUnknownException() throws Throwable {
+		AsynchronousJobStatus jobStatus = new AsynchronousJobStatus();
+		jobStatus.setJobState(AsynchJobState.FAILED);
+		jobStatus.setException("foo.bar.NotRealException");
+		AsynchJobFailedException result = assertThrows(AsynchJobFailedException.class, ()->{
+			// call under test
+			AsynchJobUtils.throwExceptionIfFailed(jobStatus);
+		});
+		assertEquals(result.getStatus(), jobStatus);
 	}
 }
