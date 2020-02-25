f8122148d55a6f2df32388988581c5e388c3c30f
Xavier Schildwachter <x.schildwachter@sagebase.org>
2020-02-24 16:24:11 -0800

    REST client - build passed. Needs more tests of client itself.

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/util/jrjc/JiraClientImpl.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/util/jrjc/JiraClientImpl.java
@@ -3,73 +3,187 @@ package org.sagebionetworks.repo.util.jrjc;
 import java.net.URI;
 import java.util.concurrent.ExecutionException;
 
-import io.atlassian.util.concurrent.Promise;
 import org.sagebionetworks.StackConfigurationSingleton;
 
-import com.atlassian.jira.rest.client.api.IssueRestClient;
-import com.atlassian.jira.rest.client.api.JiraRestClient;
-import com.atlassian.jira.rest.client.api.MetadataRestClient;
-import com.atlassian.jira.rest.client.api.ProjectRestClient;
-import com.atlassian.jira.rest.client.api.domain.BasicIssue;
-import com.atlassian.jira.rest.client.api.domain.Field;
-import com.atlassian.jira.rest.client.api.domain.Project;
-import com.atlassian.jira.rest.client.api.domain.input.IssueInput;
-import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.sagebionetworks.repo.model.UnauthorizedException;
+import org.sagebionetworks.repo.web.NotFoundException;
+import org.sagebionetworks.repo.web.ServiceUnavailableException;
+import org.sagebionetworks.simpleHttpClient.*;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
- * An implementation of JiraClient using the Jira-Rest-Java-Client
- * 
- * @author brucehoff
- *
+ * Mini JIRA REST client
  */
 public class JiraClientImpl implements JiraClient {
 	private static final String JIRA_URL = "https://sagebionetworks.jira.com";
 
-	private JiraRestClient restClient;
-	
-	private static <T> T waitForPromise(Promise<T> promise) {
+	private final SimpleHttpClient httpClient;
+	private String USERNAME;
+	private String APIKEY;
+	private static final Integer TIME_OUT = 30 * 1000; // 30 seconds
+	private static final String JIRA_API_PROJECT_URL = "/rest/api/3/project/";
+	private static final String JIRA_API_FIELDS_URL = "/rest/api/3/field";
+	private static final String JIRA_API_ISSUE_URL = "/rest/api/3/issue";
+	private static final String USER_AGENT = "Synapse";
+	private static JSONParser parser;
+	private static final String JIRA_PROJECT_ISSUE_TYPES_KEY = "issueTypes";
+	private static final String JIRA_PROJECT_ID_KEY = "id";
+
+	public JiraClientImpl() {
+		SimpleHttpClientConfig httpClientConfig = new SimpleHttpClientConfig();
+		httpClientConfig.setSocketTimeoutMs(TIME_OUT);
+		httpClient = new SimpleHttpClientImpl(httpClientConfig);
+
+		USERNAME = StackConfigurationSingleton.singleton().getJiraUserEmail();
+		APIKEY = StackConfigurationSingleton.singleton().getJiraUserApikey();
+		parser = new JSONParser();
+	}
+
+	@Override
+	public JSONObject getProjectInfo(String projectKey, String issueTypeName) {
+		SimpleHttpRequest req = createRequest(JIRA_API_PROJECT_URL, "SG", "application/json");
+		SimpleHttpResponse resp = null;
 		try {
-			while (!promise.isDone()) {
-				Thread.sleep(100L);
+			resp = httpClient.get(req);
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+		handleResponseStatus(resp);
+		String json = null;
+		json = resp.getContent();
+
+		Long issueTypeId = -1L;
+		String projectId = null;
+		try (Reader reader = new StringReader(json)) {
+			JSONObject pInfo = (JSONObject) parser.parse(reader);
+			JSONArray issueTypeNames = (JSONArray)pInfo.get(JIRA_PROJECT_ISSUE_TYPES_KEY);
+			Iterator<JSONObject> it = issueTypeNames.iterator();
+			while (it.hasNext()) {
+				JSONObject issueType = it.next();
+				String name = (String) issueType.get("name");
+				if (issueTypeName.equalsIgnoreCase(name)) {
+					issueTypeId = Long.parseLong((String) issueType.get("id"));
+					break;
+				}
 			}
-			return promise.get();
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		} catch (ExecutionException e) {
-			throw new RuntimeException(e);
+			if (issueTypeId == -1L) {
+				throw new IllegalStateException(String.format("Could not find the issue typeId for issue type {}", issueTypeName));
+			}
+			projectId = (String) pInfo.get(JIRA_PROJECT_ID_KEY);
+		}
+		catch (IOException | ParseException e) {
+			throw new RuntimeException("JIRA client: error processing JSON");
+		}
+		JSONObject projInfo = new JSONObject();
+		projInfo.put("issueTypeId", issueTypeId);
+		projInfo.put("id", projectId);
+		return projInfo;
+	}
+
+	@Override
+	public Map<String,String> getFields() {
+		SimpleHttpRequest req = createRequest(JIRA_API_FIELDS_URL, null, "application/json");
+		SimpleHttpResponse resp = null;
+		try {
+			resp = httpClient.get(req);
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+		handleResponseStatus(resp);
+		String json = null;
+		json = resp.getContent();
+
+		JSONArray fields = null;
+		Map<String, String> fieldsMap = new HashMap<>();
+		try (Reader reader = new StringReader(json)) {
+			fields = (JSONArray) parser.parse(reader);
+			Iterator<JSONObject> it = fields.iterator();
+			while (it.hasNext()) {
+				JSONObject fieldDetail = it.next();
+				String fieldName = (String) fieldDetail.get("name");
+				String fieldId = (String) fieldDetail.get("id");
+				fieldsMap.put(fieldName, fieldId);
+			}
+		}
+		catch (IOException | ParseException e) {
+			throw new RuntimeException(("JIRA client: error processing JSON\n"));
+		}
+		return fieldsMap;
+	}
+
+	@Override
+	public JSONObject createIssue(JSONObject issue) {
+		SimpleHttpRequest req = createRequest(JIRA_API_ISSUE_URL, null, "application/json");
+		SimpleHttpResponse resp = null;
+		try {
+			resp = httpClient.post(req, issue.toJSONString());
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+		handleResponseStatus(resp);
+
+		String json = null;
+		json = resp.getContent();
+
+		JSONObject createdIssue = null;
+		try (Reader reader = new StringReader(json)) {
+			createdIssue = (JSONObject) parser.parse(reader);
+		}
+		catch (IOException | ParseException e) {
+			throw new RuntimeException("JIRA client: error processing JSON\n");
+		}
+		return createdIssue;
+	}
+
+	SimpleHttpRequest createRequest(String path, String resource, String contentType) {
+		SimpleHttpRequest request = new SimpleHttpRequest();
+		if (resource == null) {
+			request.setUri(JIRA_URL + path);
+		} else {
+			request.setUri(JIRA_URL + path + resource);
+		}
+		Map<String, String> headers = new HashMap<>();
+		try {
+			headers.put(HttpHeaders.AUTHORIZATION, "Basic " + Base64.getEncoder().encodeToString((USERNAME + ":" + APIKEY).getBytes("utf-8")));
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("Error encoding credentials");
+		}
+		if (contentType != null) {
+			headers.put(HttpHeaders.CONTENT_TYPE, contentType);
+		}
+		headers.put(HttpHeaders.USER_AGENT, USER_AGENT);
+		request.setHeaders(headers);
+
+		return request;
+	}
+
+	static void handleResponseStatus(SimpleHttpResponse resp) {
+		int status = resp.getStatusCode();
+		switch (status) {
+			case HttpStatus.SC_BAD_REQUEST:
+				throw new RuntimeException("JIRA: invalid request");
+			case HttpStatus.SC_NOT_FOUND:
+				throw new RuntimeException("JIRA: not found");
+			case HttpStatus.SC_UNAUTHORIZED:
+				throw new RuntimeException("JIRA: credentials wrong or missing");
+			case HttpStatus.SC_FORBIDDEN:
+				throw new RuntimeException("JIRA: insufficient permission");
+			default:
+				throw new RuntimeException("JIRA: unexpected status received - " + status);
 		}
 	}
-	
-	public JiraClientImpl() {
-    	final AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
-    	URI jiraServerUri = URI.create(JIRA_URL);
-    	this.restClient = factory.createWithBasicHttpAuthentication(
-    			jiraServerUri, 
-    			StackConfigurationSingleton.singleton().getJiraUserEmail(), 
-    			StackConfigurationSingleton.singleton().getJiraUserApikey());
-	}
-
-	@Override
-	public Project getProject(String projectKey) {
-		ProjectRestClient prc = restClient.getProjectClient();
-		return waitForPromise(prc.getProject(projectKey));
-	}
-
-	@Override
-	public Iterable<Field> getFields() {
-		// NOTE:  In order to appear in this list a Jira 'custom field' 
-		// must have a 'global context'.  If it applies just to selected
-		// projects it will not appear in this list.
-    	MetadataRestClient mrc = restClient.getMetadataClient();
-    	return waitForPromise(mrc.getFields());
-	}
-
-	@Override
-	public BasicIssue createIssue(IssueInput issueInput) {
-        IssueRestClient irc = restClient.getIssueClient();
-        Promise<BasicIssue> basicIssuePromise = irc.createIssue(issueInput);
-        return waitForPromise(basicIssuePromise);
-	}
-
-
 }
