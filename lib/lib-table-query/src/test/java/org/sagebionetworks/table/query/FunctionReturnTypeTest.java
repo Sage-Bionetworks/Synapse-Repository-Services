24d414fe879c12aa73670e2c6df89509e7a1259f
Ziming Dong <ziming.dong@sagebase.org>
2020-04-23 20:33:43 -0700

    fix unnest return type

--- lib/lib-table-query/src/test/java/org/sagebionetworks/table/query/FunctionReturnTypeTest.java
+++ lib/lib-table-query/src/test/java/org/sagebionetworks/table/query/FunctionReturnTypeTest.java
@@ -1,8 +1,10 @@
 package org.sagebionetworks.table.query;
 
-import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 import org.sagebionetworks.repo.model.table.ColumnType;
 import org.sagebionetworks.table.query.model.FunctionReturnType;
 
@@ -36,5 +38,26 @@ public class FunctionReturnTypeTest {
 	public void testGetMatchesParameterNull() {
 		ColumnType parameterType = null;
 		assertEquals(null, FunctionReturnType.MATCHES_PARAMETER.getColumnType(parameterType));
+	}
+
+	@Test
+	public void testGetUnnestParameterNonList() {
+		ColumnType parameterType = ColumnType.STRING;
+		assertThrows(IllegalArgumentException.class, ()-> FunctionReturnType.UNNEST_PARAMETER.getColumnType(parameterType));
+	}
+
+	@Test
+	public void testGetUnnestParameterLList() {
+		assertEquals(ColumnType.STRING, FunctionReturnType.UNNEST_PARAMETER.getColumnType(ColumnType.STRING_LIST));
+		assertEquals(ColumnType.INTEGER, FunctionReturnType.UNNEST_PARAMETER.getColumnType(ColumnType.INTEGER_LIST));
+		assertEquals(ColumnType.DATE, FunctionReturnType.UNNEST_PARAMETER.getColumnType(ColumnType.DATE_LIST));
+		assertEquals(ColumnType.BOOLEAN, FunctionReturnType.UNNEST_PARAMETER.getColumnType(ColumnType.BOOLEAN_LIST));
+
+	}
+
+	@Test
+	public void testGetUnnestParameterNull() {
+		ColumnType parameterType = null;
+		assertThrows(IllegalArgumentException.class, ()-> FunctionReturnType.UNNEST_PARAMETER.getColumnType(parameterType));
 	}
 }
