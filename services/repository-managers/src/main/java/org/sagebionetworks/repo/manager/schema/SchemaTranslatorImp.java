41de2afa72f6946494a28253a1f6854487ca0dc0
john-hill <john.hill@sagebase.org>
2020-04-30 13:29:54 -0700

    use LinkedHashMaps in JsonSchema property serialization and more transation

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/schema/SchemaTranslatorImp.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/schema/SchemaTranslatorImp.java
@@ -5,14 +5,18 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringJoiner;
 
 import org.apache.commons.io.IOUtils;
 import org.sagebionetworks.repo.model.schema.JsonSchema;
+import org.sagebionetworks.repo.model.schema.Type;
 import org.sagebionetworks.repo.web.NotFoundException;
 import org.sagebionetworks.schema.ObjectSchema;
 import org.sagebionetworks.schema.ObjectSchemaImpl;
+import org.sagebionetworks.schema.TYPE;
 import org.sagebionetworks.schema.adapter.JSONObjectAdapterException;
 import org.sagebionetworks.schema.adapter.org.json.EntityFactory;
 import org.sagebionetworks.util.ValidateArgument;
@@ -21,13 +25,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class SchemaTranslatorImp implements SchemaTranslator {
 
+	public static final String DELIMITER = "/";
 	public static final String SYNAPSE_ORGANIZATION_NAME = "org.sagebionetworks";
-
 
 	@Override
 	public ObjectSchemaImpl loadSchemaFromClasspath(String id) throws IOException, JSONObjectAdapterException {
 		ValidateArgument.required(id, "id");
-		String fileName = "schema/" + id.replaceAll("\\.", "/") + ".json";
+		String fileName = "schema/" + id.replaceAll("\\.", DELIMITER) + ".json";
 		try (InputStream input = SynapseSchemaBootstrapImpl.class.getClassLoader().getResourceAsStream(fileName);) {
 			if (input == null) {
 				throw new NotFoundException("Cannot find: '" + fileName + "' on the classpath");
@@ -41,17 +45,6 @@ public class SchemaTranslatorImp implements SchemaTranslator {
 			}
 			return schema;
 		}
-	}
-
-	@Override
-	public JsonSchema translate(ObjectSchema objectSchema) {
-		ValidateArgument.required(objectSchema, "objectSchema");
-		JsonSchema jsonSchema = new JsonSchema();
-		jsonSchema.set$id(convertFromInternalIdToExternalId(objectSchema.getId()));
-		// implements maps to allOf.
-		jsonSchema.setAllOf(translateSchema(objectSchema.getImplements()));
-		jsonSchema.set$ref(convertFromInternalIdToExternalId(objectSchema.getRef()));
-		return jsonSchema;
 	}
 
 	/**
@@ -70,7 +63,7 @@ public class SchemaTranslatorImp implements SchemaTranslator {
 			throw new IllegalArgumentException("Id has an unknown organization name: '" + id + "'");
 		}
 		String schemaName = id.substring(SYNAPSE_ORGANIZATION_NAME.length() + 1);
-		StringJoiner joiner = new StringJoiner("/");
+		StringJoiner joiner = new StringJoiner(DELIMITER);
 		joiner.add(SYNAPSE_ORGANIZATION_NAME);
 		joiner.add(schemaName);
 		return joiner.toString();
@@ -82,7 +75,7 @@ public class SchemaTranslatorImp implements SchemaTranslator {
 	 * @param array
 	 * @return
 	 */
-	public List<JsonSchema> translateSchema(ObjectSchema[] array) {
+	public List<JsonSchema> translateArray(ObjectSchema[] array) {
 		if (array == null) {
 			return null;
 		}
@@ -91,5 +84,66 @@ public class SchemaTranslatorImp implements SchemaTranslator {
 			newList.add(translate(sub));
 		}
 		return newList;
+	}
+
+	/**
+	 * Convert the ObjectSchema types to JsonSchema types.
+	 * @param type
+	 * @return
+	 */
+	public Type translateType(TYPE type) {
+		if (type == null) {
+			return null;
+		}
+		switch (type) {
+		case STRING:
+			return Type.string;
+		case INTEGER:
+		case NUMBER:
+			return Type.number;
+		case ARRAY:
+			return Type.array;
+		case BOOLEAN:
+			return Type._boolean;
+		case NULL:
+			return Type._null;
+		case OBJECT:
+		case INTERFACE:
+			return Type.object;
+		default:
+			throw new IllegalArgumentException("There is no translation for type: '"+type.name()+"'");
+		}
+	}
+
+	/**
+	 * Translate from a map of ObjectSchema to a Map
+	 * 
+	 * @param inputMap
+	 * @return
+	 */
+	public LinkedHashMap<String, JsonSchema> translateMap(Map<String, ObjectSchema> inputMap) {
+		if (inputMap == null) {
+			return null;
+		}
+		LinkedHashMap<String, JsonSchema> resultMap = new LinkedHashMap<String, JsonSchema>(inputMap.size());
+		for (String key : inputMap.keySet()) {
+			ObjectSchema objectSchema = inputMap.get(key);
+			resultMap.put(key, translate(objectSchema));
+		}
+		return resultMap;
+	}
+	
+	@Override
+	public JsonSchema translate(ObjectSchema objectSchema) {
+		ValidateArgument.required(objectSchema, "objectSchema");
+		JsonSchema jsonSchema = new JsonSchema();
+		jsonSchema.set$id(convertFromInternalIdToExternalId(objectSchema.getId()));
+		// implements maps to allOf.
+		jsonSchema.setAllOf(translateArray(objectSchema.getImplements()));
+		jsonSchema.set$ref(convertFromInternalIdToExternalId(objectSchema.getRef()));
+		jsonSchema.setProperties(translateMap(objectSchema.getProperties()));
+		jsonSchema.setType(translateType(objectSchema.getType()));
+		jsonSchema.setDescription(objectSchema.getDescription());
+		return jsonSchema;
 	}
 }
--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/schema/SchemaTranslatorImplTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/schema/SchemaTranslatorImplTest.java
@@ -6,7 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,16 +17,23 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.sagebionetworks.repo.model.FileEntity;
 import org.sagebionetworks.repo.model.schema.JsonSchema;
+import org.sagebionetworks.repo.model.schema.Type;
 import org.sagebionetworks.repo.web.NotFoundException;
+import org.sagebionetworks.schema.LinkDescription;
 import org.sagebionetworks.schema.ObjectSchema;
 import org.sagebionetworks.schema.ObjectSchemaImpl;
+import org.sagebionetworks.schema.TYPE;
 import org.sagebionetworks.schema.adapter.JSONObjectAdapterException;
+
+import com.google.common.collect.Lists;
 
 @ExtendWith(MockitoExtension.class)
 public class SchemaTranslatorImplTest {
 
 	@InjectMocks
 	SchemaTranslatorImp translator;
+	
+	SchemaTranslatorImp spyTranslator;
 	
 	@Test
 	public void testLoadSchemaFromClasspath() throws IOException, JSONObjectAdapterException {
@@ -77,14 +87,14 @@ public class SchemaTranslatorImplTest {
 	}
 	
 	@Test
-	public void testTranslateSchemaArray() {
+	public void testTranslateArray() {
 		ObjectSchemaImpl one = new ObjectSchemaImpl();
 		one.setId("org.sagebionetworks.one");
 		ObjectSchemaImpl two = new ObjectSchemaImpl();
 		two.setId("org.sagebionetworks.two");
 		ObjectSchema[] array = new ObjectSchema[] {one, two};
 		// Call under test
-		List<JsonSchema> result = translator.translateSchema(array);
+		List<JsonSchema> result = translator.translateArray(array);
 		assertNotNull(result);
 		assertEquals(2, result.size());
 		assertEquals("org.sagebionetworks/one", result.get(0).get$id());
@@ -92,24 +102,175 @@ public class SchemaTranslatorImplTest {
 	}
 	
 	@Test
-	public void testTranslateSchemaArrayWithNullArray() {
+	public void testTranslateArrayWithNullArray() {
 		ObjectSchema[] array = null;
 		// Call under test
-		List<JsonSchema> result = translator.translateSchema(array);
+		List<JsonSchema> result = translator.translateArray(array);
 		assertNull(result);
 	}
-
+	
 	@Test
-	public void testTranslateSchema() throws IOException, JSONObjectAdapterException {
-		ObjectSchemaImpl fileEntityObjecSchema = translator.loadSchemaFromClasspath("org.sagebionetworks.repo.model.FileEntity");
+	public void testTranslateTypeWithString() {
+		TYPE input = TYPE.STRING;
+		// call under test
+		Type result = translator.translateType(input);
+		assertEquals(Type.string, result);
+	}
+	
+	@Test
+	public void testTranslateTypeWithArray() {
+		TYPE input = TYPE.ARRAY;
+		// call under test
+		Type result = translator.translateType(input);
+		assertEquals(Type.array, result);
+	}
+	
+	@Test
+	public void testTranslateTypeWithNumber() {
+		TYPE input = TYPE.NUMBER;
+		// call under test
+		Type result = translator.translateType(input);
+		assertEquals(Type.number, result);
+	}
+	
+	@Test
+	public void testTranslateTypeWithInteger() {
+		TYPE input = TYPE.INTEGER;
+		// call under test
+		Type result = translator.translateType(input);
+		assertEquals(Type.number, result);
+	}
+	
+	@Test
+	public void testTranslateTypeWithBoolean() {
+		TYPE input = TYPE.BOOLEAN;
+		// call under test
+		Type result = translator.translateType(input);
+		assertEquals(Type._boolean, result);
+	}
+	
+	@Test
+	public void testTranslateTypeWithNull() {
+		TYPE input = TYPE.NULL;
+		// call under test
+		Type result = translator.translateType(input);
+		assertEquals(Type._null, result);
+	}
+	
+	@Test
+	public void testTranslateTypeWithObject() {
+		TYPE input = TYPE.OBJECT;
+		// call under test
+		Type result = translator.translateType(input);
+		assertEquals(Type.object, result);
+	}
+	
+	@Test
+	public void testTranslateTypeWithInterface() {
+		TYPE input = TYPE.INTERFACE;
+		// call under test
+		Type result = translator.translateType(input);
+		assertEquals(Type.object, result);
+	}
+	
+	@Test
+	public void testTranslateTypeWithAny() {
+		TYPE input = TYPE.ANY;
+		String message = assertThrows(IllegalArgumentException.class, ()->{
+			translator.translateType(input);
+		}).getMessage();
+		assertEquals("There is no translation for type: 'ANY'", message);
+	}
+	
+	@Test
+	public void testTranslateTypeWithMap() {
+		TYPE input = TYPE.MAP;
+		String message = assertThrows(IllegalArgumentException.class, ()->{
+			translator.translateType(input);
+		}).getMessage();
+		assertEquals("There is no translation for type: 'MAP'", message);
+	}
+	
+	@Test
+	public void testTranslateTypeWithTupleArrayMap() {
+		TYPE input = TYPE.TUPLE_ARRAY_MAP;
+		String message = assertThrows(IllegalArgumentException.class, ()->{
+			translator.translateType(input);
+		}).getMessage();
+		assertEquals("There is no translation for type: 'TUPLE_ARRAY_MAP'", message);
+	}
+	
+	@Test
+	public void testTranslateTypeWithNullType() {
+		TYPE input = null;
+		// call under test
+		Type result = translator.translateType(input);
+		assertNull(result);
+	}
+	
+	@Test
+	public void testTranslateMap() {
+		ObjectSchemaImpl one = new ObjectSchemaImpl(TYPE.STRING);
+		one.setDescription("one");
+		ObjectSchemaImpl two = new ObjectSchemaImpl(TYPE.NUMBER);
+		two.setDescription("two");
+		ObjectSchemaImpl three = new ObjectSchemaImpl(TYPE.BOOLEAN);
+		three.setDescription("three");
+		Map<String, ObjectSchema> inputMap = new LinkedHashMap<String, ObjectSchema>(3);
+		inputMap.put("one", one);
+		inputMap.put("two", two);
+		inputMap.put("three", three);
+		
 		// Call under test
-		JsonSchema resultSchema = translator.translate(fileEntityObjecSchema);
+		LinkedHashMap<String, JsonSchema> result = translator.translateMap(inputMap);
+		assertNotNull(result);
+		// The order must match the order of the input
+		List<String> keyOrder = new LinkedList<String>();
+		for(String key: result.keySet()) {
+			keyOrder.add(key);
+		}
+		assertEquals(Lists.newArrayList("one","two","three"), keyOrder);
+		assertNotNull(result.get("one"));
+		assertEquals("one", result.get("one").getDescription());
+		assertNotNull(result.get("two"));
+		assertEquals("two", result.get("two").getDescription());
+		assertNotNull(result.get("three"));
+		assertEquals("three", result.get("three").getDescription());
+	}
+	
+	
+	@Test
+	public void testTranslateMapWithNull() {
+		Map<String, ObjectSchema> inputMap = null;
+		// Call under test
+		LinkedHashMap<String, JsonSchema> result = translator.translateMap(inputMap);
+		assertNull(result);
+	}
+	
+	//
+	// Test on public translate.
+	//
+	
+	@Test
+	public void testTranslateWithNullObjectSchema() {
+		ObjectSchemaImpl objectSchema = null;
+		assertThrows(IllegalArgumentException.class, ()->{
+			translator.translate(objectSchema);
+		});
+	}
+	
+	@Test
+	public void testTranslateWithId() throws IOException, JSONObjectAdapterException {
+		ObjectSchemaImpl objectSchema = new ObjectSchemaImpl();
+		objectSchema.setId("org.sagebionetworks.repo.model.FileEntity");
+		// Call under test
+		JsonSchema resultSchema = translator.translate(objectSchema);
 		assertNotNull(resultSchema);
 		assertEquals("org.sagebionetworks/repo.model.FileEntity", resultSchema.get$id());
 	}
 	
 	@Test
-	public void testTranslateSchemaWith$ref() throws IOException, JSONObjectAdapterException {
+	public void testTranslateWith$ref() throws IOException, JSONObjectAdapterException {
 		ObjectSchemaImpl objectSchema = new ObjectSchemaImpl();
 		objectSchema.setRef("org.sagebionetworks.repo.model.FileEntity");
 		// Call under test
@@ -119,7 +280,7 @@ public class SchemaTranslatorImplTest {
 	}
 	
 	@Test
-	public void testTranslateSchemaWithImplments() throws IOException, JSONObjectAdapterException {
+	public void testTranslateWithImplments() throws IOException, JSONObjectAdapterException {
 		ObjectSchemaImpl objectSchema = new ObjectSchemaImpl();
 		objectSchema.setId("org.sagebionetworks.repo.model.FileEntity");
 		ObjectSchemaImpl imp = new ObjectSchemaImpl();
@@ -135,5 +296,54 @@ public class SchemaTranslatorImplTest {
 		assertEquals("org.sagebionetworks/repo.model.Versionable", resultSchema.getAllOf().get(0).get$ref());
 	}
 	
+	@Test
+	public void testTranslateWithProperties() {
+		LinkedHashMap<String, ObjectSchema> properties = new LinkedHashMap<String, ObjectSchema>(2);
+		properties.put("a", new ObjectSchemaImpl(TYPE.STRING));
+		properties.put("b", new ObjectSchemaImpl(TYPE.NUMBER));
+		ObjectSchemaImpl inputSchema = new ObjectSchemaImpl(TYPE.OBJECT);
+		inputSchema.setProperties(properties);
+		
+		// Call under test
+		JsonSchema result = translator.translate(inputSchema);
+		assertNotNull(result);
+		assertNotNull(result.getProperties());
+		assertEquals(2, result.getProperties().size());
+		JsonSchema propA = result.getProperties().get("a");
+		assertNotNull(propA);
+		assertEquals(Type.string, propA.getType());
+		JsonSchema propB = result.getProperties().get("b");
+		assertNotNull(propB);
+		assertEquals(Type.number, propB.getType());
+	}
+	
+	@Test
+	public void testTranslateWithType() {
+		ObjectSchemaImpl one = new ObjectSchemaImpl();
+		one.setType(TYPE.STRING);
+		// call under test
+		JsonSchema result = translator.translate(one);
+		assertNotNull(result);
+		assertEquals(Type.string, result.getType());
+	}
+	
+	@Test
+	public void testTranslateWithDescription() {
+		ObjectSchemaImpl one = new ObjectSchemaImpl(TYPE.STRING);
+		one.setDescription("a description");
+		// call under test
+		JsonSchema result = translator.translate(one);
+		assertNotNull(result);
+		assertEquals(one.getDescription(), result.getDescription());
+	}
+	
+	@Test
+	public void testTranslateWithFileEntity() throws IOException, JSONObjectAdapterException {
+		ObjectSchemaImpl fileEntityObjecSchema = translator.loadSchemaFromClasspath("org.sagebionetworks.repo.model.FileEntity");
+		// Call under test
+		JsonSchema resultSchema = translator.translate(fileEntityObjecSchema);
+		assertNotNull(resultSchema);
+		assertEquals("org.sagebionetworks/repo.model.FileEntity", resultSchema.get$id());
+	}
 
 }
