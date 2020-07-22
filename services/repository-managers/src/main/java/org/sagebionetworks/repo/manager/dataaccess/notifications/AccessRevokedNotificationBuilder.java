8b8b7ad40bd7d8aa91fdc04bdca642136d4d4192
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2020-07-21 17:43:39 -0700

    PLFM-6299: Add revoke notification message builder

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/dataaccess/notifications/AccessRevokedNotificationBuilder.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/dataaccess/notifications/AccessRevokedNotificationBuilder.java
@@ -1,16 +1,38 @@
 package org.sagebionetworks.repo.manager.dataaccess.notifications;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+import org.sagebionetworks.repo.manager.EmailUtils;
+import org.sagebionetworks.repo.manager.UserProfileManager;
 import org.sagebionetworks.repo.model.AccessApproval;
 import org.sagebionetworks.repo.model.ManagedACTAccessRequirement;
 import org.sagebionetworks.repo.model.UserInfo;
+import org.sagebionetworks.repo.model.UserProfile;
 import org.sagebionetworks.repo.model.dataaccess.DataAccessNotificationType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccessRevokedNotificationBuilder implements DataAccessNotificationBuilder {
+	
+	private static final String TEMPLATE_FILE = "message/AccessApprovalRevokedTemplate.html";
+	private static final String SUBJECT_TEMPLATE = "%s Access Revoked";
+	private static final String REQUIREMENT_URL_TEMPLATE = "https://www.synapse.org/#!AccessRequirement:AR_ID=%s&TYPE=ENTITY&ID=syn%s";
+	
+	private static final String PARAM_DISPLAY_NAME = "#displayName#";
+	private static final String PARAM_REQUIREMENT_DESCRIPTION = "#requirementDescription#";
+	private static final String PARAM_REQUIREMENT_URL = "#requirementUrl#";
+	
+	private UserProfileManager userProfileManager;
+	
+	@Autowired
+	public AccessRevokedNotificationBuilder(final UserProfileManager userProfileManager) {
+		this.userProfileManager = userProfileManager;
+	}
 
 	@Override
 	public List<DataAccessNotificationType> supportedTypes() {
@@ -19,14 +41,62 @@ public class AccessRevokedNotificationBuilder implements DataAccessNotificationB
 
 	@Override
 	public String buildSubject(ManagedACTAccessRequirement accessRequirement, AccessApproval approval, UserInfo recipient) {
-		// TODO Auto-generated method stub
-		return null;
+		String prefix = "Data";
+		
+		if (!StringUtils.isBlank(accessRequirement.getDescription())) {
+			prefix = accessRequirement.getDescription();
+		}
+		
+		return String.format(SUBJECT_TEMPLATE, prefix);
 	}
 
 	@Override
 	public String buildMessageBody(ManagedACTAccessRequirement accessRequirement, AccessApproval approval, UserInfo recipient) {
-		// TODO Auto-generated method stub
-		return null;
+		final Map<String, String> templateValues = new HashMap<>();
+		
+		if (accessRequirement.getSubjectIds() == null || accessRequirement.getSubjectIds().isEmpty()) {
+			throw new IllegalStateException("The access requirement with id " + accessRequirement.getId() + " does not reference any subject.");
+		}
+		
+		// We need an entity to reference the AR in the web client, since we cannot back to the original submission from an access approval
+		// just take the first on the list
+		final String referenceEntityId = accessRequirement.getSubjectIds().get(0).getId();
+		
+		final String requirementUrl = String.format(REQUIREMENT_URL_TEMPLATE, accessRequirement.getId(), referenceEntityId);
+		
+		templateValues.put(PARAM_REQUIREMENT_URL, requirementUrl);
+
+		String description = accessRequirement.getDescription();
+		
+		StringBuilder descriptionBuidler = new StringBuilder();
+		
+		if (StringUtils.isBlank(description)) {
+			descriptionBuidler.append("an <a href=\"");
+			descriptionBuidler.append(requirementUrl);
+			descriptionBuidler.append("\">");
+		} else {
+			descriptionBuidler.append("the <a href=\"");
+			descriptionBuidler.append(requirementUrl);
+			descriptionBuidler.append("\">");
+			descriptionBuidler.append(description);
+			descriptionBuidler.append(" ");
+		}
+		
+		descriptionBuidler.append("access requirement (");
+		descriptionBuidler.append(accessRequirement.getId());
+		descriptionBuidler.append(")</a>");
+		
+		templateValues.put(PARAM_REQUIREMENT_DESCRIPTION, descriptionBuidler.toString());
+		
+		final String recipientId = recipient.getId().toString();
+		
+		UserProfile recipientProfile = userProfileManager.getUserProfile(recipientId);
+		
+		String displayName = EmailUtils.getDisplayNameWithUsername(recipientProfile);
+		
+		templateValues.put(PARAM_DISPLAY_NAME, displayName);
+		
+		return EmailUtils.readMailTemplate(TEMPLATE_FILE, templateValues);
 	}
 
 }
---
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/dataaccess/notifications/AccessRevokedNotificationBuilderTest.java
@@ -0,0 +1,182 @@
+package org.sagebionetworks.repo.manager.dataaccess.notifications;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.sagebionetworks.repo.manager.UserProfileManager;
+import org.sagebionetworks.repo.model.AccessApproval;
+import org.sagebionetworks.repo.model.ManagedACTAccessRequirement;
+import org.sagebionetworks.repo.model.RestrictableObjectDescriptor;
+import org.sagebionetworks.repo.model.UserInfo;
+import org.sagebionetworks.repo.model.UserProfile;
+
+@ExtendWith(MockitoExtension.class)
+public class AccessRevokedNotificationBuilderTest {
+
+	@Mock
+	private UserProfileManager mockProfileManager;
+	
+	@InjectMocks
+	private AccessRevokedNotificationBuilder builder;
+	
+	@Mock
+	private ManagedACTAccessRequirement mockRequirement;
+
+	@Mock
+	private AccessApproval mockApproval;
+	
+	@Mock
+	private UserInfo mockRecipient;
+	
+	@Mock
+	private UserProfile mockUserProfile;
+	
+	@Test
+	public void testBuildSubjectWithoutDescription() {
+		
+		String expected = "Data Access Revoked";
+		
+		// Call under test
+		String result = builder.buildSubject(mockRequirement, mockApproval, mockRecipient);
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testBuildSubjectWithDescription() {
+		
+		String description = "Some Dataset";
+		String expected = description + " Access Revoked";
+		
+		when(mockRequirement.getDescription()).thenReturn(description);
+		
+		// Call under test
+		String result = builder.buildSubject(mockRequirement, mockApproval, mockRecipient);
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testBuildMessageBodyWithoutDescription() {
+		
+		RestrictableObjectDescriptor entityDescriptor = new RestrictableObjectDescriptor();
+		entityDescriptor.setId("1234");
+		
+		List<RestrictableObjectDescriptor> subjectIds = Collections.singletonList(entityDescriptor);
+		Long requirementId = 1L;
+		String firstName = "First";
+		String lastName = "Last";
+		String userName = "username";
+		
+		
+		when(mockUserProfile.getFirstName()).thenReturn(firstName);
+		when(mockUserProfile.getLastName()).thenReturn(lastName);
+		when(mockUserProfile.getUserName()).thenReturn(userName);
+		
+		when(mockProfileManager.getUserProfile(any())).thenReturn(mockUserProfile);
+		when(mockRequirement.getId()).thenReturn(requirementId);
+		when(mockRequirement.getSubjectIds()).thenReturn(subjectIds);
+		
+
+		String expected = "<html>\r\n" + 
+				"<body>\r\n" + 
+				"<p>\r\n" + 
+				"Dear First Last (username),\r\n" + 
+				"</p>\r\n" + 
+				"<p>\r\n" + 
+				"We did not receive your renewal information for an <a href=\"https://www.synapse.org/#!AccessRequirement:AR_ID=1&TYPE=ENTITY&ID=syn1234\">access requirement (1)</a>, and your access has now been revoked.\r\n" + 
+				"</p>\r\n" + 
+				"<p>\r\n" + 
+				"Please delete all copies of the data and have the other members of your group do this as well. The terms of use also requires that you provide a brief summary of what you accomplished with the data.\r\n" + 
+				"</p>\r\n" + 
+				"<p>\r\n" + 
+				"Should you wish to re-request access, you can do so at the following link:\r\n" + 
+				"</p>\r\n" + 
+				"<p>\r\n" + 
+				"<a href=\"https://www.synapse.org/#!AccessRequirement:AR_ID=1&TYPE=ENTITY&ID=syn1234\">https://www.synapse.org/#!AccessRequirement:AR_ID=1&TYPE=ENTITY&ID=syn1234</a>\r\n" + 
+				"</p>\r\n" + 
+				"\r\n" + 
+				"<p style=\"-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;margin: 0 0 10px;\">\r\n" + 
+				"  Sincerely,\r\n" + 
+				"</p>\r\n" + 
+				"<p style=\"-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;margin: 0 0 10px;\">\r\n" + 
+				"  <img src=\"https://s3.amazonaws.com/static.synapse.org/images/SynapseLogo2.png\" style=\"display: inline;width: 40px;height: 40px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;border: 0;vertical-align: middle;\"> Synapse ACT Team\r\n" + 
+				"</p>\r\n" + 
+				"\r\n" + 
+				"</body>\r\n" + 
+				"</html>\r\n";
+		
+		// Call under test
+		String result = builder.buildMessageBody(mockRequirement, mockApproval, mockRecipient);
+		
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testBuildMessageBodyWithDescription() {
+		
+		RestrictableObjectDescriptor entityDescriptor = new RestrictableObjectDescriptor();
+		entityDescriptor.setId("1234");
+		
+		List<RestrictableObjectDescriptor> subjectIds = Collections.singletonList(entityDescriptor);
+		Long requirementId = 1L;
+		String requirementDescription = "Some Dataset";
+		String firstName = "First";
+		String lastName = "Last";
+		String userName = "username";
+		
+		
+		when(mockUserProfile.getFirstName()).thenReturn(firstName);
+		when(mockUserProfile.getLastName()).thenReturn(lastName);
+		when(mockUserProfile.getUserName()).thenReturn(userName);
+		
+		when(mockProfileManager.getUserProfile(any())).thenReturn(mockUserProfile);
+		when(mockRequirement.getId()).thenReturn(requirementId);
+		when(mockRequirement.getDescription()).thenReturn(requirementDescription);
+		when(mockRequirement.getSubjectIds()).thenReturn(subjectIds);
+		
+
+		String expected = "<html>\r\n" + 
+				"<body>\r\n" + 
+				"<p>\r\n" + 
+				"Dear First Last (username),\r\n" + 
+				"</p>\r\n" + 
+				"<p>\r\n" + 
+				"We did not receive your renewal information for the <a href=\"https://www.synapse.org/#!AccessRequirement:AR_ID=1&TYPE=ENTITY&ID=syn1234\">Some Dataset access requirement (1)</a>, and your access has now been revoked.\r\n" + 
+				"</p>\r\n" + 
+				"<p>\r\n" + 
+				"Please delete all copies of the data and have the other members of your group do this as well. The terms of use also requires that you provide a brief summary of what you accomplished with the data.\r\n" + 
+				"</p>\r\n" + 
+				"<p>\r\n" + 
+				"Should you wish to re-request access, you can do so at the following link:\r\n" + 
+				"</p>\r\n" + 
+				"<p>\r\n" + 
+				"<a href=\"https://www.synapse.org/#!AccessRequirement:AR_ID=1&TYPE=ENTITY&ID=syn1234\">https://www.synapse.org/#!AccessRequirement:AR_ID=1&TYPE=ENTITY&ID=syn1234</a>\r\n" + 
+				"</p>\r\n" + 
+				"\r\n" + 
+				"<p style=\"-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;margin: 0 0 10px;\">\r\n" + 
+				"  Sincerely,\r\n" + 
+				"</p>\r\n" + 
+				"<p style=\"-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;margin: 0 0 10px;\">\r\n" + 
+				"  <img src=\"https://s3.amazonaws.com/static.synapse.org/images/SynapseLogo2.png\" style=\"display: inline;width: 40px;height: 40px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;border: 0;vertical-align: middle;\"> Synapse ACT Team\r\n" + 
+				"</p>\r\n" + 
+				"\r\n" + 
+				"</body>\r\n" + 
+				"</html>\r\n";
+		
+		// Call under test
+		String result = builder.buildMessageBody(mockRequirement, mockApproval, mockRecipient);
+		
+		assertEquals(expected, result);
+	}
+	
+}
