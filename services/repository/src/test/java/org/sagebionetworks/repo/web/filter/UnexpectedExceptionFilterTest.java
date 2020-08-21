d9051d5401e1925c0506c882cd361d91fda5a018
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2020-08-21 15:01:12 -0700

    Revert "PLFM-6195: Unhandled exception return 500, log to cloudwatch"

--- services/repository/src/test/java/org/sagebionetworks/repo/web/filter/UnexpectedExceptionFilterTest.java
+++ services/repository/src/test/java/org/sagebionetworks/repo/web/filter/UnexpectedExceptionFilterTest.java
@@ -1,6 +1,5 @@
 package org.sagebionetworks.repo.web.filter;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doThrow;
@@ -10,9 +9,6 @@ import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -22,45 +18,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.sagebionetworks.StackConfiguration;
-import org.sagebionetworks.cloudwatch.Consumer;
-import org.sagebionetworks.cloudwatch.MetricUtils;
-import org.sagebionetworks.cloudwatch.ProfileData;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.sagebionetworks.repo.model.AuthorizationConstants;
 import org.springframework.http.MediaType;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
-import com.amazonaws.services.cloudwatch.model.StandardUnit;
-import com.google.common.collect.ImmutableMap;
-
-@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
 public class UnexpectedExceptionFilterTest {
 
 	@Mock
-	private StackConfiguration mockStackConfig;
+	HttpServletRequest mockRequest;
 	@Mock
-	private Consumer mockConsumer;
+	HttpServletResponse mockResponse;
 	@Mock
-	private HttpServletRequest mockRequest;
+	FilterChain mockChain;
 	@Mock
-	private HttpServletResponse mockResponse;
-	@Mock
-	private FilterChain mockChain;
-	@Mock
-	private PrintWriter mockWriter;
-	@InjectMocks
-	private UnexpectedExceptionFilter filter;
-	
-	@Captor
-	private ArgumentCaptor<List<ProfileData>> dataCaptor;
+	PrintWriter mockWriter;
 
-	private String stackInstance = "stackInstance";
+	UnexpectedExceptionFilter filter;
+
+
+	@Before
+	public void before() throws IOException{
+		filter = new UnexpectedExceptionFilter();
+		when(mockResponse.getWriter()).thenReturn(mockWriter);
+	}
 	
 	@Test
 	public void testNoException() throws IOException, ServletException{
@@ -70,47 +56,34 @@ public class UnexpectedExceptionFilterTest {
 		verify(mockResponse, never()).getWriter();
 	}
 
+	/**
+	 * We expect the server to recover from exceptions, so 503 is returned.
+	 * @throws IOException
+	 * @throws ServletException
+	 */
 	@Test
-	public void testWithException() throws IOException, ServletException{
-		ServletException exception = new ServletException("Exception");
-		
-		doThrow(exception).when(mockChain).doFilter(any(ServletRequest.class), any(ServletResponse.class));
-		when(mockResponse.getWriter()).thenReturn(mockWriter);
-		when(mockStackConfig.getStackInstance()).thenReturn(stackInstance);
-		
-		// Call under test
+	public void testServletException() throws IOException, ServletException{
+		ServletException error = new ServletException("Exception");
+		doThrow(error).when(mockChain).doFilter(any(ServletRequest.class), any(ServletResponse.class));
 		filter.doFilter(mockRequest, mockResponse, mockChain);
-		
 		// The response should not be changed
-		verify(mockResponse).setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+		verify(mockResponse).setStatus(HttpStatus.SC_SERVICE_UNAVAILABLE);
 		verify(mockResponse).setContentType(MediaType.APPLICATION_JSON_VALUE);
-		verify(mockWriter).println(AuthorizationConstants.REASON_SERVER_ERROR);
-		verify(mockConsumer).addProfileData(dataCaptor.capture());
-		
-		List<ProfileData> profileData = dataCaptor.getValue();
-		
-		assertEquals(2, profileData.size());
-		assertEquals(expectedProfileData(profileData.get(0).getTimestamp(), null), profileData.get(0));
-		assertEquals(expectedProfileData(profileData.get(1).getTimestamp(), exception), profileData.get(1));
+		verify(mockWriter).println("{\"reason\":\"Server error, try again later: Exception\"}");
 	}
 	
-	private ProfileData expectedProfileData(Date timestamp, Throwable ex) {
-		ProfileData logEvent = new ProfileData();
-
-		logEvent.setNamespace("UnexpectedExceptionFilter - " + stackInstance);
-		logEvent.setName("UnhandledException");
-		logEvent.setValue(1.0);
-		logEvent.setUnit(StandardUnit.Count.toString());
-		logEvent.setTimestamp(timestamp);
-		
-		if (ex != null) {
-			Map<String, String> dimensions = ImmutableMap.of(
-					"exceptionClass", ex.getClass().getSimpleName(),
-					"message", MetricUtils.stackTracetoString(ex)
-			);
-			logEvent.setDimension(dimensions);
-		}
-		
-		return logEvent;
+	/**
+	 * We do not expect the server to recover from errors, so 500 is returned.
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	@Test
+	public void testOutOfMemoryError() throws IOException, ServletException{
+		OutOfMemoryError error = new OutOfMemoryError("Exception");
+		doThrow(error).when(mockChain).doFilter(any(ServletRequest.class), any(ServletResponse.class));
+		filter.doFilter(mockRequest, mockResponse, mockChain);
+		// The response should not be changed
+		verify(mockResponse).setStatus(HttpStatus.SC_INTERNAL_SERVER_ERROR);
+		verify(mockWriter).println(AuthorizationConstants.REASON_SERVER_ERROR);
 	}
 }
