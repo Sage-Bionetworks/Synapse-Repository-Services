0f4c4f4b86e4127d500ba6766391c38537caad46
Nicoletta Gilbertson <62961756+nlgilber@users.noreply.github.com>
2020-09-04 14:24:26 -0700

    more specific test name , used assertEquals

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/table/TableQueryUtilsTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/table/TableQueryUtilsTest.java
@@ -19,9 +19,7 @@ import org.sagebionetworks.repo.model.table.SortItem;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TableQueryUtilsTest {
 
@@ -172,13 +170,21 @@ public class TableQueryUtilsTest {
 	 * PLFM-6392 Add a more informative message to the user for cases where keywords
 	 * must be escaped.
 	 */
-	public void testUnquotedKeywordsErrorMessage() {
+	public void testExtractTableIdFromSqlWithParserException() {
 		String sql = "select year from syn123 where year = 1";
 		IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()->{
 			TableQueryUtils.extractTableIdFromSql(sql);
 		});
-		assertNotNull(exception);
-		assertNotNull(exception.getMessage());
-		assertTrue(exception.getMessage().contains(TableExceptionTranslator.UNQUOTED_KEYWORDS_ERROR_MESSAGE));
+		assertEquals(exception.getMessage(), "Encountered \" <date_time_field> \"year \"\" at line 1, column 31.\n" +
+				"Was expecting one of:\n" +
+				"    \"\\\"\" ...\n" +
+				"    \"`\" ...\n" +
+				"    \"NOT\" ...\n" +
+				"    \"ISNAN\" ...\n" +
+				"    \"ISINFINITY\" ...\n" +
+				"    <entity_id> ...\n" +
+				"    <regular_identifier> ...\n" +
+				"    \"(\" ...\n" +
+				"    " + TableExceptionTranslator.UNQUOTED_KEYWORDS_ERROR_MESSAGE);
 	}
 }
