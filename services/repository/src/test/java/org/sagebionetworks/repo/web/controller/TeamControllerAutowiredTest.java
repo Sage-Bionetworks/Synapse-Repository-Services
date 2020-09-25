37f59c33216c4fcde6b9a9722f9fabdcb576a6fe
Nicoletta Gilbertson <62961756+nlgilber@users.noreply.github.com>
2020-09-25 10:11:26 -0700

    junit 5

--- services/repository/src/test/java/org/sagebionetworks/repo/web/controller/TeamControllerAutowiredTest.java
+++ services/repository/src/test/java/org/sagebionetworks/repo/web/controller/TeamControllerAutowiredTest.java
@@ -1,10 +1,8 @@
 package org.sagebionetworks.repo.web.controller;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.sagebionetworks.repo.manager.UserManager;
 import org.sagebionetworks.repo.model.AuthorizationConstants.BOOTSTRAP_PRINCIPAL;
 import org.sagebionetworks.repo.model.IdList;
@@ -35,7 +33,7 @@ public class TeamControllerAutowiredTest extends AbstractAutowiredControllerTest
 	private static final String TEAM_NAME = "MIS_CONTRL_AW_TEST";
 	private Team teamToDelete;
 
-	@Before
+	@BeforeEach
 	public void before() throws Exception {
 		adminUserId = BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId();
 		adminUserInfo = userManager.getUserInfo(adminUserId);
@@ -50,21 +48,19 @@ public class TeamControllerAutowiredTest extends AbstractAutowiredControllerTest
 		user.setUserName(UUID.randomUUID().toString());
 	}
 
-	@After
+	@AfterEach
 	public void after() throws Exception {
 		servletTestHelper.deleteTeam(dispatchServlet, adminUserId, teamToDelete);
 		 teamToDelete = null;
 		 
 	}
 
-	@Test
 	public void testListTeams() throws Exception {
 		IdList idList = new IdList();
 		idList.setList(Collections.EMPTY_LIST);
 		List<Team> teams = servletTestHelper.listTeams(dispatchServlet, idList);
 	}
 
-	@Test
 	public void testGetTeamIdsByMember() throws Exception {
 		servletTestHelper.getTeamIdsByMember(dispatchServlet, 1L, TeamSortOrder.TEAM_NAME, true);
 	}
@@ -73,7 +69,6 @@ public class TeamControllerAutowiredTest extends AbstractAutowiredControllerTest
 	 * PLFM-6390
 	 * @throws Exception
 	 */
-	@Test
 	public void testGetTeamMembersByInvalidId() throws Exception {
 		String invalidTeamId = "000";
 		String expectedResponse = "{\"reason\":\"Team does not exist for teamId: " + invalidTeamId + "\"}";
@@ -81,15 +76,14 @@ public class TeamControllerAutowiredTest extends AbstractAutowiredControllerTest
 			// Call under test
 			servletTestHelper.getTeamMembersWithTeamId(dispatchServlet, adminUserId, invalidTeamId);
 		});
-		Assert.assertEquals(expectedResponse, exception.getMessage());
+		Assertions.assertEquals(expectedResponse, exception.getMessage());
 	}
 
-	@Test
 	public void testGetTeamMembersByValidId() throws Exception {
 		String expectedResponse = "{\"totalNumberOfResults\":1,\"results\":[{\"teamId\":\"" + teamToDelete.getId() + "\",\"member\":" +
 				"{\"ownerId\":\"1\",\"userName\":\"migrationAdmin\",\"isIndividual\":true},\"isAdmin\":true}]}";
 		// Call under test
 		MockHttpServletResponse response = servletTestHelper.getTeamMembersWithTeamId(dispatchServlet, adminUserId, teamToDelete.getId());
-		Assert.assertEquals(expectedResponse, response.getContentAsString());
+		Assertions.assertEquals(expectedResponse, response.getContentAsString());
 	}
 }
