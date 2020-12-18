f3c766e7a92d64308bc339f1c9735137394d037e
john-hill <john.hill@sagebase.org>
2020-12-17 17:50:19 -0800

    new test helpers and more tests

--- lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/ar/AccessRestrictionStatusDaoImplTest.java
+++ lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/ar/AccessRestrictionStatusDaoImplTest.java
@@ -15,8 +15,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.sagebionetworks.repo.model.ACCESS_TYPE;
 import org.sagebionetworks.repo.model.ACTAccessRequirement;
+import org.sagebionetworks.repo.model.AccessApproval;
 import org.sagebionetworks.repo.model.AccessApprovalDAO;
 import org.sagebionetworks.repo.model.AccessRequirementDAO;
+import org.sagebionetworks.repo.model.ApprovalState;
 import org.sagebionetworks.repo.model.DatastoreException;
 import org.sagebionetworks.repo.model.EntityType;
 import org.sagebionetworks.repo.model.LockAccessRequirement;
@@ -27,6 +29,7 @@ import org.sagebionetworks.repo.model.RestrictableObjectType;
 import org.sagebionetworks.repo.model.TermsOfUseAccessRequirement;
 import org.sagebionetworks.repo.model.UserGroup;
 import org.sagebionetworks.repo.model.UserGroupDAO;
+import org.sagebionetworks.repo.model.helper.DoaObjectHelper;
 import org.sagebionetworks.repo.model.jdo.KeyFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -51,8 +54,21 @@ public class AccessRestrictionStatusDaoImplTest {
 	@Autowired
 	private NodeDAO nodeDao;
 
+	@Autowired
+	private DoaObjectHelper<Node> nodeDaoHelper;
+	
+	@Autowired
+	private DoaObjectHelper<UserGroup> userGroupHelpler;
+	
+	@Autowired
+	private DoaObjectHelper<TermsOfUseAccessRequirement> termsOfUseHelper;
+	
+	@Autowired
+	private DoaObjectHelper<AccessApproval> accessApprovalHelper;
+	
 	Long userOneId;
 	Long userTwoId;
+	Long userThreeId;
 
 	Node project;
 	Node folder;
@@ -68,8 +84,9 @@ public class AccessRestrictionStatusDaoImplTest {
 		UserGroup ug = new UserGroup();
 		ug.setIsIndividual(true);
 		ug.setCreationDate(new Date());
-		userOneId = userGroupDAO.create(ug);
-		userTwoId = userGroupDAO.create(ug);
+		userOneId = Long.parseLong(userGroupHelpler.create(u->{}).getId());
+		userTwoId = Long.parseLong(userGroupHelpler.create(u->{}).getId());
+		userThreeId = Long.parseLong(userGroupHelpler.create(u->{}).getId());
 	}
 
 	@AfterEach
@@ -82,6 +99,9 @@ public class AccessRestrictionStatusDaoImplTest {
 		}
 		if (userTwoId != null) {
 			userGroupDAO.delete(userTwoId.toString());
+		}
+		if (userThreeId != null) {
+			userGroupDAO.delete(userThreeId.toString());
 		}
 		accessApprovalDAO.clear();
 		accessRequirementDAO.clear();
@@ -108,23 +128,247 @@ public class AccessRestrictionStatusDaoImplTest {
 	@Test
 	public void testGeEntityStatusWithUnmetRestriction() {
 		setupNodeHierarchy(userTwoId);
-		TermsOfUseAccessRequirement projectToU = createNewTermsOfUse(userTwoId, project.getId(),
-				RestrictableObjectType.ENTITY);
+		
+		TermsOfUseAccessRequirement projectToU = termsOfUseHelper.create(t->{
+			t.setCreatedBy(userThreeId.toString());
+			t.getSubjectIds().get(0).setId(project.getId());
+		});
 		List<Long> subjectIds = KeyFactory.stringToKey(Arrays.asList(file.getId(), fileTwo.getId()));
 		// call under test
 		List<SubjectStatus> results = accessRestrictionStatusDao.getEntityStatus(subjectIds, userOneId);
 		validateBasicSubjectStatus(subjectIds, results, userOneId);
-		
+
 		List<UsersRequirementStatus> expected = Arrays
 				.asList(new UsersRequirementStatus().withRequirementId(projectToU.getId())
 						.withRequirementType(projectToU.getConcreteType()).withIsUnmet(true));
-		
+
 		SubjectStatus result = results.get(0);
 		assertTrue(result.hasUnmet());
 		assertEquals(expected, result.getAccessRestrictions());
-		
+
 		result = results.get(1);
 		assertTrue(result.hasUnmet());
+		assertEquals(expected, result.getAccessRestrictions());
+	}
+
+	@Test
+	public void testGeEntityStatusWithUnmetRestrictionAsFileCreator() {
+		setupNodeHierarchy(userTwoId);
+		TermsOfUseAccessRequirement projectToU = termsOfUseHelper.create(t->{
+			t.setCreatedBy(userThreeId.toString());
+			t.getSubjectIds().get(0).setId(project.getId());
+		});
+		List<Long> subjectIds = KeyFactory.stringToKey(Arrays.asList(file.getId(), fileTwo.getId()));
+		// call under test
+		List<SubjectStatus> results = accessRestrictionStatusDao.getEntityStatus(subjectIds, userTwoId);
+		validateBasicSubjectStatus(subjectIds, results, userTwoId);
+
+		List<UsersRequirementStatus> expected = Arrays
+				.asList(new UsersRequirementStatus().withRequirementId(projectToU.getId())
+						.withRequirementType(projectToU.getConcreteType()).withIsUnmet(false));
+
+		SubjectStatus result = results.get(0);
+		assertFalse(result.hasUnmet());
+		assertEquals(expected, result.getAccessRestrictions());
+
+		result = results.get(1);
+		assertFalse(result.hasUnmet());
+		assertEquals(expected, result.getAccessRestrictions());
+	}
+
+	@Test
+	public void testGeEntityStatusWithApprovedRestriction() {
+		setupNodeHierarchy(userTwoId);
+		TermsOfUseAccessRequirement projectToU = termsOfUseHelper.create(t->{
+			t.setCreatedBy(userThreeId.toString());
+			t.getSubjectIds().get(0).setId(project.getId());
+		});
+		accessApprovalHelper.create(a->{
+			a.setCreatedBy(userThreeId.toString());
+			a.setSubmitterId(userTwoId.toString());
+			a.setRequirementId(projectToU.getId());
+			a.setRequirementVersion(projectToU.getVersionNumber());
+			a.setState(ApprovalState.APPROVED);
+			a.setAccessorId(userOneId.toString());
+		});
+
+		List<Long> subjectIds = KeyFactory.stringToKey(Arrays.asList(file.getId()));
+		// call under test
+		List<SubjectStatus> results = accessRestrictionStatusDao.getEntityStatus(subjectIds, userOneId);
+		validateBasicSubjectStatus(subjectIds, results, userOneId);
+
+		List<UsersRequirementStatus> expected = Arrays
+				.asList(new UsersRequirementStatus().withRequirementId(projectToU.getId())
+						.withRequirementType(projectToU.getConcreteType()).withIsUnmet(false));
+
+		SubjectStatus result = results.get(0);
+		assertFalse(result.hasUnmet());
+		assertEquals(expected, result.getAccessRestrictions());
+	}
+	
+	@Test
+	public void testGeEntityStatusWithRevokedRestriction() {
+		setupNodeHierarchy(userTwoId);
+		TermsOfUseAccessRequirement projectToU = termsOfUseHelper.create(t->{
+			t.setCreatedBy(userThreeId.toString());
+			t.getSubjectIds().get(0).setId(project.getId());
+		});
+		accessApprovalHelper.create(a->{
+			a.setCreatedBy(userThreeId.toString());
+			a.setSubmitterId(userTwoId.toString());
+			a.setRequirementId(projectToU.getId());
+			a.setRequirementVersion(projectToU.getVersionNumber());
+			a.setState(ApprovalState.REVOKED);
+			a.setAccessorId(userOneId.toString());
+		});
+
+		List<Long> subjectIds = KeyFactory.stringToKey(Arrays.asList(file.getId()));
+		// call under test
+		List<SubjectStatus> results = accessRestrictionStatusDao.getEntityStatus(subjectIds, userOneId);
+		validateBasicSubjectStatus(subjectIds, results, userOneId);
+
+		List<UsersRequirementStatus> expected = Arrays
+				.asList(new UsersRequirementStatus().withRequirementId(projectToU.getId())
+						.withRequirementType(projectToU.getConcreteType()).withIsUnmet(true));
+
+		SubjectStatus result = results.get(0);
+		assertTrue(result.hasUnmet());
+		assertEquals(expected, result.getAccessRestrictions());
+	}
+	
+	@Test
+	public void testGeEntityStatusWithRevokeAndApproved() {
+		setupNodeHierarchy(userTwoId);
+		TermsOfUseAccessRequirement projectToUV1 = termsOfUseHelper.create(t->{
+			t.setCreatedBy(userThreeId.toString());
+			t.getSubjectIds().get(0).setId(project.getId());
+		});
+		
+		List<Long> subjectIds = KeyFactory.stringToKey(Arrays.asList(file.getId()));
+		
+		// create a second version of this ToU
+		TermsOfUseAccessRequirement toUpdate = (TermsOfUseAccessRequirement) accessRequirementDAO.get(projectToUV1.getId().toString());
+		toUpdate.setVersionNumber(toUpdate.getVersionNumber()+1L);
+		TermsOfUseAccessRequirement projectToUV2 = accessRequirementDAO.update(toUpdate);
+		// revoke the first version
+		accessApprovalHelper.create(a->{
+			a.setCreatedBy(userThreeId.toString());
+			a.setSubmitterId(userTwoId.toString());
+			a.setRequirementId(projectToUV1.getId());
+			a.setRequirementVersion(projectToUV1.getVersionNumber());
+			a.setState(ApprovalState.REVOKED);
+			a.setAccessorId(userOneId.toString());
+		});
+		
+		// approve the second version
+		accessApprovalHelper.create(a->{
+			a.setCreatedBy(userThreeId.toString());
+			a.setSubmitterId(userTwoId.toString());
+			a.setRequirementId(projectToUV2.getId());
+			a.setRequirementVersion(projectToUV2.getVersionNumber());
+			a.setState(ApprovalState.APPROVED);
+			a.setAccessorId(userOneId.toString());
+		});
+
+		// call under test
+		List<SubjectStatus> results = accessRestrictionStatusDao.getEntityStatus(subjectIds, userOneId);
+		validateBasicSubjectStatus(subjectIds, results, userOneId);
+
+		List<UsersRequirementStatus> expected = Arrays
+				.asList(new UsersRequirementStatus().withRequirementId(projectToUV2.getId())
+						.withRequirementType(projectToUV2.getConcreteType()).withIsUnmet(false));
+
+		SubjectStatus result = results.get(0);
+		assertFalse(result.hasUnmet());
+		assertEquals(expected, result.getAccessRestrictions());
+	}
+	
+	@Test
+	public void testGeEntityStatusWithMultipleRevoke() {
+		setupNodeHierarchy(userTwoId);
+		TermsOfUseAccessRequirement projectToUV1 = termsOfUseHelper.create(t->{
+			t.setCreatedBy(userThreeId.toString());
+			t.getSubjectIds().get(0).setId(project.getId());
+		});
+		
+		List<Long> subjectIds = KeyFactory.stringToKey(Arrays.asList(file.getId()));
+		
+		// create a second version of this ToU
+		TermsOfUseAccessRequirement toUpdate = (TermsOfUseAccessRequirement) accessRequirementDAO.get(projectToUV1.getId().toString());
+		toUpdate.setVersionNumber(toUpdate.getVersionNumber()+1L);
+		TermsOfUseAccessRequirement projectToUV2 = accessRequirementDAO.update(toUpdate);
+		// revoke the first version
+		accessApprovalHelper.create(a->{
+			a.setCreatedBy(userThreeId.toString());
+			a.setSubmitterId(userTwoId.toString());
+			a.setRequirementId(projectToUV1.getId());
+			a.setRequirementVersion(projectToUV1.getVersionNumber());
+			a.setState(ApprovalState.REVOKED);
+			a.setAccessorId(userOneId.toString());
+		});
+		
+		// approve the second version
+		accessApprovalHelper.create(a->{
+			a.setCreatedBy(userThreeId.toString());
+			a.setSubmitterId(userTwoId.toString());
+			a.setRequirementId(projectToUV2.getId());
+			a.setRequirementVersion(projectToUV2.getVersionNumber());
+			a.setState(ApprovalState.REVOKED);
+			a.setAccessorId(userOneId.toString());
+		});
+
+		// call under test
+		List<SubjectStatus> results = accessRestrictionStatusDao.getEntityStatus(subjectIds, userOneId);
+		validateBasicSubjectStatus(subjectIds, results, userOneId);
+
+		List<UsersRequirementStatus> expected = Arrays
+				.asList(new UsersRequirementStatus().withRequirementId(projectToUV2.getId())
+						.withRequirementType(projectToUV2.getConcreteType()).withIsUnmet(true));
+
+		SubjectStatus result = results.get(0);
+		assertTrue(result.hasUnmet());
+		assertEquals(expected, result.getAccessRestrictions());
+	}
+	
+	/**
+	 * If a user has been approved for one version of an access requirement, they 
+	 * are still approved even if a new version of the requirement is created.
+	 */
+	@Test
+	public void testGeEntityStatusWithWithOldApproval() {
+		setupNodeHierarchy(userTwoId);
+		TermsOfUseAccessRequirement projectToUV1 = termsOfUseHelper.create(t->{
+			t.setCreatedBy(userThreeId.toString());
+			t.getSubjectIds().get(0).setId(project.getId());
+		});
+		
+		// approve the first version only
+		accessApprovalHelper.create(a->{
+			a.setCreatedBy(userThreeId.toString());
+			a.setSubmitterId(userTwoId.toString());
+			a.setRequirementId(projectToUV1.getId());
+			a.setRequirementVersion(projectToUV1.getVersionNumber());
+			a.setState(ApprovalState.APPROVED);
+			a.setAccessorId(userOneId.toString());
+		});
+
+		// create a second version of this ToU that the user has not been approved for.
+		TermsOfUseAccessRequirement toUpdate = (TermsOfUseAccessRequirement) accessRequirementDAO.get(projectToUV1.getId().toString());
+		toUpdate.setVersionNumber(toUpdate.getVersionNumber()+1L);
+		TermsOfUseAccessRequirement projectToUV2 = accessRequirementDAO.update(toUpdate);
+		
+		List<Long> subjectIds = KeyFactory.stringToKey(Arrays.asList(file.getId()));
+
+		// call under test
+		List<SubjectStatus> results = accessRestrictionStatusDao.getEntityStatus(subjectIds, userOneId);
+		validateBasicSubjectStatus(subjectIds, results, userOneId);
+
+		List<UsersRequirementStatus> expected = Arrays
+				.asList(new UsersRequirementStatus().withRequirementId(projectToUV2.getId())
+						.withRequirementType(projectToUV2.getConcreteType()).withIsUnmet(false));
+
+		SubjectStatus result = results.get(0);
+		assertFalse(result.hasUnmet());
 		assertEquals(expected, result.getAccessRestrictions());
 	}
 
@@ -155,62 +399,38 @@ public class AccessRestrictionStatusDaoImplTest {
 	 * @param userId
 	 */
 	public void setupNodeHierarchy(Long userId) {
-		project = createNewNode("aProject", userId, null, EntityType.project);
-		folder = createNewNode("aFolder", userId, project.getId(), EntityType.folder);
-		file = createNewNode("aFile", userId, folder.getId(), EntityType.file);
-		folderTwo = createNewNode("folderTwo", userTwoId, project.getId(), EntityType.folder);
-		fileTwo = createNewNode("fileTwo", userTwoId, folderTwo.getId(), EntityType.file);
+		
+		project = nodeDaoHelper.create(n ->{
+			n.setName("aProject");
+			n.setCreatedByPrincipalId(userId);
+		});
+		folder = nodeDaoHelper.create(n -> {
+			n.setName("aFolder");
+			n.setCreatedByPrincipalId(userId);
+			n.setParentId(project.getId());
+			n.setNodeType(EntityType.folder);
+		});
+		file = nodeDaoHelper.create(n -> {
+			n.setName("aFile");
+			n.setCreatedByPrincipalId(userId);
+			n.setParentId(folder.getId());
+			n.setNodeType(EntityType.file);
+		});
+		folderTwo = nodeDaoHelper.create(n -> {
+			n.setName("folderTwo");
+			n.setCreatedByPrincipalId(userId);
+			n.setParentId(project.getId());
+			n.setNodeType(EntityType.folder);
+		});
+		fileTwo = nodeDaoHelper.create(n -> {
+			n.setName("fileTwo");
+			n.setCreatedByPrincipalId(userId);
+			n.setParentId(folderTwo.getId());
+			n.setNodeType(EntityType.file);
+		});
 	}
-
-	/**
-	 * Helper to create a new node.
-	 * 
-	 * @param name
-	 * @param userId
-	 * @param parentId
-	 * @param type
-	 * @return
-	 */
-	public Node createNewNode(String name, Long userId, String parentId, EntityType type) {
-		Node node = new Node();
-		node.setName(name);
-		node.setCreatedByPrincipalId(userId);
-		node.setModifiedByPrincipalId(userId);
-		node.setCreatedOn(new Date());
-		node.setModifiedOn(node.getCreatedOn());
-		node.setNodeType(type);
-		node.setParentId(parentId);
-		return nodeDao.createNewNode(node);
-	}
-
-	/**
-	 * Helper to create a TermsOfUseAccessRequirement.
-	 * 
-	 * @param userId
-	 * @param subjectId
-	 * @param subjectType
-	 * @param text
-	 * @return
-	 * @throws DatastoreException
-	 */
-	public TermsOfUseAccessRequirement createNewTermsOfUse(Long userId, String subjectId,
-			RestrictableObjectType subjectType) throws DatastoreException {
-		TermsOfUseAccessRequirement ar = new TermsOfUseAccessRequirement();
-		ar.setCreatedBy(userId.toString());
-		ar.setCreatedOn(new Date());
-		ar.setModifiedBy(userId.toString());
-		ar.setModifiedOn(new Date());
-		ar.setEtag("10");
-		ar.setAccessType(ACCESS_TYPE.DOWNLOAD);
-		ar.setVersionNumber(1L);
-		RestrictableObjectDescriptor rod = new RestrictableObjectDescriptor();
-		rod.setId(subjectId);
-		rod.setType(subjectType);
-		ar.setSubjectIds(Arrays.asList(new RestrictableObjectDescriptor[] { rod }));
-		ar.setTermsOfUse("Must agree");
-		return accessRequirementDAO.create(ar);
-	}
-
+	
+	
 	/**
 	 * Helper to create a new LockAccessRequirement.
 	 * 
