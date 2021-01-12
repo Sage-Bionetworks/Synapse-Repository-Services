fb8b4e641e9fe7082294e37550779d06173eff03
Ziming Dong <ziming.dong@sagebase.org>
2021-01-11 19:38:15 -0800

    cherry pick + minor adjustments to PLFM-4892

--- lib/lib-table-query/src/main/java/org/sagebionetworks/table/query/model/ValueExpression.java
+++ lib/lib-table-query/src/main/java/org/sagebionetworks/table/query/model/ValueExpression.java
@@ -9,4 +9,17 @@ public class ValueExpression extends SimpleBranch {
 		super(numericValueExpression);
 	}
 
+	public String getDisplayName(){
+		NumericPrimary numericPrimary = this.getFirstElementOfType(NumericPrimary.class);
+		SQLElement numericPrimaryChild = numericPrimary.getChild();
+		if (numericPrimaryChild instanceof  ValueExpressionPrimary){
+			ValueExpressionPrimary valueExpressionPrimary = (ValueExpressionPrimary) numericPrimaryChild;
+			if(valueExpressionPrimary.getChild() instanceof ColumnReference || valueExpressionPrimary.getChild() instanceof UnsignedValueSpecification) {
+				return this.toSqlWithoutQuotes();
+			}
+		}
+
+		return this.toSql();
+	}
+
 }
