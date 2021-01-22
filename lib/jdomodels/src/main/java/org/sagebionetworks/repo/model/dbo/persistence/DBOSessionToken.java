61728374919aa38cf1fb1c44f3a177f9f8cd0c07
john-hill <john.hill@sagebase.org>
2021-01-21 18:14:15 -0800

    remove extra columns

--- lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/persistence/DBOSessionToken.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/repo/model/dbo/persistence/DBOSessionToken.java
@@ -1,7 +1,5 @@
 package org.sagebionetworks.repo.model.dbo.persistence;
 
-import static org.sagebionetworks.repo.model.query.SQLConstants.COL_SUBMISSION_CONTRIBUTOR_PRINCIPAL_ID;
-import static org.sagebionetworks.repo.model.query.SQLConstants.COL_SUBMISSION_CONTRIBUTOR_SUBMISSION_ID;
 import static org.sagebionetworks.repo.model.query.jdo.SqlConstants.COL_SESSION_TOKEN_PRINCIPAL_ID;
 import static org.sagebionetworks.repo.model.query.jdo.SqlConstants.COL_SESSION_TOKEN_SESSION_TOKEN;
 import static org.sagebionetworks.repo.model.query.jdo.SqlConstants.COL_SESSION_TOKEN_VALIDATED_ON;
