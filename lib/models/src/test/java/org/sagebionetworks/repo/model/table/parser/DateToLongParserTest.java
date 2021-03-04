52c41241ced893e5ebbe0a20d6ed67bcc0c24031
Ziming Dong <ziming.dong@sagebase.org>
2021-03-03 18:58:40 -0800

    remove unused import

--- lib/models/src/test/java/org/sagebionetworks/repo/model/table/parser/DateToLongParserTest.java
+++ lib/models/src/test/java/org/sagebionetworks/repo/model/table/parser/DateToLongParserTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
