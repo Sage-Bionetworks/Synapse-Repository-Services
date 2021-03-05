327cdc4e0e222f77e81253c11864a073118b033c
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2021-03-05 12:08:07 -0800

    Refactoring: worker logger allows sending more usable metrics

--- lib/logging/src/main/java/org/sagebionetworks/cloudwatch/WorkerLogger.java
+++ lib/logging/src/main/java/org/sagebionetworks/cloudwatch/WorkerLogger.java
@@ -9,30 +9,52 @@ import org.sagebionetworks.repo.model.message.ChangeMessage;
  *
  */
 public interface WorkerLogger {
-	public void setShouldProfile(boolean shouldProfile);
-	
+
+	String WORKER_NAMESPACE = "Asynchronous Workers";
+	String DIMENSION_WILL_RETRY = "willRetry";
+	String DIMENSION_CHANGE_TYPE = "changeType";
+	String DIMENSION_OBJECT_TYPE = "objectType";
+	String DIMENSION_STACK_TRACE = "stackTrace";
+	String DIMENSION_WORKER_CLASS = "workerClass";
+
 	/**
-	 * Log a change message driven worker event.
+	 * Log a change message driven worker event. The given class name (Class.getName) will be used as
+	 * the metric name. The namespace follows the pattern <{@link #WORKER_NAMESPACE} - stackInstance>,
+	 * the stack trace and the willRetry will be used as dimensions. If a changeMessage is supplied its
+	 * changeType and objectType will be added as dimensions.
+	 * 
 	 * @param workerClass
 	 * @param changeMessage
 	 * @param cause
 	 * @param willRetry
 	 */
-	public void logWorkerFailure(Class<? extends Object> workerClass, ChangeMessage changeMessage, Throwable cause, boolean willRetry);
-	
+	void logWorkerFailure(Class<? extends Object> workerClass, ChangeMessage changeMessage, Throwable cause, boolean willRetry);
+
 	/**
-	 * Log a generic worker failure.
+	 * Log a generic worker failure. The namespace follows the pattern <{@link #WORKER_NAMESPACE} -
+	 * stackInstance>, the stack trace and the willRetry will be used as dimensions.
+	 * 
 	 * @param metricName This will be main name of the metric.
-	 * @param cause The exception that caused the failure.
-	 * @param willRetry Will the worker re-try this job? 
+	 * @param cause      The exception that caused the failure.
+	 * @param willRetry  Will the worker re-try this job?
 	 */
-	public void logWorkerFailure(String metricName, Throwable cause, boolean willRetry);
-	
+	void logWorkerFailure(String metricName, Throwable cause, boolean willRetry);
+
+	/**
+	 * Sends a cloudwatch count metric (with a value of 1) for a worker defined by the given class. The
+	 * namespace follows the pattern <{@link #WORKER_NAMESPACE} - stackInstance>, the worker (simple)
+	 * class name is used as the workerClass dimension. The willRetry is used as dimension.
+	 * 
+	 * @param workerClass The class whose {@link Class#getSimpleName()} will be used as the workerClass dimension
+	 * @param metricName  The name of the metric
+	 * @param willRetry The value for specifying if the worker will retry, a dimension willRetry will be added to the metric
+	 */
+	void logWorkerMetric(Class<?> workerClass, String metricName, boolean willRetry);
+
 	/**
 	 * Log a custom metric
+	 * 
 	 * @param pd
 	 */
-	public void logCustomMetric(ProfileData pd);
-	
-	public void setConsumer(Consumer consumer);
+	void logCustomMetric(ProfileData pd);
 }
--- lib/logging/src/main/java/org/sagebionetworks/cloudwatch/WorkerLoggerImpl.java
+++ lib/logging/src/main/java/org/sagebionetworks/cloudwatch/WorkerLoggerImpl.java
@@ -4,38 +4,95 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.sagebionetworks.StackConfigurationSingleton;
+import org.sagebionetworks.StackConfiguration;
 import org.sagebionetworks.repo.model.message.ChangeMessage;
+import org.sagebionetworks.util.ValidateArgument;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.amazonaws.services.cloudwatch.model.StandardUnit;
+import com.google.common.collect.ImmutableMap;
 
 public class WorkerLoggerImpl implements WorkerLogger {
 
+	private Consumer consumer;
+	private String workersNamespace;
+
 	@Autowired
-	Consumer consumer;
-	
+	public WorkerLoggerImpl(Consumer consumer, StackConfiguration config) {
+		this.consumer = consumer;
+		this.workersNamespace = WORKER_NAMESPACE + " - " + config.getStackInstance();
+		this.shouldProfile = config.getCloudWatchOnOff();
+
+	}
+
 	private boolean shouldProfile;
-	
+
 	/**
 	 * Spring will inject this value.
+	 * 
 	 * @param shouldProfile
 	 */
 	public void setShouldProfile(boolean shouldProfile) {
 		this.shouldProfile = shouldProfile;
 	}
-	
+
 	/**
 	 * Default no parameter ControllerProfiler constructor.
 	 */
-	public WorkerLoggerImpl(){
+	public WorkerLoggerImpl() {
 	}
 
 	/**
-	 * One parameter constructor for ControllerProfiler.
-	 * @param consumer who receives the latency information
+	 * 
+	 * @param workerClass
+	 * @param changeMessage
+	 * @param cause         can be null
+	 * @param willRetry
 	 */
-	public WorkerLoggerImpl(Consumer consumer){
-		this.consumer = consumer;
-	}	
+	@Override
+	public void logWorkerFailure(Class<? extends Object> workerClass, ChangeMessage changeMessage, Throwable cause, boolean willRetry) {
+		if (!shouldProfile) {
+			return;
+		}
+		ProfileData profileData = buildProfileData(workerClass, changeMessage, cause, willRetry, new Date());
+		consumer.addProfileData(profileData);
+	}
+
+	@Override
+	public void logWorkerFailure(String metricName, Throwable cause, boolean willRetry) {
+		if (!shouldProfile) {
+			return;
+		}
+		ProfileData profileData = buildProfileData(metricName, null, cause, willRetry, new Date());
+		consumer.addProfileData(profileData);
+	}
+
+	@Override
+	public void logWorkerMetric(Class<?> workerClass, String metricName, boolean willRetry) {
+		if (!shouldProfile) {
+			return;
+		}
+
+		ValidateArgument.required(workerClass, "The workerClass");
+		ValidateArgument.required(metricName, "The metricName");
+		
+		Map<String, String> dimensions = ImmutableMap.of(
+				DIMENSION_WORKER_CLASS, workerClass.getSimpleName(),
+				DIMENSION_WILL_RETRY, String.valueOf(willRetry)
+		);
+
+		ProfileData profileData = buildProfileData(new Date(), metricName, dimensions);
+		consumer.addProfileData(profileData);
+
+	}
+
+	@Override
+	public void logCustomMetric(ProfileData profileData) {
+		if (!shouldProfile) {
+			return;
+		}
+		consumer.addProfileData(profileData);
+	}
 	
 	/**
 	 * Makes transfer object and returns it.
@@ -46,95 +103,50 @@ public class WorkerLoggerImpl implements WorkerLogger {
 	 * @param willRetry
 	 * @return
 	 */
-	public static ProfileData makeProfileDataDTO(
-			Class<?> workerClass, 
-			ChangeMessage changeMessage, 
-			Throwable cause, 
-			boolean willRetry,
-			Date timestamp) {
-		return makeProfileDataDTO(workerClass.getName(), changeMessage, cause, willRetry, timestamp);
-	}
-	
-	/**
-	 * The more generic form of 
-	 * @param name The name of the metric.
-	 * @param changeMessage This is an optional parameter;
-	 * @param cause The exception that caused this error.
-	 * @param willRetry Will this work be re-tried?
-	 * @param timestamp The time the error occurred.
-	 * @return
-	 */
-	public static ProfileData makeProfileDataDTO(
-			String name, 
-			ChangeMessage changeMessage, 
-			Throwable cause, 
-			boolean willRetry,
-			Date timestamp) {
-		ProfileData nextPD = new ProfileData();
-		nextPD.setNamespace(WORKER_NAMESPACE+" - "+ StackConfigurationSingleton.singleton().getStackInstance()); 
-		nextPD.setName(name);
-		nextPD.setValue(1D); // i.e. we are counting discrete events
-		nextPD.setUnit("Count"); // for allowed values see http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/cloudwatch/model/StandardUnit.html
-		nextPD.setTimestamp(timestamp);
-		Map<String,String> dimension = new HashMap<String, String>();
-		dimension.put(WILL_RETRY_KEY, ""+willRetry);
-		if(changeMessage != null){
-			dimension.put(CHANGE_TYPE_KEY, changeMessage.getChangeType().name());
-			dimension.put(OBJECT_TYPE_KEY, changeMessage.getObjectType().name());
-		}
-		String stackTraceAsString = MetricUtils.stackTracetoString(cause);
-		dimension.put(STACK_TRACE_KEY, stackTraceAsString);
-		nextPD.setDimension(dimension);
-		
-		return nextPD;
-	}
-	
-	
-	public static final String WORKER_NAMESPACE = "Asynchronous Workers";
-	private static final String WILL_RETRY_KEY = "willRetry";
-	private static final String CHANGE_TYPE_KEY = "changeType";
-	private static final String OBJECT_TYPE_KEY = "objectType";
-	private static final String STACK_TRACE_KEY = "stackTrace";
-	
-	/**
-	 * 
-	 * @param workerClass
-	 * @param changeMessage
-	 * @param cause can be null
-	 * @param willRetry
-	 */
-	public void logWorkerFailure(Class<? extends Object> workerClass, ChangeMessage changeMessage, Throwable cause, boolean willRetry) {
-		if (!shouldProfile) return;
-		ProfileData profileData = makeProfileDataDTO(workerClass, changeMessage, cause, willRetry, new Date());
-		consumer.addProfileData(profileData);
-	}
-	
-	@Override
-	public void logWorkerFailure(String metricName, Throwable cause, boolean willRetry) {
-		if (!shouldProfile) return;
-		ProfileData profileData = makeProfileDataDTO(metricName, null, cause, willRetry, new Date());
-		consumer.addProfileData(profileData);
-	}
-	
-	/**
-	 * Setter for consumer.  
-	 * @param consumer
-	 */
-	public void setConsumer(Consumer consumer) {
-		this.consumer = consumer;
-	}
-	
-	/**
-	 * Getter for consumer
-	 * @return Consumer
-	 */
-	public Consumer getConsumer() {
-		return this.consumer;
+	private ProfileData buildProfileData(Class<?> workerClass, ChangeMessage changeMessage, Throwable cause, boolean willRetry, Date timestamp) {
+		return buildProfileData(workerClass.getName(), changeMessage, cause, willRetry, timestamp);
 	}
 
-	@Override
-	public void logCustomMetric(ProfileData profileData) {
-		if (!shouldProfile) return;
-		consumer.addProfileData(profileData);
+	/**
+	 * The more generic form of
+	 * 
+	 * @param name          The name of the metric.
+	 * @param changeMessage This is an optional parameter;
+	 * @param cause         The exception that caused this error.
+	 * @param willRetry     Will this work be re-tried?
+	 * @param timestamp     The time the error occurred.
+	 * @return
+	 */
+	private ProfileData buildProfileData(String name, ChangeMessage changeMessage, Throwable cause, boolean willRetry, Date timestamp) {
+		Map<String, String> dimension = new HashMap<String, String>();
+		
+		dimension.put(DIMENSION_WILL_RETRY, String.valueOf(willRetry));
+
+		if (changeMessage != null) {
+			dimension.put(DIMENSION_CHANGE_TYPE, changeMessage.getChangeType().name());
+			dimension.put(DIMENSION_OBJECT_TYPE, changeMessage.getObjectType().name());
+		}
+
+		String stackTraceAsString = MetricUtils.stackTracetoString(cause);
+
+		dimension.put(DIMENSION_STACK_TRACE, stackTraceAsString);
+
+		return buildProfileData(timestamp, name, dimension);
+	}
+
+	private ProfileData buildProfileData(Date timestamp, String metricName, Map<String, String> dimensions) {
+		ProfileData data = new ProfileData();
+
+		data.setNamespace(workersNamespace);
+		data.setName(metricName);
+		data.setUnit(StandardUnit.Count.name());
+		data.setValue(1D);
+		data.setTimestamp(timestamp);
+
+		if (dimensions != null && !dimensions.isEmpty()) {
+			data.setDimension(dimensions);
+		}
+
+		return data;
 	}
 }
