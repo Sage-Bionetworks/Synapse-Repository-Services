3bfe0df4b19f7f28d157371cfbea1dd22f2323eb
williamvietnguyen <william.nguyen@sagebase.org>
2021-05-10 18:18:50 -0700

    Remove objectacl, add sts to docker

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/sts/StsManagerImpl.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/sts/StsManagerImpl.java
@@ -47,7 +47,7 @@ public class StsManagerImpl implements StsManager {
 			ImmutableMap.<StsPermission, String>builder()
 					.put(StsPermission.read_only, "\"s3:GetObject\",\"s3:ListBucket\"")
 					.put(StsPermission.read_write, "\"s3:AbortMultipartUpload\",\"s3:DeleteObject\",\"s3:GetObject\",\"s3:ListBucket\"," +
-							"\"s3:ListMultipartUploadParts\",\"s3:PutObject\",\"s3:ListBucketMultipartUploads\",\"s3:PutObjectAcl\"")
+							"\"s3:ListMultipartUploadParts\",\"s3:PutObject\",\"s3:ListBucketMultipartUploads\"")
 					.build();
 
 	@Autowired
