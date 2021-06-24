275e2791f63e0de0c9570057be8f0bc54568ffd7
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2021-06-24 15:44:49 -0700

    PLFM-6764: Method to fetch unlinked files

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/file/FileHandleArchivalManagerTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/file/FileHandleArchivalManagerTest.java
@@ -2,21 +2,37 @@ package org.sagebionetworks.repo.manager.file;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.sagebionetworks.StackConfiguration;
 import org.sagebionetworks.repo.model.UnauthorizedException;
 import org.sagebionetworks.repo.model.UserInfo;
 import org.sagebionetworks.repo.model.dbo.file.FileHandleDao;
 import org.sagebionetworks.repo.model.file.FileHandleArchivalRequest;
 import org.sagebionetworks.repo.model.file.FileHandleArchivalResponse;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.GetQueueUrlResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @ExtendWith(MockitoExtension.class)
 public class FileHandleArchivalManagerTest {
+	
+	@Mock
+	private StackConfiguration mockConfig;
+	
+	@Mock
+	private ObjectMapper mockMapper;
+	
+	@Mock
+	private AmazonSQS mockSqs;
 	
 	@Mock
 	private FileHandleDao mockFileDao;
@@ -29,6 +45,16 @@ public class FileHandleArchivalManagerTest {
 	
 	@Mock
 	private FileHandleArchivalRequest mockRequest;
+	
+	private String queueUrl = "queueUrl";
+	
+	@BeforeEach
+	public void setup() {
+		when(mockConfig.getQueueName(anyString())).thenReturn("queueName");
+		when(mockSqs.getQueueUrl(anyString())).thenReturn(new GetQueueUrlResult().withQueueUrl(queueUrl));
+		// This is invoked automatically by spring
+		manager.configureQueue(mockConfig);
+	}
 	
 	@Test
 	public void testProcessArchivalRequest() {
