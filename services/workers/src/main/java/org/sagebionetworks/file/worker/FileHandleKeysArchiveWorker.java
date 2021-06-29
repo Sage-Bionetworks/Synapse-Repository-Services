6135a3ef8ca959332f3b870e4346b96d4e3dee22
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2021-06-28 22:36:00 -0700

    PLFM-6764: Keep going with other keys on exception

--- services/workers/src/main/java/org/sagebionetworks/file/worker/FileHandleKeysArchiveWorker.java
+++ services/workers/src/main/java/org/sagebionetworks/file/worker/FileHandleKeysArchiveWorker.java
@@ -2,11 +2,14 @@ package org.sagebionetworks.file.worker;
 
 import java.time.Instant;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.sagebionetworks.common.util.progress.ProgressCallback;
 import org.sagebionetworks.repo.manager.UserManager;
 import org.sagebionetworks.repo.manager.file.FileHandleArchivalManager;
 import org.sagebionetworks.repo.model.AuthorizationConstants.BOOTSTRAP_PRINCIPAL;
 import org.sagebionetworks.repo.model.UserInfo;
+import org.sagebionetworks.repo.model.file.FileHandleKeyArchiveResult;
 import org.sagebionetworks.repo.model.file.FileHandleKeysArchiveRequest;
 import org.sagebionetworks.workers.util.aws.message.MessageDrivenRunner;
 import org.sagebionetworks.workers.util.aws.message.RecoverableMessageException;
@@ -21,6 +24,8 @@ import com.amazonaws.services.sqs.model.Message;
  *
  */
 public class FileHandleKeysArchiveWorker implements MessageDrivenRunner {
+	
+	private static final Logger LOG = LogManager.getLogger(FileHandleKeysArchiveWorker.class);
 
 	private UserManager userManager;
 	private FileHandleArchivalManager archivalManager;
@@ -41,7 +46,17 @@ public class FileHandleKeysArchiveWorker implements MessageDrivenRunner {
 		Instant modifiedBefore = Instant.ofEpochMilli(request.getModifiedBefore());
 
 		for (String key : request.getKeys()) {
-			archivalManager.archiveUnlinkedFileHandlesByKey(adminUser, bucket, key, modifiedBefore);
+			try {
+				FileHandleKeyArchiveResult result = archivalManager.archiveUnlinkedFileHandlesByKey(adminUser, bucket, key, modifiedBefore);
+				LOG.debug("Key {} in bucket {} processed for archival ({} archived, wasTagged: {})", key, bucket, result.getArchivedCount(), result.isWasTagged());
+			} catch (Exception ex) {
+				boolean retryBatch = ex instanceof RecoverableMessageException;
+				LOG.error("Attempt to archive key {} in bucket {} failed (will retry: {}): {}", key, bucket, retryBatch, ex.getMessage(), ex);
+				if (retryBatch) {
+					throw ex;
+				}
+				// Try to keep going with the rest of the keys
+			}
 		}
 	}
 
--- services/workers/src/test/java/org/sagebionetworks/file/worker/FileHandleKeysArchiveWorkerTest.java
+++ services/workers/src/test/java/org/sagebionetworks/file/worker/FileHandleKeysArchiveWorkerTest.java
@@ -1,6 +1,7 @@
 package org.sagebionetworks.file.worker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
@@ -25,6 +26,7 @@ import org.sagebionetworks.repo.manager.UserManager;
 import org.sagebionetworks.repo.manager.file.FileHandleArchivalManager;
 import org.sagebionetworks.repo.model.UserInfo;
 import org.sagebionetworks.repo.model.AuthorizationConstants.BOOTSTRAP_PRINCIPAL;
+import org.sagebionetworks.repo.model.file.FileHandleKeyArchiveResult;
 import org.sagebionetworks.repo.model.file.FileHandleKeysArchiveRequest;
 import org.sagebionetworks.workers.util.aws.message.RecoverableMessageException;
 
@@ -51,6 +53,9 @@ public class FileHandleKeysArchiveWorkerTest {
 	@Mock
 	private FileHandleKeysArchiveRequest mockRequest;
 	
+	@Mock
+	private FileHandleKeyArchiveResult mockResult;
+	
 	@Captor
 	private ArgumentCaptor<String> keyCaptor;
 	
@@ -68,6 +73,62 @@ public class FileHandleKeysArchiveWorkerTest {
 		when(mockRequest.getBucket()).thenReturn(bucket);
 		when(mockRequest.getModifiedBefore()).thenReturn(modifiedBefore);
 		when(mockRequest.getKeys()).thenReturn(keys);
+		when(mockManager.archiveUnlinkedFileHandlesByKey(any(), any(), any(), any())).thenReturn(mockResult);
+		
+		// Call under test
+		worker.run(mockCallback, mockMessage);
+		
+		verify(mockManager).parseArchiveKeysRequestFromSqsMessage(mockMessage);
+		verify(mockUserManager).getUserInfo(BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId());
+		verify(mockManager, times(3)).archiveUnlinkedFileHandlesByKey(eq(adminUser), eq(bucket), keyCaptor.capture(), eq(Instant.ofEpochMilli(modifiedBefore)));
+		
+		assertEquals(keys, keyCaptor.getAllValues());
+	}
+	
+	@Test
+	public void testRunWithRecoverableException() throws RecoverableMessageException, Exception {
+		
+		String bucket = "bucket";
+		Long modifiedBefore = System.currentTimeMillis();
+		List<String> keys = Arrays.asList("key1", "key2", "key3");
+		
+		when(mockUserManager.getUserInfo(any())).thenReturn(adminUser);
+		when(mockManager.parseArchiveKeysRequestFromSqsMessage(any())).thenReturn(mockRequest);
+		when(mockRequest.getBucket()).thenReturn(bucket);
+		when(mockRequest.getModifiedBefore()).thenReturn(modifiedBefore);
+		when(mockRequest.getKeys()).thenReturn(keys);
+		
+		RecoverableMessageException ex = new RecoverableMessageException();
+		
+		when(mockManager.archiveUnlinkedFileHandlesByKey(any(), any(), any(), any())).thenThrow(ex);
+		
+		RecoverableMessageException result = assertThrows(RecoverableMessageException.class, () -> {			
+			// Call under test
+			worker.run(mockCallback, mockMessage);
+		});
+		
+		assertEquals(ex, result);
+		
+		verify(mockManager).parseArchiveKeysRequestFromSqsMessage(mockMessage);
+		verify(mockUserManager).getUserInfo(BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId());
+		verify(mockManager).archiveUnlinkedFileHandlesByKey(adminUser, bucket, "key1", Instant.ofEpochMilli(modifiedBefore));
+	}
+	
+	@Test
+	public void testRunWithOtherException() throws RecoverableMessageException, Exception {
+		
+		String bucket = "bucket";
+		Long modifiedBefore = System.currentTimeMillis();
+		List<String> keys = Arrays.asList("key1", "key2", "key3");
+		
+		when(mockUserManager.getUserInfo(any())).thenReturn(adminUser);
+		when(mockManager.parseArchiveKeysRequestFromSqsMessage(any())).thenReturn(mockRequest);
+		when(mockRequest.getBucket()).thenReturn(bucket);
+		when(mockRequest.getModifiedBefore()).thenReturn(modifiedBefore);
+		when(mockRequest.getKeys()).thenReturn(keys);
+		
+		when(mockManager.archiveUnlinkedFileHandlesByKey(any(), any(), any(), any())).thenReturn(mockResult);		
+		when(mockManager.archiveUnlinkedFileHandlesByKey(any(), any(), eq("key2"), any())).thenThrow(RuntimeException.class);
 		
 		// Call under test
 		worker.run(mockCallback, mockMessage);
