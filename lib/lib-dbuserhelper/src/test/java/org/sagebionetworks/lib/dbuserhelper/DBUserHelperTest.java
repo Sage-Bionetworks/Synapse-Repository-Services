4d912bdf101d77fc2627124d34e55d2b4f5aba91
Xavier Schildwachter <x.schildwachter@sagebase.org>
2021-10-04 16:42:24 -0700

    Add test

---
+++ lib/lib-dbuserhelper/src/test/java/org/sagebionetworks/lib/dbuserhelper/DBUserHelperTest.java
@@ -0,0 +1,36 @@
+package org.sagebionetworks.lib.dbuserhelper;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.sagebionetworks.StackConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DBUserHelperTest {
+
+	@Mock
+	StackConfiguration mockConfig;
+	@Mock
+	JdbcTemplate mockTemplate;
+
+	@Test
+	void createReadOnlyUser() {
+		when(mockConfig.getDbReadOnlyUserName()).thenReturn("userName");
+		when(mockConfig.getDbReadOnlyPassword()).thenReturn("userPassword");
+		when(mockTemplate.update(anyString())).thenReturn(1, 1);
+		DBUserHelper dbUserHelper = new DBUserHelper(mockConfig);
+		dbUserHelper.createDbReadOnlyUser(mockTemplate);
+		verify(mockTemplate).update("CREATE USER IF NOT EXISTS 'userName'@'%' IDENTIFIED BY 'userPassword'");
+		verify(mockTemplate).update("GRANT SELECT ON *.* TO 'userName'@'%'");
+	}
+}
