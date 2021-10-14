7e4821fb6c55169cf9b148727a8ac7f8c2f0a4d5
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2021-10-14 12:40:08 -0400

    Prefer code over config

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/table/TableTransactionManagerProviderImpl.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/table/TableTransactionManagerProviderImpl.java
@@ -4,28 +4,22 @@ import java.util.Map;
 
 import org.sagebionetworks.repo.model.EntityType;
 import org.sagebionetworks.util.ValidateArgument;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Simple mapping of entity types to transaction managers.
  *
  */
 public class TableTransactionManagerProviderImpl implements TableTransactionManagerProvider {
-	
-	/**
-	 * Injected with spring.
-	 */
+
+	@Autowired
 	Map<EntityType, TableTransactionManager> managerMap;
-	
-	
-	public void setManagerMap(Map<EntityType, TableTransactionManager> managerMap) {
-		this.managerMap = managerMap;
-	}
 
 	@Override
 	public TableTransactionManager getTransactionManagerForType(EntityType type) {
 		ValidateArgument.required(type, "type");
 		TableTransactionManager manager = managerMap.get(type);
-		if(manager == null){
+		if (manager == null){
 			throw new IllegalArgumentException("Unknown type: "+type);
 		}
 		return manager;
