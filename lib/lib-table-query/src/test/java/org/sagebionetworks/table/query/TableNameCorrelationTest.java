fccc525022788af3e6f7bfe2c9b8b0d682772e5e
john-hill <john.hill@sagebase.org>
2021-11-18 10:54:38 -0800

    fixed test

--- lib/lib-table-query/src/test/java/org/sagebionetworks/table/query/TableNameCorrelationTest.java
+++ lib/lib-table-query/src/test/java/org/sagebionetworks/table/query/TableNameCorrelationTest.java
@@ -52,7 +52,7 @@ public class TableNameCorrelationTest {
 	@Test
 	public void testGetTableAliasWithoutAs() throws ParseException {
 		TableNameCorrelation tableName = new TableQueryParser("T123 foo").tableNameCorrelation();
-		assertEquals("f", tableName.getTableAlias().orElse(null));
+		assertEquals("foo", tableName.getTableAlias().orElse(null));
 	}
 	
 	@Test
