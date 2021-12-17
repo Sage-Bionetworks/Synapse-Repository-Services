acff6bbe6856332009db08a335bd655c346cb4a5
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2021-12-16 17:03:56 -0800

    PLFM-7029: DiscussionSearchIndexManager implementation

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/discussion/DiscussionSearchIndexManager.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/discussion/DiscussionSearchIndexManager.java
@@ -3,7 +3,7 @@ package org.sagebionetworks.repo.manager.discussion;
 import org.sagebionetworks.repo.model.UserInfo;
 import org.sagebionetworks.repo.model.discussion.DiscussionSearchRequest;
 import org.sagebionetworks.repo.model.discussion.DiscussionSearchResponse;
-import org.sagebionetworks.repo.model.message.ChangeType;
+import org.sagebionetworks.workers.util.aws.message.RecoverableMessageException;
 
 /**
  * Manager for operations related to the search index for forums
@@ -13,11 +13,13 @@ public interface DiscussionSearchIndexManager {
 	/* API methods */
 	
 	/**
+	 * Performs a full text search in the forum with the given id using the query and page token from the given request. The user needs to have read access to the forum. 
+	 * Does not return threads or replies that were marked as deleted.
 	 * 
-	 * @param userInfo
-	 * @param forumId
-	 * @param request
-	 * @return
+	 * @param userInfo The user performing the request
+	 * @param forumId The id of the forum to search in
+	 * @param request The search request
+	 * @return The response will contain a page or results matching the query in the request
 	 */
 	DiscussionSearchResponse search(UserInfo userInfo, Long forumId, DiscussionSearchRequest request);
 	
@@ -32,9 +34,23 @@ public interface DiscussionSearchIndexManager {
 	// all the records for that thread will be marked as deleted.
 	
 	// A thread can also be restored, this would send an update message. In this case we need to make sure all the records matching the thread as "restored" (e.g. marked as not deleted).
-		
-	void processThreadChange(Long threadId, ChangeType changeType);
 	
-	void processReplyChange(Long replyId, ChangeType changeType);
+	/**
+	 * When a thread change message is sent and consumed by a worker this method will update the search index accordingly.
+	 * In particular if a thread has been marked as deleted all the records for that thread will be marked as deleted so they won't appear in the search results.
+	 * If a thread is instead available the thread will be added or updated in the index and all the records associated with that thread will be marked as not deleted. 
+	 *  
+	 * @param threadId The id of the thread that was changed (created or updated)
+	 */
+	void processThreadChange(Long threadId) throws RecoverableMessageException;
+	
+	/**
+	 * When a reply change message is send and consumed by a worker this method will updated the search index accordingly.
+	 * In particular if a reply has been marked as deleted the record for the reply will be marked as deleted so it won't appear in the search results.
+	 * If a reply is instead available the reply will be added or updated in the index and marked as not deleted.
+	 *  
+	 * @param replyId The id of the reply that was changed (created or updated)
+	 */
+	void processReplyChange(Long replyId) throws RecoverableMessageException;
 	
 }
---
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/discussion/DiscussionSearchIndexManagerImpl.java
@@ -0,0 +1,196 @@
+package org.sagebionetworks.repo.manager.discussion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpStatus;
+import org.sagebionetworks.repo.manager.AuthorizationManager;
+import org.sagebionetworks.repo.model.ACCESS_TYPE;
+import org.sagebionetworks.repo.model.NextPageToken;
+import org.sagebionetworks.repo.model.ObjectType;
+import org.sagebionetworks.repo.model.UploadContentToS3DAO;
+import org.sagebionetworks.repo.model.UserInfo;
+import org.sagebionetworks.repo.model.dbo.dao.discussion.DiscussionReplyDAO;
+import org.sagebionetworks.repo.model.dbo.dao.discussion.DiscussionSearchIndexDao;
+import org.sagebionetworks.repo.model.dbo.dao.discussion.DiscussionThreadDAO;
+import org.sagebionetworks.repo.model.discussion.DiscussionFilter;
+import org.sagebionetworks.repo.model.discussion.DiscussionReplyBundle;
+import org.sagebionetworks.repo.model.discussion.DiscussionSearchRequest;
+import org.sagebionetworks.repo.model.discussion.DiscussionSearchResponse;
+import org.sagebionetworks.repo.model.discussion.DiscussionThreadBundle;
+import org.sagebionetworks.repo.model.discussion.Match;
+import org.sagebionetworks.repo.transactions.WriteTransaction;
+import org.sagebionetworks.repo.web.NotFoundException;
+import org.sagebionetworks.util.ValidateArgument;
+import org.sagebionetworks.workers.util.aws.message.RecoverableMessageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.AmazonServiceException.ErrorType;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+
+@Service
+public class DiscussionSearchIndexManagerImpl implements DiscussionSearchIndexManager {
+
+	private static final int MIN_SEARCH_STRING_LENGTH = 3;
+	
+	private static boolean isThreadDeleted(DiscussionThreadBundle thread) {
+		return thread == null || Boolean.TRUE.equals(thread.getIsDeleted());
+	}
+	
+	private static boolean isReplyDeleted(DiscussionReplyBundle reply) {
+		return reply == null || Boolean.TRUE.equals(reply.getIsDeleted());
+	}
+	
+	static String buildSearchContent(String ...contentArray) {
+		StringBuilder contentBuilder = new StringBuilder();
+		
+		for (String content : contentArray) {
+			contentBuilder
+				.append(StringUtils.trimToEmpty(content))
+				.append(" ");
+		}
+		
+		return StringUtils.trimToEmpty(contentBuilder.toString());		
+	}
+
+	private DiscussionSearchIndexDao searchIndexDao;
+	
+	private DiscussionThreadDAO threadDao;
+	
+	private DiscussionReplyDAO replyDao;
+	
+	private UploadContentToS3DAO contentDao;
+
+	private AuthorizationManager authManager;
+	
+	@Autowired
+	public DiscussionSearchIndexManagerImpl(DiscussionSearchIndexDao searchIndexDao, DiscussionThreadDAO threadDao, DiscussionReplyDAO replyDao, UploadContentToS3DAO contentDao, AuthorizationManager authManager) {
+		this.searchIndexDao = searchIndexDao;
+		this.threadDao = threadDao;
+		this.replyDao = replyDao;
+		this.contentDao = contentDao;
+		this.authManager = authManager;
+	}
+
+	@Override
+	public DiscussionSearchResponse search(UserInfo userInfo, Long forumId, DiscussionSearchRequest request) {
+		ValidateArgument.required(userInfo, "The user");
+		ValidateArgument.required(forumId, "The forumId");
+		ValidateArgument.required(request, "The search request");
+		ValidateArgument.requiredNotBlank(request.getSearchString(), "The request.searchString");
+		
+		String searchString = buildSearchContent(request.getSearchString());
+		
+		ValidateArgument.requirement(searchString.length() >= MIN_SEARCH_STRING_LENGTH, "The search string should be at least " + MIN_SEARCH_STRING_LENGTH + " characters.");
+		
+		authManager.canAccess(userInfo, forumId.toString(), ObjectType.ENTITY, ACCESS_TYPE.READ).checkAuthorizationOrElseThrow();
+		
+		NextPageToken pageToken = new NextPageToken(request.getNextPageToken());
+		
+		// Since the getNextPageTokenForCurrentResults might potentially alters the result list (removing the last element) we make sure that the list is mutable
+		List<Match> matches = new ArrayList<>(searchIndexDao.search(forumId, searchString, pageToken.getLimitForQuery(), pageToken.getOffset()));
+		
+		String nextPageToken = pageToken.getNextPageTokenForCurrentResults(matches);
+		
+		return new DiscussionSearchResponse().setMatches(matches).setNextPageToken(nextPageToken);
+	}
+
+	@Override
+	@WriteTransaction
+	public void processThreadChange(Long threadId) {
+		ValidateArgument.required(threadId, "The threadId");
+		
+		DiscussionThreadBundle thread = null;
+		
+		try {
+			thread = threadDao.getThread(threadId, DiscussionFilter.NO_FILTER);
+		} catch (NotFoundException ex) {
+			// If a thread does not exists anymore, we can mark it as deleted.
+			// Note that this should not happen in general, unless the forum was deleted by cascade from the owning entity
+		}
+		
+		if (isThreadDeleted(thread)) {
+			// We mark all the records matching that thread as deleted so they won't appear in the search results
+			searchIndexDao.markThreadAsDeleted(threadId);
+		} else {
+			String title = thread.getTitle();
+			String content = fetchContent(thread.getMessageKey());
+			
+			String searchContent = buildSearchContent(title, content);
+			
+			Long forumId = Long.valueOf(thread.getForumId());
+			
+			searchIndexDao.createOrUpdateRecordForThread(forumId, threadId, searchContent);
+			// Makes sure that the thread is searchable (e.g. if this is an update due to a restore)
+			searchIndexDao.markThreadAsNotDeleted(threadId);
+		}
+ 		
+	}
+
+	@Override
+	@WriteTransaction
+	public void processReplyChange(Long replyId) {
+		ValidateArgument.required(replyId, "The replyId");
+		
+		Long threadId = null;
+		DiscussionReplyBundle reply = null;
+		DiscussionThreadBundle thread = null;
+		
+		try {
+			reply = replyDao.getReply(replyId, DiscussionFilter.NO_FILTER);
+			threadId = Long.valueOf(reply.getThreadId());
+			thread = threadDao.getThread(threadId, DiscussionFilter.NO_FILTER);
+		} catch (NotFoundException ex) {
+			// If a reply does not exists anymore, we can mark it as deleted.
+			// Note that this should not happen in general, unless the forum was deleted by cascade from the owning entity
+		}
+		
+		if (isReplyDeleted(reply)) {
+			searchIndexDao.markReplyAsDeleted(replyId);
+		} else {
+			String content = fetchContent(reply.getMessageKey());
+			
+			String searchContent = buildSearchContent(content);
+			
+			Long forumId = Long.valueOf(reply.getForumId());
+			
+			searchIndexDao.createOrUpdateRecordForReply(forumId, threadId, replyId, searchContent);
+			// Makes sure that the reply is searchable 
+			searchIndexDao.markReplyAsNotDeleted(replyId);
+		}
+		
+		if (threadId != null) {
+			// If we have a threadId (e.g. the reply was found) we also need to make sure that the thread deleted flag is synced 
+			// because the messages of thread and replies are independent and might come out of order.
+			// For example if a message for a thread that was deleted comes before the message for a reply in the thread, the record for 
+			// the thread would be set as deleted but the record for the message does not exist yet, so a new one with a 
+			// default threadDeleted=false would be created
+			if (isThreadDeleted(thread)) {
+				searchIndexDao.markThreadAsDeleted(threadId);
+			} else {
+				searchIndexDao.markThreadAsNotDeleted(threadId);
+			}
+		}
+		
+	}
+		
+	private String fetchContent(String messageKey) {
+		try {
+			return contentDao.getMessage(messageKey);
+		} catch (AmazonServiceException ex) {
+			// The key does not exists anymore, the search content is empty
+			if (ex instanceof AmazonS3Exception && ex.getStatusCode() == HttpStatus.SC_NOT_FOUND) {
+				return "";
+			}
+			// Service error can be retried
+			if (ErrorType.Service == ex.getErrorType()) {
+				throw new RecoverableMessageException(ex);
+			}
+			throw ex;
+		}
+	}
+
+}
