23ee2d36ea00814bdce92b575af9efd8a1902a67
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2022-01-14 14:10:43 -0800

    Refactor DoiWorker to use AsyncJobRunner

--- services/workers/src/test/java/org/sagebionetworks/doi/worker/DoiWorkerTest.java
+++ services/workers/src/test/java/org/sagebionetworks/doi/worker/DoiWorkerTest.java
@@ -1,102 +1,63 @@
 package org.sagebionetworks.doi.worker;
 
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.sagebionetworks.repo.manager.UserManager;
-import org.sagebionetworks.repo.manager.asynch.AsynchJobStatusManager;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.sagebionetworks.common.util.progress.ProgressCallback;
 import org.sagebionetworks.repo.manager.doi.DoiManager;
-import org.sagebionetworks.repo.model.AuthorizationConstants.BOOTSTRAP_PRINCIPAL;
 import org.sagebionetworks.repo.model.UserInfo;
-import org.sagebionetworks.repo.model.asynch.AsynchronousJobStatus;
 import org.sagebionetworks.repo.model.doi.v2.Doi;
 import org.sagebionetworks.repo.model.doi.v2.DoiRequest;
 import org.sagebionetworks.repo.model.doi.v2.DoiResponse;
-import org.sagebionetworks.workers.util.aws.message.RecoverableMessageException;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.sagebionetworks.worker.AsyncJobProgressCallback;
 
-import com.amazonaws.services.sqs.model.Message;
-
-
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class DoiWorkerTest {
-	
-	public static final long MAX_WAIT_MS = 1000 * 60;
+
+	@Mock
+	private DoiManager mockDoiManager;
 
 	@InjectMocks
 	private DoiWorker doiWorker;
+
 	@Mock
-	private UserManager mockUserManager;
+	private ProgressCallback mockCallback;
 	@Mock
-	private DoiManager mockDoiManager;
+	private AsyncJobProgressCallback mockJobCallback;
 	@Mock
-	private AsynchJobStatusManager mockAsyncMgr;
+	private DoiRequest mockRequest;
 	@Mock
-	private AsynchronousJobStatus mockStatus;
+	private Doi mockRequestDoi;
+	@Mock
+	private Doi mockResponseDoi;
+	@Mock
+	private UserInfo mockUser;
+	private String jobId;
 
-
-	DoiRequest request;
-	Doi requestDoi;
-	Doi responseDoi;
-	UserInfo adminUser;
-	Long adminUserId = BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId();
-	String jobId;
-
-	private static final Message message = new Message();
-
-	@Before
-	public void before(){
-		request = new DoiRequest();
-		requestDoi = new Doi();
-		request.setDoi(requestDoi);
-		adminUser = new UserInfo(true);
-		adminUser.setId(adminUserId);
-		message.setMessageId("messageID");
-		jobId="jobId";
-
-		when(mockAsyncMgr.lookupJobStatus(message.getBody())).thenReturn(mockStatus);
-		when(mockStatus.getRequestBody()).thenReturn(request);
-		when(mockStatus.getStartedByUserId()).thenReturn(adminUserId);
-		when(mockStatus.getJobId()).thenReturn(jobId);
-		when(mockUserManager.getUserInfo(BOOTSTRAP_PRINCIPAL.THE_ADMIN_USER.getPrincipalId())).thenReturn(adminUser);
+	@BeforeEach
+	public void before() {
+		jobId = "jobId";
 	}
 
 	@Test
 	public void testSuccess() throws Exception {
-		when(mockDoiManager.createOrUpdateDoi(adminUser, request.getDoi())).thenReturn(responseDoi);
-		doiWorker.run(null, message);
-		// We can't use Mockito to verify that the correct job status was called because we cannot access the DoiResponse.
-		verify(mockAsyncMgr).setComplete(eq(jobId), any(DoiResponse.class));
-	}
+		when(mockRequest.getDoi()).thenReturn(mockRequestDoi);
+		when(mockDoiManager.createOrUpdateDoi(any(), any())).thenReturn(mockResponseDoi);
 
+		DoiResponse expected = new DoiResponse().setDoi(mockResponseDoi);
 
-	@Test
-	public void testRecoverable() throws Exception {
-		when(mockDoiManager.createOrUpdateDoi(adminUser, request.getDoi())).thenThrow(new RecoverableMessageException());
-		try { // Call under test
-			doiWorker.run(null, message);
-			fail();
-		} catch(RecoverableMessageException e) {
-			// As expected.
-		}
-		verify(mockAsyncMgr, never()).setJobFailed(any(), any());
-		verify(mockAsyncMgr, never()).setComplete(any(), any());
-	}
+		DoiResponse result = doiWorker.run(mockCallback, jobId, mockUser, mockRequest, mockJobCallback);
 
-	@Test
-	public void testFailure() throws Exception {
-		when(mockDoiManager.createOrUpdateDoi(adminUser, request.getDoi())).thenThrow(new IllegalArgumentException());
-		doiWorker.run(null, message);
-		verify(mockAsyncMgr).setJobFailed(eq(jobId), any(IllegalArgumentException.class));
+		assertEquals(expected, result);
+
+		verify(mockDoiManager).createOrUpdateDoi(mockUser, mockRequestDoi);
 	}
 }
