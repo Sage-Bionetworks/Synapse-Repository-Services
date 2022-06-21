829371105895e81551e39e477ab270048e3ef4e8
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2022-06-21 13:45:58 -0700

    PLFM-7092: Replication index provider for dataset collections

---
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/table/metadata/providers/DatasetCollectionMetadataIndexProvider.java
@@ -0,0 +1,114 @@
+package org.sagebionetworks.repo.manager.table.metadata.providers;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.sagebionetworks.repo.manager.NodeManager;
+import org.sagebionetworks.repo.manager.table.metadata.DefaultColumnModel;
+import org.sagebionetworks.repo.manager.table.metadata.MetadataIndexProvider;
+import org.sagebionetworks.repo.model.EntityRef;
+import org.sagebionetworks.repo.model.NodeDAO;
+import org.sagebionetworks.repo.model.UserInfo;
+import org.sagebionetworks.repo.model.annotation.v2.Annotations;
+import org.sagebionetworks.repo.model.jdo.KeyFactory;
+import org.sagebionetworks.repo.model.table.ColumnModel;
+import org.sagebionetworks.repo.model.table.ColumnType;
+import org.sagebionetworks.repo.model.table.ReplicationType;
+import org.sagebionetworks.repo.model.table.SubType;
+import org.sagebionetworks.repo.model.table.TableConstants;
+import org.sagebionetworks.repo.model.table.ViewObjectType;
+import org.sagebionetworks.table.cluster.view.filter.FlatIdAndVersionFilter;
+import org.sagebionetworks.table.cluster.view.filter.FlatIdsFilter;
+import org.sagebionetworks.table.cluster.view.filter.IdVersionPair;
+import org.sagebionetworks.table.cluster.view.filter.ViewFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Sets;
+
+@Service
+public class DatasetCollectionMetadataIndexProvider implements MetadataIndexProvider {
+
+	private final NodeDAO nodeDao;
+	private final NodeManager nodeManager;
+
+	static final DefaultColumnModel DATASET_COLLECTION_COLUMNS = DefaultColumnModel.builder(ViewObjectType.DATASET_COLLECTION)
+			.withObjectField(Constants.BASIC_DEAFULT_COLUMNS).build();
+
+	@Autowired
+	public DatasetCollectionMetadataIndexProvider(NodeDAO nodeDao, NodeManager nodeManager) {
+		super();
+		this.nodeDao = nodeDao;
+		this.nodeManager = nodeManager;
+	}
+
+	@Override
+	public ViewObjectType getObjectType() {
+		return ViewObjectType.DATASET_COLLECTION;
+	}
+
+	@Override
+	public ColumnType getIdColumnType() {
+		return ColumnType.ENTITYID;
+	}
+
+	@Override
+	public ColumnType getParentIdColumnType() {
+		return ColumnType.ENTITYID;
+	}
+
+	@Override
+	public ColumnType getBenefactorIdColumnType() {
+		return ColumnType.ENTITYID;
+	}
+
+	@Override
+	public DefaultColumnModel getDefaultColumnModel(Long viewTypeMask) {
+		return DATASET_COLLECTION_COLUMNS;
+	}
+
+	@Override
+	public Optional<Annotations> getAnnotations(UserInfo userInfo, String objectId) {
+		return Optional.ofNullable(nodeManager.getUserAnnotations(userInfo, objectId));
+	}
+
+	@Override
+	public void updateAnnotations(UserInfo userInfo, String objectId, Annotations annotations) {
+		nodeManager.updateUserAnnotations(userInfo, objectId, annotations);
+	}
+
+	@Override
+	public boolean canUpdateAnnotation(ColumnModel model) {
+		return true;
+	}
+
+	@Override
+	public ViewFilter getViewFilter(Long viewId) {
+		List<EntityRef> items = nodeDao.getNodeItems(viewId);
+		Set<IdVersionPair> scope = items.stream().map(i -> new IdVersionPair()
+				.setId(KeyFactory.stringToKey(i.getEntityId())).setVersion(i.getVersionNumber()))
+				.collect(Collectors.toSet());
+		return new FlatIdAndVersionFilter(ReplicationType.ENTITY, getSubTypes(), scope);
+	}
+
+	@Override
+	public ViewFilter getViewFilter(Long viewTypeMask, Set<Long> containerIds) {
+		return new FlatIdsFilter(ReplicationType.ENTITY, getSubTypes(), containerIds);
+	}
+
+	Set<SubType> getSubTypes() {
+		// currently only datasets are supported.
+		return Sets.newHashSet(SubType.dataset);
+	}
+
+	@Override
+	public void validateScopeAndType(Long typeMask, Set<Long> scopeIds, int maxContainersPerView) {
+		if (scopeIds != null && scopeIds.size() > maxContainersPerView) {
+			throw new IllegalArgumentException(
+					String.format(TableConstants.MAXIMUM_OF_ITEMS_IN_A_DATASET_COLLECTION_EXCEEDED, maxContainersPerView));
+		}
+	}
+
+}
---
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/table/metadata/providers/DatasetCollectionMetadataIndexProviderTest.java
@@ -0,0 +1,128 @@
+package org.sagebionetworks.repo.manager.table.metadata.providers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.sagebionetworks.repo.manager.NodeManager;
+import org.sagebionetworks.repo.manager.table.metadata.DefaultColumnModel;
+import org.sagebionetworks.repo.model.NodeDAO;
+import org.sagebionetworks.repo.model.jdo.KeyFactory;
+import org.sagebionetworks.repo.model.table.ColumnType;
+import org.sagebionetworks.repo.model.EntityRef;
+import org.sagebionetworks.repo.model.table.ReplicationType;
+import org.sagebionetworks.repo.model.table.SubType;
+import org.sagebionetworks.repo.model.table.ViewObjectType;
+import org.sagebionetworks.repo.model.table.ViewScopeType;
+import org.sagebionetworks.table.cluster.view.filter.FlatIdAndVersionFilter;
+import org.sagebionetworks.table.cluster.view.filter.FlatIdsFilter;
+import org.sagebionetworks.table.cluster.view.filter.IdVersionPair;
+import org.sagebionetworks.table.cluster.view.filter.ViewFilter;
+
+import com.google.common.collect.Sets;
+
+@ExtendWith(MockitoExtension.class)
+public class DatasetCollectionMetadataIndexProviderTest {
+
+	@Mock
+	private NodeDAO mockNodeDao;
+	@Mock
+	private NodeManager mockNodeManager;
+	@InjectMocks
+	private DatasetCollectionMetadataIndexProvider provider;
+
+	private Long viewId;
+	private List<EntityRef> items;
+	private Set<IdVersionPair> scope;
+	private Set<SubType> subTypes;
+	private ViewScopeType viewScopeType;
+	private Set<Long> containerIds;
+
+	@BeforeEach
+	public void before() {
+		viewId = 123L;
+		items = Arrays.asList(new EntityRef().setEntityId("syn11").setVersionNumber(2L),
+				new EntityRef().setEntityId("syn22").setVersionNumber(3L));
+
+		scope = items.stream().map(i -> new IdVersionPair().setId(KeyFactory.stringToKey(i.getEntityId()))
+				.setVersion(i.getVersionNumber())).collect(Collectors.toSet());
+		subTypes = Sets.newHashSet(SubType.dataset);
+
+		viewScopeType = new ViewScopeType(ViewObjectType.DATASET_COLLECTION, 0L);
+		containerIds = Sets.newHashSet(1L, 2L, 3L);
+	}
+
+	@Test
+	public void testGetObjectType() {
+		assertEquals(ViewObjectType.DATASET_COLLECTION, provider.getObjectType());
+	}
+
+	@Test
+	public void testGetColumnTypes() {
+		assertEquals(ColumnType.ENTITYID, provider.getIdColumnType());
+		assertEquals(ColumnType.ENTITYID, provider.getParentIdColumnType());
+		assertEquals(ColumnType.ENTITYID, provider.getBenefactorIdColumnType());
+	}
+
+	@Test
+	public void testGetViewFilter() {
+		when(mockNodeDao.getNodeItems(viewId)).thenReturn(items);
+		// call under test
+		ViewFilter filter = provider.getViewFilter(viewId);
+		ViewFilter expected = new FlatIdAndVersionFilter(ReplicationType.ENTITY, subTypes, scope);
+		assertEquals(filter, expected);
+	}
+
+	@Test
+	public void testGetViewFilterWithTypeAndScope() {
+		// call under test
+		ViewFilter filter = provider.getViewFilter(viewScopeType.getTypeMask(), containerIds);
+		ViewFilter expected = new FlatIdsFilter(ReplicationType.ENTITY, subTypes, containerIds);
+		assertEquals(filter, expected);
+	}
+
+	@Test
+	public void testValidateScopeAndTypeWithUnderLimit() {
+		Long typeMask = 0L;
+		Set<Long> scopeIds = Sets.newHashSet(1L, 2L);
+		int maxContainersPerView = 3;
+		// call under test
+		provider.validateScopeAndType(typeMask, scopeIds, maxContainersPerView);
+	}
+	
+	@Test
+	public void testValidateScopeAndTypeWithOverLimit() {
+		Long typeMask = 0L;
+		Set<Long> scopeIds = Sets.newHashSet(1L, 2L);
+		int maxContainersPerView = 1;
+		String message = assertThrows(IllegalArgumentException.class, ()->{
+			// call under test
+			provider.validateScopeAndType(typeMask, scopeIds, maxContainersPerView);
+		}).getMessage();
+		assertEquals("Maximum of 1 items in a dataset collection exceeded.", message);
+	}
+	
+	/**
+	 * Test for PLFM
+	 */
+	@Test
+	public void testGetDefaultColumnModel() {
+		Long viewTypeMask = null;
+		DefaultColumnModel expected = DefaultColumnModel.builder(ViewObjectType.DATASET_COLLECTION)
+		.withObjectField(Constants.BASIC_DEAFULT_COLUMNS).build();
+		// call under test
+		DefaultColumnModel model = provider.getDefaultColumnModel(viewTypeMask);
+		assertEquals(expected, model);
+	}
+}
