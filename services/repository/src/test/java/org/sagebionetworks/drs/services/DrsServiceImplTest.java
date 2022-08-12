90a06e6a53cf50c47cd519a121ff02647952193b
SandhraSokhal <sandhra.sokhal@sagebase.org>
2022-08-12 10:03:06 -0700

    additional test for Drs exception

--- services/repository/src/test/java/org/sagebionetworks/drs/services/DrsServiceImplTest.java
+++ services/repository/src/test/java/org/sagebionetworks/drs/services/DrsServiceImplTest.java
@@ -6,12 +6,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.sagebionetworks.repo.manager.drs.DrsManager;
-import org.sagebionetworks.repo.model.Project;
+import org.sagebionetworks.repo.model.UnauthorizedException;
+import org.sagebionetworks.repo.web.NotFoundException;
 import org.sagebionetworks.repo.web.rest.doc.DrsException;
-
-import java.time.LocalDate;
-import java.time.ZoneOffset;
-import java.util.Date;
+import org.springframework.http.HttpStatus;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -27,24 +25,44 @@ public class DrsServiceImplTest {
     DrsManager drsManager;
 
     private static final Long USER_ID = 1L;
+    private static final String PROJECT_ID = "syn123456";
 
     @Test
     public void testGETDrsObjectWithInvalidIDThrowsDrsException() {
-        final Project project = getProject("syn1.1","project");
         when(drsManager.getDrsObject(any(),any())).thenThrow(new IllegalArgumentException(ILLEGAL_ARGUMENT_ERROR_MESSAGE));
         final DrsException exception = assertThrows(DrsException.class, () -> {
-            drsService.getDrsObject(USER_ID, project.getId());
+            drsService.getDrsObject(USER_ID, PROJECT_ID);
         });
         assertEquals(ILLEGAL_ARGUMENT_ERROR_MESSAGE, exception.getMessage());
+        assertEquals(exception.getStatusCode(), HttpStatus.BAD_REQUEST.value());
     }
 
-    private Project getProject(final String id, final String name){
-        final Project project = new Project();
-        project.setId(id);
-        project.setName(name);
-        project.setDescription("test");
-        project.setCreatedOn(Date.from(LocalDate.of(2022, 8, 10).atStartOfDay(ZoneOffset.UTC).toInstant()));
-        project.setModifiedOn(Date.from(LocalDate.of(2022, 8, 10).atStartOfDay(ZoneOffset.UTC).toInstant()));
-        return project;
+    @Test
+    public void testGETDrsObjectThrowsNullPointerException() {
+        final String errorMessage = "Null pointer exception";
+        when(drsManager.getDrsObject(any(),any())).thenThrow(new NullPointerException(errorMessage));
+        final DrsException exception=assertThrows(DrsException.class, () -> drsService.getDrsObject(USER_ID, PROJECT_ID));
+        assertEquals(exception.getMsg(),errorMessage);
+        assertEquals(exception.getStatusCode(), HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
+
+    @Test
+    public void testGETDrsObjectThrowsNotFoundException() {
+        final  NotFoundException notFoundException = new NotFoundException(String.format("Drs object id %s does not exists", PROJECT_ID));
+        when(drsManager.getDrsObject(any(),any())).thenThrow(notFoundException);
+        final DrsException exception = assertThrows(DrsException.class, () -> {
+            drsService.getDrsObject(USER_ID, PROJECT_ID);
+        });
+
+        assertEquals(exception.getMsg(),notFoundException.getMessage());
+        assertEquals(exception.getStatusCode(), HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    public void testGETDrsObjectThrowsUnauthorizedException() {
+        when(drsManager.getDrsObject(any(),any())).thenThrow(new UnauthorizedException());
+        final DrsException exception=assertThrows(DrsException.class, () -> drsService.getDrsObject(USER_ID, PROJECT_ID));
+        assertEquals(exception.getStatusCode(), HttpStatus.UNAUTHORIZED.value());
+    }
+
 }
