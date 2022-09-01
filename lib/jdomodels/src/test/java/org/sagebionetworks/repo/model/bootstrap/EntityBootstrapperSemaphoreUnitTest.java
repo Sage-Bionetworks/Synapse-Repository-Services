c550878e290fa52ff86d68f635ac9030f7d06bf8
john-hill <john.hill@sagebase.org>
2022-09-01 10:13:39 -0700

    build fixes

--- lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/bootstrap/EntityBootstrapperSemaphoreUnitTest.java
+++ lib/jdomodels/src/test/java/org/sagebionetworks/repo/model/bootstrap/EntityBootstrapperSemaphoreUnitTest.java
@@ -1,5 +1,6 @@
 package org.sagebionetworks.repo.model.bootstrap;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -8,17 +9,16 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.sagebionetworks.database.semaphore.CountingSemaphore;
 import org.sagebionetworks.repo.model.AccessControlList;
 import org.sagebionetworks.repo.model.AccessControlListDAO;
-import org.sagebionetworks.repo.model.auth.AuthenticationDAO;
+import org.sagebionetworks.repo.model.AccessRequirementDAO;
 import org.sagebionetworks.repo.model.AuthorizationConstants.ACL_SCHEME;
 import org.sagebionetworks.repo.model.EntityType;
 import org.sagebionetworks.repo.model.GroupMembersDAO;
@@ -27,9 +27,11 @@ import org.sagebionetworks.repo.model.NodeDAO;
 import org.sagebionetworks.repo.model.ObjectType;
 import org.sagebionetworks.repo.model.UserGroupDAO;
 import org.sagebionetworks.repo.model.UserProfileDAO;
+import org.sagebionetworks.repo.model.auth.AuthenticationDAO;
 import org.springframework.test.util.ReflectionTestUtils;
 
-@RunWith(MockitoJUnitRunner.class)
+
+@ExtendWith(MockitoExtension.class)
 public class EntityBootstrapperSemaphoreUnitTest {
 	
 	@Mock
@@ -46,6 +48,8 @@ public class EntityBootstrapperSemaphoreUnitTest {
 	private AccessControlListDAO mockAclDao;
 	@Mock
 	private CountingSemaphore mockSemaphoreDao;
+	@Mock
+	private AccessRequirementDAO mockAccessRequirementDao;
 	
 	private List<EntityBootstrapData> bootstrapData;
 	
@@ -54,7 +58,7 @@ public class EntityBootstrapperSemaphoreUnitTest {
 	
 	Node node;
 
-	@Before
+	@BeforeEach
 	public void before() throws Exception {
 		// Setup EntityBootstrapData
 		bootstrapData = new ArrayList<EntityBootstrapData>();
@@ -72,9 +76,6 @@ public class EntityBootstrapperSemaphoreUnitTest {
 		node.setId("syn123");
 	}
 
-	@After
-	public void after() throws Exception {
-	}
 
 	@Test
 	public void testBootsrapSemaphore() throws Exception {
@@ -87,27 +88,26 @@ public class EntityBootstrapperSemaphoreUnitTest {
 		verify(mockUserProfileDao).bootstrapProfiles();
 		verify(mockGroupMembersDao).bootstrapGroups();
 		verify(mockAuthenticationDao).bootstrapCredentials();
+		verify(mockAccessRequirementDao).bootstrap();
 		verify(mockNodeDao).bootstrapNode(any(Node.class), any(Long.class));
 		verify(mockAclDao).create(any(AccessControlList.class), any(ObjectType.class));
 		verify(mockSemaphoreDao).releaseLock("ENTITYBOOTSTRAPPERLOCK", "token");
 	}
 	
-	@Test (expected=IllegalArgumentException.class)
+	@Test
 	public void testBootstrapSemaphoreException() throws Exception {
 		when(mockSemaphoreDao.attemptToAcquireLock("ENTITYBOOTSTRAPPERLOCK", 30L, 1)).thenReturn(null, null, null, "token");
 		when(mockNodeDao.getNodeIdForPath(bootstrapData.get(0).getEntityPath())).thenReturn(null, bootstrapData.get(0).getEntityId().toString()); // Should force node creation
-		when(mockNodeDao.bootstrapNode(any(Node.class), any(Long.class))).thenReturn(node);
 		when(mockNodeDao.bootstrapNode(any(Node.class), any(Long.class))).thenThrow(new IllegalArgumentException());
-		try {
+		assertThrows(IllegalArgumentException.class, ()->{
 			bootstrapper.bootstrapAll();
-		} finally {
-			verify(mockSemaphoreDao, times(4)).attemptToAcquireLock("ENTITYBOOTSTRAPPERLOCK", 30L, 1);
-			verify(mockUserGroupDao).bootstrapUsers();
-			verify(mockUserProfileDao).bootstrapProfiles();
-			verify(mockGroupMembersDao).bootstrapGroups();
-			verify(mockAuthenticationDao).bootstrapCredentials();
-			verify(mockSemaphoreDao).releaseLock("ENTITYBOOTSTRAPPERLOCK", "token");
-		}
+		});
+		verify(mockSemaphoreDao, times(4)).attemptToAcquireLock("ENTITYBOOTSTRAPPERLOCK", 30L, 1);
+		verify(mockUserGroupDao).bootstrapUsers();
+		verify(mockUserProfileDao).bootstrapProfiles();
+		verify(mockGroupMembersDao).bootstrapGroups();
+		verify(mockAuthenticationDao).bootstrapCredentials();
+		verify(mockSemaphoreDao).releaseLock("ENTITYBOOTSTRAPPERLOCK", "token");
 	}
 
 }
