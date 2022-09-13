990cd378f25541716d4c0df63a4de44056fbb375
Peter Harvey <52708955+harveyx22@users.noreply.github.com>
2022-09-13 14:22:52 -0700

    PLFM-7235: Updated jdom to jdom2 as fix for CVE-2021-33813

--- lib/stackConfiguration/src/main/java/org/sagebionetworks/SettingsLoader.java
+++ lib/stackConfiguration/src/main/java/org/sagebionetworks/SettingsLoader.java
@@ -9,10 +9,10 @@ import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 /**
  * A helper class to load properties from the users .m2 settings file.
