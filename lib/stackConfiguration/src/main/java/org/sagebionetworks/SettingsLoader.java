61ce983fa42109986a932193fe8da2ae22016fa3
Peter Harvey <52708955+harveyx22@users.noreply.github.com>
2022-09-13 11:01:44 -0700

    PLFM-7235: Fix for CVE-2021-33813

--- lib/stackConfiguration/src/main/java/org/sagebionetworks/SettingsLoader.java
+++ lib/stackConfiguration/src/main/java/org/sagebionetworks/SettingsLoader.java
@@ -38,7 +38,7 @@ public class SettingsLoader {
 		FileInputStream in = new FileInputStream(settingsFile);
 		try{
 			SAXBuilder builder = new SAXBuilder();
-			builder.setExpandEntities(false);
+			//builder.setExpandEntities(false);
 			builder.setValidation(false);
 			builder.setIgnoringElementContentWhitespace(true);
 			Document doc = builder.build(settingsFile);
