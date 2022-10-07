0727092b130db62f3360d69a6e0d48f744adf82a
john-hill <john.hill@sagebase.org>
2022-10-07 10:25:35 -0700

    test fix

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/table/TableUpdateRequestManagerProviderImplTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/table/TableUpdateRequestManagerProviderImplTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.sagebionetworks.repo.model.EntityType;
 import org.sagebionetworks.repo.model.dao.table.TableType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -21,7 +20,7 @@ public class TableUpdateRequestManagerProviderImplTest {
 	public void testAllTypes() {
 		for (TableType type : TableType.values()) {
 			// We do not support updates from a materialized view yet
-			if (EntityType.materializedview.equals(type)) {
+			if (TableType.materializedview.equals(type)) {
 				continue;
 			}
 			assertNotNull(provider.getUpdateRequestManagerForType(type));
