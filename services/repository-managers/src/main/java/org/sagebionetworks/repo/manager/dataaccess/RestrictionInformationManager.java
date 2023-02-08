ec840cbfcee50f731bbe59e42173de254f3ac8ca
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2023-02-08 11:24:43 -0800

    PLFM-7553: Remove unused method

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/dataaccess/RestrictionInformationManager.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/dataaccess/RestrictionInformationManager.java
@@ -1,11 +1,8 @@
 package org.sagebionetworks.repo.manager.dataaccess;
 
-import java.util.List;
-
 import org.sagebionetworks.repo.model.RestrictionInformationRequest;
 import org.sagebionetworks.repo.model.RestrictionInformationResponse;
 import org.sagebionetworks.repo.model.UserInfo;
-import org.sagebionetworks.repo.model.ar.UsersRestrictionStatus;
 
 public interface RestrictionInformationManager {
 	
@@ -16,14 +13,6 @@ public interface RestrictionInformationManager {
 	 * @param request
 	 * @return
 	 */
-	public RestrictionInformationResponse getRestrictionInformation(UserInfo userInfo, RestrictionInformationRequest request);
-
-	/**
-	 * Get the restriction information for a batch of entity ids for the given user.
-	 * @param userInfo
-	 * @param entityIds
-	 * @return
-	 */
-	List<UsersRestrictionStatus> getEntityRestrictionInformation(UserInfo userInfo, List<Long> entityIds);
+	RestrictionInformationResponse getRestrictionInformation(UserInfo userInfo, RestrictionInformationRequest request);
 
 }
