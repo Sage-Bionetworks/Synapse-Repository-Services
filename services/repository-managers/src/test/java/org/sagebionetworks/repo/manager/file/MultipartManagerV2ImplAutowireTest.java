3ac7387044ba8457d6e36112f15a79b810618405
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2023-04-07 15:43:32 -0700

    Revert "Fix failing tests"
    This reverts commit 18468c09451ad6f11f402b0dd455c05738197a31.

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/file/MultipartManagerV2ImplAutowireTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/file/MultipartManagerV2ImplAutowireTest.java
@@ -39,12 +39,16 @@ import org.sagebionetworks.repo.model.AuthorizationConstants.BOOTSTRAP_PRINCIPAL
 import org.sagebionetworks.repo.model.FileEntity;
 import org.sagebionetworks.repo.model.StorageLocationDAO;
 import org.sagebionetworks.repo.model.UserInfo;
+import org.sagebionetworks.repo.model.dbo.file.CompositeMultipartUploadStatus;
 import org.sagebionetworks.repo.model.dbo.file.FileHandleDao;
+import org.sagebionetworks.repo.model.dbo.file.MultipartRequestUtils;
+import org.sagebionetworks.repo.model.dbo.file.MultipartUploadDAO;
 import org.sagebionetworks.repo.model.file.AddPartResponse;
 import org.sagebionetworks.repo.model.file.AddPartState;
 import org.sagebionetworks.repo.model.file.BatchPresignedUploadUrlRequest;
 import org.sagebionetworks.repo.model.file.BatchPresignedUploadUrlResponse;
 import org.sagebionetworks.repo.model.file.CloudProviderFileHandleInterface;
+import org.sagebionetworks.repo.model.file.FileHandle;
 import org.sagebionetworks.repo.model.file.FileHandleAssociateType;
 import org.sagebionetworks.repo.model.file.FileHandleAssociation;
 import org.sagebionetworks.repo.model.file.GoogleCloudFileHandle;
@@ -78,8 +82,8 @@ import com.amazonaws.util.Md5Utils;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = { "classpath:test-context.xml" })
 public class MultipartManagerV2ImplAutowireTest {
-		
-	private static final Pattern ETAG_PATTERN = Pattern.compile("<ETag>\"(.+)\"</ETag>");
+
+	private static final Pattern ETAG_PATTERN = Pattern.compile("<ETag>&quot;(.+)&quot;</ETag>");
 	
 	@Autowired
 	StackConfiguration stackConfiguration;
@@ -832,8 +836,6 @@ public class MultipartManagerV2ImplAutowireTest {
 		if (matcher.find()) {			
 			return matcher.group(1);
 		}
-		
-		System.out.println(response.getContent());
 		
 		throw new IllegalStateException("Could not extract ETag value");
 	}
