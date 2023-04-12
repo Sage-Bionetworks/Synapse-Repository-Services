b67252363a46c7d4f6b4bcd4bf25fe6f8c3d87a2
leonli33 <leonli3three@gmail.com>
2023-04-12 00:24:54 -0700

    Created ControllerModel, added DocletToControllerModelTranslator, and added unit tests for DocletToControllerModelTranslator.

---
+++ lib/lib-openapi/src/main/java/org/sagebionetworks/controller/model/ParameterLocation.java
@@ -0,0 +1,13 @@
+package org.sagebionetworks.controller.model;
+
+/**
+ * Represents all possible locations that a parameter can be.
+ * @author lli
+ *
+ */
+public enum ParameterLocation {
+	path,
+	query,
+	header,
+	cookie
+}
---
+++ lib/lib-openapi/src/main/java/org/sagebionetworks/controller/model/ParameterModel.java
@@ -0,0 +1,87 @@
+package org.sagebionetworks.controller.model;
+
+import java.util.Objects;
+
+import org.sagebionetworks.repo.model.schema.JsonSchema;
+
+/**
+ * Describes a parameter passed into a method in a controller.
+ * @author lli
+ *
+ */
+public class ParameterModel {
+	private String name;
+	private ParameterLocation in;
+	private boolean required;
+	private String description;
+	private JsonSchema schema;
+	
+	public String getName() {
+		return name;
+	}
+
+	public ParameterModel withName(String name) {
+		this.name = name;
+		return this;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public ParameterModel withDescription(String description) {
+		this.description = description;
+		return this;
+	}
+	
+	public ParameterLocation getIn() {
+		return in;
+	}
+	
+	public ParameterModel withIn(ParameterLocation in) {
+		this.in = in;
+		return this;
+	}
+	
+	public boolean isRequired() {
+		return required;
+	}
+	
+	public ParameterModel withRequired(boolean required) {
+		this.required = required;
+		return this;
+	}
+	
+	public JsonSchema getSchema() {
+		return schema;
+	}
+	
+	public ParameterModel withSchema(JsonSchema schema) {
+		this.schema = schema;
+		return this;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, in, name, required, schema);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ParameterModel other = (ParameterModel) obj;
+		return Objects.equals(description, other.description) && in == other.in && Objects.equals(name, other.name)
+				&& required == other.required && Objects.equals(schema, other.schema);
+	}
+
+	@Override
+	public String toString() {
+		return "ParameterModel [name=" + name + ", in=" + in + ", required=" + required + ", schema=" + schema
+				+ ", description=" + description + "]";
+	}
+}
