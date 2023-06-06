a0d0d9fa18ebe16687e88d815ef2a654397160ae
SandhraSokhal <sandhra.sokhal@sagebase.org>
2023-06-06 15:27:41 -0700

    test for acl kinesis stream

--- services/workers/src/test/java/org/sagebionetworks/snapshot/workers/writers/AclObjectRecordWriterTest.java
+++ services/workers/src/test/java/org/sagebionetworks/snapshot/workers/writers/AclObjectRecordWriterTest.java
@@ -1,6 +1,7 @@
 package org.sagebionetworks.snapshot.workers.writers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -12,11 +13,15 @@ import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -24,6 +29,9 @@ import org.sagebionetworks.asynchronous.workers.sqs.MessageUtils;
 import org.sagebionetworks.audit.dao.ObjectRecordDAO;
 import org.sagebionetworks.audit.utils.ObjectRecordBuilderUtils;
 import org.sagebionetworks.common.util.progress.ProgressCallback;
+import org.sagebionetworks.kinesis.AbstractAwsKinesisLogRecord;
+import org.sagebionetworks.kinesis.AwsKinesisFirehoseLogger;
+import org.sagebionetworks.repo.manager.audit.KinesisJsonEntityRecord;
 import org.sagebionetworks.repo.model.AccessControlList;
 import org.sagebionetworks.repo.model.AccessControlListDAO;
 import org.sagebionetworks.repo.model.ObjectType;
@@ -34,6 +42,7 @@ import org.sagebionetworks.repo.model.message.ChangeMessage;
 import org.sagebionetworks.repo.model.message.ChangeType;
 
 import com.amazonaws.services.sqs.model.Message;
+import org.sagebionetworks.snapshot.workers.KinesisObjectSnapshotRecord;
 
 @ExtendWith(MockitoExtension.class)
 public class AclObjectRecordWriterTest {
@@ -43,9 +52,13 @@ public class AclObjectRecordWriterTest {
 	private ObjectRecordDAO mockObjectRecordDao;
 	@Mock
 	private ProgressCallback mockCallback;
+	@Mock
+	private AwsKinesisFirehoseLogger logger;
 	
 	@InjectMocks
 	private AclObjectRecordWriter writer;
+	@Captor
+	private ArgumentCaptor<List<KinesisObjectSnapshotRecord<?>>> recordCaptor;
 	private long id = 123L;
 
 	@Test
@@ -77,10 +90,18 @@ public class AclObjectRecordWriterTest {
 		ChangeMessage changeMessage = MessageUtils.extractMessageBody(message);
 		AclRecord record = AclObjectRecordWriter.buildAclRecord(acl, ObjectType.ENTITY);
 		ObjectRecord expected = ObjectRecordBuilderUtils.buildObjectRecord(record, changeMessage.getTimestamp().getTime());
+		KinesisObjectSnapshotRecord<?> expectedRecordOne =KinesisObjectSnapshotRecord.map(changeMessage, record);
+		KinesisObjectSnapshotRecord<?> expectedRecordTwo =KinesisObjectSnapshotRecord.map(changeMessage, record);
 		writer.buildAndWriteRecords(mockCallback, Arrays.asList(changeMessage, changeMessage));
 		verify(mockAccessControlListDao, times(2)).get(id);
 		verify(mockAccessControlListDao, times(2)).getOwnerType(id);
 		verify(mockObjectRecordDao).saveBatch(eq(Arrays.asList(expected, expected)), eq(expected.getJsonClassName()));
+		verify(logger).logBatch(eq("aclSnapshots"), recordCaptor.capture());
+		assertNotNull(recordCaptor.getAllValues().get(0).get(0).getSnapshotTimestamp());
+		expectedRecordOne.withSnapshotTimestamp(recordCaptor.getAllValues().get(0).get(0).getSnapshotTimestamp());
+		assertNotNull(recordCaptor.getAllValues().get(0).get(1).getSnapshotTimestamp());
+		expectedRecordTwo.withSnapshotTimestamp(recordCaptor.getAllValues().get(0).get(1).getSnapshotTimestamp());
+		assertEquals(List.of(expectedRecordOne, expectedRecordTwo), recordCaptor.getValue());
 	}
 
 	@Test
