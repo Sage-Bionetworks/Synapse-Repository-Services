d04f9c348dc9608048e8c7c10186fc806a0748a9
john-hill <john.hill@sagebase.org>
2023-09-20 20:35:26 -0700

    added missing files

---
+++ lib/lib-table-cluster/src/main/java/org/sagebionetworks/table/cluster/TranslatedQuery.java
@@ -0,0 +1,22 @@
+package org.sagebionetworks.table.cluster;
+
+import java.util.List;
+import java.util.Map;
+
+import org.sagebionetworks.repo.model.table.SelectColumn;
+
+public interface TranslatedQuery {
+
+	List<SelectColumn> getSelectColumns();
+
+	Map<String, ?> getParameters();
+
+	String getOutputSQL();
+
+	boolean includesRowIdAndVersion();
+
+	boolean includeEntityEtag();
+
+	String getSingleTableId();
+
+}
