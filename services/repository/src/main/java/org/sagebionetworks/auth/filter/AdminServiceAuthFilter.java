fe99d19d357ade50a34d37201c931555e4171bed
bhoff <bruce.hoff@sagebase.org>
2023-11-03 12:53:05 -0700

    PLFM-6973

--- services/repository/src/main/java/org/sagebionetworks/auth/filter/AdminServiceAuthFilter.java
+++ services/repository/src/main/java/org/sagebionetworks/auth/filter/AdminServiceAuthFilter.java
@@ -1,18 +1,13 @@
 package org.sagebionetworks.auth.filter;
 
-import java.io.IOException;
 import java.util.Optional;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.sagebionetworks.StackConfiguration;
 import org.sagebionetworks.auth.HttpAuthUtil;
 import org.sagebionetworks.auth.UserNameAndPassword;
 import org.sagebionetworks.cloudwatch.Consumer;
-import org.sagebionetworks.util.TemporaryCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -32,34 +27,12 @@ public class AdminServiceAuthFilter extends BasicAuthServiceFilter {
 			return Optional.empty();
 		}
 	}
-
-	@TemporaryCode(author = "marco.marasca@sagebase.org", comment = "This allows backward compatibility, can be removed once all the admin clients are updated.")
-	@Override
-	protected boolean credentialsRequired() {
-		return false;
-	}
 	
 	// Set as administrative service so that the admin user can be injected down the filter chain (e.g. for throttling, terms of use etc filters)
 	@Override
 	protected boolean isAdminService() {
 		return true;
 	}
-	
-	/**
-	 * Note: We attempted to remove this temporary code but it failed since all of the IT integration tests depend on the calling the 
-	 * administration services with an API key. See: PLFM-6973.
-	 */
-	@TemporaryCode(author = "marco.marasca@sagebase.org", comment = "This allows backward compatibility, can be removed once all the admin clients are updated.")
-	@Override
-	protected void validateCredentialsAndDoFilterInternal(HttpServletRequest httpRequest,
-			HttpServletResponse httpResponse, FilterChain filterChain, Optional<UserNameAndPassword> credentials)
-			throws IOException, ServletException {
-		if (credentials.isPresent()) {
-			super.validateCredentialsAndDoFilterInternal(httpRequest, httpResponse, filterChain, credentials);
-			return;
-		}
-		// Pass through to allow the previous auth mechanism to work
-		filterChain.doFilter(httpRequest, httpResponse);
-	}
+
 
 }
