3a13d85a6449811707f6e2e5daa928c5357bb366
john-hill <john.hill@sagebase.org>
2024-02-06 15:51:07 -0800

    set the limit to 50

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/table/query/QueryContextTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/table/query/QueryContextTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -36,6 +37,7 @@ public class QueryContextTest {
 	private Long userId;
 	private Long maxBytesPerPage;
 	private Long maxRowsPerCall;
+	private ColumnSingleValueQueryFilter singleValueFilter;
 	private QueryContext.Builder builder;
 	private String startingSql;
 	private List<QueryFilter> additionalFilters;
@@ -57,23 +59,23 @@ public class QueryContextTest {
 
 		startingSql = "select * from " + tableId;
 
-		additionalFilters = List.of(new ColumnSingleValueQueryFilter().setColumnName("two")
-				.setOperator(ColumnSingleValueFilterOperator.EQUAL).setValues(List.of("99", "89")));
+		singleValueFilter = new ColumnSingleValueQueryFilter().setColumnName("two")
+				.setOperator(ColumnSingleValueFilterOperator.EQUAL).setValues(List.of("99", "89"));
+		additionalFilters = List.of(singleValueFilter);
 		selectedFacets = List.of(new FacetColumnValuesRequest().setColumnName("one").setFacetValues(Set.of("cat")));
 		selectFileColumn = 99L;
-		
+
 		includeEntityEtag = true;
 		limit = 11L;
 		offset = 3L;
-		
+
 		sort = List.of(new SortItem().setColumn("three").setDirection(SortDirection.DESC));
-		
-		builder = QueryContext.builder().setIndexDescription(mockIndexDescription)
-				.setSchemaProvider(mockSchemaProvider).setUserId(userId).setMaxBytesPerPage(maxBytesPerPage)
-				.setStartingSql(startingSql).setMaxRowsPerCall(maxRowsPerCall).setAdditionalFilters(additionalFilters)
-				.setSelectedFacets(selectedFacets).setSelectFileColumn(selectFileColumn).setIncludeEntityEtag(includeEntityEtag)
-				.setLimit(limit)
-				.setOffset(offset).setSort(sort);
+
+		builder = QueryContext.builder().setIndexDescription(mockIndexDescription).setSchemaProvider(mockSchemaProvider)
+				.setUserId(userId).setMaxBytesPerPage(maxBytesPerPage).setStartingSql(startingSql)
+				.setMaxRowsPerCall(maxRowsPerCall).setAdditionalFilters(additionalFilters)
+				.setSelectedFacets(selectedFacets).setSelectFileColumn(selectFileColumn)
+				.setIncludeEntityEtag(includeEntityEtag).setLimit(limit).setOffset(offset).setSort(sort);
 	}
 
 	@Test
@@ -99,47 +101,67 @@ public class QueryContextTest {
 
 		assertNotNull(expansion);
 	}
-	
+
 	@Test
 	public void testBuildWithNullSql() {
-		
+
 		builder.setStartingSql(null);
-		String message = assertThrows(IllegalArgumentException.class, ()->{
+		String message = assertThrows(IllegalArgumentException.class, () -> {
 			// call under test
 			builder.build();
 		}).getMessage();
 		assertEquals("startingSql is required.", message);
-		
+
 	}
-	
+
 	@Test
 	public void testBuildWithNullProvider() {
 		builder.setSchemaProvider(null);
-		String message = assertThrows(IllegalArgumentException.class, ()->{
+		String message = assertThrows(IllegalArgumentException.class, () -> {
 			// call under test
 			builder.build();
 		}).getMessage();
 		assertEquals("schemaProvider is required.", message);
-		
+
 	}
-	
+
 	@Test
 	public void testBuildWithDescription() {
 		builder.setIndexDescription(null);
-		String message = assertThrows(IllegalArgumentException.class, ()->{
+		String message = assertThrows(IllegalArgumentException.class, () -> {
 			// call under test
 			builder.build();
 		}).getMessage();
-		assertEquals("indexDescription is required.", message);	
+		assertEquals("indexDescription is required.", message);
 	}
-	
+
 	@Test
 	public void testBuildWithUserid() {
 		builder.setUserId(null);
-		String message = assertThrows(IllegalArgumentException.class, ()->{
+		String message = assertThrows(IllegalArgumentException.class, () -> {
 			// call under test
 			builder.build();
 		}).getMessage();
-		assertEquals("userId is required.", message);	
+		assertEquals("userId is required.", message);
+	}
+
+	@Test
+	public void testMaximumNumberOfAdditionalFiltersWithAtLimit() {
+
+		builder.setAdditionalFilters(Collections.nCopies(50, singleValueFilter));
+		// call under test
+		QueryContext context = builder.build();
+		assertNotNull(context.getAdditionalFilters());
+		assertEquals(50, context.getAdditionalFilters().size());
+	}
+	@Test
+	public void testMaximumNumberOfAdditionalFiltersWithOverLimit() {
+
+		builder.setAdditionalFilters(Collections.nCopies(51, singleValueFilter));
+		String message = assertThrows(IllegalArgumentException.class, () -> {
+			// call under test
+			builder.build();
+		}).getMessage();
+		assertEquals("The size of the provided additionalFilters is 51 which exceeds the maximum of 50", message);
 	}
 }
