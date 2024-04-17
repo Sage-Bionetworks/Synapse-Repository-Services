53a7b123e19eb7e2eb56f5136428ce49c1690722
Luke Moenning <luke.moenning@sagebase.org>
2024-04-17 09:15:17 -0500

    PLFM-8317: change '&#38' to '&' in UserGroupController

--- services/repository/src/main/java/org/sagebionetworks/repo/web/controller/UserGroupController.java
+++ services/repository/src/main/java/org/sagebionetworks/repo/web/controller/UserGroupController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerInfo(displayName="User &#38 Group Services", path="repo/v1")
+@ControllerInfo(displayName="User & Group Services", path="repo/v1")
 @Controller
 @RequestMapping(UrlHelpers.REPO_PATH)
 public class UserGroupController {
