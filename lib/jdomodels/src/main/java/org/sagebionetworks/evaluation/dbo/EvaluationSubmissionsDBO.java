5e53a3141d31ad654da36eb9250708ebc7f56598
john-hill <john.hill@sagebase.org>
2024-07-15 13:00:32 -0700

    more tables converted to basic mode

--- lib/jdomodels/src/main/java/org/sagebionetworks/evaluation/dbo/EvaluationSubmissionsDBO.java
+++ lib/jdomodels/src/main/java/org/sagebionetworks/evaluation/dbo/EvaluationSubmissionsDBO.java
@@ -1,42 +1,68 @@
 package org.sagebionetworks.evaluation.dbo;
 
-import static org.sagebionetworks.repo.model.query.SQLConstants.COL_EVALUATION_ID;
 import static org.sagebionetworks.repo.model.query.SQLConstants.COL_EVALUATION_SUBMISSIONS_ETAG;
 import static org.sagebionetworks.repo.model.query.SQLConstants.COL_EVALUATION_SUBMISSIONS_EVAL_ID;
 import static org.sagebionetworks.repo.model.query.SQLConstants.COL_EVALUATION_SUBMISSIONS_ID;
-import static org.sagebionetworks.repo.model.query.SQLConstants.TABLE_EVALUATION;
+import static org.sagebionetworks.repo.model.query.SQLConstants.DDL_EVALUATION_SUBMISSIONS;
 import static org.sagebionetworks.repo.model.query.SQLConstants.TABLE_EVALUATION_SUBMISSIONS;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
-import org.sagebionetworks.repo.model.dbo.AutoTableMapping;
-import org.sagebionetworks.repo.model.dbo.Field;
-import org.sagebionetworks.repo.model.dbo.ForeignKey;
+import org.sagebionetworks.repo.model.dbo.FieldColumn;
 import org.sagebionetworks.repo.model.dbo.MigratableDatabaseObject;
-import org.sagebionetworks.repo.model.dbo.Table;
 import org.sagebionetworks.repo.model.dbo.TableMapping;
 import org.sagebionetworks.repo.model.dbo.migration.BasicMigratableTableTranslation;
 import org.sagebionetworks.repo.model.dbo.migration.MigratableTableTranslation;
 import org.sagebionetworks.repo.model.migration.MigrationType;
 
-@Table(name = TABLE_EVALUATION_SUBMISSIONS)
-public class EvaluationSubmissionsDBO implements MigratableDatabaseObject<EvaluationSubmissionsDBO, EvaluationSubmissionsDBO> {
-	
-	@Field(name = COL_EVALUATION_SUBMISSIONS_ID, backupId = true, primary = true, nullable = false)
+public class EvaluationSubmissionsDBO
+		implements MigratableDatabaseObject<EvaluationSubmissionsDBO, EvaluationSubmissionsDBO> {
+
+	private static FieldColumn[] FIELDS = new FieldColumn[] {
+			new FieldColumn("id", COL_EVALUATION_SUBMISSIONS_ID).withIsPrimaryKey(true).withIsBackupId(true),
+			new FieldColumn("etag", COL_EVALUATION_SUBMISSIONS_ETAG).withIsEtag(true),
+			new FieldColumn("evaluationId", COL_EVALUATION_SUBMISSIONS_EVAL_ID) };
+
 	private Long id;
-	
-	@Field(name = COL_EVALUATION_SUBMISSIONS_ETAG, backupId = false, etag = true, varchar = 256, primary = false, nullable = false)
 	private String etag;
-	
-	@Field(name = COL_EVALUATION_SUBMISSIONS_EVAL_ID, backupId = false, primary = false, nullable = false)
-	@ForeignKey(table = TABLE_EVALUATION, field = COL_EVALUATION_ID, cascadeDelete = true)
 	private Long evaluationId;
-	
-	private static final TableMapping<EvaluationSubmissionsDBO> TABLE_MAPPING = AutoTableMapping.create(EvaluationSubmissionsDBO.class);
 
 	@Override
 	public TableMapping<EvaluationSubmissionsDBO> getTableMapping() {
-		return TABLE_MAPPING;
+		return new TableMapping<EvaluationSubmissionsDBO>() {
+
+			@Override
+			public EvaluationSubmissionsDBO mapRow(ResultSet rs, int rowNum) throws SQLException {
+				EvaluationSubmissionsDBO dbo = new EvaluationSubmissionsDBO();
+				dbo.setId(rs.getLong(COL_EVALUATION_SUBMISSIONS_ID));
+				dbo.setEtag(rs.getString(COL_EVALUATION_SUBMISSIONS_ETAG));
+				dbo.setEvaluationId(rs.getLong(COL_EVALUATION_SUBMISSIONS_EVAL_ID));
+				return dbo;
+			}
+
+			@Override
+			public String getTableName() {
+				return TABLE_EVALUATION_SUBMISSIONS;
+			}
+
+			@Override
+			public FieldColumn[] getFieldColumns() {
+				return FIELDS;
+			}
+
+			@Override
+			public String getDDLFileName() {
+				return DDL_EVALUATION_SUBMISSIONS;
+			}
+
+			@Override
+			public Class<? extends EvaluationSubmissionsDBO> getDBOClass() {
+				return EvaluationSubmissionsDBO.class;
+			}
+		};
 	}
 
 	@Override
@@ -60,7 +86,7 @@ public class EvaluationSubmissionsDBO implements MigratableDatabaseObject<Evalua
 	}
 
 	@Override
-	public List<MigratableDatabaseObject<?,?>> getSecondaryTypes() {
+	public List<MigratableDatabaseObject<?, ?>> getSecondaryTypes() {
 		return null;
 	}
 
@@ -90,13 +116,7 @@ public class EvaluationSubmissionsDBO implements MigratableDatabaseObject<Evalua
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((etag == null) ? 0 : etag.hashCode());
-		result = prime * result
-				+ ((evaluationId == null) ? 0 : evaluationId.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+		return Objects.hash(etag, evaluationId, id);
 	}
 
 	@Override
@@ -108,29 +128,13 @@ public class EvaluationSubmissionsDBO implements MigratableDatabaseObject<Evalua
 		if (getClass() != obj.getClass())
 			return false;
 		EvaluationSubmissionsDBO other = (EvaluationSubmissionsDBO) obj;
-		if (etag == null) {
-			if (other.etag != null)
-				return false;
-		} else if (!etag.equals(other.etag))
-			return false;
-		if (evaluationId == null) {
-			if (other.evaluationId != null)
-				return false;
-		} else if (!evaluationId.equals(other.evaluationId))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+		return Objects.equals(etag, other.etag) && Objects.equals(evaluationId, other.evaluationId)
+				&& Objects.equals(id, other.id);
 	}
 
 	@Override
 	public String toString() {
-		return "EvaluationSubmissionsDBO [id=" + id + ", etag=" + etag
-				+ ", evaluationId=" + evaluationId + "]";
+		return "EvaluationSubmissionsDBO [id=" + id + ", etag=" + etag + ", evaluationId=" + evaluationId + "]";
 	}
-	
 
 }
