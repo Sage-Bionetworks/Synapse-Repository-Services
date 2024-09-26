f1bc8807743fb7d1127d28b2dd1ec5613e288688
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2024-09-26 10:43:27 -0700

    PLFM-8645: Additional logging

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/oauth/AccessTokenResponse.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/oauth/AccessTokenResponse.java
@@ -3,6 +3,8 @@ package org.sagebionetworks.repo.manager.oauth;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.sagebionetworks.repo.model.UnauthorizedException;
@@ -10,6 +12,8 @@ import org.sagebionetworks.repo.model.oauth.OIDCClaimName;
 import org.scribe.model.Token;
 
 public class AccessTokenResponse extends Token {
+	
+	private static final Logger LOGGER = LogManager.getLogger(AccessTokenResponse.class);
 	
 	private String idToken;
 	
@@ -30,6 +34,7 @@ public class AccessTokenResponse extends Token {
 		String[] idTokenParts = idToken.split("\\.");
 		
 		if (idTokenParts.length < 2) {
+			LOGGER.warn("Malformed id_token, not enough parts: {}", idToken);
 			throw new UnauthorizedException("The id_token included by the provider was malformed");
 		}
 				
@@ -54,6 +59,7 @@ public class AccessTokenResponse extends Token {
 			}
 			return info;
 		} catch (JSONException | IllegalArgumentException e) {
+			LOGGER.warn("Malformed id_token: {}", idToken, e);
 			throw new UnauthorizedException("The id_token included by the provider was malformed", e);
 		}
 	}
