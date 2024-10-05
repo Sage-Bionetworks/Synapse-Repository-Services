fa5433c0a7ef2389b8f5f667484f04add0d98378
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2024-10-05 14:18:09 -0700

    PLFM-8641: Refactor email sender to its own class

---
+++ lib/lib-scripts/src/main/java/org/sagebionetworks/repo/scripts/TOSUpdateEmailJob.java
@@ -0,0 +1,38 @@
+package org.sagebionetworks.repo.scripts;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class TOSUpdateEmailJob {
+	
+	public static void main(String[] args) throws SQLException, IOException, InterruptedException {
+		String subject = "Updates to our Terms of Service and Privacy Policy";
+		String emailTemplatePath = "message/TermsOfServiceUpdateTemplate.html";
+		
+		String emailCsvFile = args[0];
+		int sendLimit = Integer.parseInt(args[1]);
+		int maxSendRate = Integer.parseInt(args[2]);
+		boolean doSend = Boolean.parseBoolean(args[3]);
+		
+		try (BasicDataSource dataSource = new BasicDataSource()) {	
+			dataSource.setUrl(args[4]);
+			dataSource.setUsername(args[5]);
+			dataSource.setPassword(args[6]);
+			dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");		
+			
+			EmailListSender sender = new EmailListSender("TOSUpdate", new JdbcTemplate(dataSource), sendLimit, maxSendRate);
+			
+			try {
+				sender.start(emailCsvFile, subject, emailTemplatePath, doSend);
+			} finally {
+				sender.shutdown();
+			}
+		}
+	}
+	
+	
+
+}
